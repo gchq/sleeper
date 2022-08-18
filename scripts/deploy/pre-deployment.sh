4683cf65e9f1adb3d5335b8d615911527579247e
patchwork01 <110390516+patchwork01@users.noreply.github.com>
2022-08-18 15:23:10 +0100

    Give sed an empty parameter for -i in pre-deployment.sh

--- scripts/deploy/pre-deployment.sh
+++ scripts/deploy/pre-deployment.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 # Copyright 2022 Crown Copyright
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -106,7 +106,7 @@ sed \
 	-e "s|^sleeper.subnet=.*|sleeper.subnet=${SUBNET}|" \
 	-e "s|^sleeper.tags.file=.*|sleeper.tags.file=${TAGS}|" \
 	-e "s|^sleeper.table.properties=.*|sleeper.table.properties=${TABLE_PROPERTIES}|" \
-	-i"" ${INSTANCE_PROPERTIES}
+	-i "" ${INSTANCE_PROPERTIES}
 
 ###################################
 # Build and publish Docker images #
