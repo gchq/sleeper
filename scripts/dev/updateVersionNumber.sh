51fc7c21f0c3592bd824cf1a5cdc58bb2818be12
patchwork01 <110390516+patchwork01@users.noreply.github.com>
2022-08-19 13:12:05 +0100

    Give sed an empty parameter for -i in updateVersionNumber.sh

--- scripts/dev/updateVersionNumber.sh
+++ scripts/dev/updateVersionNumber.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # Copyright 2022 Crown Copyright
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -33,16 +33,16 @@ popd
 
 # Update the version number in the Python module
 sed \
-  -i"" \
+  -i "" \
   -e "s|^    version=.*|    version='${NEW_VERSION}',|" \
   python/setup.py
 
 # Update the version numbers in the example property files
 sed \
-  -i"" \
+  -i "" \
   -e "s|^sleeper.version=.*|sleeper.version=${NEW_VERSION}|"\
   example/basic/instance.properties
 sed \
-  -i"" \
+  -i "" \
   -e "s|^sleeper.version=.*|sleeper.version=${NEW_VERSION}|"\
   example/full/instance.properties
