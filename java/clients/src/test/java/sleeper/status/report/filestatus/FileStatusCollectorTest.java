b62a335437ac8066f933c63b032c6797b2d95967
patchwork01 <110390516+patchwork01@users.noreply.github.com>
2022-08-23 10:18:26 +0100

    Specify active files exactly

--- java/clients/src/test/java/sleeper/status/report/filestatus/FileStatusCollectorTest.java
+++ java/clients/src/test/java/sleeper/status/report/filestatus/FileStatusCollectorTest.java
@@ -19,8 +19,8 @@ import com.google.common.io.Resources;
 import org.junit.Test;
 import sleeper.core.key.Key;
 import sleeper.core.partition.Partition;
+import sleeper.core.partition.PartitionTree;
 import sleeper.core.partition.PartitionsFromSplitPoints;
-import sleeper.core.range.Region;
 import sleeper.core.schema.Field;
 import sleeper.core.schema.Schema;
 import sleeper.core.schema.type.StringType;
@@ -31,7 +31,6 @@ import java.nio.charset.Charset;
 import java.time.Instant;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import static com.google.common.io.Resources.getResource;
 import static net.javacrumbs.jsonunit.assertj.JsonAssertions.assertThatJson;
@@ -46,7 +45,16 @@ public class FileStatusCollectorTest {
         Schema schema = Schema.builder().rowKeyFields(new Field("key1", new StringType())).build();
         List<Partition> partitions = PartitionsFromSplitPoints.sequentialIds(schema,
                 Arrays.asList("aaa", "bbb", "ccc", "ddd", "eee", "fff", "ggg"));
-        List<FileInfo> activeFiles = oneFilePerLeafPartition(schema, partitions, 50000000, lastStateStoreUpdate);
+        FileInfoFactory fileInfoFactory = new FileInfoFactory(schema, partitions, lastStateStoreUpdate);
+        List<FileInfo> activeFiles = Arrays.asList(
+                fileInfoFactory.file(50000000, "123", "456"),
+                fileInfoFactory.file(50000000, "abc", "az"),
+                fileInfoFactory.file(50000000, "bcd", "bz"),
+                fileInfoFactory.file(50000000, "cde", "cz"),
+                fileInfoFactory.file(50000000, "def", "dz"),
+                fileInfoFactory.file(50000000, "efg", "ez"),
+                fileInfoFactory.file(50000000, "fgh", "fz"),
+                fileInfoFactory.file(50000000, "ghi", "gz"));
 
         // When
         FileStatus status = FileStatusCollector.run(StateStoreFiles.builder()
@@ -61,41 +69,41 @@ public class FileStatusCollectorTest {
                 .isEqualTo(example("reports/json/oneActiveFilePerLeaf.json"));
     }
 
-    private static List<FileInfo> oneFilePerLeafPartition(Schema schema, List<Partition> partitions, long recordsPerFile, Instant lastStateStoreUpdate) {
-        return partitions.stream()
-                .filter(Partition::isLeafPartition)
-                .map(partition -> fileForPartition(schema, partition, recordsPerFile, lastStateStoreUpdate))
-                .collect(Collectors.toList());
-    }
-
-    private static FileInfo fileForPartition(Schema schema, Partition partition, long records, Instant lastStateStoreUpdate) {
-        FileInfo file = new FileInfo();
-        file.setRowKeyTypes(partition.getRowKeyTypes());
-        file.setMinRowKey(minRowKey(schema, partition));
-        file.setMaxRowKey(maxRowKey(schema, partition));
-        file.setFilename(partition.getId() + ".parquet");
-        file.setPartitionId(partition.getId());
-        file.setNumberOfRecords(records);
-        file.setFileStatus(FileInfo.FileStatus.ACTIVE);
-        file.setLastStateStoreUpdateTime(lastStateStoreUpdate.toEpochMilli());
-        return file;
-    }
-
-    private static Key minRowKey(Schema schema, Partition partition) {
-        Region region = partition.getRegion();
-        return Key.create(schema.getRowKeyFieldNames().stream()
-                .map(field -> region.getRange(field).getMin())
-                .collect(Collectors.toList()));
-    }
-
-    private static Key maxRowKey(Schema schema, Partition partition) {
-        Region region = partition.getRegion();
-        return Key.create(schema.getRowKeyFieldNames().stream()
-                .map(field -> region.getRange(field).getMax())
-                .collect(Collectors.toList()));
-    }
-
     private static String example(String path) throws IOException {
         return Resources.toString(getResource(path), Charset.defaultCharset());
     }
+
+    private static class FileInfoFactory {
+        private final Schema schema;
+        private final PartitionTree partitionTree;
+        private final Instant lastStateStoreUpdate;
+
+        private FileInfoFactory(Schema schema, List<Partition> partitions, Instant lastStateStoreUpdate) {
+            this.schema = schema;
+            this.lastStateStoreUpdate = lastStateStoreUpdate;
+            partitionTree = new PartitionTree(schema, partitions);
+        }
+
+        private FileInfo file(long records, Object min, Object max) {
+            Partition partition = partitionTree.getLeafPartition(Key.create(min));
+            if (!partition.isRowKeyInPartition(schema, Key.create(max))) {
+                throw new IllegalArgumentException("Not in same leaf partition: " + min + ", " + max);
+            }
+            return fileForPartition(partition, records, min, max);
+        }
+
+        private FileInfo fileForPartition(Partition partition, long records, Object min, Object max) {
+            FileInfo file = new FileInfo();
+            file.setRowKeyTypes(partition.getRowKeyTypes());
+            file.setMinRowKey(Key.create(min));
+            file.setMaxRowKey(Key.create(max));
+            file.setFilename(partition.getId() + ".parquet");
+            file.setPartitionId(partition.getId());
+            file.setNumberOfRecords(records);
+            file.setFileStatus(FileInfo.FileStatus.ACTIVE);
+            file.setLastStateStoreUpdateTime(lastStateStoreUpdate.toEpochMilli());
+            return file;
+        }
+    }
+
 }
