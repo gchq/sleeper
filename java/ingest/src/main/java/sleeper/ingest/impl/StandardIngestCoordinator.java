a2f50cc547775b0b003ae24aa0285207458bbbad
patchwork01 <110390516+patchwork01@users.noreply.github.com>
2022-09-06 14:42:42 +0100

    Add builder to StandardIngestCoordinator

--- java/ingest/src/main/java/sleeper/ingest/impl/StandardIngestCoordinator.java
+++ java/ingest/src/main/java/sleeper/ingest/impl/StandardIngestCoordinator.java
@@ -24,6 +24,10 @@ public class StandardIngestCoordinator {
     private StandardIngestCoordinator() {
     }
 
+    public static Builder builder() {
+        return new Builder();
+    }
+
     public static IngestCoordinator<Record> directWriteBackedByArrayList(ObjectFactory objectFactory,
                                                                          StateStore sleeperStateStore,
                                                                          Schema sleeperSchema,
@@ -37,38 +41,57 @@ public class StandardIngestCoordinator {
                                                                          int ingestPartitionRefreshFrequencyInSeconds,
                                                                          String filePathPrefix,
                                                                          int maxNoOfRecordsInMemory,
-                                                                         long maxNoOfRecordsInLocalStore)
-            throws StateStoreException {
+                                                                         long maxNoOfRecordsInLocalStore) {
+        return builder()
+                .objectFactory(objectFactory)
+                .stateStore(sleeperStateStore)
+                .schema(sleeperSchema)
+                .localWorkingDirectory(localWorkingDirectory)
+                .parquetRowGroupSize(parquetRowGroupSize)
+                .parquetPageSize(parquetPageSize)
+                .parquetCompressionCodec(parquetCompressionCodec)
+                .hadoopConfiguration(hadoopConfiguration)
+                .iteratorClassName(sleeperIteratorClassName)
+                .iteratorConfig(sleeperIteratorConfig)
+                .ingestPartitionRefreshFrequencyInSeconds(ingestPartitionRefreshFrequencyInSeconds)
+                .backedByArrayList()
+                .maxNoOfRecordsInMemory(maxNoOfRecordsInMemory)
+                .maxNoOfRecordsInLocalStore(maxNoOfRecordsInLocalStore)
+                .buildDirectWrite(filePathPrefix);
+    }
+
+    private static IngestCoordinator<Record> directWriteBackedByArrayList(
+            Builder builder, BackedByArrayBuilder arrayBuilder, String filePathPrefix) {
         Supplier<RecordBatch<Record>> recordBatchFactoryFn = () -> new ArrayListRecordBatchAcceptingRecords(
-                sleeperSchema,
-                localWorkingDirectory,
-                maxNoOfRecordsInMemory,
-                maxNoOfRecordsInLocalStore,
-                parquetRowGroupSize,
-                parquetPageSize,
-                parquetCompressionCodec,
-                hadoopConfiguration);
+                builder.schema,
+                builder.localWorkingDirectory,
+                arrayBuilder.maxNoOfRecordsInMemory,
+                arrayBuilder.maxNoOfRecordsInLocalStore,
+                builder.parquetRowGroupSize,
+                builder.parquetPageSize,
+                builder.parquetCompressionCodec,
+                builder.hadoopConfiguration);
         Function<Partition, PartitionFileWriter> partitionFileFactoryFn = partition -> {
             try {
                 return new DirectPartitionFileWriter(
-                        sleeperSchema,
+                        builder.schema,
                         partition,
-                        parquetRowGroupSize,
-                        parquetPageSize,
-                        parquetCompressionCodec,
-                        hadoopConfiguration,
+                        builder.parquetRowGroupSize,
+                        builder.parquetPageSize,
+                        builder.parquetCompressionCodec,
+                        builder.hadoopConfiguration,
                         filePathPrefix);
             } catch (Exception e) {
                 throw new RuntimeException(e);
             }
         };
         return new IngestCoordinator<>(
-                objectFactory,
-                sleeperStateStore,
-                sleeperSchema,
-                sleeperIteratorClassName,
-                sleeperIteratorConfig,
-                ingestPartitionRefreshFrequencyInSeconds,
+                builder.objectFactory,
+                builder.stateStore,
+                builder.schema,
+                builder.iteratorClassName,
+                builder.iteratorConfig,
+                builder.ingestPartitionRefreshFrequencyInSeconds,
                 recordBatchFactoryFn,
                 partitionFileFactoryFn);
     }
@@ -90,38 +113,62 @@ public class StandardIngestCoordinator {
                                                                      long workingArrowBufferAllocatorBytes,
                                                                      long minBatchArrowBufferAllocatorBytes,
                                                                      long maxBatchArrowBufferAllocatorBytes,
-                                                                     long maxNoOfBytesToWriteLocally) throws StateStoreException {
+                                                                     long maxNoOfBytesToWriteLocally) {
+        return builder()
+                .objectFactory(objectFactory)
+                .stateStore(sleeperStateStore)
+                .schema(sleeperSchema)
+                .localWorkingDirectory(localWorkingDirectory)
+                .parquetRowGroupSize(parquetRowGroupSize)
+                .parquetPageSize(parquetPageSize)
+                .parquetCompressionCodec(parquetCompressionCodec)
+                .hadoopConfiguration(hadoopConfiguration)
+                .iteratorClassName(sleeperIteratorClassName)
+                .iteratorConfig(sleeperIteratorConfig)
+                .ingestPartitionRefreshFrequencyInSeconds(ingestPartitionRefreshFrequencyInSeconds)
+                .backedByArrow()
+                .arrowBufferAllocator(arrowBufferAllocator)
+                .maxNoOfRecordsToWriteToArrowFileAtOnce(maxNoOfRecordsToWriteToArrowFileAtOnce)
+                .workingArrowBufferAllocatorBytes(workingArrowBufferAllocatorBytes)
+                .minBatchArrowBufferAllocatorBytes(minBatchArrowBufferAllocatorBytes)
+                .maxBatchArrowBufferAllocatorBytes(maxBatchArrowBufferAllocatorBytes)
+                .maxNoOfBytesToWriteLocally(maxNoOfBytesToWriteLocally)
+                .buildDirectWrite(filePathPrefix);
+    }
+
+    private static IngestCoordinator<Record> directWriteBackedByArrow(
+            Builder builder, BackedByArrowBuilder arrowBuilder, String filePathPrefix) {
         Supplier<RecordBatch<Record>> recordBatchFactoryFn = () ->
                 new ArrowRecordBatchAcceptingRecords(
-                        arrowBufferAllocator,
-                        sleeperSchema,
-                        localWorkingDirectory,
-                        workingArrowBufferAllocatorBytes,
-                        minBatchArrowBufferAllocatorBytes,
-                        maxBatchArrowBufferAllocatorBytes,
-                        maxNoOfBytesToWriteLocally,
-                        maxNoOfRecordsToWriteToArrowFileAtOnce);
+                        arrowBuilder.arrowBufferAllocator,
+                        builder.schema,
+                        builder.localWorkingDirectory,
+                        arrowBuilder.workingArrowBufferAllocatorBytes,
+                        arrowBuilder.minBatchArrowBufferAllocatorBytes,
+                        arrowBuilder.maxBatchArrowBufferAllocatorBytes,
+                        arrowBuilder.maxNoOfBytesToWriteLocally,
+                        arrowBuilder.maxNoOfRecordsToWriteToArrowFileAtOnce);
         Function<Partition, PartitionFileWriter> partitionFileFactoryFn = partition -> {
             try {
                 return new DirectPartitionFileWriter(
-                        sleeperSchema,
+                        builder.schema,
                         partition,
-                        parquetRowGroupSize,
-                        parquetPageSize,
-                        parquetCompressionCodec,
-                        hadoopConfiguration,
+                        builder.parquetRowGroupSize,
+                        builder.parquetPageSize,
+                        builder.parquetCompressionCodec,
+                        builder.hadoopConfiguration,
                         filePathPrefix);
             } catch (Exception e) {
                 throw new RuntimeException(e);
             }
         };
         return new IngestCoordinator<>(
-                objectFactory,
-                sleeperStateStore,
-                sleeperSchema,
-                sleeperIteratorClassName,
-                sleeperIteratorConfig,
-                ingestPartitionRefreshFrequencyInSeconds,
+                builder.objectFactory,
+                builder.stateStore,
+                builder.schema,
+                builder.iteratorClassName,
+                builder.iteratorConfig,
+                builder.ingestPartitionRefreshFrequencyInSeconds,
                 recordBatchFactoryFn,
                 partitionFileFactoryFn);
     }
@@ -145,40 +192,221 @@ public class StandardIngestCoordinator {
                                                                       long minBatchArrowBufferAllocatorBytes,
                                                                       long maxBatchArrowBufferAllocatorBytes,
                                                                       long maxNoOfBytesToWriteLocally) throws StateStoreException {
+        return builder()
+                .objectFactory(objectFactory)
+                .stateStore(sleeperStateStore)
+                .schema(sleeperSchema)
+                .localWorkingDirectory(localWorkingDirectory)
+                .parquetRowGroupSize(parquetRowGroupSize)
+                .parquetPageSize(parquetPageSize)
+                .parquetCompressionCodec(parquetCompressionCodec)
+                .hadoopConfiguration(hadoopConfiguration)
+                .iteratorClassName(sleeperIteratorClassName)
+                .iteratorConfig(sleeperIteratorConfig)
+                .ingestPartitionRefreshFrequencyInSeconds(ingestPartitionRefreshFrequencyInSeconds)
+                .backedByArrow()
+                .arrowBufferAllocator(arrowBufferAllocator)
+                .maxNoOfRecordsToWriteToArrowFileAtOnce(maxNoOfRecordsToWriteToArrowFileAtOnce)
+                .workingArrowBufferAllocatorBytes(workingArrowBufferAllocatorBytes)
+                .minBatchArrowBufferAllocatorBytes(minBatchArrowBufferAllocatorBytes)
+                .maxBatchArrowBufferAllocatorBytes(maxBatchArrowBufferAllocatorBytes)
+                .maxNoOfBytesToWriteLocally(maxNoOfBytesToWriteLocally)
+                .buildAsyncS3Write(s3BucketName, s3AsyncClient);
+    }
+
+    private static IngestCoordinator<Record> asyncS3WriteBackedByArrow(
+            Builder builder, BackedByArrowBuilder arrowBuilder, String s3BucketName, S3AsyncClient s3AsyncClient) {
         Supplier<RecordBatch<Record>> recordBatchFactoryFn = () ->
                 new ArrowRecordBatchAcceptingRecords(
-                        arrowBufferAllocator,
-                        sleeperSchema,
-                        localWorkingDirectory,
-                        workingArrowBufferAllocatorBytes,
-                        minBatchArrowBufferAllocatorBytes,
-                        maxBatchArrowBufferAllocatorBytes,
-                        maxNoOfBytesToWriteLocally,
-                        maxNoOfRecordsToWriteToArrowFileAtOnce);
+                        arrowBuilder.arrowBufferAllocator,
+                        builder.schema,
+                        builder.localWorkingDirectory,
+                        arrowBuilder.workingArrowBufferAllocatorBytes,
+                        arrowBuilder.minBatchArrowBufferAllocatorBytes,
+                        arrowBuilder.maxBatchArrowBufferAllocatorBytes,
+                        arrowBuilder.maxNoOfBytesToWriteLocally,
+                        arrowBuilder.maxNoOfRecordsToWriteToArrowFileAtOnce);
         Function<Partition, PartitionFileWriter> partitionFileFactoryFn = partition -> {
             try {
                 return new AsyncS3PartitionFileWriter(
-                        sleeperSchema,
+                        builder.schema,
                         partition,
-                        parquetRowGroupSize,
-                        parquetPageSize,
-                        parquetCompressionCodec,
-                        hadoopConfiguration,
+                        builder.parquetRowGroupSize,
+                        builder.parquetPageSize,
+                        builder.parquetCompressionCodec,
+                        builder.hadoopConfiguration,
                         s3BucketName,
                         s3AsyncClient,
-                        localWorkingDirectory);
+                        builder.localWorkingDirectory);
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
         };
         return new IngestCoordinator<>(
-                objectFactory,
-                sleeperStateStore,
-                sleeperSchema,
-                sleeperIteratorClassName,
-                sleeperIteratorConfig,
-                ingestPartitionRefreshFrequencyInSeconds,
+                builder.objectFactory,
+                builder.stateStore,
+                builder.schema,
+                builder.iteratorClassName,
+                builder.iteratorConfig,
+                builder.ingestPartitionRefreshFrequencyInSeconds,
                 recordBatchFactoryFn,
                 partitionFileFactoryFn);
+    }
+
+    public static class BackedByArrayBuilder {
+        private final Builder builder;
+        private int maxNoOfRecordsInMemory;
+        private long maxNoOfRecordsInLocalStore;
+
+        private BackedByArrayBuilder(Builder builder) {
+            this.builder = builder;
+        }
+
+        public BackedByArrayBuilder maxNoOfRecordsInMemory(int maxNoOfRecordsInMemory) {
+            this.maxNoOfRecordsInMemory = maxNoOfRecordsInMemory;
+            return this;
+        }
+
+        public BackedByArrayBuilder maxNoOfRecordsInLocalStore(long maxNoOfRecordsInLocalStore) {
+            this.maxNoOfRecordsInLocalStore = maxNoOfRecordsInLocalStore;
+            return this;
+        }
+
+        public IngestCoordinator<Record> buildDirectWrite(String filePathPrefix) {
+            return directWriteBackedByArrayList(builder, this, filePathPrefix);
+        }
+    }
+
+    public static class BackedByArrowBuilder {
+        private final Builder builder;
+        private BufferAllocator arrowBufferAllocator;
+        private int maxNoOfRecordsToWriteToArrowFileAtOnce;
+        private long workingArrowBufferAllocatorBytes;
+        private long minBatchArrowBufferAllocatorBytes;
+        private long maxBatchArrowBufferAllocatorBytes;
+        private long maxNoOfBytesToWriteLocally;
+
+        private BackedByArrowBuilder(Builder builder) {
+            this.builder = builder;
+        }
+
+        public BackedByArrowBuilder arrowBufferAllocator(BufferAllocator arrowBufferAllocator) {
+            this.arrowBufferAllocator = arrowBufferAllocator;
+            return this;
+        }
+
+        public BackedByArrowBuilder maxNoOfRecordsToWriteToArrowFileAtOnce(int maxNoOfRecordsToWriteToArrowFileAtOnce) {
+            this.maxNoOfRecordsToWriteToArrowFileAtOnce = maxNoOfRecordsToWriteToArrowFileAtOnce;
+            return this;
+        }
+
+        public BackedByArrowBuilder workingArrowBufferAllocatorBytes(long workingArrowBufferAllocatorBytes) {
+            this.workingArrowBufferAllocatorBytes = workingArrowBufferAllocatorBytes;
+            return this;
+        }
+
+        public BackedByArrowBuilder minBatchArrowBufferAllocatorBytes(long minBatchArrowBufferAllocatorBytes) {
+            this.minBatchArrowBufferAllocatorBytes = minBatchArrowBufferAllocatorBytes;
+            return this;
+        }
+
+        public BackedByArrowBuilder maxBatchArrowBufferAllocatorBytes(long maxBatchArrowBufferAllocatorBytes) {
+            this.maxBatchArrowBufferAllocatorBytes = maxBatchArrowBufferAllocatorBytes;
+            return this;
+        }
+
+        public BackedByArrowBuilder maxNoOfBytesToWriteLocally(long maxNoOfBytesToWriteLocally) {
+            this.maxNoOfBytesToWriteLocally = maxNoOfBytesToWriteLocally;
+            return this;
+        }
+
+        public IngestCoordinator<Record> buildDirectWrite(String filePathPrefix) {
+            return directWriteBackedByArrow(builder, this, filePathPrefix);
+        }
+
+        public IngestCoordinator<Record> buildAsyncS3Write(String s3BucketName, S3AsyncClient s3AsyncClient) {
+            return asyncS3WriteBackedByArrow(builder, this, s3BucketName, s3AsyncClient);
+        }
+    }
+
+    public static class Builder {
+        private ObjectFactory objectFactory;
+        private String localWorkingDirectory;
+        private int parquetRowGroupSize;
+        private int parquetPageSize;
+        private String parquetCompressionCodec;
+        private StateStore stateStore;
+        private Schema schema;
+        private String iteratorClassName;
+        private String iteratorConfig;
+        private int ingestPartitionRefreshFrequencyInSeconds;
+        private Configuration hadoopConfiguration;
+
+        private Builder() {
+        }
+
+        public Builder objectFactory(ObjectFactory objectFactory) {
+            this.objectFactory = objectFactory;
+            return this;
+        }
+
+        public Builder localWorkingDirectory(String localWorkingDirectory) {
+            this.localWorkingDirectory = localWorkingDirectory;
+            return this;
+        }
+
+        public Builder parquetRowGroupSize(int parquetRowGroupSize) {
+            this.parquetRowGroupSize = parquetRowGroupSize;
+            return this;
+        }
+
+        public Builder parquetPageSize(int parquetPageSize) {
+            this.parquetPageSize = parquetPageSize;
+            return this;
+        }
+
+        public Builder parquetCompressionCodec(String parquetCompressionCodec) {
+            this.parquetCompressionCodec = parquetCompressionCodec;
+            return this;
+        }
+
+        public Builder stateStore(StateStore stateStore) {
+            this.stateStore = stateStore;
+            return this;
+        }
+
+        public Builder schema(Schema schema) {
+            this.schema = schema;
+            return this;
+        }
+
+        public Builder iteratorClassName(String iteratorClassName) {
+            this.iteratorClassName = iteratorClassName;
+            return this;
+        }
+
+        public Builder iteratorConfig(String iteratorConfig) {
+            this.iteratorConfig = iteratorConfig;
+            return this;
+        }
+
+        public Builder ingestPartitionRefreshFrequencyInSeconds(int ingestPartitionRefreshFrequencyInSeconds) {
+            this.ingestPartitionRefreshFrequencyInSeconds = ingestPartitionRefreshFrequencyInSeconds;
+            return this;
+        }
+
+        public Builder hadoopConfiguration(Configuration hadoopConfiguration) {
+            this.hadoopConfiguration = hadoopConfiguration;
+            return this;
+        }
+
+        public BackedByArrayBuilder backedByArrayList() {
+            return new BackedByArrayBuilder(this);
+        }
+
+        public BackedByArrowBuilder backedByArrow() {
+            return new BackedByArrowBuilder(this);
+        }
+
     }
 }
