68e70980c90ad7c177948eb2c8f3fb2faedb6663
patchwork01 <110390516+patchwork01@users.noreply.github.com>
2022-09-23 12:54:49 +0100

    Revert "Add builder.jobIdsMissingCreatedStatus"
    This reverts commit fc6eb6a7e57d4703cc7baf72429d23e12e87c542.

--- java/compaction-core/src/test/java/sleeper/compaction/job/CompactionJobStatusesBuilderTest.java
+++ java/compaction-core/src/test/java/sleeper/compaction/job/CompactionJobStatusesBuilderTest.java
@@ -25,7 +25,6 @@ import sleeper.compaction.job.status.CompactionJobStatusesBuilder;
 import java.time.Instant;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -34,81 +33,48 @@ public class CompactionJobStatusesBuilderTest {
     @Test
     public void shouldBuildCompactionJobStatusFromIndividualUpdates() {
         // Given
-        CompactionJobStatusesBuilder builder = new CompactionJobStatusesBuilder()
-                .jobCreated("job1", createdStatus1())
-                .jobStarted("job1", startedStatus1())
-                .jobFinished("job1", finishedStatus1())
-                .jobCreated("job2", createdStatus2())
-                .jobStarted("job2", startedStatus2())
-                .jobFinished("job2", finishedStatus2());
-
-        // When
-        List<CompactionJobStatus> statuses = builder.build();
-
-        // Then
-        assertThat(statuses).containsExactly(
-                CompactionJobStatus.builder().jobId("job1").createdStatus(createdStatus1())
-                        .startedStatus(startedStatus1()).finishedStatus(finishedStatus1()).build(),
-                CompactionJobStatus.builder().jobId("job2").createdStatus(createdStatus2())
-                        .startedStatus(startedStatus2()).finishedStatus(finishedStatus2()).build());
-    }
-
-    @Test
-    public void shouldGetJobIdsMissingCreatedRecord() {
-        // Given
-        CompactionJobStatusesBuilder builder = new CompactionJobStatusesBuilder()
-                .jobCreated("job1", createdStatus1())
-                .jobStarted("job2", startedStatus1())
-                .jobFinished("job3", finishedStatus1());
-
-        // When
-        Set<String> jobsMissingCreatedStatus = builder.jobIdsMissingCreatedStatus();
-
-        // When / Then
-        assertThat(jobsMissingCreatedStatus).containsExactly("job2", "job3");
-    }
-
-    private CompactionJobCreatedStatus createdStatus1() {
-        return CompactionJobCreatedStatus.builder()
+        CompactionJobCreatedStatus created1 = CompactionJobCreatedStatus.builder()
                 .updateTime(Instant.parse("2022-09-23T09:23:00.012Z"))
                 .partitionId("partition1").childPartitionIds(null)
                 .inputFilesCount(11)
                 .build();
-    }
-
-    private CompactionJobStartedStatus startedStatus1() {
-        return CompactionJobStartedStatus.updateAndStartTime(
+        CompactionJobStartedStatus started1 = CompactionJobStartedStatus.updateAndStartTime(
                 Instant.parse("2022-09-23T09:23:30.012Z"),
                 Instant.parse("2022-09-23T09:23:30.001Z"));
-    }
-
-    private CompactionJobFinishedStatus finishedStatus1() {
-        return CompactionJobFinishedStatus.updateTimeAndSummary(
+        CompactionJobFinishedStatus finished1 = CompactionJobFinishedStatus.updateTimeAndSummary(
                 Instant.parse("2022-09-23T09:24:00.012Z"),
                 new CompactionJobSummary(new CompactionJobRecordsProcessed(200L, 100L),
                         Instant.parse("2022-09-23T09:23:30.001Z"),
                         Instant.parse("2022-09-23T09:24:00.001Z")));
-    }
-
-    private CompactionJobCreatedStatus createdStatus2() {
-        return CompactionJobCreatedStatus.builder()
+        CompactionJobCreatedStatus created2 = CompactionJobCreatedStatus.builder()
                 .updateTime(Instant.parse("2022-09-24T09:23:00.012Z"))
                 .partitionId("partition2").childPartitionIds(Arrays.asList("A", "B"))
                 .inputFilesCount(12)
                 .build();
-    }
-
-    private CompactionJobStartedStatus startedStatus2() {
-        return CompactionJobStartedStatus.updateAndStartTime(
+        CompactionJobStartedStatus started2 = CompactionJobStartedStatus.updateAndStartTime(
                 Instant.parse("2022-09-24T09:23:30.012Z"),
                 Instant.parse("2022-09-24T09:23:30.001Z"));
-    }
-
-    private CompactionJobFinishedStatus finishedStatus2() {
-        return CompactionJobFinishedStatus.updateTimeAndSummary(
+        CompactionJobFinishedStatus finished2 = CompactionJobFinishedStatus.updateTimeAndSummary(
                 Instant.parse("2022-09-24T09:24:00.012Z"),
                 new CompactionJobSummary(new CompactionJobRecordsProcessed(450L, 300L),
                         Instant.parse("2022-09-24T09:23:30.001Z"),
                         Instant.parse("2022-09-24T09:24:00.001Z")));
+
+        // When
+        List<CompactionJobStatus> statuses = new CompactionJobStatusesBuilder()
+                .jobCreated("job1", created1)
+                .jobStarted("job1", started1)
+                .jobFinished("job1", finished1)
+                .jobCreated("job2", created2)
+                .jobStarted("job2", started2)
+                .jobFinished("job2", finished2)
+                .build();
+
+        // Then
+        assertThat(statuses).containsExactly(
+                CompactionJobStatus.builder().jobId("job1").createdStatus(created1)
+                        .startedStatus(started1).finishedStatus(finished1).build(),
+                CompactionJobStatus.builder().jobId("job2").createdStatus(created2)
+                        .startedStatus(started2).finishedStatus(finished2).build());
     }
 }
