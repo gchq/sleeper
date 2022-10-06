c27f55c5de00582462747e0e11f9707180a7ddef
kr565370 <112558283+kr565370@users.noreply.github.com>
2022-10-06 14:00:35 +0100

    Fix checkstyle issues in clients

--- java/clients/src/test/java/sleeper/status/report/compactionjobstatus/StatusReporterTest.java
+++ java/clients/src/test/java/sleeper/status/report/compactionjobstatus/StatusReporterTest.java
@@ -71,8 +71,8 @@ public abstract class StatusReporterTest {
                 new CompactionJobRecordsProcessed(600L, 300L), startUpdateTime, finishedTime);
         return CompactionJobStatus.builder().jobId(job.getId())
                 .createdStatus(CompactionJobCreatedStatus.from(job, creationTime))
-                .jobRun(CompactionJobRun.finished(taskId, CompactionJobStartedStatus.updateAndStartTime(startUpdateTime, startTime)
-                        , CompactionJobFinishedStatus.updateTimeAndSummary(finishedTime, summary)))
+                .jobRun(CompactionJobRun.finished(taskId, CompactionJobStartedStatus.updateAndStartTime(startUpdateTime, startTime),
+                        CompactionJobFinishedStatus.updateTimeAndSummary(finishedTime, summary)))
                 .build();
     }
 
