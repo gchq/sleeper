ad6ff47d4c5a55344ca063c004c596b0a817813a
patchwork01 <110390516+patchwork01@users.noreply.github.com>
2022-10-25 11:54:58 +0100

    Test sedInPlace.sh

--- scripts/functions/testSedInPlace.sh
+++ scripts/functions/testSedInPlace.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 # Copyright 2022 Crown Copyright
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,33 +13,13 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-fail_test() {
-  echo "$@"
-  ((TEST_FAILURES++))
-}
+THIS_DIR=$(cd $(dirname $0) && pwd)
+source "${THIS_DIR}/sedInPlace.sh"
+source "${THIS_DIR}/runTestUtils.sh"
 
-start_tests() {
-  ((TESTS_NESTING++))
-}
+echo "Hello name!" > testSedInPlace.txt
+sed_in_place -e "s|name|dude|" testSedInPlace.txt
+expect_string_for_actual "Hello dude!" "$(cat testSedInPlace.txt)"
+rm testSedInPlace.txt
 
-end_tests() {
-  ((TESTS_NESTING--))
-  if [[ $TESTS_NESTING -eq 0 ]]; then
-    report_test_results
-  fi
-}
-
-report_test_results() {
-  echo "$((TEST_FAILURES)) failures"
-  if [[ $TEST_FAILURES -gt 0 ]]; then
-    exit 1
-  fi
-}
-
-expect_string_for_actual() {
-  if [[ "$1" != "$2" ]]; then
-    fail_test "Expected '$1', got $2"
-  fi
-}
-
-start_tests
+end_tests
