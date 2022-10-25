d45fd940515728de4a9ac96f78067fc081b75d1a
patchwork01 <110390516+patchwork01@users.noreply.github.com>
2022-10-25 12:23:10 +0100

    Avoid using local -n for macOS compatibility

--- scripts/functions/arrayUtils.sh
+++ scripts/functions/arrayUtils.sh
@@ -14,43 +14,42 @@
 
 is_in_array() {
   local FIND_VALUE=$1
-  local -n ARRAY=$2
-  for item in "${ARRAY[@]}"; do
+  local ARRAY_REF="$2[@]"
+  for item in "${!ARRAY_REF}"; do
     [[ "$FIND_VALUE" == "$item" ]] && return 0
   done
   return 1
 }
 
 any_in_array() {
-  local -n FIND=$1
-  for find_item in "${FIND[@]}"; do
-    is_in_array "$find_item" $2 && return 0
+  local FIND_REF="$1[@]"
+  for find_item in "${!FIND_REF}"; do
+    is_in_array "$find_item" "$2" && return 0
   done
   return 1
 }
 
 union_arrays_to_variable() {
-  local -n ARRAY_1=$1
-  local -n ARRAY_2=$2
-  local -n ARRAY_OUT=$3
-  ARRAY_OUT=()
-  for item in "${ARRAY_1[@]}"; do
-    if is_in_array "$item" ARRAY_2; then
-      ARRAY_OUT+=("$item")
+  local ARRAY_1_REF="$1[@]"
+  local ARRAY_2_NAME=$2
+  local ARRAY_OUT_NAME=$3
+  eval "$ARRAY_OUT_NAME=()"
+  for item in "${!ARRAY_1_REF}"; do
+    if is_in_array "$item" "$ARRAY_2_NAME"; then
+      eval "$ARRAY_OUT_NAME+=(\"$item\")"
     fi
   done
 }
 
 array_equals() {
-  local -n ARRAY_1=$1
-  local -n ARRAY_2=$2
-  local LENGTH_1=${#ARRAY_1[@]}
-  local LENGTH_2=${#ARRAY_2[@]}
-  if [ $LENGTH_1 != $LENGTH_2 ]; then
+  # Use eval to get lengths of named arrays
+  if [[ $(eval "echo \${#$1[@]}") -ne $(eval "echo \${#$2[@]}") ]]; then
     return 1
   fi
   for ((i=0;i<=LENGTH_1;i++)); do
-    [[ "${ARRAY_1[i]}" != "${ARRAY_2[i]}" ]] && return 1
+    local ARRAY_1_REF="$1[$i]"
+    local ARRAY_2_REF="$2[$i]"
+    [[ "${!ARRAY_1_REF}" != "${!ARRAY_2_REF}" ]] && return 1
   done
   return 0
 }
