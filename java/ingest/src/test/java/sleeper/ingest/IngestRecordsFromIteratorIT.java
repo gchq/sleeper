0a4631de7cb7112d192418a16f5eb8bb7509e00d
kr565370 <112558283+kr565370@users.noreply.github.com>
2022-10-25 12:17:09 +0100

    Use new test helper methods in IngestRecordsFromIteratorIT

--- java/ingest/src/test/java/sleeper/ingest/IngestRecordsFromIteratorIT.java
+++ java/ingest/src/test/java/sleeper/ingest/IngestRecordsFromIteratorIT.java
@@ -26,15 +26,12 @@ import org.apache.datasketches.quantiles.ItemsUnion;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.parquet.hadoop.ParquetReader;
-import org.apache.parquet.hadoop.ParquetWriter;
 import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
 import org.testcontainers.containers.GenericContainer;
-import sleeper.configuration.jars.ObjectFactory;
 import sleeper.configuration.jars.ObjectFactoryException;
-import sleeper.configuration.properties.InstanceProperties;
 import sleeper.core.CommonTestConstants;
 import sleeper.core.iterator.CloseableIterator;
 import sleeper.core.iterator.IteratorException;
@@ -57,7 +54,6 @@ import sleeper.io.parquet.record.ParquetRecordReader;
 import sleeper.sketches.Sketches;
 import sleeper.sketches.s3.SketchesSerDeToS3;
 import sleeper.statestore.FileInfo;
-import sleeper.statestore.StateStore;
 import sleeper.statestore.StateStoreException;
 import sleeper.statestore.dynamodb.DynamoDBStateStore;
 import sleeper.statestore.dynamodb.DynamoDBStateStoreCreator;
@@ -77,6 +73,15 @@ import java.util.UUID;
 import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static sleeper.ingest.testutils.IngestRecordsTestDataHelper.defaultPropertiesBuilder;
+import static sleeper.ingest.testutils.IngestRecordsTestDataHelper.getLotsOfRecords;
+import static sleeper.ingest.testutils.IngestRecordsTestDataHelper.getRecords;
+import static sleeper.ingest.testutils.IngestRecordsTestDataHelper.getRecords2DimByteArrayKey;
+import static sleeper.ingest.testutils.IngestRecordsTestDataHelper.getRecordsByteArrayKey;
+import static sleeper.ingest.testutils.IngestRecordsTestDataHelper.getRecordsForAggregationIteratorTest;
+import static sleeper.ingest.testutils.IngestRecordsTestDataHelper.getRecordsInFirstPartitionOnly;
+import static sleeper.ingest.testutils.IngestRecordsTestDataHelper.getUnsortedRecords;
+import static sleeper.ingest.testutils.IngestRecordsTestDataHelper.schemaWithRowKeys;
 
 public class IngestRecordsFromIteratorIT {
     private static final int DYNAMO_PORT = 8000;
@@ -90,152 +95,6 @@ public class IngestRecordsFromIteratorIT {
 
     private final Field field = new Field("key", new LongType());
     private final Schema schema = schemaWithRowKeys(field);
-
-    private Schema schemaWithRowKeys(Field... fields) {
-        return Schema.builder()
-                .rowKeyFields(fields)
-                .valueFields(new Field("value1", new LongType()), new Field("value2", new LongType()))
-                .build();
-    }
-
-    public static List<Record> getRecords() {
-        List<Record> records = new ArrayList<>();
-        Record record1 = new Record();
-        record1.put("key", 1L);
-        record1.put("value1", 2L);
-        record1.put("value2", 3L);
-        Record record2 = new Record();
-        record2.put("key", 3L);
-        record2.put("value1", 4L);
-        record2.put("value2", 6L);
-        records.add(record1);
-        records.add(record2);
-        return records;
-    }
-
-    private static List<Record> getLotsOfRecords() {
-        List<Record> records = new ArrayList<>();
-        for (int i = 0; i < 200; i++) {
-            Record record1 = new Record();
-            record1.put("key", 1L - i);
-            record1.put("value1", 2L * i);
-            record1.put("value2", 3L * i);
-            Record record2 = new Record();
-            record2.put("key", 2L + i);
-            record2.put("value1", 4L * i);
-            record2.put("value2", 6L * i);
-            records.add(record1);
-            records.add(record2);
-        }
-        return records;
-    }
-
-    private static List<Record> getRecordsInFirstPartitionOnly() {
-        List<Record> records = new ArrayList<>();
-        Record record1 = new Record();
-        record1.put("key", 1L);
-        record1.put("value1", 2L);
-        record1.put("value2", 3L);
-        Record record2 = new Record();
-        record2.put("key", 0L);
-        record2.put("value1", 4L);
-        record2.put("value2", 6L);
-        records.add(record1);
-        records.add(record2);
-        return records;
-    }
-
-    private static List<Record> getRecordsByteArrayKey() {
-        List<Record> records = new ArrayList<>();
-        Record record1 = new Record();
-        record1.put("key", new byte[]{1, 1});
-        record1.put("value1", 2L);
-        record1.put("value2", 3L);
-        Record record2 = new Record();
-        record2.put("key", new byte[]{2, 2});
-        record2.put("value1", 2L);
-        record2.put("value2", 3L);
-        Record record3 = new Record();
-        record3.put("key", new byte[]{64, 65});
-        record3.put("value1", 4L);
-        record3.put("value2", 6L);
-        records.add(record1);
-        records.add(record2);
-        records.add(record3);
-        return records;
-    }
-
-    private static List<Record> getRecords2DimByteArrayKey() {
-        List<Record> records = new ArrayList<>();
-        Record record1 = new Record();
-        record1.put("key1", new byte[]{1, 1});
-        record1.put("key2", new byte[]{2, 3});
-        record1.put("value1", 2L);
-        record1.put("value2", 3L);
-        Record record2 = new Record();
-        record2.put("key1", new byte[]{11, 2});
-        record2.put("key2", new byte[]{2, 2});
-        record2.put("value1", 2L);
-        record2.put("value2", 3L);
-        Record record3 = new Record();
-        record3.put("key1", new byte[]{64, 65});
-        record3.put("key2", new byte[]{67, 68});
-        record3.put("value1", 4L);
-        record3.put("value2", 6L);
-        Record record4 = new Record();
-        record4.put("key1", new byte[]{5});
-        record4.put("key2", new byte[]{99});
-        record4.put("value1", 2L);
-        record4.put("value2", 3L);
-        records.add(record1);
-        records.add(record2);
-        records.add(record3);
-        records.add(record3); // Add twice so that one file has more entries so we can tell them apart
-        records.add(record4);
-        return records;
-    }
-
-    private static List<Record> getUnsortedRecords() {
-        List<Record> records = new ArrayList<>();
-        for (int i = 10; i > 0; i--) {
-            Record record1 = new Record();
-            record1.put("key", (long) i);
-            record1.put("value1", 2L);
-            record1.put("value2", 3L);
-            records.add(record1);
-            Record record2 = new Record();
-            record2.put("key", 5L);
-            record2.put("value1", 4L);
-            record2.put("value2", 6L);
-            records.add(record2);
-        }
-        return records;
-    }
-
-    private static List<Record> getRecordsForAggregationIteratorTest() {
-        List<Record> records = new ArrayList<>();
-        Record record1 = new Record();
-        record1.put("key", new byte[]{1, 1});
-        record1.put("sort", 2L);
-        record1.put("value", 1L);
-        Record record2 = new Record();
-        record2.put("key", new byte[]{11, 2});
-        record2.put("sort", 1L);
-        record2.put("value", 1L);
-        Record record3 = new Record();
-        record3.put("key", new byte[]{1, 1});
-        record3.put("sort", 2L);
-        record3.put("value", 6L);
-        Record record4 = new Record();
-        record4.put("key", new byte[]{11, 2});
-        record4.put("sort", 1L);
-        record4.put("value", 3L);
-        records.add(record1);
-        records.add(record2);
-        records.add(record3);
-        records.add(record4);
-        return records;
-    }
 
     private static DynamoDBStateStore getStateStore(Schema schema) throws StateStoreException {
         return getStateStore(schema, new PartitionsFromSplitPoints(schema, Collections.emptyList()).construct());
@@ -1210,23 +1069,5 @@ public class IngestRecordsFromIteratorIT {
         for (double d = 0.0D; d < 1.0D; d += 0.1D) {
             assertThat(readSketches.getQuantilesSketch("key").getQuantile(d)).isEqualTo(expectedSketch.getQuantile(d));
         }
-    }
-
-    private static IngestProperties.Builder defaultPropertiesBuilder(StateStore stateStore,
-                                                                     Schema sleeperSchema,
-                                                                     String ingestLocalWorkingDirectory,
-                                                                     String bucketName) throws IOException, ObjectFactoryException {
-        return IngestProperties.builder()
-                .objectFactory(new ObjectFactory(new InstanceProperties(), null, ingestLocalWorkingDirectory))
-                .localDir(ingestLocalWorkingDirectory)
-                .maxRecordsToWriteLocally(10L)
-                .maxInMemoryBatchSize(1000L)
-                .rowGroupSize(ParquetWriter.DEFAULT_BLOCK_SIZE)
-                .pageSize(ParquetWriter.DEFAULT_PAGE_SIZE)
-                .compressionCodec("zstd")
-                .stateStore(stateStore)
-                .schema(sleeperSchema)
-                .bucketName(bucketName)
-                .ingestPartitionRefreshFrequencyInSecond(120);
     }
 }
