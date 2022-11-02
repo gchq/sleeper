1dfbae1426d170944b9ad469e7072db07dfc0437
kr565370 <112558283+kr565370@users.noreply.github.com>
2022-11-02 10:52:32 +0000

    Remove unnecessary tests

--- java/ingest/src/test/java/sleeper/ingest/IngestRecordsFromIteratorTest.java
+++ java/ingest/src/test/java/sleeper/ingest/IngestRecordsFromIteratorTest.java
@@ -17,35 +17,26 @@
 package sleeper.ingest;
 
 import org.junit.Test;
-import sleeper.core.iterator.impl.AdditionIterator;
 import sleeper.core.partition.Partition;
 import sleeper.core.range.Range;
 import sleeper.core.range.Region;
 import sleeper.core.record.Record;
-import sleeper.core.schema.Field;
-import sleeper.core.schema.Schema;
-import sleeper.core.schema.type.ByteArrayType;
 import sleeper.core.schema.type.LongType;
 import sleeper.ingest.testutils.AssertQuantiles;
 import sleeper.statestore.FileInfo;
 import sleeper.statestore.StateStore;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import static com.facebook.collections.ByteArray.wrap;
 import static org.assertj.core.api.Assertions.assertThat;
 import static sleeper.ingest.testutils.IngestRecordsTestDataHelper.createLeafPartition;
 import static sleeper.ingest.testutils.IngestRecordsTestDataHelper.createRootPartition;
 import static sleeper.ingest.testutils.IngestRecordsTestDataHelper.getRecords;
-import static sleeper.ingest.testutils.IngestRecordsTestDataHelper.getRecordsForAggregationIteratorTest;
 import static sleeper.ingest.testutils.IngestRecordsTestDataHelper.getSingleRecord;
 import static sleeper.ingest.testutils.IngestRecordsTestDataHelper.getSketches;
-import static sleeper.ingest.testutils.IngestRecordsTestDataHelper.getUnsortedRecords;
 import static sleeper.ingest.testutils.IngestRecordsTestDataHelper.readRecordsFromParquetFile;
 
 public class IngestRecordsFromIteratorTest extends IngestRecordsTestBase {
@@ -157,96 +148,6 @@ public class IngestRecordsFromIteratorTest extends IngestRecordsTestBase {
                 .quantile(0.4, 1L).quantile(0.5, 1L)
                 .quantile(0.6, 1L).quantile(0.7, 1L)
                 .quantile(0.8, 1L).quantile(0.9, 1L).verify();
-    }
-
-    @Test
-    public void shouldSortRecords() throws Exception {
-        // Given
-        StateStore stateStore = getStateStore(schema);
-
-        // When
-        IngestProperties properties = defaultPropertiesBuilder(stateStore, schema).build();
-        long numWritten = new IngestRecordsFromIterator(properties, getUnsortedRecords().iterator()).write();
-
-        // Then:
-        //  - Check the correct number of records were written
-        assertThat(numWritten).isEqualTo(getUnsortedRecords().size());
-        //  - Check StateStore has correct information
-        List<FileInfo> activeFiles = stateStore.getActiveFiles();
-        assertThat(activeFiles).hasSize(1);
-        FileInfo fileInfo = activeFiles.get(0);
-        assertThat((long) fileInfo.getMinRowKey().get(0)).isOne();
-        assertThat((long) fileInfo.getMaxRowKey().get(0)).isEqualTo(10L);
-        assertThat(fileInfo.getNumberOfRecords().longValue()).isEqualTo(20L);
-        assertThat(fileInfo.getPartitionId()).isEqualTo(stateStore.getAllPartitions().get(0).getId());
-        //  - Read file and check it has correct records
-        List<Record> readRecords = readRecordsFromParquetFile(fileInfo.getFilename(), schema);
-        assertThat(readRecords.size()).isEqualTo(20L);
-        List<Record> sortedRecords = new ArrayList<>(getUnsortedRecords());
-        sortedRecords.sort(Comparator.comparing(o -> ((Long) o.get("key"))));
-        int i = 0;
-        for (Record record1 : sortedRecords) {
-            assertThat(readRecords.get(i)).isEqualTo(record1);
-            i++;
-        }
-        //  - Check quantiles sketches have been written and are correct (NB the sketches are stochastic so may not be identical)
-        AssertQuantiles.forSketch(getSketches(schema, activeFiles.get(0).getFilename()).getQuantilesSketch("key"))
-                .min(1L).max(10L)
-                .quantile(0.0, 1L).quantile(0.1, 3L)
-                .quantile(0.2, 5L).quantile(0.3, 5L)
-                .quantile(0.4, 5L).quantile(0.5, 5L)
-                .quantile(0.6, 5L).quantile(0.7, 5L)
-                .quantile(0.8, 7L).quantile(0.9, 9L).verify();
-    }
-
-    @Test
-    public void shouldApplyIterator() throws Exception {
-        // Given
-        Schema schema = Schema.builder()
-                .rowKeyFields(new Field("key", new ByteArrayType()))
-                .sortKeyFields(new Field("sort", new LongType()))
-                .valueFields(new Field("value", new LongType()))
-                .build();
-
-        StateStore stateStore = getStateStore(schema);
-
-        // When
-        IngestProperties properties = defaultPropertiesBuilder(stateStore, schema)
-                .iteratorClassName(AdditionIterator.class.getName())
-                .build();
-        long numWritten = new IngestRecordsFromIterator(properties, getRecordsForAggregationIteratorTest().iterator()).write();
-
-        // Then:
-        //  - Check the correct number of records were written
-        assertThat(numWritten).isEqualTo(2L);
-        //  - Check StateStore has correct information
-        List<FileInfo> activeFiles = stateStore.getActiveFiles();
-        assertThat(activeFiles).hasSize(1);
-        FileInfo fileInfo = activeFiles.get(0);
-        assertThat((byte[]) fileInfo.getMinRowKey().get(0)).containsExactly(new byte[]{1, 1});
-        assertThat((byte[]) fileInfo.getMaxRowKey().get(0)).containsExactly(new byte[]{11, 2});
-        assertThat(fileInfo.getNumberOfRecords().longValue()).isEqualTo(2L);
-        assertThat(fileInfo.getPartitionId()).isEqualTo(stateStore.getAllPartitions().get(0).getId());
-        //  - Read file and check it has correct records
-        List<Record> readRecords = readRecordsFromParquetFile(fileInfo.getFilename(), schema);
-        Record expectedRecord1 = new Record();
-        expectedRecord1.put("key", new byte[]{1, 1});
-        expectedRecord1.put("sort", 2L);
-        expectedRecord1.put("value", 7L);
-        Record expectedRecord2 = new Record();
-        expectedRecord2.put("key", new byte[]{11, 2});
-        expectedRecord2.put("sort", 1L);
-        expectedRecord2.put("value", 4L);
-        assertThat(readRecords).containsExactly(expectedRecord1, expectedRecord2);
-
-        //  - Check quantiles sketches have been written and are correct (NB the sketches are stochastic so may not be identical)
-        AssertQuantiles.forSketch(getSketches(schema, activeFiles.get(0).getFilename()).getQuantilesSketch("key"))
-                .min(wrap(new byte[]{1, 1})).max(wrap(new byte[]{11, 2}))
-                .quantile(0.0, wrap(new byte[]{1, 1})).quantile(0.1, wrap(new byte[]{1, 1}))
-                .quantile(0.2, wrap(new byte[]{1, 1})).quantile(0.3, wrap(new byte[]{1, 1}))
-                .quantile(0.4, wrap(new byte[]{1, 1})).quantile(0.5, wrap(new byte[]{11, 2}))
-                .quantile(0.6, wrap(new byte[]{11, 2})).quantile(0.7, wrap(new byte[]{11, 2}))
-                .quantile(0.8, wrap(new byte[]{11, 2})).quantile(0.9, wrap(new byte[]{11, 2})).verify();
     }
 
     @Test
