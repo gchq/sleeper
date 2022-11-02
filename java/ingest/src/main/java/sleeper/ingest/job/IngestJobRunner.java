de7d4a36c5663d574c9c2204838a60d543e4fdd1
kr565370 <112558283+kr565370@users.noreply.github.com>
2022-11-02 17:03:20 +0000

    Extract all non-AWS operations into IngestJobRunner

--- java/ingest/src/main/java/sleeper/ingest/job/IngestJobRunner.java
+++ java/ingest/src/main/java/sleeper/ingest/job/IngestJobRunner.java
@@ -13,31 +13,170 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package sleeper.ingest.job;
 
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.parquet.hadoop.ParquetReader;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import sleeper.configuration.jars.ObjectFactory;
+import sleeper.configuration.properties.InstanceProperties;
+import sleeper.configuration.properties.table.TableProperties;
+import sleeper.configuration.properties.table.TablePropertiesProvider;
+import sleeper.core.iterator.CloseableIterator;
+import sleeper.core.iterator.ConcatenatingIterator;
 import sleeper.core.iterator.IteratorException;
 import sleeper.core.record.Record;
-import sleeper.ingest.IngestRecordsFromIterator;
-import sleeper.ingest.IngestResult;
-import sleeper.ingest.impl.IngestCoordinator;
+import sleeper.core.schema.Schema;
+import sleeper.ingest.IngestRecordsUsingPropertiesSpecifiedMethod;
+import sleeper.io.parquet.record.ParquetReaderIterator;
+import sleeper.io.parquet.record.ParquetRecordReader;
+import sleeper.statestore.FileInfo;
+import sleeper.statestore.StateStore;
 import sleeper.statestore.StateStoreException;
+import sleeper.statestore.StateStoreProvider;
+import software.amazon.awssdk.services.s3.S3AsyncClient;
 
 import java.io.IOException;
-import java.util.Iterator;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Supplier;
 
+import static sleeper.configuration.properties.UserDefinedInstanceProperty.FILE_SYSTEM;
+import static sleeper.configuration.properties.UserDefinedInstanceProperty.MAX_IN_MEMORY_BATCH_SIZE;
+import static sleeper.configuration.properties.UserDefinedInstanceProperty.MAX_RECORDS_TO_WRITE_LOCALLY;
+import static sleeper.configuration.properties.UserDefinedInstanceProperty.S3A_INPUT_FADVISE;
+import static sleeper.configuration.properties.table.TableProperty.ITERATOR_CLASS_NAME;
+import static sleeper.configuration.properties.table.TableProperty.ITERATOR_CONFIG;
+
+/**
+ * An IngestJobQueueConsumer pulls ingest jobs off an SQS queue and runs them.
+ */
 public class IngestJobRunner {
-    private final IngestRecordsFromIterator ingestRecordsFromIterator;
+    private static final Logger LOGGER = LoggerFactory.getLogger(IngestJobRunner.class);
 
-    private IngestJobRunner(IngestCoordinator<Record> ingestCoordinator, Iterator<Record> recordIterator) {
-        this.ingestRecordsFromIterator = new IngestRecordsFromIterator(ingestCoordinator, recordIterator);
+    private final ObjectFactory objectFactory;
+    private final InstanceProperties instanceProperties;
+    private final TablePropertiesProvider tablePropertiesProvider;
+    private final StateStoreProvider stateStoreProvider;
+    private final String localDir;
+    private final long maxLinesInLocalFile;
+    private final long maxInMemoryBatchSize;
+    private final String fs;
+    private final S3AsyncClient s3AsyncClient;
+    private final Configuration hadoopConfiguration;
+    private List<IngestJob> ingestJobList;
+
+    public IngestJobRunner(ObjectFactory objectFactory,
+                           InstanceProperties instanceProperties,
+                           TablePropertiesProvider tablePropertiesProvider,
+                           StateStoreProvider stateStoreProvider,
+                           String localDir,
+                           S3AsyncClient s3AsyncClient,
+                           List<IngestJob> jobList) {
+        this(objectFactory,
+                instanceProperties,
+                tablePropertiesProvider,
+                stateStoreProvider,
+                localDir,
+                s3AsyncClient,
+                defaultHadoopConfiguration(instanceProperties.get(S3A_INPUT_FADVISE)),
+                jobList);
     }
 
-    public static IngestJobRunner with(IngestCoordinator<Record> ingestCoordinator, Iterator<Record> recordIterator) {
-        return new IngestJobRunner(ingestCoordinator, recordIterator);
+    public IngestJobRunner(ObjectFactory objectFactory,
+                           InstanceProperties instanceProperties,
+                           TablePropertiesProvider tablePropertiesProvider,
+                           StateStoreProvider stateStoreProvider,
+                           String localDir,
+                           S3AsyncClient s3AsyncClient,
+                           Configuration hadoopConfiguration,
+                           List<IngestJob> jobList) {
+        this.objectFactory = objectFactory;
+        this.instanceProperties = instanceProperties;
+        this.tablePropertiesProvider = tablePropertiesProvider;
+        this.stateStoreProvider = stateStoreProvider;
+        this.localDir = localDir;
+        this.maxLinesInLocalFile = instanceProperties.getLong(MAX_RECORDS_TO_WRITE_LOCALLY);
+        this.maxInMemoryBatchSize = instanceProperties.getLong(MAX_IN_MEMORY_BATCH_SIZE);
+        this.fs = instanceProperties.get(FILE_SYSTEM);
+        this.hadoopConfiguration = hadoopConfiguration;
+        this.ingestJobList = new ArrayList<>(jobList);
+        this.s3AsyncClient = s3AsyncClient;
     }
 
-    public IngestResult run() throws StateStoreException, IteratorException, IOException {
-        return ingestRecordsFromIterator.write();
+    private static Configuration defaultHadoopConfiguration(String fadvise) {
+        Configuration conf = new Configuration();
+        conf.set("fs.s3a.connection.maximum", "10");
+        conf.set("fs.s3a.aws.credentials.provider", "com.amazonaws.auth.EC2ContainerCredentialsProviderWrapper");
+        conf.set("fs.s3a.experimental.input.fadvise", fadvise);
+        return conf;
+    }
+
+    public void run() throws InterruptedException, IOException, StateStoreException, IteratorException {
+        for (IngestJob ingestJob : ingestJobList) {
+            LOGGER.info("Deserialised message to ingest job {}", ingestJob);
+
+            TableProperties tableProperties = tablePropertiesProvider.getTableProperties(ingestJob.getTableName());
+            Schema schema = tableProperties.getSchema();
+
+            long recordsWritten = ingest(ingestJob, schema, tableProperties);
+            LOGGER.info("{} records were written", recordsWritten);
+        }
+
+    }
+
+    public long ingest(IngestJob job, Schema schema, TableProperties tableProperties) throws InterruptedException, IteratorException, StateStoreException, IOException {
+        // Create list of all files to be read
+        List<Path> paths = IngestJobUtils.getPaths(job.getFiles(), hadoopConfiguration, fs);
+        LOGGER.info("There are {} files to ingest", paths.size());
+        LOGGER.debug("Files to ingest are: {}", paths);
+        LOGGER.info("Max number of records to read into memory is {}", maxInMemoryBatchSize);
+        LOGGER.info("Max number of records to write to local disk is {}", maxLinesInLocalFile);
+
+        // Create supplier of iterator of records from each file (using a supplier avoids having multiple files open
+        // at the same time)
+        List<Supplier<CloseableIterator<Record>>> inputIterators = new ArrayList<>();
+        for (Path path : paths) {
+            String pathString = path.toString();
+            if (pathString.endsWith(".parquet")) {
+                inputIterators.add(() -> {
+                    try {
+                        ParquetReader<Record> reader = new ParquetRecordReader.Builder(path, schema).withConf(hadoopConfiguration).build();
+                        return new ParquetReaderIterator(reader);
+                    } catch (IOException e) {
+                        throw new RuntimeException("Ingest job: " + job.getId() + " IOException creating reader for file "
+                                + path + ": " + e.getMessage());
+                    }
+                });
+            } else {
+                LOGGER.error("A file with a currently unsupported format has been found on ingest, file path: {}"
+                        + ". This file will be ignored and will not be ingested.", pathString);
+            }
+        }
+
+        // Concatenate iterators into one iterator
+        CloseableIterator<Record> concatenatingIterator = new ConcatenatingIterator(inputIterators);
+
+        // Get StateStore for this table
+        StateStore stateStore = stateStoreProvider.getStateStore(tableProperties);
+
+        // Run the ingest
+        List<FileInfo> ingestedFileInfoList = IngestRecordsUsingPropertiesSpecifiedMethod.ingestFromRecordIterator(
+                objectFactory,
+                stateStore,
+                instanceProperties,
+                tableProperties,
+                localDir,
+                s3AsyncClient,
+                hadoopConfiguration,
+                tableProperties.get(ITERATOR_CLASS_NAME),
+                tableProperties.get(ITERATOR_CONFIG),
+                concatenatingIterator);
+        long numRecordsWritten = ingestedFileInfoList.stream().mapToLong(FileInfo::getNumberOfRecords).sum();
+        LOGGER.info("Ingest job {}: Wrote {} records from files {}", job.getId(), numRecordsWritten, paths);
+
+        return numRecordsWritten;
     }
 }
