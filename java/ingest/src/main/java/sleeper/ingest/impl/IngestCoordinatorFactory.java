0db2f10511870dbfbe1db17a249101ecdcce856e
kr565370 <112558283+kr565370@users.noreply.github.com>
2022-11-07 15:28:04 +0000

    Create IngestCoordinatorFactory

---
+++ java/ingest/src/main/java/sleeper/ingest/impl/IngestCoordinatorFactory.java
@@ -0,0 +1,170 @@
+/*
+ * Copyright 2022 Crown Copyright
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package sleeper.ingest.impl;
+
+import org.apache.arrow.memory.BufferAllocator;
+import org.apache.hadoop.conf.Configuration;
+import sleeper.configuration.jars.ObjectFactory;
+import sleeper.configuration.jars.ObjectFactoryException;
+import sleeper.configuration.properties.InstanceProperties;
+import sleeper.configuration.properties.table.TableProperties;
+import sleeper.core.record.Record;
+import sleeper.ingest.IngestProperties;
+import sleeper.statestore.StateStore;
+import software.amazon.awssdk.services.s3.S3AsyncClient;
+
+import java.util.Locale;
+
+import static sleeper.configuration.properties.UserDefinedInstanceProperty.ARROW_INGEST_BATCH_BUFFER_BYTES;
+import static sleeper.configuration.properties.UserDefinedInstanceProperty.ARROW_INGEST_MAX_SINGLE_WRITE_TO_FILE_RECORDS;
+import static sleeper.configuration.properties.UserDefinedInstanceProperty.ARROW_INGEST_WORKING_BUFFER_BYTES;
+import static sleeper.configuration.properties.UserDefinedInstanceProperty.FILE_SYSTEM;
+import static sleeper.configuration.properties.UserDefinedInstanceProperty.INGEST_PARTITION_FILE_WRITER_TYPE;
+import static sleeper.configuration.properties.UserDefinedInstanceProperty.INGEST_PARTITION_REFRESH_PERIOD_IN_SECONDS;
+import static sleeper.configuration.properties.UserDefinedInstanceProperty.INGEST_RECORD_BATCH_TYPE;
+import static sleeper.configuration.properties.UserDefinedInstanceProperty.MAX_IN_MEMORY_BATCH_SIZE;
+import static sleeper.configuration.properties.UserDefinedInstanceProperty.MAX_RECORDS_TO_WRITE_LOCALLY;
+import static sleeper.configuration.properties.table.TableProperty.COMPRESSION_CODEC;
+import static sleeper.configuration.properties.table.TableProperty.DATA_BUCKET;
+import static sleeper.configuration.properties.table.TableProperty.ITERATOR_CLASS_NAME;
+import static sleeper.configuration.properties.table.TableProperty.ITERATOR_CONFIG;
+import static sleeper.configuration.properties.table.TableProperty.PAGE_SIZE;
+import static sleeper.configuration.properties.table.TableProperty.ROW_GROUP_SIZE;
+
+public class IngestCoordinatorFactory {
+    private ObjectFactory objectFactory;
+    private String localDir;
+    private StateStore stateStore;
+    private Configuration hadoopConfiguration;
+    private BufferAllocator bufferAllocator;
+    private S3AsyncClient s3AsyncClient;
+
+    private IngestCoordinatorFactory(Builder builder) {
+        this.objectFactory = builder.objectFactory;
+        this.localDir = builder.localDir;
+        this.stateStore = builder.stateStore;
+        this.hadoopConfiguration = builder.hadoopConfiguration;
+        this.bufferAllocator = builder.bufferAllocator;
+        this.s3AsyncClient = builder.s3AsyncClient;
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public IngestCoordinator<Record> createIngestCoordinator(InstanceProperties instanceProperties, TableProperties tableProperties) throws ObjectFactoryException {
+        IngestProperties ingestProperties = createIngestProperties(instanceProperties, tableProperties);
+        String recordBatchType = instanceProperties.get(INGEST_RECORD_BATCH_TYPE).toLowerCase(Locale.ROOT);
+        String fileWriterType = instanceProperties.get(INGEST_PARTITION_FILE_WRITER_TYPE).toLowerCase(Locale.ROOT);
+        StandardIngestCoordinator.BackedBuilder ingestCoordinatorBuilder;
+        if (recordBatchType.equals("arraylist")) {
+            ingestCoordinatorBuilder = StandardIngestCoordinator.builder().fromProperties(ingestProperties)
+                    .backedByArrayList()
+                    .maxNoOfRecordsInMemory((int) ingestProperties.getMaxInMemoryBatchSize())
+                    .maxNoOfRecordsInLocalStore(ingestProperties.getMaxRecordsToWriteLocally());
+        } else if (recordBatchType.equals("arrow")) {
+            ingestCoordinatorBuilder = StandardIngestCoordinator.builder().fromProperties(ingestProperties)
+                    .backedByArrow()
+                    .arrowBufferAllocator(bufferAllocator)
+                    .maxNoOfRecordsToWriteToArrowFileAtOnce(instanceProperties.getInt(ARROW_INGEST_MAX_SINGLE_WRITE_TO_FILE_RECORDS))
+                    .workingArrowBufferAllocatorBytes(instanceProperties.getLong(ARROW_INGEST_WORKING_BUFFER_BYTES))
+                    .minBatchArrowBufferAllocatorBytes(instanceProperties.getLong(ARROW_INGEST_BATCH_BUFFER_BYTES))
+                    .maxBatchArrowBufferAllocatorBytes(instanceProperties.getLong(ARROW_INGEST_BATCH_BUFFER_BYTES))
+                    .maxNoOfBytesToWriteLocally(ingestProperties.getMaxRecordsToWriteLocally());
+        } else {
+            throw new UnsupportedOperationException(String.format("Record batch type %s not supported", recordBatchType));
+        }
+        if (fileWriterType.equals("direct")) {
+            return ingestCoordinatorBuilder.buildDirectWrite(ingestProperties.getFilePrefix() + ingestProperties.getBucketName());
+        } else if (fileWriterType.equals("async")) {
+            if (!instanceProperties.get(FILE_SYSTEM).toLowerCase(Locale.ROOT).equals("s3a://")) {
+                throw new UnsupportedOperationException("Attempting an asynchronous write to a file system that is not s3a://");
+            } else {
+                return ingestCoordinatorBuilder.buildAsyncS3Write(ingestProperties.getBucketName(), s3AsyncClient);
+            }
+        } else {
+            throw new UnsupportedOperationException(String.format("Record batch type %s not supported", recordBatchType));
+        }
+    }
+
+    private IngestProperties createIngestProperties(
+            InstanceProperties instanceProperties, TableProperties tableProperties) {
+
+        return IngestProperties.builder()
+                .objectFactory(objectFactory)
+                .localDir(localDir)
+                .rowGroupSize(tableProperties.getInt(ROW_GROUP_SIZE))
+                .pageSize(tableProperties.getInt(PAGE_SIZE))
+                .stateStore(stateStore)
+                .schema(tableProperties.getSchema())
+                .iteratorClassName(tableProperties.get(ITERATOR_CLASS_NAME))
+                .iteratorConfig(tableProperties.get(ITERATOR_CONFIG))
+                .compressionCodec(tableProperties.get(COMPRESSION_CODEC))
+                .filePathPrefix(instanceProperties.get(FILE_SYSTEM))
+                .bucketName(tableProperties.get(DATA_BUCKET))
+                .hadoopConfiguration(hadoopConfiguration)
+                .maxInMemoryBatchSize(instanceProperties.getInt(MAX_IN_MEMORY_BATCH_SIZE))
+                .maxRecordsToWriteLocally(instanceProperties.getLong(MAX_RECORDS_TO_WRITE_LOCALLY))
+                .ingestPartitionRefreshFrequencyInSecond(instanceProperties.getInt(INGEST_PARTITION_REFRESH_PERIOD_IN_SECONDS)).build();
+    }
+
+    public static final class Builder {
+        private ObjectFactory objectFactory;
+        private String localDir;
+        private StateStore stateStore;
+        private Configuration hadoopConfiguration;
+        private BufferAllocator bufferAllocator;
+        private S3AsyncClient s3AsyncClient;
+
+        private Builder() {
+        }
+
+        public Builder objectFactory(ObjectFactory objectFactory) {
+            this.objectFactory = objectFactory;
+            return this;
+        }
+
+        public Builder localDir(String localDir) {
+            this.localDir = localDir;
+            return this;
+        }
+
+        public Builder stateStore(StateStore stateStore) {
+            this.stateStore = stateStore;
+            return this;
+        }
+
+        public Builder hadoopConfiguration(Configuration hadoopConfiguration) {
+            this.hadoopConfiguration = hadoopConfiguration;
+            return this;
+        }
+
+        public Builder bufferAllocator(BufferAllocator bufferAllocator) {
+            this.bufferAllocator = bufferAllocator;
+            return this;
+        }
+
+        public Builder s3AsyncClient(S3AsyncClient s3AsyncClient) {
+            this.s3AsyncClient = s3AsyncClient;
+            return this;
+        }
+
+        public IngestCoordinatorFactory build() {
+            return new IngestCoordinatorFactory(this);
+        }
+    }
+}
