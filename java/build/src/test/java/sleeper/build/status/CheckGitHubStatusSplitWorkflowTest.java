aaefadcf78bbfde88f0e74a3c0fac01a224e0318
kr565370 <112558283+kr565370@users.noreply.github.com>
2022-11-10 12:23:03 +0000

    Rename test to indicate split workflow

--- java/build/src/test/java/sleeper/build/status/CheckGitHubStatusSplitWorkflowTest.java
+++ java/build/src/test/java/sleeper/build/status/CheckGitHubStatusSplitWorkflowTest.java
@@ -26,7 +26,7 @@ import java.util.Collections;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class CheckGitHubStatusTest {
+public class CheckGitHubStatusSplitWorkflowTest {
 
     private final TestGitHubWorkflowRuns workflowRuns = new TestGitHubWorkflowRuns();
 
