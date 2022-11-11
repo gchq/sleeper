6b45f8d6c843db0a6af71cff77286cbc672bd083
patchwork01 <110390516+patchwork01@users.noreply.github.com>
2022-11-11 11:44:03 +0000

    Explicitly register Java 8 time module in JacksonProvider

--- java/build/src/main/java/sleeper/build/github/JacksonProvider.java
+++ java/build/src/main/java/sleeper/build/github/JacksonProvider.java
@@ -16,6 +16,7 @@
 package sleeper.build.github;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 import javax.ws.rs.ext.ContextResolver;
 import javax.ws.rs.ext.Provider;
@@ -26,7 +27,7 @@ public class JacksonProvider implements ContextResolver<ObjectMapper> {
     @Override
     public ObjectMapper getContext(Class<?> type) {
         ObjectMapper mapper = new ObjectMapper();
-        mapper.findAndRegisterModules();
+        mapper.registerModule(new JavaTimeModule());
         return mapper;
     }
 }
