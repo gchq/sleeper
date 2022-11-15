4d90402d3b2ff03209c7103b573f05943f372b89
kr565370 <112558283+kr565370@users.noreply.github.com>
2022-11-15 16:13:24 +0000

    Fix spotbugs issues by adding temporary getters

--- java/ingest-status-store/src/main/java/sleeper/ingest/status/DynamoDBIngestJobStatusStore.java
+++ java/ingest-status-store/src/main/java/sleeper/ingest/status/DynamoDBIngestJobStatusStore.java
@@ -21,13 +21,14 @@ import sleeper.configuration.properties.InstanceProperties;
 import sleeper.configuration.properties.UserDefinedInstanceProperty;
 
 import static sleeper.configuration.properties.UserDefinedInstanceProperty.ID;
+import static sleeper.configuration.properties.UserDefinedInstanceProperty.INGEST_STATUS_STORE_ENABLED;
 import static sleeper.dynamodb.tools.DynamoDBUtils.instanceTableName;
 
 public class DynamoDBIngestJobStatusStore implements IngestJobStatusStore {
 
     private final AmazonDynamoDB dynamoDB;
     private final String statusTableName;
-    private long timeToLive;
+    private final long timeToLive;
 
     private DynamoDBIngestJobStatusStore(AmazonDynamoDB dynamoDB, InstanceProperties properties) {
         this.dynamoDB = dynamoDB;
@@ -35,7 +36,27 @@ public class DynamoDBIngestJobStatusStore implements IngestJobStatusStore {
         this.timeToLive = properties.getLong(UserDefinedInstanceProperty.INGEST_JOB_STATUS_TTL_IN_SECONDS) * 1000;
     }
 
+    public static IngestJobStatusStore from(AmazonDynamoDB dynamoDB, InstanceProperties properties) {
+        if (Boolean.TRUE.equals(properties.getBoolean(INGEST_STATUS_STORE_ENABLED))) {
+            return new DynamoDBIngestJobStatusStore(dynamoDB, properties);
+        } else {
+            return IngestJobStatusStore.none();
+        }
+    }
+
     public static String jobStatusTableName(String instanceId) {
         return instanceTableName(instanceId, "ingest-job-status");
+    }
+
+    public AmazonDynamoDB getDynamoDB() {
+        return dynamoDB;
+    }
+
+    public String getStatusTableName() {
+        return statusTableName;
+    }
+
+    public long getTimeToLive() {
+        return timeToLive;
     }
 }
