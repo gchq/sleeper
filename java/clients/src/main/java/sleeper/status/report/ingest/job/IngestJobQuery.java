3861aa76c2ae4579d522b7efb39c672bf029031c
patchwork01 <110390516+patchwork01@users.noreply.github.com>
2022-11-22 13:01:31 +0000

    Define a method on IngestJobQuery

--- java/clients/src/main/java/sleeper/status/report/ingest/job/IngestJobQuery.java
+++ java/clients/src/main/java/sleeper/status/report/ingest/job/IngestJobQuery.java
@@ -19,4 +19,8 @@ package sleeper.status.report.ingest.job;
 public interface IngestJobQuery {
     IngestJobQuery ALL = new IngestJobQuery() {
     };
+
+    // stub method for checkstyle
+    default void run() {
+    }
 }
