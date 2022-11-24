92749fa329b156781fe7ed18e6279ae9912b0a26
kr565370 <112558283+kr565370@users.noreply.github.com>
2022-11-24 16:06:36 +0000

    Move job id generator into StatusReporterTestHelper

--- java/clients/src/test/java/sleeper/status/report/compactionjob/CompactionJobStatusReporterTestBase.java
+++ java/clients/src/test/java/sleeper/status/report/compactionjob/CompactionJobStatusReporterTestBase.java
@@ -26,6 +26,7 @@ import sleeper.core.record.process.RecordsProcessedSummary;
 import sleeper.core.record.process.status.ProcessFinishedStatus;
 import sleeper.core.record.process.status.ProcessRun;
 import sleeper.core.record.process.status.ProcessStartedStatus;
+import sleeper.status.report.StatusReporterTestHelper;
 import sleeper.status.report.compactionjob.CompactionJobStatusReporter.QueryType;
 
 import java.io.PrintStream;
@@ -44,10 +45,6 @@ import static sleeper.status.report.StatusReporterTestHelper.jobRunStartedInTask
 import static sleeper.status.report.StatusReporterTestHelper.task;
 
 public abstract class CompactionJobStatusReporterTestBase {
-
-    protected static String job(int number) {
-        return exampleUUID("job", number);
-    }
 
     protected static String partition(String letter) {
         return exampleUUID("partn", letter);
@@ -141,7 +138,7 @@ public abstract class CompactionJobStatusReporterTestBase {
     }
 
     protected static String replaceStandardJobIds(List<CompactionJobStatus> jobs, String example) {
-        return replaceJobIds(jobs, CompactionJobStatusReporterTestBase::job, example);
+        return replaceJobIds(jobs, StatusReporterTestHelper::job, example);
     }
 
     protected static String replaceBracketedJobIds(List<CompactionJobStatus> jobs, String example) {
