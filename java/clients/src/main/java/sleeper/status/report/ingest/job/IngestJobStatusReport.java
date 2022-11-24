a803808b69cdb1f502d3602f89275c090bad768d
kr565370 <112558283+kr565370@users.noreply.github.com>
2022-11-24 14:22:06 +0000

    Use StandardProcessStatusReporter and TableWriter in IngestJobStatusReport

--- java/clients/src/main/java/sleeper/status/report/ingest/job/IngestJobStatusReport.java
+++ java/clients/src/main/java/sleeper/status/report/ingest/job/IngestJobStatusReport.java
@@ -16,41 +16,82 @@
 
 package sleeper.status.report.ingest.job;
 
+import sleeper.core.record.process.AverageRecordRate;
+import sleeper.core.record.process.status.ProcessRun;
+import sleeper.ingest.job.status.IngestJobStatus;
+import sleeper.status.report.StandardProcessStatusReporter;
 import sleeper.status.report.table.TableField;
+import sleeper.status.report.table.TableRow;
+import sleeper.status.report.table.TableWriter;
 import sleeper.status.report.table.TableWriterFactory;
 
 import java.io.PrintStream;
+import java.util.List;
+
+import static sleeper.status.report.StandardProcessStatusReporter.formatDecimal;
 
 public class IngestJobStatusReport {
-    private static final TableWriterFactory.Builder TABLE_FACTORY_BUILDER = TableWriterFactory.builder();
 
-    private static final TableField STATE = TABLE_FACTORY_BUILDER.addField("STATE");
-    private static final TableField JOB_ID = TABLE_FACTORY_BUILDER.addField("JOB_ID");
-    private static final TableField TASK_ID = TABLE_FACTORY_BUILDER.addField("TASK_ID");
-    private static final TableField START_TIME = TABLE_FACTORY_BUILDER.addField("START_TIME");
-    private static final TableField FINISH_TIME = TABLE_FACTORY_BUILDER.addField("FINISH_TIME");
-    private static final TableField DURATION = TABLE_FACTORY_BUILDER.fieldBuilder("DURATION (s)").alignRight().build();
-    private static final TableField TOTAL_FILES = TABLE_FACTORY_BUILDER.addField("TOTAL_FILES");
-    private static final TableField LINES_READ = TABLE_FACTORY_BUILDER.fieldBuilder("LINES_READ").alignRight().build();
-    private static final TableField LINES_WRITTEN = TABLE_FACTORY_BUILDER.fieldBuilder("LINES_WRITTEN").alignRight().build();
-    private static final TableField READ_RATE = TABLE_FACTORY_BUILDER.fieldBuilder("READ_RATE (s)").alignRight().build();
-    private static final TableField WRITE_RATE = TABLE_FACTORY_BUILDER.fieldBuilder("WRITE_RATE (s)").alignRight().build();
-    private static final TableWriterFactory TABLE_FACTORY = TABLE_FACTORY_BUILDER.build();
+    private final TableField stateField;
+    private final TableField jobIdField;
+    private final TableField totalFilesField;
+    private final TableWriterFactory tableFactory;
+    private final StandardProcessStatusReporter standardProcessStatusReporter;
 
     private final PrintStream out;
 
     public IngestJobStatusReport(PrintStream out) {
         this.out = out;
+        TableWriterFactory.Builder tableFactoryBuilder = TableWriterFactory.builder();
+        stateField = tableFactoryBuilder.addField("STATE");
+        jobIdField = tableFactoryBuilder.addField("JOB_ID");
+        totalFilesField = tableFactoryBuilder.fieldBuilder("TOTAL_FILES").alignRight().build();
+        standardProcessStatusReporter = new StandardProcessStatusReporter(tableFactoryBuilder);
+        tableFactory = tableFactoryBuilder.build();
     }
 
-    public void run(IngestJobQuery query) {
+    public void run(IngestJobQuery query, List<IngestJobStatus> statusList, int numberInQueue) {
         out.println();
         out.println("Ingest Job Status Report");
         out.println("------------------------");
-        out.printf("Total jobs waiting in queue (excluded from report): %s%n", 0);
-        out.printf("Total jobs in progress: %s%n", 0);
-        out.printf("Total jobs finished: %s%n", 0);
-        TABLE_FACTORY.tableBuilder()
+        out.printf("Total jobs waiting in queue (excluded from report): %s%n", numberInQueue);
+        out.printf("Total jobs in progress: %s%n", statusList.stream().filter(status -> !status.isFinished()).count());
+        out.printf("Total jobs finished: %s%n", statusList.stream().filter(IngestJobStatus::isFinished).count());
+        printAverageIngestRate("Average ingest rate: %s%n", statusList);
+        tableFactory.tableBuilder()
+                .itemsAndSplittingWriter(statusList, this::writeJob)
                 .build().write(out);
+    }
+
+    private void printAverageIngestRate(String formatString, List<IngestJobStatus> jobs) {
+        AverageRecordRate average = recordRate(jobs);
+        if (average.getJobCount() < 1) {
+            return;
+        }
+        String rateString = String.format("%s read/s, %s write/s",
+                formatDecimal(average.getRecordsReadPerSecond()),
+                formatDecimal(average.getRecordsWrittenPerSecond()));
+        out.printf(formatString, rateString);
+    }
+
+    private static AverageRecordRate recordRate(List<IngestJobStatus> jobs) {
+        return AverageRecordRate.of(jobs.stream()
+                .flatMap(job -> job.getJobRuns().stream())
+                .filter(ProcessRun::isFinished)
+                .map(ProcessRun::getFinishedSummary));
+    }
+
+    private void writeJob(IngestJobStatus job, TableWriter.Builder table) {
+        job.getJobRuns().forEach(run -> table.row(row -> {
+            writeJobFields(job, row);
+            row.value(stateField, StandardProcessStatusReporter.getState(run));
+            standardProcessStatusReporter.writeRunFields(run, row);
+        }));
+
+    }
+
+    private void writeJobFields(IngestJobStatus job, TableRow.Builder builder) {
+        builder.value(jobIdField, job.getJobId())
+                .value(totalFilesField, job.getInputFileCount());
     }
 }
--- java/clients/src/test/java/sleeper/status/report/ingest/job/IngestJobStatusReportTest.java
+++ java/clients/src/test/java/sleeper/status/report/ingest/job/IngestJobStatusReportTest.java
@@ -16,9 +16,20 @@
 
 package sleeper.status.report.ingest.job;
 
-import org.junit.Ignore;
 import org.junit.Test;
 import sleeper.ToStringPrintStream;
+import sleeper.core.record.process.RecordsProcessed;
+import sleeper.core.record.process.RecordsProcessedSummary;
+import sleeper.core.record.process.status.ProcessFinishedStatus;
+import sleeper.core.record.process.status.ProcessRun;
+import sleeper.core.record.process.status.ProcessStartedStatus;
+import sleeper.ingest.job.IngestJob;
+import sleeper.ingest.job.status.IngestJobStatus;
+
+import java.time.Instant;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static sleeper.ClientTestUtils.example;
@@ -33,18 +44,87 @@ public class IngestJobStatusReportTest {
     }
 
     @Test
-    @Ignore("TODO")
     public void shouldReportMixedIngestJobs() throws Exception {
+        // Given
+        List<IngestJobStatus> mixedJobs = createMixedJobs();
 
         // When / Then
-        assertThat(getStandardReport(IngestJobQuery.ALL)).hasToString(
+        assertThat(getStandardReport(IngestJobQuery.ALL, mixedJobs, 2)).hasToString(
                 example("reports/ingest/job/standard/all/mixedJobs.txt"));
     }
 
 
     private String getStandardReport(IngestJobQuery query) {
+        return getStandardReport(query, Collections.emptyList(), 0);
+    }
+
+    private String getStandardReport(IngestJobQuery query, List<IngestJobStatus> statusList, int numberInQueue) {
         ToStringPrintStream output = new ToStringPrintStream();
-        new IngestJobStatusReport(output.getPrintStream()).run(query);
+        new IngestJobStatusReport(output.getPrintStream()).run(query, statusList, numberInQueue);
         return output.toString();
+    }
+
+    private List<IngestJobStatus> createMixedJobs() {
+        IngestJob job1 = IngestJob.builder()
+                .files("test11.parquet")
+                .id("job22222-2222-2222-2222-222222222222")
+                .build();
+        Instant updateTime1 = Instant.parse("2022-09-18T13:34:12.001Z");
+        Instant startTime1 = Instant.parse("2022-09-18T13:34:12.001Z");
+
+        IngestJob job2 = IngestJob.builder()
+                .files("test22.parquet", "test22.parquet")
+                .id("job33333-3333-3333-3333-333333333333")
+                .build();
+        Instant updateTime2 = Instant.parse("2022-09-19T13:34:12.001Z");
+        Instant startTime2 = Instant.parse("2022-09-19T13:34:12.001Z");
+        Instant finishTime2 = Instant.parse("2022-09-19T13:35:12.001Z");
+        RecordsProcessedSummary summary2 = new RecordsProcessedSummary(
+                new RecordsProcessed(600L, 300L), startTime2, finishTime2);
+
+        IngestJob job3 = IngestJob.builder()
+                .files("test31.parquet", "test32.parquet", "test33.parquet")
+                .id("job55555-5555-5555-5555-555555555555")
+                .build();
+        Instant updateTime3 = Instant.parse("2022-09-21T13:34:12.001Z");
+        Instant startTime3 = Instant.parse("2022-09-21T13:34:12.001Z");
+
+        IngestJob job4 = IngestJob.builder()
+                .files("test41.parquet", "test42.parquet", "test43.parquet", "test44.parquet")
+                .id("job66666-6666-6666-6666-666666666666")
+                .build();
+        Instant updateTime4 = Instant.parse("2022-09-22T13:34:12.001Z");
+        Instant startTime4 = Instant.parse("2022-09-22T13:34:12.001Z");
+        Instant finishTime4 = Instant.parse("2022-09-22T13:35:12.001Z");
+        RecordsProcessedSummary summary4 = new RecordsProcessedSummary(
+                new RecordsProcessed(600L, 300L), startTime4, finishTime4);
+        // When
+        IngestJobStatus status1 = IngestJobStatus.builder()
+                .jobId(job1.getId())
+                .inputFileCount(job1.getFiles().size())
+                .jobRun(ProcessRun.started("task1111-1111-1111-1111-111111111111",
+                        ProcessStartedStatus.updateAndStartTime(updateTime1, startTime1)))
+                .build();
+        IngestJobStatus status2 = IngestJobStatus.builder()
+                .jobId(job2.getId())
+                .inputFileCount(job2.getFiles().size())
+                .jobRun(ProcessRun.finished("task1111-1111-1111-1111-111111111111",
+                        ProcessStartedStatus.updateAndStartTime(updateTime2, startTime2),
+                        ProcessFinishedStatus.updateTimeAndSummary(finishTime2, summary2)))
+                .build();
+        IngestJobStatus status3 = IngestJobStatus.builder()
+                .jobId(job3.getId())
+                .inputFileCount(job3.getFiles().size())
+                .jobRun(ProcessRun.started("task2222-2222-2222-2222-222222222222",
+                        ProcessStartedStatus.updateAndStartTime(updateTime3, startTime3)))
+                .build();
+        IngestJobStatus status4 = IngestJobStatus.builder()
+                .jobId(job4.getId())
+                .inputFileCount(job4.getFiles().size())
+                .jobRun(ProcessRun.finished("task2222-2222-2222-2222-222222222222",
+                        ProcessStartedStatus.updateAndStartTime(updateTime4, startTime4),
+                        ProcessFinishedStatus.updateTimeAndSummary(finishTime4, summary4)))
+                .build();
+        return Arrays.asList(status1, status2, status3, status4);
     }
 }
