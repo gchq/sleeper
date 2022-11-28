cf6bdff01195199363be17d0273a8c081b18cf8e
kr565370 <112558283+kr565370@users.noreply.github.com>
2022-11-28 15:43:34 +0000

    Add test coverage for CommonJobUtils.getNumberOfMessagesInQueue

---
+++ java/common-job/src/test/java/sleeper/job/common/action/CommonJobUtilsIT.java
@@ -0,0 +1,101 @@
+/*
+ * Copyright 2022 Crown Copyright
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package sleeper.job.common.action;
+
+import com.amazonaws.services.sqs.AmazonSQS;
+import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
+import com.amazonaws.services.sqs.model.QueueAttributeName;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.testcontainers.containers.localstack.LocalStackContainer;
+import org.testcontainers.utility.DockerImageName;
+import sleeper.core.CommonTestConstants;
+import sleeper.job.common.CommonJobUtils;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CommonJobUtilsIT {
+    private static final String TEST_QUEUE_NAME = "test-queue-url";
+
+    @ClassRule
+    public static LocalStackContainer localStackContainer = new LocalStackContainer(DockerImageName.parse(CommonTestConstants.LOCALSTACK_DOCKER_IMAGE)).withServices(
+            LocalStackContainer.Service.SQS
+    );
+
+    private AmazonSQS createSQSClient() {
+        return AmazonSQSClientBuilder.standard()
+                .withEndpointConfiguration(localStackContainer.getEndpointConfiguration(LocalStackContainer.Service.SQS))
+                .withCredentials(localStackContainer.getDefaultCredentialsProvider())
+                .build();
+    }
+
+    private String createQueue(AmazonSQS sqs) {
+        return sqs.createQueue(TEST_QUEUE_NAME).getQueueUrl();
+    }
+
+    @Test
+    public void shouldReportNoMessagesWhenQueueIsEmpty() {
+        // Given / When
+        AmazonSQS sqsClient = createSQSClient();
+        String queueUrl = createQueue(sqsClient);
+
+        // Then
+        int numberOfMessages = CommonJobUtils.getNumberOfMessagesInQueue(queueUrl, sqsClient)
+                .get(QueueAttributeName.ApproximateNumberOfMessages.toString());
+        assertThat(numberOfMessages).isEqualTo(0);
+        sqsClient.shutdown();
+    }
+
+    @Test
+    public void shouldReportNumberOfMessagesWhenQueueIsNotEmpty() {
+        // Given
+        AmazonSQS sqsClient = createSQSClient();
+        String queueUrl = createQueue(sqsClient);
+
+        // When
+        for (int i = 1; i <= 10; i++) {
+            sqsClient.sendMessage(queueUrl, "{testMessageId:" + i + "}");
+        }
+
+        // Then
+        int numberOfMessages = CommonJobUtils.getNumberOfMessagesInQueue(queueUrl, sqsClient)
+                .get(QueueAttributeName.ApproximateNumberOfMessages.toString());
+        assertThat(numberOfMessages).isEqualTo(10);
+        sqsClient.shutdown();
+    }
+
+    @Test
+    public void shouldReportNumberOfMessagesWhenSomeMessagesHasBeenProcessed() {
+        // Given
+        AmazonSQS sqsClient = createSQSClient();
+        String queueUrl = createQueue(sqsClient);
+
+        // When
+        for (int i = 1; i <= 10; i++) {
+            sqsClient.sendMessage(queueUrl, "{testMessageId:" + i + "}");
+        }
+        for (int i = 1; i <= 3; i++) {
+            sqsClient.receiveMessage(queueUrl);
+        }
+
+        // Then
+        int numberOfMessages = CommonJobUtils.getNumberOfMessagesInQueue(queueUrl, sqsClient)
+                .get(QueueAttributeName.ApproximateNumberOfMessages.toString());
+        assertThat(numberOfMessages).isEqualTo(7);
+        sqsClient.shutdown();
+    }
+}
