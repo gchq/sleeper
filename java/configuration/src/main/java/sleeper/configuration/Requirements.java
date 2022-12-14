17e374c7105869ef0bff64db061f54dc43ec8794
m09526 <m09526@users.noreply.github.com>
2022-12-14 10:20:38 +0000

    Added GPU support to Requirements class

--- java/configuration/src/main/java/sleeper/configuration/Requirements.java
+++ java/configuration/src/main/java/sleeper/configuration/Requirements.java
@@ -22,31 +22,38 @@ import static sleeper.configuration.properties.UserDefinedInstanceProperty.COMPA
 import static sleeper.configuration.properties.UserDefinedInstanceProperty.COMPACTION_TASK_ARM_MEMORY;
 import static sleeper.configuration.properties.UserDefinedInstanceProperty.COMPACTION_TASK_X86_CPU;
 import static sleeper.configuration.properties.UserDefinedInstanceProperty.COMPACTION_TASK_X86_MEMORY;
-
+import static sleeper.configuration.properties.UserDefinedInstanceProperty.COMPACTION_TASK_ARM_GPU;
+import static sleeper.configuration.properties.UserDefinedInstanceProperty.COMPACTION_TASK_X86_GPU;
 public final class Requirements {
 
   private Requirements() {
   }
 
-  /**
-   * Retrieves architecture specific CPU and memory requirements. This returns a triple containing
-   * the CPU requirement in the left element and memory requirement in the right element.
-   *
-   * @param architecture CPU architecture
-   * @param launchType the container launch type
-   * @param instanceProperties Sleeper instance properties
-   * @return CPU and memory requirements as per the CPU architecture
-   */
-  public static Pair<Integer, Integer> getArchRequirements(String architecture, String launchType, InstanceProperties instanceProperties) {
-    int cpu;
-    int memoryLimitMiB;
-    if (architecture.startsWith("ARM")) {
-      cpu = instanceProperties.getInt(COMPACTION_TASK_ARM_CPU);
-      memoryLimitMiB = instanceProperties.getInt(COMPACTION_TASK_ARM_MEMORY);
-    } else {
-      cpu = instanceProperties.getInt(COMPACTION_TASK_X86_CPU);
-      memoryLimitMiB = instanceProperties.getInt(COMPACTION_TASK_X86_MEMORY);
+ /**
+     * Retrieves architecture specific CPU and memory requirements. This returns
+     * a triple containing the CPU requirement in the left element and memory
+     * requirement in the middle and GPU requirement in the right element.
+     *
+     * @param architecture       CPU architecture
+     * @param launchType         the container launch type
+     * @param instanceProperties Sleeper instance properties
+     * @return CPU, memory and GPU requirements as per the CPU architecture
+     */
+    private static Triple<Integer, Integer, Integer> getArchRequirements(String architecture,
+            String launchType,
+            InstanceProperties instanceProperties) {
+        int cpu;
+        int memoryLimitMiB;
+        int gpu;
+        if (architecture.startsWith("ARM")) {
+            cpu = instanceProperties.getInt(COMPACTION_TASK_ARM_CPU);
+            memoryLimitMiB = instanceProperties.getInt(COMPACTION_TASK_ARM_MEMORY);
+            gpu = instanceProperties.getInt(COMPACTION_TASK_ARM_GPU);
+        } else {
+            cpu = instanceProperties.getInt(COMPACTION_TASK_X86_CPU);
+            memoryLimitMiB = instanceProperties.getInt(COMPACTION_TASK_X86_MEMORY);
+            gpu = instanceProperties.getInt(COMPACTION_TASK_X86_GPU);
+        }
+        return Triple.of(cpu, memoryLimitMiB, gpu);
     }
-    return Pair.of(cpu, memoryLimitMiB);
-  }
 }
