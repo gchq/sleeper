7a6817bd089886468046db89f4da42d723b68deb
patchwork01 <110390516+patchwork01@users.noreply.github.com>
2022-12-22 15:38:13 +0000

    Wait for ingest queue to empty

--- java/system-test/src/main/java/sleeper/systemtest/ingest/WaitForIngestTasks.java
+++ java/system-test/src/main/java/sleeper/systemtest/ingest/WaitForIngestTasks.java
@@ -19,6 +19,11 @@ import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.sqs.AmazonSQS;
+import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
+import com.amazonaws.services.sqs.model.GetQueueAttributesRequest;
+import com.amazonaws.services.sqs.model.GetQueueAttributesResult;
+import com.amazonaws.services.sqs.model.QueueAttributeName;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import sleeper.ingest.status.store.task.DynamoDBIngestTaskStatusStore;
@@ -30,21 +35,40 @@ import sleeper.systemtest.util.PollWithRetries;
 import java.io.IOException;
 import java.util.List;
 
+import static sleeper.configuration.properties.SystemDefinedInstanceProperty.INGEST_JOB_QUEUE_URL;
+
 public class WaitForIngestTasks {
     private static final Logger LOGGER = LoggerFactory.getLogger(WaitForIngestTasks.class);
     private static final long POLL_INTERVAL_MILLIS = 30000;
     private static final int MAX_POLLS = 30;
 
+    private final SystemTestProperties systemTestProperties;
+    private final AmazonSQS sqsClient;
     private final IngestTaskStatusStore statusStore;
     private final PollWithRetries poll = PollWithRetries.intervalAndMaxPolls(POLL_INTERVAL_MILLIS, MAX_POLLS);
 
     public WaitForIngestTasks(
+            SystemTestProperties systemTestProperties,
+            AmazonSQS sqsClient,
             IngestTaskStatusStore statusStore) {
+        this.systemTestProperties = systemTestProperties;
+        this.sqsClient = sqsClient;
         this.statusStore = statusStore;
     }
 
     public void pollUntilFinished() throws InterruptedException {
+        poll.pollUntil("ingest queue is empty", this::isIngestQueueEmpty);
         poll.pollUntil("ingest tasks finished", this::isIngestTasksFinished);
+    }
+
+    private boolean isIngestQueueEmpty() {
+        GetQueueAttributesResult result = sqsClient.getQueueAttributes(new GetQueueAttributesRequest()
+                .withQueueUrl(systemTestProperties.get(INGEST_JOB_QUEUE_URL))
+                .withAttributeNames(QueueAttributeName.ApproximateNumberOfMessages));
+        int numberOfMessages = Integer.parseInt(result.getAttributes()
+                .get(QueueAttributeName.ApproximateNumberOfMessages.toString()));
+        LOGGER.info("Jobs on ingest queue: {}", numberOfMessages);
+        return numberOfMessages == 0;
     }
 
     private boolean isIngestTasksFinished() {
@@ -62,13 +86,14 @@ public class WaitForIngestTasks {
         String instanceId = args[0];
 
         AmazonS3 s3Client = AmazonS3ClientBuilder.defaultClient();
+        AmazonSQS sqsClient = AmazonSQSClientBuilder.defaultClient();
         AmazonDynamoDB dynamoDBClient = AmazonDynamoDBClientBuilder.defaultClient();
 
         SystemTestProperties systemTestProperties = new SystemTestProperties();
         systemTestProperties.loadFromS3GivenInstanceId(s3Client, instanceId);
         IngestTaskStatusStore ingestTaskStatusStore = DynamoDBIngestTaskStatusStore.from(dynamoDBClient, systemTestProperties);
 
-        WaitForIngestTasks wait = new WaitForIngestTasks(ingestTaskStatusStore);
+        WaitForIngestTasks wait = new WaitForIngestTasks(systemTestProperties, sqsClient, ingestTaskStatusStore);
         wait.pollUntilFinished();
         s3Client.shutdown();
         dynamoDBClient.shutdown();
