e925fb5b44eaa32abcb25a351e46072caf7f5545
patchwork01 <110390516+patchwork01@users.noreply.github.com>
2023-01-09 10:49:44 +0000

    Rename lambda parameter for invoke request builder in InvokeLambda

--- java/system-test/src/main/java/sleeper/systemtest/util/InvokeLambda.java
+++ java/system-test/src/main/java/sleeper/systemtest/util/InvokeLambda.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2022 Crown Copyright
+ * Copyright 2023 Crown Copyright
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -37,7 +37,7 @@ public class InvokeLambda {
         s3Client.shutdown();
 
         try (LambdaClient lambda = LambdaClient.create()) {
-            lambda.invoke(invoke -> invoke
+            lambda.invoke(builder -> builder
                     .functionName(systemTestProperties.get(lambdaFunctionProperty))
                     .payload(SdkBytes.fromUtf8String("{}")));
         }
