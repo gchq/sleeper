dc8193017c02f4df4de30aa030b9a7490197b69c
m09526 <m09526@users.noreply.github.com>
2023-01-12 12:12:26 +0000

    Revert system properties

--- scripts/build/build.sh
+++ scripts/build/build.sh
@@ -33,7 +33,7 @@ echo "Started at $(recorded_time_str "$START_BUILD_TIME")"
 
 VERSION=$(mvn -q -DforceStdout help:evaluate -Dexpression=project.version)
 SCRIPTS_DISTRIBUTION_DIR="$MAVEN_DIR/distribution/target/distribution-$VERSION-bin/scripts"
-#mvn clean install -Pquick
+mvn -q clean install -Pquick
 
 mkdir -p "$JARS_DIR"
 mkdir -p "$DOCKER_DIR"
