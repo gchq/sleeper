9ea711c55b39ced7a6a77cee4b091acec7f458d8
patchwork01 <110390516+patchwork01@users.noreply.github.com>
2023-01-12 10:14:43 +0000

    Fix Docker install in cdk-environment cloud-init.sh

--- java/cdk-environment/src/main/resources/cloud-init.sh
+++ java/cdk-environment/src/main/resources/cloud-init.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
+# Copyright 2022-2023 Crown Copyright
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 set -e # Exit on error
 set -u # Treat unset variable as error
 set -x # Trace commands
@@ -15,7 +29,7 @@ sudo apt update && sudo apt -y dist-upgrade
 sudo apt remove -y docker.io containerd runc
 sudo apt install -y ca-certificates curl gnupg lsb-release
 sudo mkdir -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor --no-tty -o /etc/apt/keyrings/docker.gpg
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
