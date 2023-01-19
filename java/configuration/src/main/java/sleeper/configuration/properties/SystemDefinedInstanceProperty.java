1e1168df3469915fc5fb3b2176843faf3370d40f
kr565370 <112558283+kr565370@users.noreply.github.com>
2023-01-19 09:47:25 +0000

    Use new implemented constants in SystemDefinedInstanceProperty

--- java/configuration/src/main/java/sleeper/configuration/properties/SystemDefinedInstanceProperty.java
+++ java/configuration/src/main/java/sleeper/configuration/properties/SystemDefinedInstanceProperty.java
@@ -15,98 +15,96 @@
  */
 package sleeper.configuration.properties;
 
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+
+import sleeper.configuration.Utils;
+
+import static sleeper.configuration.properties.SystemDefinedInstancePropertyImpl.named;
+
 /**
  * Properties set internally by sleeper and not by the user. These are set by the system itself at deployment time
  * so require no default or validation. Even if you set these in your properties file, they will be overwritten.
  */
-public enum SystemDefinedInstanceProperty implements InstanceProperty {
+@SuppressFBWarnings("IC_SUPERCLASS_USES_SUBCLASS_DURING_INITIALIZATION") // Suppress as superclass is an interface
+public interface SystemDefinedInstanceProperty extends InstanceProperty {
     // Configuration
-    CONFIG_BUCKET("sleeper.config.bucket"),
+    SystemDefinedInstanceProperty CONFIG_BUCKET = named("sleeper.config.bucket");
 
     // Table management
-    TABLE_REQUEST_FUNCTION_NAME("sleeper.table.requests.function.name"),
+    SystemDefinedInstanceProperty TABLE_REQUEST_FUNCTION_NAME = named("sleeper.table.requests.function.name");
 
     // Table metrics
-    TABLE_METRICS_RULES("sleeper.table.metrics.rulenames"),
+    SystemDefinedInstanceProperty TABLE_METRICS_RULES = named("sleeper.table.metrics.rulenames");
 
     // Query
-    QUERY_LAMBDA_ROLE("sleeper.query.role.arn"),
-    QUERY_WEBSOCKET_API_URL("sleeper.query.websocket.api.url"),
-    QUERY_QUEUE_URL("sleeper.query.queue.url"),
-    QUERY_DLQ_URL("sleeper.query.dlq.url"),
-    QUERY_RESULTS_QUEUE_URL("sleeper.query.results.queue.url"),
-    QUERY_RESULTS_BUCKET("sleeper.query.results.bucket"),
-    QUERY_TRACKER_TABLE_NAME("sleeper.query.tracker.table.name"),
+    SystemDefinedInstanceProperty QUERY_LAMBDA_ROLE = named("sleeper.query.role.arn");
+    SystemDefinedInstanceProperty QUERY_WEBSOCKET_API_URL = named("sleeper.query.websocket.api.url");
+    SystemDefinedInstanceProperty QUERY_QUEUE_URL = named("sleeper.query.queue.url");
+    SystemDefinedInstanceProperty QUERY_DLQ_URL = named("sleeper.query.dlq.url");
+    SystemDefinedInstanceProperty QUERY_RESULTS_QUEUE_URL = named("sleeper.query.results.queue.url");
+    SystemDefinedInstanceProperty QUERY_RESULTS_BUCKET = named("sleeper.query.results.bucket");
+    SystemDefinedInstanceProperty QUERY_TRACKER_TABLE_NAME = named("sleeper.query.tracker.table.name");
 
     // Compactions
-    COMPACTION_CONTAINER("sleeper.compaction.container"),
-    COMPACTION_CLUSTER("sleeper.compaction.cluster"),
-    SPLITTING_COMPACTION_CLUSTER("sleeper.compaction.splitting.cluster"),
-    COMPACTION_TASK_DEFINITION_FAMILY("sleeper.compaction.task.definition"),
-    COMPACTION_JOB_CREATION_LAMBDA_FUNCTION("sleeper.compaction.job.creation.lambda.function"),
-    COMPACTION_JOB_CREATION_CLOUDWATCH_RULE("sleeper.compaction.job.creation.rule"),
-    COMPACTION_JOB_QUEUE_URL("sleeper.compaction.job.queue.url"),
-    COMPACTION_JOB_DLQ_URL("sleeper.compaction.job.dlq.url"),
-    COMPACTION_TASK_CREATION_LAMBDA_FUNCTION("sleeper.compaction.task.creation.lambda.function"),
-    COMPACTION_TASK_CREATION_CLOUDWATCH_RULE("sleeper.compaction.task.creation.rule"),
-    SPLITTING_COMPACTION_TASK_DEFINITION_FAMILY("sleeper.compaction.splitting.task.definition"),
-    SPLITTING_COMPACTION_JOB_QUEUE_URL("sleeper.compaction.splitting.job.queue.url"),
-    SPLITTING_COMPACTION_JOB_DLQ_URL("sleeper.compaction.splitting.job.dlq.url"),
-    SPLITTING_COMPACTION_TASK_CREATION_LAMBDA_FUNCTION("sleeper.compaction.splitting.task.creation.lambda.function"),
-    SPLITTING_COMPACTION_TASK_CREATION_CLOUDWATCH_RULE("sleeper.compaction.splitting.task.creation.rule"),
+    SystemDefinedInstanceProperty COMPACTION_CONTAINER = named("sleeper.compaction.container");
+    SystemDefinedInstanceProperty COMPACTION_CLUSTER = named("sleeper.compaction.cluster");
+    SystemDefinedInstanceProperty SPLITTING_COMPACTION_CLUSTER = named("sleeper.compaction.splitting.cluster");
+    SystemDefinedInstanceProperty COMPACTION_TASK_DEFINITION_FAMILY = named("sleeper.compaction.task.definition");
+    SystemDefinedInstanceProperty COMPACTION_JOB_CREATION_LAMBDA_FUNCTION = named("sleeper.compaction.job.creation.lambda.function");
+    SystemDefinedInstanceProperty COMPACTION_JOB_CREATION_CLOUDWATCH_RULE = named("sleeper.compaction.job.creation.rule");
+    SystemDefinedInstanceProperty COMPACTION_JOB_QUEUE_URL = named("sleeper.compaction.job.queue.url");
+    SystemDefinedInstanceProperty COMPACTION_JOB_DLQ_URL = named("sleeper.compaction.job.dlq.url");
+    SystemDefinedInstanceProperty COMPACTION_TASK_CREATION_LAMBDA_FUNCTION = named("sleeper.compaction.task.creation.lambda.function");
+    SystemDefinedInstanceProperty COMPACTION_TASK_CREATION_CLOUDWATCH_RULE = named("sleeper.compaction.task.creation.rule");
+    SystemDefinedInstanceProperty SPLITTING_COMPACTION_TASK_DEFINITION_FAMILY = named("sleeper.compaction.splitting.task.definition");
+    SystemDefinedInstanceProperty SPLITTING_COMPACTION_JOB_QUEUE_URL = named("sleeper.compaction.splitting.job.queue.url");
+    SystemDefinedInstanceProperty SPLITTING_COMPACTION_JOB_DLQ_URL = named("sleeper.compaction.splitting.job.dlq.url");
+    SystemDefinedInstanceProperty SPLITTING_COMPACTION_TASK_CREATION_LAMBDA_FUNCTION = named("sleeper.compaction.splitting.task.creation.lambda.function");
+    SystemDefinedInstanceProperty SPLITTING_COMPACTION_TASK_CREATION_CLOUDWATCH_RULE = named("sleeper.compaction.splitting.task.creation.rule");
 
     // Partition splitting
-    PARTITION_SPLITTING_QUEUE_URL("sleeper.partition.splitting.queue.url"),
-    PARTITION_SPLITTING_DLQ_URL("sleeper.partition.splitting.dlq.url"),
-    PARTITION_SPLITTING_LAMBDA_FUNCTION("sleeper.partition.splitting.lambda.function"),
-    PARTITION_SPLITTING_CLOUDWATCH_RULE("sleeper.partition.splitting.rule"),
+    SystemDefinedInstanceProperty PARTITION_SPLITTING_QUEUE_URL = named("sleeper.partition.splitting.queue.url");
+    SystemDefinedInstanceProperty PARTITION_SPLITTING_DLQ_URL = named("sleeper.partition.splitting.dlq.url");
+    SystemDefinedInstanceProperty PARTITION_SPLITTING_LAMBDA_FUNCTION = named("sleeper.partition.splitting.lambda.function");
+    SystemDefinedInstanceProperty PARTITION_SPLITTING_CLOUDWATCH_RULE = named("sleeper.partition.splitting.rule");
 
     // Garbage collection
-    GARBAGE_COLLECTOR_CLOUDWATCH_RULE("sleeper.gc.rule"),
+    SystemDefinedInstanceProperty GARBAGE_COLLECTOR_CLOUDWATCH_RULE = named("sleeper.gc.rule");
 
     // Ingest
-    INGEST_LAMBDA_FUNCTION("sleeper.ingest.lambda.function"),
-    INGEST_CLOUDWATCH_RULE("sleeper.ingest.rule"),
-    INGEST_JOB_QUEUE_URL("sleeper.ingest.job.queue.url"),
-    INGEST_JOB_DLQ_URL("sleeper.ingest.job.dlq.url"),
-    INGEST_TASK_DEFINITION_FAMILY("sleeper.ingest.task.definition.family"),
-    INGEST_CLUSTER("sleeper.ingest.cluster"),
+    SystemDefinedInstanceProperty INGEST_LAMBDA_FUNCTION = named("sleeper.ingest.lambda.function");
+    SystemDefinedInstanceProperty INGEST_CLOUDWATCH_RULE = named("sleeper.ingest.rule");
+    SystemDefinedInstanceProperty INGEST_JOB_QUEUE_URL = named("sleeper.ingest.job.queue.url");
+    SystemDefinedInstanceProperty INGEST_JOB_DLQ_URL = named("sleeper.ingest.job.dlq.url");
+    SystemDefinedInstanceProperty INGEST_TASK_DEFINITION_FAMILY = named("sleeper.ingest.task.definition.family");
+    SystemDefinedInstanceProperty INGEST_CLUSTER = named("sleeper.ingest.cluster");
 
     // Bulk import
-    BULK_IMPORT_BUCKET("sleeper.bulk.import.bucket"),
+    SystemDefinedInstanceProperty BULK_IMPORT_BUCKET = named("sleeper.bulk.import.bucket");
 
     // Bulk import using EMR - these properties are used by both the persistent
     // and non-persistent EMR stacks
-    BULK_IMPORT_EMR_EC2_ROLE_NAME("sleeper.bulk.import.emr.ec2.role.name"),
-    BULK_IMPORT_EMR_CLUSTER_ROLE_NAME("sleeper.bulk.import.emr.role.name"),
-    BULK_IMPORT_EMR_SECURITY_CONF_NAME("sleeper.bulk.import.emr.security.conf.name"),
+    SystemDefinedInstanceProperty BULK_IMPORT_EMR_EC2_ROLE_NAME = named("sleeper.bulk.import.emr.ec2.role.name");
+    SystemDefinedInstanceProperty BULK_IMPORT_EMR_CLUSTER_ROLE_NAME = named("sleeper.bulk.import.emr.role.name");
+    SystemDefinedInstanceProperty BULK_IMPORT_EMR_SECURITY_CONF_NAME = named("sleeper.bulk.import.emr.security.conf.name");
 
     // Bulk import using EMR
-    BULK_IMPORT_EMR_JOB_QUEUE_URL("sleeper.bulk.import.emr.job.queue.url"),
+    SystemDefinedInstanceProperty BULK_IMPORT_EMR_JOB_QUEUE_URL = named("sleeper.bulk.import.emr.job.queue.url");
 
     // Bulk import using persistent EMR
-    BULK_IMPORT_PERSISTENT_EMR_JOB_QUEUE_URL("sleeper.bulk.import.persistent.emr.job.queue.url"),
-    BULK_IMPORT_PERSISTENT_EMR_MASTER_DNS("sleeper.bulk.import.persistent.emr.master"),
+    SystemDefinedInstanceProperty BULK_IMPORT_PERSISTENT_EMR_JOB_QUEUE_URL = named("sleeper.bulk.import.persistent.emr.job.queue.url");
+    SystemDefinedInstanceProperty BULK_IMPORT_PERSISTENT_EMR_MASTER_DNS = named("sleeper.bulk.import.persistent.emr.master");
 
     // Bulk import using EKS
-    BULK_IMPORT_EKS_JOB_QUEUE_URL("sleeper.bulk.import.eks.job.queue.url"),
-    BULK_IMPORT_EKS_STATE_MACHINE_ARN("sleeper.bulk.import.eks.statemachine.arn"),
-    BULK_IMPORT_EKS_NAMESPACE("sleeper.bulk.import.eks.k8s.namespace"),
-    BULK_IMPORT_EKS_CLUSTER_ENDPOINT("sleeper.bulk.import.eks.k8s.endpoint");
+    SystemDefinedInstanceProperty BULK_IMPORT_EKS_JOB_QUEUE_URL = named("sleeper.bulk.import.eks.job.queue.url");
+    SystemDefinedInstanceProperty BULK_IMPORT_EKS_STATE_MACHINE_ARN = named("sleeper.bulk.import.eks.statemachine.arn");
+    SystemDefinedInstanceProperty BULK_IMPORT_EKS_NAMESPACE = named("sleeper.bulk.import.eks.k8s.namespace");
+    SystemDefinedInstanceProperty BULK_IMPORT_EKS_CLUSTER_ENDPOINT = named("sleeper.bulk.import.eks.k8s.endpoint");
 
-    private final String propertyName;
+    String name();
 
-    SystemDefinedInstanceProperty(String propertyName) {
-        this.propertyName = propertyName;
-    }
-
-    @Override
-    public String getDefaultValue() {
-        return null;
-    }
-
-    @Override
-    public String getPropertyName() {
-        return propertyName;
+    static SystemDefinedInstanceProperty[] values() {
+        return Utils.getAllProperties(SystemDefinedInstanceProperty.class)
+                .toArray(new SystemDefinedInstanceProperty[0]);
     }
 }
