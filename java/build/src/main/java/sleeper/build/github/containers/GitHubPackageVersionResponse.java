e942e331e77f2f5322b43ac20a7ea25d02734e16
kr565370 <112558283+kr565370@users.noreply.github.com>
2023-01-20 14:06:46 +0000

    Read tags from metadata in GitHubPackageVersionResponse

--- java/build/src/main/java/sleeper/build/github/containers/GitHubPackageVersionResponse.java
+++ java/build/src/main/java/sleeper/build/github/containers/GitHubPackageVersionResponse.java
@@ -19,17 +19,43 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import java.util.List;
+
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class GitHubPackageVersionResponse {
 
     private final String id;
+    private final Metadata metadata;
 
     @JsonCreator(mode = JsonCreator.Mode.PROPERTIES)
-    public GitHubPackageVersionResponse(@JsonProperty("id") String id) {
+    public GitHubPackageVersionResponse(@JsonProperty("id") String id, @JsonProperty("metadata") Metadata metadata) {
         this.id = id;
+        this.metadata = metadata;
     }
 
     public String getId() {
         return id;
+    }
+
+    public List<String> getTags() {
+        return metadata.container.tags;
+    }
+
+    public static class Metadata {
+        private final ContainerMetadata container;
+
+        @JsonCreator(mode = JsonCreator.Mode.PROPERTIES)
+        public Metadata(@JsonProperty("container") ContainerMetadata container) {
+            this.container = container;
+        }
+    }
+
+    public static class ContainerMetadata {
+        private final List<String> tags;
+
+        @JsonCreator(mode = JsonCreator.Mode.PROPERTIES)
+        public ContainerMetadata(@JsonProperty("tags") List<String> tags) {
+            this.tags = tags;
+        }
     }
 }
