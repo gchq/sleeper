ca8ea4590720f89a481dbd292a826b7834ab98b7
kr565370 <112558283+kr565370@users.noreply.github.com>
2023-01-20 15:54:44 +0000

    Start work on test to combine ignoreTagsPattern and keepMostRecent

--- java/build/src/test/java/sleeper/build/github/containers/DeleteOldGHCRImagesTest.java
+++ java/build/src/test/java/sleeper/build/github/containers/DeleteOldGHCRImagesTest.java
@@ -19,6 +19,7 @@ package sleeper.build.github.containers;
 import com.github.tomakehurst.wiremock.junit5.WireMockRuntimeInfo;
 import com.github.tomakehurst.wiremock.junit5.WireMockTest;
 import com.github.tomakehurst.wiremock.matching.RequestPatternBuilder;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import java.time.Instant;
@@ -98,6 +99,24 @@ class DeleteOldGHCRImagesTest {
         // Then
         verify(1, packageVersionDeleted("test-image", 1));
         verify(0, packageVersionDeleted("test-image", 2));
+    }
+
+    @Test
+    @Disabled("TODO")
+    void shouldKeepMostRecentImageMatchingTagPattern(WireMockRuntimeInfo runtimeInfo) {
+        // Given
+        packageVersionListReturns("test-image",
+                image().id(1).updatedAt(Instant.parse("2023-01-20T15:00:12Z")).tags("test-tag-1").build(),
+                image().id(2).updatedAt(Instant.parse("2023-01-20T15:30:12Z")).tags("test-tag-2").build(),
+                image().id(3).updatedAt(Instant.parse("2023-01-20T15:00:12Z")).tags("other-tag-1").build(),
+                image().id(4).updatedAt(Instant.parse("2023-01-20T15:00:12Z")).tags("other-tag-1").build());
+        packageVersionDeleteSucceeds("test-image", 1);
+        packageVersionDeleteSucceeds("test-image", 3);
+
+        //When
+        deleteImages(runtimeInfo, builder -> builder.imageName("test-image")
+                .ignoreTagsPattern("test-tag.*")
+                .keepMostRecent(1));
     }
 
     private void deleteImages(WireMockRuntimeInfo runtimeInfo, Consumer<DeleteGHCRImages.Builder> configuration) {
