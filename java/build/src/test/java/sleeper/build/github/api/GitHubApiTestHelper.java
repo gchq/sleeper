9a3a3f8b82f2e39b14ddb3e82ec6d4eea05308d6
kr565370 <112558283+kr565370@users.noreply.github.com>
2023-01-20 09:49:46 +0000

    Start with GHCR container tests

---
+++ java/build/src/test/java/sleeper/build/github/api/GitHubApiTestHelper.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2022-2023 Crown Copyright
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package sleeper.build.github.api;
+
+import com.github.tomakehurst.wiremock.client.MappingBuilder;
+import com.github.tomakehurst.wiremock.client.ResponseDefinitionBuilder;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.equalTo;
+import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static sleeper.build.testutil.TestResources.exampleString;
+
+public class GitHubApiTestHelper {
+    private MappingBuilder gitHubRequest(MappingBuilder builder) {
+        return builder.withHeader("Accept", equalTo("application/vnd.github+json"))
+                .withHeader("Authorization", equalTo("Bearer test-bearer-token"));
+    }
+
+    private ResponseDefinitionBuilder gitHubResponse() {
+        return aResponse()
+                .withHeader("Content-Type", "application/vnd.github+json");
+    }
+
+    public MappingBuilder createStub() {
+        return gitHubRequest(get("/rate_limit"))
+                .willReturn(gitHubResponse()
+                        .withStatus(200)
+                        .withBody(exampleString("examples/github-api/rate-limit.json")));
+    }
+}
---
+++ java/build/src/test/java/sleeper/build/github/containers/DeleteOldGHCRContainersTest.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2022-2023 Crown Copyright
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package sleeper.build.github.containers;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.Instant;
+import java.util.List;
+
+public class DeleteOldGHCRContainersTest {
+    @Test
+    void shouldDeleteAContainer() {
+        // Given
+        GHCRContainer container = GHCRContainer.builder()
+                .id("test-container")
+                .created(Instant.parse("2023-01-20T09:30:00.001Z"))
+                .tags(List.of("tag1", "tag2"))
+                .build();
+        // When
+
+    }
+}
