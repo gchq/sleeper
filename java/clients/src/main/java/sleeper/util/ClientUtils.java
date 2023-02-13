df4a93d735bce978a92cf8ab5734df414f130c05
kr565370 <112558283+kr565370@users.noreply.github.com>
2023-02-13 15:05:53 +0000

    Add test for clearing directory

--- java/clients/src/main/java/sleeper/util/ClientUtils.java
+++ java/clients/src/main/java/sleeper/util/ClientUtils.java
@@ -22,11 +22,16 @@ import org.apache.commons.lang.WordUtils;
 import sleeper.configuration.properties.InstanceProperties;
 
 import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  *
@@ -105,5 +110,19 @@ public class ClientUtils {
         return Arrays.stream(str.split("\n")).
                 map(line -> "# " + WordUtils.wrap(line, 100).replace("\n", "\n# "))
                 .collect(Collectors.joining("\n"));
+    }
+
+    public static void clearDirectory(Path tempDir) {
+        try (Stream<Path> paths = Files.walk(tempDir)) {
+            paths.sorted(Comparator.reverseOrder()).forEach(path -> {
+                try {
+                    Files.delete(path);
+                } catch (IOException e) {
+                    throw new UncheckedIOException(e);
+                }
+            });
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
     }
 }
