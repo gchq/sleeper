bc9968b9e767c3c3e28a21d73339793d44965735
patchwork01 <110390516+patchwork01@users.noreply.github.com>
2023-02-14 14:34:19 +0000

    Set up class to synchronize jars bucket, initial tests

---
+++ java/clients/src/main/java/sleeper/clients/admin/deploy/SyncJars.java
@@ -0,0 +1,101 @@
+/*
+ * Copyright 2022-2023 Crown Copyright
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package sleeper.clients.admin.deploy;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.iterable.S3Objects;
+import com.amazonaws.services.s3.model.S3ObjectSummary;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import sleeper.configuration.properties.InstanceProperties;
+import sleeper.util.ClientUtils;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
+
+import static sleeper.configuration.properties.UserDefinedInstanceProperty.JARS_BUCKET;
+
+public class SyncJars {
+    private static final Logger LOGGER = LoggerFactory.getLogger(SyncJars.class);
+    private final AmazonS3 s3;
+    private final Path jarsDirectory;
+    private final InstanceProperties instanceProperties;
+
+    private SyncJars(Builder builder) {
+        s3 = builder.s3;
+        jarsDirectory = builder.jarsDirectory;
+        instanceProperties = builder.instanceProperties;
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public void sync() throws IOException {
+        LOGGER.info("Creating jars bucket");
+        String bucketName = instanceProperties.get(JARS_BUCKET);
+        s3.createBucket(bucketName);
+        List<Path> jars = ClientUtils.listJarsInDirectory(jarsDirectory);
+        LOGGER.info("Found {} jars in local directory", jars.size());
+
+        Set<Path> unuploadedJars = new LinkedHashSet<>(jars);
+        for (S3ObjectSummary object : S3Objects.inBucket(s3, bucketName)) {
+            Path path = jarsDirectory.resolve(object.getKey());
+            unuploadedJars.remove(path);
+        }
+        LOGGER.info("Found {} jars to upload", unuploadedJars.size());
+
+        unuploadedJars.stream().parallel().forEach(jar -> {
+            LOGGER.info("Uploading jar: {}", jar.getFileName());
+            s3.putObject(bucketName,
+                    "" + jar.getFileName(),
+                    jar.toFile());
+            LOGGER.info("Finished uploading jar: {}", jar.getFileName());
+        });
+    }
+
+    public static final class Builder {
+        private AmazonS3 s3;
+        private Path jarsDirectory;
+        private InstanceProperties instanceProperties;
+
+        private Builder() {
+        }
+
+        public Builder s3(AmazonS3 s3) {
+            this.s3 = s3;
+            return this;
+        }
+
+        public Builder jarsDirectory(Path jarsDirectory) {
+            this.jarsDirectory = jarsDirectory;
+            return this;
+        }
+
+        public Builder instanceProperties(InstanceProperties instanceProperties) {
+            this.instanceProperties = instanceProperties;
+            return this;
+        }
+
+        public SyncJars build() {
+            return new SyncJars(this);
+        }
+    }
+}
---
+++ java/clients/src/test/java/sleeper/clients/admin/deploy/SyncJarsIT.java
@@ -0,0 +1,122 @@
+/*
+ * Copyright 2022-2023 Crown Copyright
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package sleeper.clients.admin.deploy;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.ListObjectsV2Result;
+import com.amazonaws.services.s3.model.S3ObjectSummary;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+import org.testcontainers.containers.localstack.LocalStackContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
+
+import sleeper.configuration.properties.InstanceProperties;
+import sleeper.core.CommonTestConstants;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static sleeper.configuration.properties.InstancePropertiesTestHelper.createTestInstanceProperties;
+import static sleeper.configuration.properties.UserDefinedInstanceProperty.JARS_BUCKET;
+
+@Testcontainers
+class SyncJarsIT {
+
+    @Container
+    public static LocalStackContainer localStackContainer = new LocalStackContainer(DockerImageName.parse(CommonTestConstants.LOCALSTACK_DOCKER_IMAGE))
+            .withServices(LocalStackContainer.Service.S3);
+
+    protected final AmazonS3 s3 = AmazonS3ClientBuilder.standard()
+            .withEndpointConfiguration(localStackContainer.getEndpointConfiguration(LocalStackContainer.Service.S3))
+            .withCredentials(localStackContainer.getDefaultCredentialsProvider())
+            .build();
+
+    @TempDir
+    private Path tempDir;
+    private final String bucketName = UUID.randomUUID().toString();
+
+    @Test
+    void shouldCreateNewBucketIfNotPresent() throws IOException {
+        // When
+        syncJarsToBucket(bucketName);
+
+        // Then
+        assertThat(s3.listObjectsV2(bucketName).getObjectSummaries()).isEmpty();
+    }
+
+    @Test
+    void shouldUploadJars() throws IOException {
+        // When
+        Files.createFile(tempDir.resolve("test1.jar"));
+        Files.createFile(tempDir.resolve("test2.jar"));
+        syncJarsToBucket(bucketName);
+
+        // Then
+        assertThat(s3.listObjectsV2(bucketName).getObjectSummaries())
+                .extracting(S3ObjectSummary::getKey)
+                .containsExactly("test1.jar", "test2.jar");
+    }
+
+    @Test
+    void shouldLeaveJarsIfAlreadyPresent() throws IOException, InterruptedException {
+        // Given
+        Files.createFile(tempDir.resolve("test1.jar"));
+        Files.createFile(tempDir.resolve("test2.jar"));
+        syncJarsToBucket(bucketName);
+        ListObjectsV2Result objectsBefore = s3.listObjectsV2(bucketName);
+
+        // When
+        Thread.sleep(1000);
+        syncJarsToBucket(bucketName);
+
+        // Then
+        ListObjectsV2Result objectsAfter = s3.listObjectsV2(bucketName);
+        assertThat(objectsAfter.getObjectSummaries())
+                .extracting(S3ObjectSummary::getKey)
+                .containsExactly("test1.jar", "test2.jar");
+        assertThat(getLastModifiedTimes(objectsAfter))
+                .isEqualTo(getLastModifiedTimes(objectsBefore));
+    }
+
+    private List<Date> getLastModifiedTimes(ListObjectsV2Result result) {
+        return result.getObjectSummaries().stream()
+                .map(S3ObjectSummary::getLastModified)
+                .collect(Collectors.toList());
+    }
+
+    private void syncJarsToBucket(String bucketName) throws IOException {
+        SyncJars.builder()
+                .s3(s3).jarsDirectory(tempDir)
+                .instanceProperties(instancePropertiesWithJarsBucket(bucketName))
+                .build().sync();
+    }
+
+    private InstanceProperties instancePropertiesWithJarsBucket(String bucketName) {
+        InstanceProperties properties = createTestInstanceProperties();
+        properties.set(JARS_BUCKET, bucketName);
+        return properties;
+    }
+
+}
