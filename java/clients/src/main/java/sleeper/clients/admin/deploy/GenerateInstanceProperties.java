1f1412aa928b944e87f7d9d16135b65674aa88e4
patchwork01 <110390516+patchwork01@users.noreply.github.com>
2023-02-15 09:10:53 +0000

    Add back property for bulk import repo as there's no way to create it otherwise

--- java/clients/src/main/java/sleeper/clients/admin/deploy/GenerateInstanceProperties.java
+++ java/clients/src/main/java/sleeper/clients/admin/deploy/GenerateInstanceProperties.java
@@ -25,6 +25,7 @@ import sleeper.configuration.properties.InstanceProperties;
 import static sleeper.configuration.properties.InstanceProperties.getConfigBucketFromInstanceId;
 import static sleeper.configuration.properties.SystemDefinedInstanceProperty.CONFIG_BUCKET;
 import static sleeper.configuration.properties.UserDefinedInstanceProperty.ACCOUNT;
+import static sleeper.configuration.properties.UserDefinedInstanceProperty.BULK_IMPORT_REPO;
 import static sleeper.configuration.properties.UserDefinedInstanceProperty.ECR_COMPACTION_REPO;
 import static sleeper.configuration.properties.UserDefinedInstanceProperty.ECR_INGEST_REPO;
 import static sleeper.configuration.properties.UserDefinedInstanceProperty.ID;
@@ -63,6 +64,7 @@ public class GenerateInstanceProperties {
         instanceProperties.set(SUBNET, subnetId);
         instanceProperties.set(ECR_COMPACTION_REPO, instanceId + "/compaction-job-execution");
         instanceProperties.set(ECR_INGEST_REPO, instanceId + "/ingest");
+        instanceProperties.set(BULK_IMPORT_REPO, instanceId + "/bulk-import-runner");
         return instanceProperties;
     }
 
