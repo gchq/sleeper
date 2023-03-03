3c6fdbd2ff380fc9371db75c476f79bd3489e8ff
patchwork01 <110390516+patchwork01@users.noreply.github.com>
2023-03-03 12:04:38 +0000

    Read jar version ID from bucket

---
+++ java/cdk/src/test/java/sleeper/cdk/jars/JarsBucketIT.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2022-2023 Crown Copyright
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package sleeper.cdk.jars;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.BucketVersioningConfiguration;
+import com.amazonaws.services.s3.model.CreateBucketRequest;
+import com.amazonaws.services.s3.model.SetBucketVersioningConfigurationRequest;
+import org.junit.jupiter.api.Test;
+import org.testcontainers.containers.localstack.LocalStackContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
+
+import sleeper.core.CommonTestConstants;
+
+import java.util.UUID;
+
+import static com.amazonaws.services.s3.model.BucketVersioningConfiguration.ENABLED;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Testcontainers
+public class JarsBucketIT {
+
+    @Container
+    public static LocalStackContainer localStackContainer = new LocalStackContainer(DockerImageName.parse(CommonTestConstants.LOCALSTACK_DOCKER_IMAGE))
+            .withServices(LocalStackContainer.Service.S3);
+
+    protected final AmazonS3 s3 = AmazonS3ClientBuilder.standard()
+            .withEndpointConfiguration(localStackContainer.getEndpointConfiguration(LocalStackContainer.Service.S3))
+            .withCredentials(localStackContainer.getDefaultCredentialsProvider())
+            .build();
+
+    private final String bucketName = UUID.randomUUID().toString();
+    private final JarsBucket jarsBucket = new JarsBucket(s3, bucketName);
+
+    @Test
+    void shouldGetLatestVersionOfAJar() {
+        s3.createBucket(new CreateBucketRequest(bucketName));
+        s3.setBucketVersioningConfiguration(new SetBucketVersioningConfigurationRequest(bucketName,
+                new BucketVersioningConfiguration(ENABLED)));
+        String versionId = s3.putObject(bucketName, "test.jar", "data").getVersionId();
+
+        assertThat(jarsBucket.getLatestVersionId(Jar.fromFormat("test.jar")))
+                .isEqualTo(versionId);
+        assertThat(versionId).isNotNull();
+    }
+}
