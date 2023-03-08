01234007445119619202f44fe64708daf5973567
kr565370 <112558283+kr565370@users.noreply.github.com>
2023-03-08 21:30:30 +0000

    Change getMappedFields to return immutable list for testing

--- java/core/src/main/java/sleeper/core/schema/Schema.java
+++ java/core/src/main/java/sleeper/core/schema/Schema.java
@@ -103,7 +103,7 @@ public class Schema {
     private <T> List<T> getMappedFields(Stream<Field> fields, Function<Field, T> mapping) {
         return fields
                 .map(mapping)
-                .collect(Collectors.toList());
+                .collect(Collectors.toUnmodifiableList());
     }
 
     public List<Field> getAllFields() {
