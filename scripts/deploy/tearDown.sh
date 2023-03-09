4f8a789c9b840597e7b0d66c9bc2a8192361cc1b
patchwork01 <110390516+patchwork01@users.noreply.github.com>
2023-03-09 15:46:30 +0000

    Convert tearDown scripts to use Java

--- scripts/deploy/tearDown.sh
+++ scripts/deploy/tearDown.sh
@@ -14,44 +14,13 @@
 # limitations under the License.
 
 set -e
-# Tears down a Sleeper instance
 
-THIS_DIR=$(cd "$(dirname "$0")" && pwd)
-SCRIPTS_DIR=$(cd "$THIS_DIR" && cd .. && pwd)
-GENERATED_DIR=${SCRIPTS_DIR}/generated
-JARS_DIR=${SCRIPTS_DIR}/jars
-INSTANCE_PROPERTIES=${GENERATED_DIR}/instance.properties
-INSTANCE_ID=$(grep -F sleeper.id "${INSTANCE_PROPERTIES}" | cut -d'=' -f2)
-CONFIG_BUCKET=$(cat "${GENERATED_DIR}/configBucket.txt")
-QUERY_BUCKET=$(cat "${GENERATED_DIR}/queryResultsBucket.txt")
+if [ "$#" -gt 1 ]; then
+  echo "Usage: $0 <optional-instance-id>"
+  exit 1
+fi
 
-echo "--------------------------------------------------------"
-echo "Tear Down"
-echo "--------------------------------------------------------"
+SCRIPTS_DIR=$(cd "$(dirname "$0")" && cd .. && pwd)
+VERSION=$(cat "${SCRIPTS_DIR}/templates/version.txt")
 
-echo "THIS_DIR: ${THIS_DIR}"
-echo "SCRIPTS_DIR: ${SCRIPTS_DIR}"
-echo "GENERATED_DIR: ${GENERATED_DIR}"
-echo "INSTANCE_PROPERTIES: ${INSTANCE_PROPERTIES}"
-echo "INSTANCE_ID: ${INSTANCE_ID}"
-echo "CONFIG_BUCKET: ${CONFIG_BUCKET}"
-echo "QUERY_BUCKET: ${QUERY_BUCKET}"
-
-# Download latest instance configuration (don't fail script if buckets don't exist)
-"${SCRIPTS_DIR}/utility/downloadConfig.sh" "${INSTANCE_ID}" || true
-
-VERSION=$(grep sleeper.version "${INSTANCE_PROPERTIES}" | cut -d'=' -f2)
-
-java -cp "${JARS_DIR}/clients-${VERSION}-utility.jar" sleeper.status.update.CleanUpBeforeDestroy "$GENERATED_DIR"
-
-echo "Running cdk destroy to remove the system"
-cdk -a "java -cp ${JARS_DIR}/cdk-${VERSION}.jar sleeper.cdk.SleeperCdkApp" \
-destroy -c propertiesfile="${INSTANCE_PROPERTIES}" -c validate=false "*"
-
-echo "Removing the Jars bucket and docker containers"
-"${THIS_DIR}/removeUploads.sh" "${INSTANCE_PROPERTIES}"
-
-echo "Removing generated files"
-rm -r "${GENERATED_DIR:?}"/*
-
-echo "Successfully torn down"
+java -cp "${JARS_DIR}/clients-${VERSION}-utility.jar" sleeper.clients.teardown.TearDownInstance "${SCRIPTS_DIR}" "$@"
