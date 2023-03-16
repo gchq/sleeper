9514e0c0abfe034c37568851ff081b89d5d8240f
kr565370 <112558283+kr565370@users.noreply.github.com>
2023-03-16 11:54:31 +0000

    Revert extracting getInputStringOrChooseExit

--- java/clients/src/main/java/sleeper/clients/admin/TableSelectHelper.java
+++ java/clients/src/main/java/sleeper/clients/admin/TableSelectHelper.java
@@ -16,22 +16,35 @@
 
 package sleeper.clients.admin;
 
-import sleeper.console.ConsoleHelper;
 import sleeper.console.ConsoleInput;
 import sleeper.console.ConsoleOutput;
 import sleeper.console.UserExitedException;
+import sleeper.console.menu.ChooseOne;
 import sleeper.console.menu.Chosen;
 import sleeper.console.menu.ConsoleChoice;
 
+import static sleeper.clients.admin.AdminCommonPrompts.RETURN_TO_MAIN_MENU;
+
 class TableSelectHelper {
-    private final ConsoleHelper consoleHelper;
+    private final ConsoleOutput out;
+    private final ChooseOne chooseOne;
 
     TableSelectHelper(ConsoleOutput out, ConsoleInput in) {
-        this.consoleHelper = new ConsoleHelper(out, in);
+        this.out = out;
+        this.chooseOne = new ChooseOne(out, in);
     }
 
     public Chosen<ConsoleChoice> chooseTable() throws UserExitedException {
-        return consoleHelper.getInputStringOrChooseExit("Which TABLE do you want to select?\n",
-                "Please enter the TABLE NAME now or use the following options:");
+        return chooseTable("")
+                .chooseUntilSomethingEntered(() ->
+                        chooseTable("\nYou did not enter anything please try again\n"));
+    }
+
+    private Chosen<ConsoleChoice> chooseTable(String message) {
+        out.clearScreen(message);
+        out.println("Which TABLE do you want to select?\n");
+        return chooseOne.chooseWithMessageFrom(
+                "Please enter the TABLE NAME now or use the following options:",
+                RETURN_TO_MAIN_MENU);
     }
 }
--- java/clients/src/main/java/sleeper/console/ConsoleHelper.java
+++ java/clients/src/main/java/sleeper/console/ConsoleHelper.java
@@ -17,33 +17,13 @@
 package sleeper.console;
 
 import sleeper.console.menu.ChooseOne;
-import sleeper.console.menu.Chosen;
-import sleeper.console.menu.ConsoleChoice;
 import sleeper.console.menu.MenuOption;
 
-import static sleeper.clients.admin.AdminCommonPrompts.RETURN_TO_MAIN_MENU;
-
 public class ConsoleHelper {
-    private final ConsoleOutput out;
     private final ChooseOne chooseOne;
 
     public ConsoleHelper(ConsoleOutput out, ConsoleInput in) {
-        this.out = out;
         this.chooseOne = new ChooseOne(out, in);
-    }
-
-    public Chosen<ConsoleChoice> getInputStringOrChooseExit(String title, String chooseMessage) throws UserExitedException {
-        return chooseWithTitleAndMessage("\n" + title, chooseMessage)
-                .chooseUntilSomethingEntered(() ->
-                        chooseWithTitleAndMessage("\nYou did not enter anything please try again\n" + title,
-                                chooseMessage));
-    }
-
-    private Chosen<ConsoleChoice> chooseWithTitleAndMessage(String title, String chooseMessage) {
-        out.clearScreen(title);
-        return chooseOne.chooseWithMessageFrom(
-                chooseMessage,
-                RETURN_TO_MAIN_MENU);
     }
 
     public MenuOption chooseOptionUntilValid(String message, MenuOption... options) {
