396a00209b44dddd679c25d33e74f120fbbeb8b7
patchwork01 <110390516+patchwork01@users.noreply.github.com>
2023-03-17 14:52:58 +0000

    Generify InstanceConfigurationScreen

--- java/clients/src/main/java/sleeper/clients/admin/InstanceConfigurationScreen.java
+++ java/clients/src/main/java/sleeper/clients/admin/InstanceConfigurationScreen.java
@@ -17,6 +17,7 @@
 package sleeper.clients.admin;
 
 import sleeper.configuration.properties.InstanceProperties;
+import sleeper.configuration.properties.SleeperProperties;
 import sleeper.configuration.properties.SleeperProperty;
 import sleeper.console.ConsoleInput;
 import sleeper.console.ConsoleOutput;
@@ -43,55 +44,89 @@ public class InstanceConfigurationScreen {
     }
 
     public void viewAndEditProperties(String instanceId) throws InterruptedException {
-        viewAndEditProperties(store.loadInstanceProperties(instanceId), PropertiesDiff.noChanges());
+        withInstanceProperties(store.loadInstanceProperties(instanceId))
+                .viewAndEditProperties();
     }
 
-    private void viewAndEditProperties(InstanceProperties properties, PropertiesDiff changesSoFar) throws InterruptedException {
-        UpdatePropertiesRequest<InstanceProperties> request = openFile(properties);
-        PropertiesDiff changes = changesSoFar.andThen(request.getDiff());
-        if (changes.isChanged()) {
-            Set<SleeperProperty> invalidProperties = request.getInvalidProperties();
-            changes.print(out, properties.getPropertiesIndex(), invalidProperties);
+    private WithProperties<InstanceProperties> withInstanceProperties(InstanceProperties properties) {
+        return new WithProperties<>(properties, editor::openPropertiesFile, store::saveInstanceProperties);
+    }
 
-            chooseFromOptions(request.getUpdatedProperties(), changes, invalidProperties.isEmpty());
+    private interface OpenFile<T extends SleeperProperties<?>> {
+        UpdatePropertiesRequest<T> openFile(T properties) throws IOException, InterruptedException;
+    }
+
+    private interface SaveChanges<T extends SleeperProperties<?>> {
+        void saveChanges(T properties, PropertiesDiff diff);
+    }
+
+    private class WithProperties<T extends SleeperProperties<?>> {
+
+        private final T properties;
+        private final OpenFile<T> editor;
+        private final SaveChanges<T> store;
+
+        WithProperties(T properties, OpenFile<T> editor, SaveChanges<T> store) {
+            this.properties = properties;
+            this.editor = editor;
+            this.store = store;
         }
-    }
 
-    private void chooseFromOptions(
-            InstanceProperties updatedProperties, PropertiesDiff changes, boolean valid) throws InterruptedException {
-        MenuOption saveChanges = new MenuOption("Save changes", () -> {
-            try {
-                store.saveInstanceProperties(updatedProperties, changes);
-                out.println("\n\n----------------------------------");
-                out.println("Saved successfully, hit enter to return to main screen");
-                in.waitForLine();
-            } catch (AdminConfigStore.CouldNotSaveInstanceProperties e) {
-                out.println("\n\n----------------------------------\n");
-                out.println("Failed saving properties with the following messages:");
-                out.println(e.getMessage());
-                out.println(e.getCause().getMessage());
-                out.println();
-                chooseFromOptions(updatedProperties, changes, valid);
+        WithProperties<T> withProperties(T properties) {
+            return new WithProperties<>(properties, editor, store);
+        }
+
+        void viewAndEditProperties() throws InterruptedException {
+            viewAndEditProperties(PropertiesDiff.noChanges());
+        }
+
+        void viewAndEditProperties(PropertiesDiff changesSoFar) throws InterruptedException {
+            UpdatePropertiesRequest<T> request = openPropertiesFile();
+            PropertiesDiff changes = changesSoFar.andThen(request.getDiff());
+            if (changes.isChanged()) {
+                Set<SleeperProperty> invalidProperties = request.getInvalidProperties();
+                changes.print(out, properties.getPropertiesIndex(), invalidProperties);
+
+                chooseFromOptions(request.getUpdatedProperties(), changes, invalidProperties.isEmpty());
             }
-        });
-        MenuOption returnToEditor = new MenuOption("Return to editor", () ->
-                viewAndEditProperties(updatedProperties, changes));
-        MenuOption discardChanges = new MenuOption("Discard changes and return to main menu", () -> {
-        });
-        if (valid) {
-            chooseOne.chooseFrom(saveChanges, returnToEditor, discardChanges)
-                    .getChoice().orElse(returnToEditor).run();
-        } else {
-            chooseOne.chooseFrom(returnToEditor, discardChanges)
-                    .getChoice().orElse(returnToEditor).run();
         }
-    }
 
-    private UpdatePropertiesRequest<InstanceProperties> openFile(InstanceProperties properties) throws InterruptedException {
-        try {
-            return editor.openPropertiesFile(properties);
-        } catch (IOException e1) {
-            throw new UncheckedIOException(e1);
+        void chooseFromOptions(
+                T updatedProperties, PropertiesDiff changes, boolean valid) throws InterruptedException {
+            MenuOption saveChanges = new MenuOption("Save changes", () -> {
+                try {
+                    store.saveChanges(updatedProperties, changes);
+                    out.println("\n\n----------------------------------");
+                    out.println("Saved successfully, hit enter to return to main screen");
+                    in.waitForLine();
+                } catch (AdminConfigStore.CouldNotSaveInstanceProperties e) {
+                    out.println("\n\n----------------------------------\n");
+                    out.println("Failed saving properties with the following messages:");
+                    out.println(e.getMessage());
+                    out.println(e.getCause().getMessage());
+                    out.println();
+                    chooseFromOptions(updatedProperties, changes, valid);
+                }
+            });
+            MenuOption returnToEditor = new MenuOption("Return to editor", () ->
+                    withProperties(updatedProperties).viewAndEditProperties(changes));
+            MenuOption discardChanges = new MenuOption("Discard changes and return to main menu", () -> {
+            });
+            if (valid) {
+                chooseOne.chooseFrom(saveChanges, returnToEditor, discardChanges)
+                        .getChoice().orElse(returnToEditor).run();
+            } else {
+                chooseOne.chooseFrom(returnToEditor, discardChanges)
+                        .getChoice().orElse(returnToEditor).run();
+            }
+        }
+
+        UpdatePropertiesRequest<T> openPropertiesFile() throws InterruptedException {
+            try {
+                return editor.openFile(properties);
+            } catch (IOException e) {
+                throw new UncheckedIOException(e);
+            }
         }
     }
 }
