eb8a7529fe6a424c59551c0beb3ae800978983ee
kr565370 <112558283+kr565370@users.noreply.github.com>
2023-04-03 14:28:28 +0100

    Add toIngestJob() to BulkImportJob

--- java/bulk-import/bulk-import-common/src/main/java/sleeper/bulkimport/job/BulkImportJob.java
+++ java/bulk-import/bulk-import-common/src/main/java/sleeper/bulkimport/job/BulkImportJob.java
@@ -18,15 +18,21 @@ package sleeper.bulkimport.job;
 import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.apache.commons.lang3.builder.HashCodeBuilder;
 import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import sleeper.ingest.job.IngestJob;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 
 /**
  * POJO containing information needed to run a bulk import job.
  */
 public class BulkImportJob {
+    private static final Logger LOGGER = LoggerFactory.getLogger(BulkImportJob.class);
     private String className;
     private List<String> files;
     private String id;
@@ -35,7 +41,12 @@ public class BulkImportJob {
     private Map<String, String> platformSpec;
 
     private BulkImportJob(Builder builder) {
-        this.id = builder.id;
+        if (builder.id == null || builder.id.isEmpty()) {
+            id = UUID.randomUUID().toString();
+            LOGGER.info("Null or empty id provided. Generated new id: {}", id);
+        } else {
+            id = builder.id;
+        }
         this.className = builder.className;
         this.files = builder.files;
         this.sparkConf = builder.sparkConf;
@@ -89,6 +100,10 @@ public class BulkImportJob {
 
     public void setPlatformSpec(Map<String, String> platformSpec) {
         this.platformSpec = platformSpec;
+    }
+
+    public IngestJob toIngestJob() {
+        return IngestJob.builder().files(files).id(id).tableName(tableName).build();
     }
 
     @Override
