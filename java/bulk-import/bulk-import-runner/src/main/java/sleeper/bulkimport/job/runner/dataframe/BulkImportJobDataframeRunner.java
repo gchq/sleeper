1019bb168904aec0510aa7a799b98255f6a96cbd
kr565370 <112558283+kr565370@users.noreply.github.com>
2023-04-05 12:23:21 +0100

    Extract BulkImportDataframePartitioner

--- java/bulk-import/bulk-import-runner/src/main/java/sleeper/bulkimport/job/runner/dataframe/BulkImportJobDataframeRunner.java
+++ java/bulk-import/bulk-import-runner/src/main/java/sleeper/bulkimport/job/runner/dataframe/BulkImportJobDataframeRunner.java
@@ -15,31 +15,18 @@
  */
 package sleeper.bulkimport.job.runner.dataframe;
 
-import com.google.common.collect.Lists;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.spark.broadcast.Broadcast;
-import org.apache.spark.sql.Column;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
-import org.apache.spark.sql.catalyst.encoders.RowEncoder;
-import org.apache.spark.sql.types.DataTypes;
-import org.apache.spark.sql.types.StructField;
-import org.apache.spark.sql.types.StructType;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import sleeper.bulkimport.job.BulkImportJob;
 import sleeper.bulkimport.job.runner.BulkImportJobRunner;
-import sleeper.bulkimport.job.runner.StructTypeFactory;
 import sleeper.configuration.properties.table.TableProperties;
 import sleeper.core.partition.Partition;
-import sleeper.core.schema.Schema;
-import sleeper.core.schema.SchemaSerDe;
 
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 /**
  * The {@link BulkImportJobDataframeRunner} is a {@link BulkImportJobRunner} which
@@ -47,7 +34,6 @@ import java.util.stream.Collectors;
  * Sleeoer partition.
  */
 public class BulkImportJobDataframeRunner extends BulkImportJobRunner {
-    private static final Logger LOGGER = LoggerFactory.getLogger(BulkImportJobDataframeRunner.class);
 
     @Override
     public Dataset<Row> createFileInfos(
@@ -55,39 +41,12 @@ public class BulkImportJobDataframeRunner extends BulkImportJobRunner {
             BulkImportJob job,
             TableProperties tableProperties,
             Broadcast<List<Partition>> broadcastedPartitions, Configuration conf) throws IOException {
-        LOGGER.info("Running bulk import job with id {}", job.getId());
-
-        Schema schema = tableProperties.getSchema();
-        String schemaAsString = new SchemaSerDe().toJson(schema);
-        StructType convertedSchema = new StructTypeFactory().getStructType(schema);
-        StructType schemaWithPartitionField = createEnhancedSchema(convertedSchema);
-
-        int numLeafPartitions = (int) broadcastedPartitions.value().stream().filter(Partition::isLeafPartition).count();
-        LOGGER.info("There are {} leaf partitions", numLeafPartitions);
-
-        Dataset<Row> dataWithPartition = rows.mapPartitions(new AddPartitionFunction(schemaAsString, broadcastedPartitions), RowEncoder.apply(schemaWithPartitionField));
-
-        Column[] sortColumns = Lists.newArrayList(
-                        Lists.newArrayList(PARTITION_FIELD_NAME), schema.getRowKeyFieldNames(), schema.getSortKeyFieldNames())
-                .stream()
-                .flatMap(list -> ((List<String>) list).stream())
-                .map(Column::new)
-                .collect(Collectors.toList())
-                .toArray(new Column[0]);
-        LOGGER.info("Sorting by columns {}", String.join(",", Arrays.stream(sortColumns).map(c -> c.toString()).collect(Collectors.toList())));
-
-        Dataset<Row> sortedRows = dataWithPartition.sort(sortColumns);
-
-        return sortedRows.mapPartitions(new WriteParquetFiles(getInstanceProperties().saveAsString(), tableProperties.saveAsString(), conf), RowEncoder.apply(createFileInfoSchema()));
+        BulkImportDataframePartitioner partitioner = new BulkImportDataframePartitioner();
+        return partitioner.createFileInfos(rows, job, getInstanceProperties(), tableProperties,
+                broadcastedPartitions, conf);
     }
 
     public static void main(String[] args) throws Exception {
         BulkImportJobRunner.start(args, new BulkImportJobDataframeRunner());
-    }
-
-    private StructType createEnhancedSchema(StructType convertedSchema) {
-        StructType structTypeWithPartition = new StructType(convertedSchema.fields());
-        return structTypeWithPartition
-                .add(new StructField(PARTITION_FIELD_NAME, DataTypes.StringType, false, null));
     }
 }
