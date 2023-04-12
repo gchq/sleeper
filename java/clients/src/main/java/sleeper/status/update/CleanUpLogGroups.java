ad9bc0d8fb80c74014b315afd87f3a6f4657ab87
patchwork01 <110390516+patchwork01@users.noreply.github.com>
2023-04-12 14:06:51 +0100

    Log deletion progress and at end

--- java/clients/src/main/java/sleeper/status/update/CleanUpLogGroups.java
+++ java/clients/src/main/java/sleeper/status/update/CleanUpLogGroups.java
@@ -28,7 +28,6 @@ import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import static sleeper.core.util.RateLimitUtils.sleepForSustainedRatePerSecond;
 
@@ -84,16 +83,17 @@ public class CleanUpLogGroups {
         LOGGER.info("Finished deleting {} empty log groups not in stacks", numToDelete);
     }
 
-    public static void run(CloudWatchLogsClient logs, CloudFormationClient cloudFormation, Instant queryTime, Runnable sleepForRateLimit) {
-        planDeletions(logs, cloudFormation, queryTime).delete(logs, sleepForRateLimit);
+    public static void run(CloudWatchLogsClient logsClient, CloudFormationClient cloudFormation, Instant queryTime, Runnable sleepForRateLimit) {
+        planDeletions(logsClient, cloudFormation, queryTime)
+                .delete(logsClient, sleepForRateLimit);
     }
 
     private static DeletionPlan planDeletions(
-            CloudWatchLogsClient logs, CloudFormationClient cloudFormation, Instant queryTime) {
+            CloudWatchLogsClient logsClient, CloudFormationClient cloudFormation, Instant queryTime) {
         Instant maxCreationTime = queryTime.minus(Duration.ofDays(30));
         Stacks stacks = new Stacks(cloudFormation);
         DeletionPlan plan = new DeletionPlan(stacks, maxCreationTime);
-        logs.describeLogGroupsPaginator().logGroups().forEach(plan::add);
+        logsClient.describeLogGroupsPaginator().logGroups().forEach(plan::add);
         return plan;
     }
 
@@ -141,9 +141,10 @@ public class CleanUpLogGroups {
     }
 
     public static class DeletionPlan {
-        private final List<String> empty = new ArrayList<>();
-        private final List<String> oldest = new ArrayList<>();
-        private final List<String> toKeep = new ArrayList<>();
+        private final List<String> delete = new ArrayList<>();
+        private int numEmpty;
+        private int numOld;
+        private int numToKeep;
         private final Stacks stacks;
         private final Instant maxCreationTime;
 
@@ -153,37 +154,55 @@ public class CleanUpLogGroups {
         }
 
         public void add(LogGroup logGroup) {
-            if (stacks.anyIn(logGroup.logGroupName())) {
-                toKeep.add(logGroup.logGroupName());
-                return;
-            }
-            if (logGroup.storedBytes() == 0) {
-                empty.add(logGroup.logGroupName());
-            } else if (isOld(logGroup, maxCreationTime)) {
-                oldest.add(logGroup.logGroupName());
+            if (isDelete(logGroup)) {
+                delete.add(logGroup.logGroupName());
             } else {
-                toKeep.add(logGroup.logGroupName());
+                numToKeep++;
             }
+        }
+
+        private boolean isDelete(LogGroup logGroup) {
+            if (stacks.anyIn(logGroup.logGroupName())) {
+                return false;
+            } else if (logGroup.storedBytes() == 0) {
+                numEmpty++;
+                return true;
+            } else if (isOld(logGroup)) {
+                numOld++;
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        private boolean isOld(LogGroup logGroup) {
+            return logGroup.retentionInDays() == null &&
+                    Instant.ofEpochMilli(logGroup.creationTime()).isBefore(maxCreationTime);
         }
 
         public void delete(CloudWatchLogsClient logsClient, Runnable sleepForRateLimit) {
             logTotals();
-            Stream.concat(empty.stream(), oldest.stream()).forEach(name -> {
+
+            int numToDelete = delete.size();
+            for (int i = 0; i < numToDelete; i++) {
+                if (i % 50 == 0) {
+                    LOGGER.info("Deleting, done {} of {}", i, numToDelete);
+                }
+                String logGroupName = delete.get(i);
+
                 sleepForRateLimit.run();
-                logsClient.deleteLogGroup(builder -> builder.logGroupName(name));
-            });
+                logsClient.deleteLogGroup(builder -> builder.logGroupName(logGroupName));
+            }
+
+            LOGGER.info("Finished deleting {} empty/old log groups not in stacks", numToDelete);
         }
 
         private void logTotals() {
-            LOGGER.info("Groups to delete: {}", empty.size() + oldest.size());
-            LOGGER.info("Empty groups to delete: {}", empty.size());
-            LOGGER.info("Old groups to delete: {}", oldest.size());
-            LOGGER.info("Groups to keep: {}", toKeep.size());
-        }
-
-        private static boolean isOld(LogGroup logGroup, Instant maxCreationTime) {
-            return logGroup.retentionInDays() == null &&
-                    Instant.ofEpochMilli(logGroup.creationTime()).isBefore(maxCreationTime);
+            LOGGER.info("Keeping all groups containing stack names: {}", stacks.stackNames);
+            LOGGER.info("Groups to delete: {}", delete.size());
+            LOGGER.info("Empty groups to delete: {}", numEmpty);
+            LOGGER.info("Old groups to delete: {}", numOld);
+            LOGGER.info("Groups to keep: {}", numToKeep);
         }
     }
 }
