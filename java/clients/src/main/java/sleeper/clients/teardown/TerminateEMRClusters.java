9548ab9b83efb6229497002d5fd924b094005c6b
kr565370 <112558283+kr565370@users.noreply.github.com>
2023-04-17 10:01:11 +0100

    Store end index in variable

--- java/clients/src/main/java/sleeper/clients/teardown/TerminateEMRClusters.java
+++ java/clients/src/main/java/sleeper/clients/teardown/TerminateEMRClusters.java
@@ -76,9 +76,10 @@ public class TerminateEMRClusters {
         // Can only terminate 10 clusters at a time
         // See https://docs.aws.amazon.com/emr/latest/APIReference/API_TerminateJobFlows.html
         for (int i = 0; i < clusters.size(); i += 10) {
-            List<String> clusterBatch = clusters.subList(i, min(i + 10, clusters.size()));
+            int endIndex = min(i + 10, clusters.size());
+            List<String> clusterBatch = clusters.subList(i, endIndex);
             emrClient.terminateJobFlows(new TerminateJobFlowsRequest().withJobFlowIds(clusterBatch));
-            LOGGER.info("Terminated {} clusters out of {}", min(i + 10, clusters.size()), clusters.size());
+            LOGGER.info("Terminated {} clusters out of {}", endIndex, clusters.size());
         }
     }
 
