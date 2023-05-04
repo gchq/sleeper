6f5bb464a6098a2daf64018cd39306f7fc23706d
patchwork01 <110390516+patchwork01@users.noreply.github.com>
2023-05-04 09:28:19 +0100

    Set up RecordNightlyTestOutputIT

---
+++ java/system-test/src/test/java/sleeper/systemtest/output/RecordNightlyTestOutputIT.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2022-2023 Crown Copyright
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package sleeper.systemtest.output;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+import org.testcontainers.containers.localstack.LocalStackContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
+
+import sleeper.core.CommonTestConstants;
+
+import java.nio.file.Path;
+
+@Testcontainers
+class RecordNightlyTestOutputIT {
+    @Container
+    public static LocalStackContainer localStackContainer = new LocalStackContainer(DockerImageName.parse(CommonTestConstants.LOCALSTACK_DOCKER_IMAGE))
+            .withServices(LocalStackContainer.Service.S3);
+
+    private final AmazonS3 s3Client = createS3Client();
+    @TempDir
+    private Path tempDir;
+
+    private AmazonS3 createS3Client() {
+        return AmazonS3ClientBuilder.standard()
+                .withEndpointConfiguration(localStackContainer.getEndpointConfiguration(LocalStackContainer.Service.S3))
+                .withCredentials(localStackContainer.getDefaultCredentialsProvider())
+                .build();
+    }
+
+    @Test
+    void shouldUploadLogFile() {
+
+    }
+}
