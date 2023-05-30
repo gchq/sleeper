848fce5341d3abc0c1ed33d963caff132c6d8f9c
patchwork01 <110390516+patchwork01@users.noreply.github.com>
2023-05-30 15:30:55 +0100

    Set template value for bulk import EMR keypair

--- java/configuration/src/main/java/sleeper/configuration/properties/format/GeneratePropertiesTemplates.java
+++ java/configuration/src/main/java/sleeper/configuration/properties/format/GeneratePropertiesTemplates.java
@@ -27,8 +27,8 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Collections;
 
-import static sleeper.configuration.properties.SystemDefinedInstanceProperty.CONFIG_BUCKET;
 import static sleeper.configuration.properties.UserDefinedInstanceProperty.ACCOUNT;
+import static sleeper.configuration.properties.UserDefinedInstanceProperty.BULK_IMPORT_EMR_EC2_KEYPAIR_NAME;
 import static sleeper.configuration.properties.UserDefinedInstanceProperty.ECR_INGEST_REPO;
 import static sleeper.configuration.properties.UserDefinedInstanceProperty.ID;
 import static sleeper.configuration.properties.UserDefinedInstanceProperty.JARS_BUCKET;
@@ -47,14 +47,19 @@ public class GeneratePropertiesTemplates {
 
     public static void fromRepositoryPath(Path tempDir) throws IOException {
         InstanceProperties properties = new InstanceProperties();
+
+        // Mandatory properties
         properties.set(ID, "full-example");
         properties.set(JARS_BUCKET, "the name of the bucket containing your jars, e.g. sleeper-<insert-unique-name-here>-jars");
         properties.set(ACCOUNT, "1234567890");
         properties.set(REGION, "eu-west-2");
         properties.set(VPC_ID, "1234567890");
         properties.set(SUBNET, "subnet-abcdefgh");
-        properties.set(CONFIG_BUCKET, "test-bucket");
+
+        // Non-mandatory properties
         properties.set(ECR_INGEST_REPO, "<insert-unique-sleeper-id>/ingest");
+        properties.set(BULK_IMPORT_EMR_EC2_KEYPAIR_NAME, "my-key");
+
         Files.createDirectories(tempDir.resolve("example/full"));
         try (BufferedWriter writer = Files.newBufferedWriter(
                 tempDir.resolve("example/full/instance.properties"))) {
