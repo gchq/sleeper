babb3ceed1c54ac27dc380cc0fbeb5033fb4d090
kr565370 <112558283+kr565370@users.noreply.github.com>
2023-06-09 14:52:40 +0100

    Bump version to 0.18.0-SNAPSHOT

--- python/setup.py
+++ python/setup.py
@@ -15,7 +15,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='sleeper',
-    version='0.17.0',
+    version='0.18.0-SNAPSHOT',
     description='Python client for Sleeper',
     packages=['sleeper', 'pq'],
     install_requires=[
