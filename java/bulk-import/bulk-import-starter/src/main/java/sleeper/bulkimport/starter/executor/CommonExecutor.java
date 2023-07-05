524c536b955185649726028278458d2b785c9902
ab295382 <134393532+ab295382@users.noreply.github.com>
2023-07-05 11:08:42 +0000

    Refactor for CommonExecutor

---
+++ java/bulk-import/bulk-import-starter/src/main/java/sleeper/bulkimport/starter/executor/CommonExecutor.java
@@ -0,0 +1,184 @@
+/*
+ * Copyright 2022-2023 Crown Copyright
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package sleeper.bulkimport.starter.executor;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.google.common.collect.Lists;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import sleeper.bulkimport.job.BulkImportJob;
+import sleeper.bulkimport.job.BulkImportJobSerDe;
+import sleeper.configuration.properties.InstanceProperties;
+import sleeper.configuration.properties.table.TablePropertiesProvider;
+import sleeper.ingest.job.status.IngestJobStatusStore;
+import sleeper.statestore.StateStoreProvider;
+
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
+import java.util.regex.Pattern;
+
+import static sleeper.bulkimport.CheckLeafPartitionCount.hasMinimumPartitions;
+import static sleeper.configuration.properties.SystemDefinedInstanceProperty.BULK_IMPORT_BUCKET;
+import static sleeper.configuration.properties.SystemDefinedInstanceProperty.CONFIG_BUCKET;
+import static sleeper.configuration.properties.UserDefinedInstanceProperty.BULK_IMPORT_CLASS_NAME;
+import static sleeper.ingest.job.status.IngestJobValidatedEvent.ingestJobAccepted;
+import static sleeper.ingest.job.status.IngestJobValidatedEvent.ingestJobRejected;
+
+public abstract class CommonExecutor {
+    private static final Logger LOGGER = LoggerFactory.getLogger(CommonExecutor.class);
+    private static final Predicate<String> LOWER_ALPHANUMERICS_AND_DASHES = Pattern.compile("^[a-z0-9-]+$").asPredicate();
+
+    protected final InstanceProperties instanceProperties;
+    protected final TablePropertiesProvider tablePropertiesProvider;
+    protected final StateStoreProvider stateStoreProvider;
+    protected final IngestJobStatusStore ingestJobStatusStore;
+    protected final AmazonS3 s3Client;
+    protected final PlatformExecutor platformExecutor;
+    protected final Supplier<Instant> validationTimeSupplier;
+
+
+    public CommonExecutor(InstanceProperties instanceProperties, TablePropertiesProvider tablePropertiesProvider,
+                    StateStoreProvider stateStoreProvider, IngestJobStatusStore ingestJobStatusStore, AmazonS3 s3Client,
+                    PlatformExecutor platformExecutor, Supplier<Instant> validationTimeSupplier) {
+        this.instanceProperties = instanceProperties;
+        this.tablePropertiesProvider = tablePropertiesProvider;
+        this.stateStoreProvider = stateStoreProvider;
+        this.ingestJobStatusStore = ingestJobStatusStore;
+        this.s3Client = s3Client;
+        this.platformExecutor = platformExecutor;
+        this.validationTimeSupplier = validationTimeSupplier;
+    }
+
+    public void runJob(BulkImportJob bulkImportJob) {
+        runJob(bulkImportJob, UUID.randomUUID().toString());
+    }
+
+    public void runJob(BulkImportJob bulkImportJob, String jobRunId) {
+        if (null == bulkImportJob) {
+            LOGGER.warn("Received null job, exiting early.");
+            return;
+        }
+        LOGGER.info("Validating job: {}", bulkImportJob);
+        if (!validateJob(bulkImportJob)) {
+            return;
+        }
+        ingestJobStatusStore.jobValidated(ingestJobAccepted(
+                bulkImportJob.toIngestJob(), validationTimeSupplier.get())
+                .jobRunId(jobRunId).build());
+        LOGGER.info("Writing job with id {} to JSON file", bulkImportJob.getId());
+        writeJobToJSONFile(bulkImportJob);
+        LOGGER.info("Submitting job with id {}", bulkImportJob.getId());
+        platformExecutor.runJobOnPlatform(bulkImportJob, jobRunId);
+        LOGGER.info("Successfully submitted job");
+    }
+
+
+    protected List<String> constructArgs(BulkImportJob bulkImportJob, String jobRunId, String taskId) {
+        Map<String, String> userConfig = bulkImportJob.getSparkConf();
+        LOGGER.info("Using Spark config {}", userConfig);
+
+        String className = bulkImportJob.getClassName() != null ? bulkImportJob.getClassName() : instanceProperties.get(BULK_IMPORT_CLASS_NAME);
+
+        List<String> args = Lists.newArrayList("spark-submit", "--deploy-mode", "cluster", "--class", className);
+
+        if (null != userConfig) {
+            for (Map.Entry<String, String> configurationItem : userConfig.entrySet()) {
+                args.add("--conf");
+                args.add(configurationItem.getKey() + "=" + configurationItem.getValue());
+            }
+        }
+
+        args.add(platformExecutor.getJarLocation());
+        args.add(instanceProperties.get(CONFIG_BUCKET));
+        args.add(bulkImportJob.getId());
+        args.add(taskId);
+        args.add(jobRunId);
+        return args;
+    }
+
+    private boolean validateJob(BulkImportJob bulkImportJob) {
+        List<String> failedChecks = new ArrayList<>();
+        String id = bulkImportJob.getId();
+        if (null == id) {
+            String newId = UUID.randomUUID().toString();
+            LOGGER.info("Bulk import job has null id, setting id to {}", newId);
+            bulkImportJob.setId(newId);
+        } else {
+            if (!LOWER_ALPHANUMERICS_AND_DASHES.test(id)) {
+                failedChecks.add("Job Ids must only contain lowercase alphanumerics and dashes.");
+            }
+            if (id.length() > 63) {
+                failedChecks.add("Job IDs are only allowed to be up to 63 characters long.");
+            }
+        }
+
+        if (null == bulkImportJob.getTableName()) {
+            failedChecks.add("The table name must be set to a non-null value.");
+        } else if (!doesTableExist(bulkImportJob.getTableName())) {
+            failedChecks.add("Table does not exist.");
+        } else if (!hasMinimumPartitions(stateStoreProvider, tablePropertiesProvider, bulkImportJob)) {
+            failedChecks.add("The minimum partition count was not reached");
+        }
+
+        if (null == bulkImportJob.getFiles() || bulkImportJob.getFiles().isEmpty()) {
+            failedChecks.add("The input files must be set to a non-null and non-empty value.");
+        }
+
+
+        if (!failedChecks.isEmpty()) {
+            String errorMessage = "The bulk import job failed validation with the following checks failing: \n"
+                    + String.join("\n", failedChecks);
+            LOGGER.warn(errorMessage);
+            ingestJobStatusStore.jobValidated(ingestJobRejected(
+                    bulkImportJob.toIngestJob(), validationTimeSupplier.get(), failedChecks));
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    private boolean doesTableExist(String tableName) {
+        try {
+            if (tablePropertiesProvider.getTableProperties(tableName) != null) {
+                return true;
+            }
+        } catch (RuntimeException ep) {
+            LOGGER.warn("Could not find properties for table");
+        }
+        return false;
+    }
+
+    private void writeJobToJSONFile(BulkImportJob bulkImportJob) {
+        String bulkImportBucket = instanceProperties.get(BULK_IMPORT_BUCKET);
+        if (null == bulkImportBucket) {
+            throw new RuntimeException("sleeper.bulk.import.bucket was not set. Has one of the bulk import stacks been deployed?");
+        }
+        String key = "bulk_import/" + bulkImportJob.getId() + ".json";
+        String bulkImportJobJSON = new BulkImportJobSerDe().toJson(bulkImportJob);
+        s3Client.putObject(bulkImportBucket, key, bulkImportJobJSON);
+        LOGGER.info("Put object for job {} to key {} in bucket {}", bulkImportJob.getId(), key, bulkImportBucket);
+    }
+
+    public InstanceProperties getInstanceProperties() {
+        return instanceProperties;
+    }
+}
---
+++ java/bulk-import/bulk-import-starter/src/main/java/sleeper/bulkimport/starter/executor/EmrPlatformExecutor.java
@@ -0,0 +1,155 @@
+/*
+ * Copyright 2022-2023 Crown Copyright
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package sleeper.bulkimport.starter.executor;
+
+import com.amazonaws.services.elasticmapreduce.AmazonElasticMapReduce;
+import com.amazonaws.services.elasticmapreduce.model.Application;
+import com.amazonaws.services.elasticmapreduce.model.EbsBlockDeviceConfig;
+import com.amazonaws.services.elasticmapreduce.model.EbsConfiguration;
+import com.amazonaws.services.elasticmapreduce.model.HadoopJarStepConfig;
+import com.amazonaws.services.elasticmapreduce.model.JobFlowInstancesConfig;
+import com.amazonaws.services.elasticmapreduce.model.ManagedScalingPolicy;
+import com.amazonaws.services.elasticmapreduce.model.RunJobFlowRequest;
+import com.amazonaws.services.elasticmapreduce.model.RunJobFlowResult;
+import com.amazonaws.services.elasticmapreduce.model.ScaleDownBehavior;
+import com.amazonaws.services.elasticmapreduce.model.StepConfig;
+import com.amazonaws.services.elasticmapreduce.model.Tag;
+import com.amazonaws.services.elasticmapreduce.model.VolumeSpecification;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import sleeper.bulkimport.configuration.BulkImportPlatformSpec;
+import sleeper.bulkimport.job.BulkImportJob;
+import sleeper.configuration.properties.InstanceProperties;
+import sleeper.configuration.properties.SystemDefinedInstanceProperty;
+import sleeper.configuration.properties.UserDefinedInstanceProperty;
+import sleeper.configuration.properties.table.TableProperties;
+import sleeper.configuration.properties.table.TablePropertiesProvider;
+
+import java.util.Collections;
+import java.util.stream.Collectors;
+
+import static sleeper.configuration.properties.SystemDefinedInstanceProperty.BULK_IMPORT_BUCKET;
+import static sleeper.configuration.properties.SystemDefinedInstanceProperty.BULK_IMPORT_EMR_CLUSTER_ROLE_NAME;
+import static sleeper.configuration.properties.SystemDefinedInstanceProperty.BULK_IMPORT_EMR_EC2_ROLE_NAME;
+import static sleeper.configuration.properties.SystemDefinedInstanceProperty.BULK_IMPORT_EMR_SECURITY_CONF_NAME;
+import static sleeper.configuration.properties.UserDefinedInstanceProperty.BULK_IMPORT_EMR_EBS_VOLUMES_PER_INSTANCE;
+import static sleeper.configuration.properties.UserDefinedInstanceProperty.BULK_IMPORT_EMR_EBS_VOLUME_SIZE_IN_GB;
+import static sleeper.configuration.properties.UserDefinedInstanceProperty.BULK_IMPORT_EMR_EBS_VOLUME_TYPE;
+import static sleeper.configuration.properties.UserDefinedInstanceProperty.BULK_IMPORT_EMR_EC2_KEYPAIR_NAME;
+import static sleeper.configuration.properties.UserDefinedInstanceProperty.BULK_IMPORT_EMR_MASTER_ADDITIONAL_SECURITY_GROUP;
+import static sleeper.configuration.properties.UserDefinedInstanceProperty.ID;
+import static sleeper.configuration.properties.table.TableProperty.BULK_IMPORT_EMR_RELEASE_LABEL;
+
+/**
+ * An {@link Executor} which runs a bulk import job on an EMR cluster.
+ */
+public class EmrPlatformExecutor implements PlatformExecutor {
+    private static final Logger LOGGER = LoggerFactory.getLogger(EmrPlatformExecutor.class);
+    private final AmazonElasticMapReduce emrClient;
+    private final EmrInstanceConfiguration instanceConfiguration;
+    private final InstanceProperties instanceProperties;
+    private final TablePropertiesProvider tablePropertiesProvider;
+
+    public EmrPlatformExecutor(AmazonElasticMapReduce emrClient,
+                       InstanceProperties instanceProperties,
+                       TablePropertiesProvider tablePropertiesProvider) {
+        this(emrClient, instanceProperties, tablePropertiesProvider, new EmrInstanceFleets(instanceProperties));
+    }
+
+    public EmrPlatformExecutor(AmazonElasticMapReduce emrClient,
+                       InstanceProperties instanceProperties,
+                       TablePropertiesProvider tablePropertiesProvider,
+                       EmrInstanceConfiguration instanceConfiguration) {
+        this.instanceProperties = instanceProperties;
+        this.tablePropertiesProvider = tablePropertiesProvider;
+        this.emrClient = emrClient;
+        this.instanceConfiguration = instanceConfiguration;
+    }
+
+    @Override
+    public void runJobOnPlatform(CommonExecutor commonExecutor, BulkImportJob bulkImportJob, String jobRunId) {
+        TableProperties tableProperties = tablePropertiesProvider.getTableProperties(bulkImportJob.getTableName());
+        String bulkImportBucket = instanceProperties.get(BULK_IMPORT_BUCKET);
+        String logUri = null == bulkImportBucket ? null : "s3://" + bulkImportBucket + "/logs";
+        BulkImportPlatformSpec platformSpec = new BulkImportPlatformSpec(tableProperties, bulkImportJob);
+
+        String clusterName = String.join("-", "sleeper",
+                instanceProperties.get(ID),
+                bulkImportJob.getTableName(),
+                bulkImportJob.getId());
+        if (clusterName.length() > 64) {
+            clusterName = clusterName.substring(0, 64);
+        }
+
+        RunJobFlowResult response = emrClient.runJobFlow(new RunJobFlowRequest()
+                .withName(clusterName)
+                .withInstances(createJobFlowInstancesConfig(platformSpec))
+                .withVisibleToAllUsers(true)
+                .withSecurityConfiguration(instanceProperties.get(BULK_IMPORT_EMR_SECURITY_CONF_NAME))
+                .withManagedScalingPolicy(new ManagedScalingPolicy()
+                        .withComputeLimits(instanceConfiguration.createComputeLimits(platformSpec)))
+                .withScaleDownBehavior(ScaleDownBehavior.TERMINATE_AT_TASK_COMPLETION)
+                .withReleaseLabel(platformSpec.get(BULK_IMPORT_EMR_RELEASE_LABEL))
+                .withApplications(new Application().withName("Spark"))
+                .withLogUri(logUri)
+                .withServiceRole(instanceProperties.get(BULK_IMPORT_EMR_CLUSTER_ROLE_NAME))
+                .withJobFlowRole(instanceProperties.get(BULK_IMPORT_EMR_EC2_ROLE_NAME))
+                .withSteps(new StepConfig()
+                        .withName("Bulk Load (job id " + bulkImportJob.getId() + ")")
+                        .withHadoopJarStep(new HadoopJarStepConfig().withJar("command-runner.jar")
+                                .withArgs(commonExecutor.constructArgs(bulkImportJob, jobRunId, clusterName + "-EMR"))))
+                .withTags(instanceProperties.getTags().entrySet().stream()
+                        .map(entry -> new Tag(entry.getKey(), entry.getValue()))
+                        .collect(Collectors.toList())));
+
+        LOGGER.info("Cluster created with ARN {}", response.getClusterArn());
+    }
+
+    private JobFlowInstancesConfig createJobFlowInstancesConfig(BulkImportPlatformSpec platformSpec) {
+
+        VolumeSpecification volumeSpecification = new VolumeSpecification()
+//                .withIops(null) // TODO Add property to control this
+                .withSizeInGB(instanceProperties.getInt(BULK_IMPORT_EMR_EBS_VOLUME_SIZE_IN_GB))
+                .withVolumeType(instanceProperties.get(BULK_IMPORT_EMR_EBS_VOLUME_TYPE));
+        EbsBlockDeviceConfig ebsBlockDeviceConfig = new EbsBlockDeviceConfig()
+                .withVolumeSpecification(volumeSpecification)
+                .withVolumesPerInstance(instanceProperties.getInt(BULK_IMPORT_EMR_EBS_VOLUMES_PER_INSTANCE));
+        EbsConfiguration ebsConfiguration = new EbsConfiguration()
+                .withEbsBlockDeviceConfigs(ebsBlockDeviceConfig)
+                .withEbsOptimized(true);
+
+        JobFlowInstancesConfig config = instanceConfiguration.createJobFlowInstancesConfig(ebsConfiguration, platformSpec);
+
+        String ec2KeyName = instanceProperties.get(BULK_IMPORT_EMR_EC2_KEYPAIR_NAME);
+        if (null != ec2KeyName && !ec2KeyName.isEmpty()) {
+            config.setEc2KeyName(ec2KeyName);
+        }
+        String additionalSecurityGroup = instanceProperties.get(BULK_IMPORT_EMR_MASTER_ADDITIONAL_SECURITY_GROUP);
+        if (null != additionalSecurityGroup && !additionalSecurityGroup.isEmpty()) {
+            config.setAdditionalMasterSecurityGroups(Collections.singletonList(additionalSecurityGroup));
+        }
+        return config;
+    }
+
+    @Override
+    public String getJarLocation() {
+        return "s3a://"
+            + instanceProperties.get(UserDefinedInstanceProperty.JARS_BUCKET)
+            + "/bulk-import-runner-"
+            + instanceProperties.get(SystemDefinedInstanceProperty.VERSION) + ".jar";
+    }
+}
