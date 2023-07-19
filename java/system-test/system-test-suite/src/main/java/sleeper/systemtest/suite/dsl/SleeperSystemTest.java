db94b2449dcb60af14b2fb86db9db9e7ce032fc2
kr565370 <112558283+kr565370@users.noreply.github.com>
2023-07-19 15:42:10 +0100

    Create test for ingesting data to system test instance

--- java/system-test/system-test-suite/src/main/java/sleeper/systemtest/suite/dsl/SleeperSystemTest.java
+++ java/system-test/system-test-suite/src/main/java/sleeper/systemtest/suite/dsl/SleeperSystemTest.java
@@ -16,14 +16,38 @@
 
 package sleeper.systemtest.suite.dsl;
 
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.apache.hadoop.conf.Configuration;
 import software.amazon.awssdk.services.cloudformation.CloudFormationClient;
 
+import sleeper.configuration.jars.ObjectFactory;
 import sleeper.configuration.properties.instance.InstanceProperties;
+import sleeper.configuration.properties.table.TableProperties;
+import sleeper.configuration.properties.table.TableProperty;
+import sleeper.core.iterator.CloseableIterator;
+import sleeper.core.partition.PartitionTree;
+import sleeper.core.record.Record;
+import sleeper.ingest.IngestFactory;
+import sleeper.query.QueryException;
+import sleeper.query.executor.QueryExecutor;
+import sleeper.query.model.Query;
+import sleeper.statestore.StateStore;
+import sleeper.statestore.StateStoreException;
+import sleeper.statestore.StateStoreProvider;
 import sleeper.systemtest.drivers.instance.SleeperInstanceContext;
 import sleeper.systemtest.drivers.instance.SystemTestParameters;
 import sleeper.systemtest.suite.fixtures.SystemTestInstance;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+import java.util.concurrent.Executors;
+import java.util.stream.Stream;
 
 public class SleeperSystemTest {
 
@@ -32,6 +56,7 @@ public class SleeperSystemTest {
     private final SystemTestParameters parameters = SystemTestParameters.loadFromSystemProperties();
     private final CloudFormationClient cloudFormationClient = CloudFormationClient.create();
     private final AmazonS3 s3Client = AmazonS3ClientBuilder.defaultClient();
+    private final AmazonDynamoDB dynamoDB = AmazonDynamoDBClientBuilder.defaultClient();
     private final SleeperInstanceContext instance = new SleeperInstanceContext(parameters, cloudFormationClient, s3Client);
 
     public static SleeperSystemTest getInstance() {
@@ -44,5 +69,37 @@ public class SleeperSystemTest {
 
     public InstanceProperties instanceProperties() {
         return instance.getCurrentInstance().getInstanceProperties();
+    }
+
+    public TableProperties tableProperties() {
+        return instance.getCurrentInstance().getTableProperties();
+    }
+
+    public void ingestData(Path tempDir, Stream<Record> recordStream) throws Exception {
+        InstanceProperties instanceProperties = instanceProperties();
+        IngestFactory.builder()
+                .objectFactory(ObjectFactory.noUserJars())
+                .localDir(tempDir.toString())
+                .stateStoreProvider(new StateStoreProvider(dynamoDB, instanceProperties))
+                .instanceProperties(instanceProperties)
+                .build().ingestFromRecordIterator(tableProperties(), recordStream.iterator());
+    }
+
+    public List<Record> allRecordsInTable() throws StateStoreException, QueryException {
+        InstanceProperties instanceProperties = instanceProperties();
+        TableProperties tableProperties = tableProperties();
+        StateStore stateStore = new StateStoreProvider(dynamoDB, instanceProperties).getStateStore(tableProperties);
+        QueryExecutor queryExecutor = new QueryExecutor(ObjectFactory.noUserJars(), tableProperties(),
+                stateStore, new Configuration(), Executors.newSingleThreadExecutor());
+        PartitionTree tree = new PartitionTree(tableProperties.getSchema(), stateStore.getAllPartitions());
+        List<Record> records = new ArrayList<>();
+        try (CloseableIterator<Record> recordIterator = queryExecutor.execute(
+                new Query.Builder(tableProperties.get(TableProperty.TABLE_NAME), UUID.randomUUID().toString(),
+                        List.of(tree.getRootPartition().getRegion())).build())) {
+            recordIterator.forEachRemaining(records::add);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return records;
     }
 }
