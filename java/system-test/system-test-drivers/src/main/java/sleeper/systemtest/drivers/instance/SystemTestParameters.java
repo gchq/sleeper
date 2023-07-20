476ad9031017c513133ed05fa7aa17a95370634a
patchwork01 <110390516+patchwork01@users.noreply.github.com>
2023-07-20 11:02:14 +0100

    Resolve Spotbugs failures

--- java/system-test/system-test-drivers/src/main/java/sleeper/systemtest/drivers/instance/SystemTestParameters.java
+++ java/system-test/system-test-drivers/src/main/java/sleeper/systemtest/drivers/instance/SystemTestParameters.java
@@ -62,7 +62,7 @@ public class SystemTestParameters {
     }
 
     private static Path findScriptsDir() {
-        return findJavaDir().getParent().resolve("scripts");
+        return getParentOrFail(findJavaDir()).resolve("scripts");
     }
 
     private static Path findJavaDir() {
@@ -70,10 +70,19 @@ public class SystemTestParameters {
     }
 
     private static Path findJavaDir(Path currentPath) {
-        if ("java".equals(currentPath.getFileName().toString())) {
+        if ("java".equals(String.valueOf(currentPath.getFileName()))) {
             return currentPath;
         } else {
-            return findJavaDir(currentPath.getParent());
+            return findJavaDir(getParentOrFail(currentPath));
+        }
+    }
+
+    private static Path getParentOrFail(Path path) {
+        Path parent = path.getParent();
+        if (parent == null) {
+            throw new IllegalArgumentException("No parent of path " + path);
+        } else {
+            return parent;
         }
     }
 
