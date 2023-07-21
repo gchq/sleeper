ed19ab137490b6c5a7aa22d0606e0729c92e26a4
kr565370 <112558283+kr565370@users.noreply.github.com>
2023-07-21 10:44:24 +0100

    Move ingest changes to new branch

--- java/parquet/src/main/java/sleeper/utils/HadoopConfigurationProvider.java
+++ java/parquet/src/main/java/sleeper/utils/HadoopConfigurationProvider.java
@@ -45,12 +45,7 @@ public class HadoopConfigurationProvider {
     public static Configuration getConfigurationForECS(InstanceProperties instanceProperties) {
         Configuration conf = new Configuration();
         conf.set("fs.s3a.connection.maximum", instanceProperties.get(MAXIMUM_CONNECTIONS_TO_S3));
-        if (System.getenv("AWS_ENDPOINT_URL") != null) {
-            conf.set("fs.s3a.endpoint", System.getenv("AWS_ENDPOINT_URL"));
-            conf.set("fs.s3a.path.style.access", "true");
-        } else {
-            conf.set("fs.s3a.aws.credentials.provider", "com.amazonaws.auth.EC2ContainerCredentialsProviderWrapper");
-        }
+        conf.set("fs.s3a.aws.credentials.provider", "com.amazonaws.auth.EC2ContainerCredentialsProviderWrapper");
         // See https://hadoop.apache.org/docs/stable/hadoop-aws/tools/hadoop-aws/performance.html#Improving_data_input_performance_through_fadvise
         // Some quick experiments showed that the following setting increases the number of records processed per second
         // by 21% in comparison to the default value of "normal".
