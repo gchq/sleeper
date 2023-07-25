a9f910e64430b4b2a6d9173f69e7875fa5a22e23
kr565370 <112558283+kr565370@users.noreply.github.com>
2023-07-25 14:38:54 +0100

    Extract GenerateRangeRecords and start on tests

---
+++ java/system-test/system-test-data-generation/src/test/java/sleeper/systemtest/datageneration/GenerateRangeRecordsTest.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2022-2023 Crown Copyright
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package sleeper.systemtest.datageneration;
+
+import org.junit.jupiter.api.Test;
+
+import sleeper.core.record.Record;
+import sleeper.core.schema.Field;
+import sleeper.core.schema.Schema;
+import sleeper.core.schema.type.StringType;
+
+import java.util.Map;
+import java.util.stream.LongStream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static sleeper.systemtest.datageneration.GenerateRangeRecords.recordsForRange;
+
+public class GenerateRangeRecordsTest {
+    @Test
+    void shouldGenerateTwoRecords() {
+        Schema schema = Schema.builder()
+                .rowKeyFields(new Field("key", new StringType()))
+                .build();
+
+        assertThat(recordsForRange(schema, LongStream.rangeClosed(1, 2)))
+                .containsExactly(
+                        new Record(Map.of("key", "record-1")),
+                        new Record(Map.of("key", "record-2")));
+    }
+}
