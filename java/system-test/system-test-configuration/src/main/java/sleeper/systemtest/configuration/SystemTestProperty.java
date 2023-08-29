ae9fe0ab23a0c757383ffb2eb86c138a3d7ea036
patchwork01 <110390516+patchwork01@users.noreply.github.com>
2023-08-29 09:35:33 +0100

    Remove hyphens in system test property names

--- java/system-test/system-test-configuration/src/main/java/sleeper/systemtest/configuration/SystemTestProperty.java
+++ java/system-test/system-test-configuration/src/main/java/sleeper/systemtest/configuration/SystemTestProperty.java
@@ -59,7 +59,7 @@ public interface SystemTestProperty extends InstanceProperty {
     SystemTestProperty SYSTEM_TEST_BUCKET_NAME = Index.propertyBuilder("sleeper.systemtest.bucket")
             .description("The name of the bucket where system test data will be stored")
             .systemDefined(true).build();
-    SystemTestProperty WRITE_DATA_TASK_DEFINITION_FAMILY = Index.propertyBuilder("sleeper.systemtest.task-definition")
+    SystemTestProperty WRITE_DATA_TASK_DEFINITION_FAMILY = Index.propertyBuilder("sleeper.systemtest.task.definition")
             .description("The name of the family of task definitions used for writing data")
             .systemDefined(true).build();
     SystemTestProperty WRITE_DATA_ROLE_NAME = Index.propertyBuilder("sleeper.systemtest.writer.role")
@@ -92,7 +92,7 @@ public interface SystemTestProperty extends InstanceProperty {
     SystemTestProperty NUMBER_OF_WRITERS = Index.propertyBuilder("sleeper.systemtest.writers")
             .description("The number of containers that write random data")
             .defaultValue("1").validationPredicate(Utils::isPositiveInteger).build();
-    SystemTestProperty NUMBER_OF_RECORDS_PER_WRITER = Index.propertyBuilder("sleeper.systemtest.records-per-writer")
+    SystemTestProperty NUMBER_OF_RECORDS_PER_WRITER = Index.propertyBuilder("sleeper.systemtest.records.per.writer")
             .description("The number of random records that each container should write")
             .defaultValue("100").validationPredicate(Utils::isPositiveInteger).build();
     SystemTestProperty MIN_RANDOM_INT = Index.propertyBuilder("sleeper.systemtest.random.int.min")
