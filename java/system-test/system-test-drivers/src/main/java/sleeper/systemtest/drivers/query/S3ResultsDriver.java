32d2e26e1948d41da375348ae03ae44c3022c1d5
kr565370 <112558283+kr565370@users.noreply.github.com>
2023-09-06 15:59:12 +0100

    Fix merge conflicts

--- java/system-test/system-test-drivers/src/main/java/sleeper/systemtest/drivers/query/S3ResultsDriver.java
+++ java/system-test/system-test-drivers/src/main/java/sleeper/systemtest/drivers/query/S3ResultsDriver.java
@@ -17,8 +17,16 @@
 package sleeper.systemtest.drivers.query;
 
 import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.S3ObjectSummary;
 
+import sleeper.core.record.Record;
+import sleeper.io.parquet.record.ParquetRecordReader;
 import sleeper.systemtest.drivers.instance.SleeperInstanceContext;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
 
 import static sleeper.configuration.properties.instance.SystemDefinedInstanceProperty.QUERY_RESULTS_BUCKET;
 
@@ -29,6 +37,31 @@ public class S3ResultsDriver {
     public S3ResultsDriver(SleeperInstanceContext instance, AmazonS3 s3) {
         this.instance = instance;
         this.s3 = s3;
+    }
+
+    public Stream<Record> results(String queryId) {
+        return s3.listObjects(instance.getInstanceProperties().get(QUERY_RESULTS_BUCKET), "query-" + queryId)
+                .getObjectSummaries().stream()
+                .flatMap(this::getRecords);
+    }
+
+    private Stream<Record> getRecords(S3ObjectSummary s3ObjectSummary) {
+        String path = "s3a://" + s3ObjectSummary.getBucketName() + "/" + s3ObjectSummary.getKey();
+        List<Record> records = new ArrayList<>();
+        try {
+            ParquetRecordReader reader = new ParquetRecordReader(new org.apache.hadoop.fs.Path(path),
+                    instance.getTableProperties().getSchema());
+
+            Record record = reader.read();
+            while (null != record) {
+                records.add(new Record(record));
+                record = reader.read();
+            }
+            reader.close();
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
+        }
+        return records.stream();
     }
 
     public void emptyBucket() {
