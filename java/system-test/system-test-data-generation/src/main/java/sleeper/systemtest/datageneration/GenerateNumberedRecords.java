eeb7554a0b8ad414df98a3a4d96b2b185c6b46bd
kr565370 <112558283+kr565370@users.noreply.github.com>
2023-09-07 13:30:58 +0100

    Refactor GenerateNumberedRecords to allow for custom configuration

--- java/system-test/system-test-data-generation/src/main/java/sleeper/systemtest/datageneration/GenerateNumberedRecords.java
+++ java/system-test/system-test-data-generation/src/main/java/sleeper/systemtest/datageneration/GenerateNumberedRecords.java
@@ -27,18 +27,31 @@ import java.util.stream.LongStream;
 import java.util.stream.Stream;
 
 public class GenerateNumberedRecords {
-    private GenerateNumberedRecords() {
+    private final Configuration configuration;
+    private final Schema schema;
+
+    private GenerateNumberedRecords(Schema schema) {
+        this(GenerateNumberedValue::forField, schema);
+    }
+
+    private GenerateNumberedRecords(Configuration configuration, Schema schema) {
+        this.configuration = configuration;
+        this.schema = schema;
     }
 
     public static Stream<Record> from(Schema schema, LongStream numbers) {
-        return numbers.mapToObj(number -> numberedRecord(schema, number));
+        return new GenerateNumberedRecords(schema).generate(numbers);
     }
 
-    public static Record numberedRecord(Schema schema, long number) {
-        return new Record(mapForNumber(schema, number));
+    public Stream<Record> generate(LongStream numbers) {
+        return numbers.mapToObj(this::numberedRecord);
     }
 
-    private static Map<String, Object> mapForNumber(Schema schema, long number) {
+    public Record numberedRecord(long number) {
+        return new Record(mapForNumber(number));
+    }
+
+    private Map<String, Object> mapForNumber(long number) {
         return Stream.of(
                         entriesForFieldType(number, KeyType.ROW, schema.getRowKeyFields()),
                         entriesForFieldType(number, KeyType.SORT, schema.getSortKeyFields()),
@@ -47,15 +60,19 @@ public class GenerateNumberedRecords {
                 .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
     }
 
-    private static Stream<Map.Entry<String, Object>> entriesForFieldType(
+    private Stream<Map.Entry<String, Object>> entriesForFieldType(
             long number, KeyType keyType, List<Field> fields) {
         return fields.stream()
                 .map(field -> entryForField(number, keyType, field));
     }
 
-    private static Map.Entry<String, Object> entryForField(
+    private Map.Entry<String, Object> entryForField(
             long number, KeyType keyType, Field field) {
         return Map.entry(field.getName(),
-                GenerateNumberedValue.forField(keyType, field).generateValue(number));
+                configuration.getGenerator(keyType, field).generateValue(number));
+    }
+
+    public interface Configuration {
+        GenerateNumberedValue getGenerator(KeyType keyType, Field field);
     }
 }
