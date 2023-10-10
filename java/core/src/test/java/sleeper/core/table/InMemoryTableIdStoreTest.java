9fb13144c16b460e8e3b8a9c273c7e88688a4279
patchwork01 <110390516+patchwork01@users.noreply.github.com>
2023-10-10 10:03:43 +0100

    Organise table ID tests into nested classes

--- java/core/src/test/java/sleeper/core/table/InMemoryTableIdStoreTest.java
+++ java/core/src/test/java/sleeper/core/table/InMemoryTableIdStoreTest.java
@@ -16,6 +16,8 @@
 
 package sleeper.core.table;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -28,79 +30,102 @@ public class InMemoryTableIdStoreTest {
 
     private final TableIdStore store = new InMemoryTableIdStore();
 
-    @Test
-    void shouldCreateATable() {
-        TableId tableId = store.createTable("test-table");
+    @Nested
+    @DisplayName("Create a table")
+    class CreateTable {
+        @Test
+        void shouldCreateATable() {
+            TableId tableId = store.createTable("test-table");
 
-        assertThat(store.streamAllTables())
-                .containsExactly(tableId);
+            assertThat(store.streamAllTables())
+                    .containsExactly(tableId);
+        }
+
+        @Test
+        void shouldFailToCreateATableWhichAlreadyExists() {
+            store.createTable("duplicate-table");
+
+            assertThatThrownBy(() -> store.createTable("duplicate-table"))
+                    .isInstanceOf(TableAlreadyExistsException.class);
+        }
+
+        @Test
+        void shouldGenerateNumericTableIds() {
+            TableId tableIdA = store.createTable("table-a");
+            TableId tableIdB = store.createTable("table-b");
+
+            assertThat(List.of(tableIdA, tableIdB))
+                    .extracting(TableId::getTableName, TableId::getTableId)
+                    .containsExactly(
+                            tuple("table-a", "table-1"),
+                            tuple("table-b", "table-2"));
+        }
     }
 
-    @Test
-    void shouldFailToCreateATableWhichAlreadyExists() {
-        store.createTable("duplicate-table");
+    @Nested
+    @DisplayName("Look up a table")
+    class LookupTable {
 
-        assertThatThrownBy(() -> store.createTable("duplicate-table"))
-                .isInstanceOf(TableAlreadyExistsException.class);
+        @Test
+        void shouldGetTableByName() {
+            TableId tableId = store.createTable("test-table");
+
+            assertThat(store.getTableByName("test-table"))
+                    .contains(tableId);
+        }
+
+        @Test
+        void shouldGetNoTableByName() {
+            store.createTable("existing-table");
+
+            assertThat(store.getTableByName("not-a-table"))
+                    .isEmpty();
+        }
+
+        @Test
+        void shouldGetTableById() {
+            TableId tableId = store.createTable("test-table");
+
+            assertThat(store.getTableById(tableId.getTableId()))
+                    .contains(tableId);
+        }
+
+        @Test
+        void shouldGetNoTableById() {
+            store.createTable("existing-table");
+
+            assertThat(store.getTableById("not-a-table"))
+                    .isEmpty();
+        }
     }
 
-    @Test
-    void shouldGetTableByName() {
-        TableId tableId = store.createTable("test-table");
+    @Nested
+    @DisplayName("List tables")
+    class ListTables {
 
-        assertThat(store.getTableByName("test-table"))
-                .contains(tableId);
-    }
+        @Test
+        void shouldGetTablesOrderedByName() {
+            store.createTable("some-table");
+            store.createTable("a-table");
+            store.createTable("this-table");
+            store.createTable("other-table");
 
-    @Test
-    void shouldGetNoTableByName() {
-        store.createTable("existing-table");
+            assertThat(store.streamAllTables())
+                    .extracting(TableId::getTableName)
+                    .containsExactly(
+                            "a-table",
+                            "other-table",
+                            "some-table",
+                            "this-table");
+        }
 
-        assertThat(store.getTableByName("not-a-table"))
-                .isEmpty();
-    }
+        @Test
+        void shouldGetTableIds() {
+            TableId table1 = store.createTable("first-table");
+            TableId table2 = store.createTable("second-table");
 
-    @Test
-    void shouldGetTableById() {
-        TableId tableId = store.createTable("test-table");
-
-        assertThat(store.getTableById(tableId.getTableId()))
-                .contains(tableId);
-    }
-
-    @Test
-    void shouldGetNoTableById() {
-        store.createTable("existing-table");
-
-        assertThat(store.getTableById("not-a-table"))
-                .isEmpty();
-    }
-
-    @Test
-    void shouldGenerateNumericTableIds() {
-        TableId tableIdA = store.createTable("table-a");
-        TableId tableIdB = store.createTable("table-b");
-
-        assertThat(List.of(tableIdA, tableIdB))
-                .extracting(TableId::getTableName, TableId::getTableId)
-                .containsExactly(
-                        tuple("table-a", "table-1"),
-                        tuple("table-b", "table-2"));
-    }
-
-    @Test
-    void shouldGetTablesOrderedByName() {
-        store.createTable("some-table");
-        store.createTable("a-table");
-        store.createTable("this-table");
-        store.createTable("other-table");
-
-        assertThat(store.streamAllTables())
-                .extracting(TableId::getTableName)
-                .containsExactly(
-                        "a-table",
-                        "other-table",
-                        "some-table",
-                        "this-table");
+            assertThat(store.streamAllTables())
+                    .containsExactly(table1, table2);
+        }
     }
 }
