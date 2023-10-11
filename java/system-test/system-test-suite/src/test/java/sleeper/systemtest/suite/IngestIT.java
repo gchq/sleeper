cf68f12e325ba31026e51a8135cae36cfb870866
kr565370 <112558283+kr565370@users.noreply.github.com>
2023-10-11 11:55:59 +0100

    Make test fail to check extension works in IngestIT

--- java/system-test/system-test-suite/src/test/java/sleeper/systemtest/suite/IngestIT.java
+++ java/system-test/system-test-suite/src/test/java/sleeper/systemtest/suite/IngestIT.java
@@ -61,7 +61,7 @@ public class IngestIT {
         // Then
         assertThat(sleeper.directQuery().allRecordsInTable())
                 .containsExactlyElementsOf(sleeper.generateNumberedRecords(LongStream.range(0, 100)));
-        assertThat(sleeper.tableFiles().active()).hasSize(1);
+        assertThat(sleeper.tableFiles().active()).hasSize(2);
     }
 
     @Test
