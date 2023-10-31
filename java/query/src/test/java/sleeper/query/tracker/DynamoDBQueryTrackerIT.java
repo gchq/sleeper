926cff98e7aab381a4d2a900cc241eaf78f74306
patchwork01 <110390516+patchwork01@users.noreply.github.com>
2023-10-31 14:34:38 +0000

    Use DynamoDBContainer in DynamoDBQueryTrackerIT

--- java/query/src/test/java/sleeper/query/tracker/DynamoDBQueryTrackerIT.java
+++ java/query/src/test/java/sleeper/query/tracker/DynamoDBQueryTrackerIT.java
@@ -22,18 +22,17 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 import sleeper.configuration.properties.instance.InstanceProperties;
-import sleeper.core.CommonTestConstants;
 import sleeper.core.range.Range;
 import sleeper.core.range.Range.RangeFactory;
 import sleeper.core.range.Region;
 import sleeper.core.schema.Field;
 import sleeper.core.schema.Schema;
 import sleeper.core.schema.type.IntType;
+import sleeper.dynamodb.tools.DynamoDBContainer;
 import sleeper.query.model.Query;
 import sleeper.query.model.SubQuery;
 import sleeper.query.model.output.ResultsOutputInfo;
@@ -57,16 +56,14 @@ import static sleeper.query.tracker.QueryState.QUEUED;
 
 @Testcontainers
 public class DynamoDBQueryTrackerIT {
-    private static final int DYNAMO_PORT = 8000;
     private static AmazonDynamoDB dynamoDBClient;
 
     @Container
-    public static GenericContainer dynamoDb = new GenericContainer(CommonTestConstants.DYNAMODB_LOCAL_CONTAINER)
-            .withExposedPorts(DYNAMO_PORT);
+    public static DynamoDBContainer dynamoDb = new DynamoDBContainer();
 
     @BeforeAll
     public static void initDynamoClient() {
-        dynamoDBClient = buildAwsV1Client(dynamoDb, DYNAMO_PORT, AmazonDynamoDBClientBuilder.standard());
+        dynamoDBClient = buildAwsV1Client(dynamoDb, dynamoDb.getDynamoPort(), AmazonDynamoDBClientBuilder.standard());
     }
 
     private final InstanceProperties instanceProperties = createInstanceProperties();
