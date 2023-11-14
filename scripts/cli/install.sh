db77eca2d60aa6a898e2e8f0127fe029107b9ffc
patchwork01 <110390516+patchwork01@users.noreply.github.com>
2023-11-14 13:58:06 +0000

    Fix install script temp path

--- scripts/cli/install.sh
+++ scripts/cli/install.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+#
 # Copyright 2022-2023 Crown Copyright
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -13,6 +14,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
 
 set -e
 unset CDPATH
@@ -24,7 +26,7 @@ else
 fi
 
 echo "Downloading Sleeper CLI"
-TEMP_PATH="/tmp/sleeper"
+TEMP_PATH="/tmp/sleeper/install-cli/sleeper"
 curl "https://raw.githubusercontent.com/gchq/sleeper/$VERSION/scripts/cli/runInDocker.sh" --output "$TEMP_PATH"
 chmod a+x "$TEMP_PATH"
 echo "Downloaded command"
