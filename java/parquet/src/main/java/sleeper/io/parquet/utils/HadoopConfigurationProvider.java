52c596f38b9197cdb64cd7d44cc9aefe64bc5fa7
patchwork01 <110390516+patchwork01@users.noreply.github.com>
2023-11-23 11:53:21 +0000

    Make parquet module package names consistent

--- java/parquet/src/main/java/sleeper/io/parquet/utils/HadoopConfigurationProvider.java
+++ java/parquet/src/main/java/sleeper/io/parquet/utils/HadoopConfigurationProvider.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package sleeper.utils;
+package sleeper.io.parquet.utils;
 
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSCredentialsProvider;
