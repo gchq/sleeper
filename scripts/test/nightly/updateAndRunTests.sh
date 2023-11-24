d9dd70c6e1eeff564d70b22891e458955457846c
patchwork01 <110390516+patchwork01@users.noreply.github.com>
2023-11-24 11:19:48 +0000

    Use a JSON file to store nightly test settings

--- scripts/test/nightly/updateAndRunTests.sh
+++ scripts/test/nightly/updateAndRunTests.sh
@@ -21,20 +21,22 @@ unset CDPATH
 THIS_DIR=$(cd "$(dirname "$0")" && pwd)
 
 if [ "$#" -ne 8 ]; then
-  echo "Usage: $0 <vpc> <subnet> <results-bucket> <test-type> <repo-path> <private-key-pem-file> <app-id> <installation-id>"
+  echo "Usage: $0 <settings-json-file> <test-type>"
   echo "Valid test types are: performance, functional"
   echo "Private key, app ID and installation ID are for authenticating as a GitHub App to push to main"
   exit 1
 fi
 
-VPC=$1
-SUBNETS=$2
-RESULTS_BUCKET=$3
-TEST_TYPE=$4
-REPO_PATH=$5
-PRIVATE_KEY=$6
-APP_ID=$7
-INSTALLATION_ID=$8
+SETTINGS_FILE=$1
+TEST_TYPE=$2
+
+VPC=$(jq ".vpc" "$SETTINGS_FILE" --raw-output)
+SUBNETS=$(jq ".subnets" "$SETTINGS_FILE" --raw-output)
+RESULTS_BUCKET=$(jq ".resultsBucket" "$SETTINGS_FILE" --raw-output)
+REPO_PATH=$(jq ".repoPath" "$SETTINGS_FILE" --raw-output)
+PRIVATE_KEY_FILE=$(jq ".gitHubApp.privateKeyFile" "$SETTINGS_FILE" --raw-output)
+APP_ID=$(jq ".gitHubApp.appId" "$SETTINGS_FILE" --raw-output)
+INSTALLATION_ID=$(jq ".gitHubApp.installationId" "$SETTINGS_FILE" --raw-output)
 
 pushd "$THIS_DIR"
 
@@ -48,7 +50,7 @@ EXIT_CODE=$?
 set -e
 
 if [ $EXIT_CODE -eq 0 ]; then
-  ./mergeToMain.sh "$REPO_PATH" "$PRIVATE_KEY" "$APP_ID" "$INSTALLATION_ID"
+  ./mergeToMain.sh "$REPO_PATH" "$PRIVATE_KEY_FILE" "$APP_ID" "$INSTALLATION_ID"
 fi
 
 popd
