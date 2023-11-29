81c497fc16800734e4a614a220531443b14cd90f
patchwork01 <110390516+patchwork01@users.noreply.github.com>
2023-11-29 09:24:34 +0000

    Add SplitFileInfo.referenceForChildPartition

--- java/core/src/main/java/sleeper/core/statestore/SplitFileInfo.java
+++ java/core/src/main/java/sleeper/core/statestore/SplitFileInfo.java
@@ -20,7 +20,8 @@ public class SplitFileInfo {
 
     /**
      * Used to create a new reference to a file that has been copied from its original partition to one of its two
-     * child partitions.
+     * child partitions. This will be paired with another call to this method for the other child partition, to split
+     * the original file reference into two. The original reference should then be deleted.
      * <p>
      * To copy a file further down the tree this split must be repeated. This will compute an estimate of the number of
      * records in the file that are in this partition.
@@ -34,6 +35,30 @@ public class SplitFileInfo {
         return FileInfo.partialFile()
                 .partitionId(childPartitionId)
                 .filename(newFilename)
+                .fileStatus(FileInfo.FileStatus.ACTIVE)
+                .numberOfRecords(file.getNumberOfRecords() / 2)
+                .build();
+    }
+
+    /**
+     * Used to create a new reference to a file in one of the two child partitions that the original reference is
+     * associated with. This will be paired with another call to this method for the other child partition, to split
+     * the original file reference into two. The original reference should then be deleted.
+     * <p>
+     * This may be used to consider that file to be in the child partition without moving or copying the file. This is
+     * useful when compacting parts of a file at a time down the partition tree to leaf partitions.
+     * <p>
+     * To split a file further down the tree this split must be repeated. This will compute an estimate of the number of
+     * records in the file that are in this partition.
+     *
+     * @param file             The file reference being split
+     * @param childPartitionId The ID of the child partition to create metadata for
+     * @return The reference to the new copy
+     */
+    public static FileInfo referenceForChildPartition(FileInfo file, String childPartitionId) {
+        return FileInfo.partialFile()
+                .partitionId(childPartitionId)
+                .filename(file.getFilename())
                 .fileStatus(FileInfo.FileStatus.ACTIVE)
                 .numberOfRecords(file.getNumberOfRecords() / 2)
                 .build();
