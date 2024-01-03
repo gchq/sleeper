1a25729fda6473307b5d302bba080242980ce434
patchwork01 <110390516+patchwork01@users.noreply.github.com>
2024-01-03 14:02:50 +0000

    Use StateStore.getAllFileReferencesWithMaxUnreferenced in report

--- java/clients/src/main/java/sleeper/clients/status/report/filestatus/StateStoreSnapshot.java
+++ java/clients/src/main/java/sleeper/clients/status/report/filestatus/StateStoreSnapshot.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2022-2023 Crown Copyright
+ * Copyright 2022-2024 Crown Copyright
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -64,9 +64,8 @@ public class StateStoreSnapshot {
     }
 
     public static StateStoreSnapshot from(StateStore stateStore, int maxNumberOfFilesWithNoReferencesToCount) throws StateStoreException {
-        AllFileReferences allFileReferences = AllFileReferences.fromStateStoreWithReadyForGCLimit(stateStore, maxNumberOfFilesWithNoReferencesToCount);
         return builder()
-                .allFileReferences(allFileReferences)
+                .allFileReferences(stateStore.getAllFileReferencesWithMaxUnreferenced(maxNumberOfFilesWithNoReferencesToCount))
                 .partitions(stateStore.getAllPartitions())
                 .build();
     }
