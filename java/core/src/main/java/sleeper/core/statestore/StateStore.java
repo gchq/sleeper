aa8c877d644c36465ccc11bc3201b95945f0d37d
kr565370 <112558283+kr565370@users.noreply.github.com>
2024-01-04 16:19:33 +0000

    Add javadoc to StateStore.clearSleeperTable

--- java/core/src/main/java/sleeper/core/statestore/StateStore.java
+++ java/core/src/main/java/sleeper/core/statestore/StateStore.java
@@ -20,7 +20,12 @@ package sleeper.core.statestore;
  * and the {@link sleeper.core.partition.Partition}s).
  */
 public interface StateStore extends FileInfoStore, PartitionStore {
-
+    /**
+     * Clears all file data and partition data from the state store.
+     * <p>
+     * Note that this does not delete any of the actual files, and after calling this
+     * method the partition store must be initialised before the Sleeper table can be used again.
+     */
     default void clearSleeperTable() {
         clearFileData();
         clearPartitionData();
