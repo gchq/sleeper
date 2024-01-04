4550a1c04b6fb7e4c4f50237ee632aecae282b58
patchwork01 <110390516+patchwork01@users.noreply.github.com>
2024-01-04 11:32:52 +0000

    Add FileInfoSerDe

---
+++ java/core/src/main/java/sleeper/core/statestore/FileInfoSerDe.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2022-2024 Crown Copyright
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package sleeper.core.statestore;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+
+import java.util.List;
+
+public class FileInfoSerDe {
+    private final Gson gson = new GsonBuilder().create();
+
+    public String toJson(FileInfo file) {
+        return gson.toJson(file);
+    }
+
+    public FileInfo fromJson(String json) {
+        return gson.fromJson(json, FileInfo.class);
+    }
+
+    public String listToJson(List<FileInfo> files) {
+        return gson.toJson(files);
+    }
+
+    public List<FileInfo> listFromJson(String json) {
+        return gson.fromJson(json, new TypeToken<>() {
+        });
+    }
+}
