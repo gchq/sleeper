98c873be18d63b547ccd2fa4ecf8e55822e8bf14
kr565370 <112558283+kr565370@users.noreply.github.com>
2024-01-17 14:40:34 +0000

    Add comment to explain why we use ParquetFileWriter.Mode.OVERWRITE

--- java/compaction/compaction-job-execution/src/main/java/sleeper/compaction/jobexecution/CompactSortedFiles.java
+++ java/compaction/compaction-job-execution/src/main/java/sleeper/compaction/jobexecution/CompactSortedFiles.java
@@ -148,6 +148,8 @@ public class CompactSortedFiles {
         // Create writer
         LOGGER.debug("Creating writer for file {}", compactionJob.getOutputFile());
         Path outputPath = new Path(compactionJob.getOutputFile());
+        // Setting file writer mode to OVERWRITE so if the same job runs again after failing to
+        // update the state store, it will overwrite the existing output file written by the previous run
         ParquetWriter<Record> writer = ParquetRecordWriterFactory.createParquetRecordWriter(
                 outputPath, tableProperties, conf, ParquetFileWriter.Mode.OVERWRITE);
 
