0eeae8267155cf5f705b143db1048bb6cfdcd18b
patchwork01 <110390516+patchwork01@users.noreply.github.com>
2024-01-22 15:27:42 +0000

    Align FileReferences more with S3FileReference

--- java/core/src/main/java/sleeper/core/statestore/FileReferences.java
+++ java/core/src/main/java/sleeper/core/statestore/FileReferences.java
@@ -18,7 +18,12 @@ package sleeper.core.statestore;
 
 import java.time.Instant;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
+import java.util.Set;
+import java.util.TreeMap;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * Reports on all the references for an individual physical file. A file may be referenced in a number of different
@@ -29,13 +34,61 @@ public class FileReferences {
     private final String filename;
     private final Instant lastUpdateTime;
     private final int totalReferenceCount;
-    private final List<FileReference> references;
+    private final List<FileReference> internalReferences;
 
     private FileReferences(Builder builder) {
         filename = builder.filename;
         lastUpdateTime = builder.lastUpdateTime;
         totalReferenceCount = builder.totalReferenceCount;
-        references = builder.references;
+        internalReferences = builder.internalReferences;
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public static List<FileReferences> listNewFilesWithReferences(List<FileReference> references, Instant updateTime) {
+        return newFilesWithReferences(references, updateTime)
+                .collect(Collectors.toUnmodifiableList());
+    }
+
+    public static Stream<FileReferences> newFilesWithReferences(List<FileReference> references, Instant updateTime) {
+        Map<String, List<FileReference>> referencesByFilename = references.stream()
+                .collect(Collectors.groupingBy(FileReference::getFilename, TreeMap::new, Collectors.toList()));
+        return referencesByFilename.entrySet().stream()
+                .map(entry -> FileReferences.builder()
+                        .filename(entry.getKey())
+                        .internalReferencesUpdatedAt(entry.getValue(), updateTime)
+                        .totalReferenceCount(entry.getValue().size())
+                        .lastUpdateTime(updateTime)
+                        .build());
+    }
+
+    public FileReferences splitReferenceFromPartition(
+            String partitionId, Instant updateTime, List<FileReference> newReferences) {
+        return toBuilder()
+                .internalReferences(Stream.concat(
+                                internalReferences.stream()
+                                        .filter(reference -> !partitionId.equals(reference.getPartitionId())),
+                                newReferences.stream())
+                        .collect(Collectors.toUnmodifiableList()))
+                .totalReferenceCount(totalReferenceCount - 1 + newReferences.size())
+                .lastUpdateTime(updateTime)
+                .build();
+    }
+
+    public FileReferences withJobIdForPartitions(String jobId, Set<String> partitionUpdates, Instant updateTime) {
+        return toBuilder()
+                .internalReferences(internalReferences.stream()
+                        .map(reference -> {
+                            if (partitionUpdates.contains(reference.getPartitionId())) {
+                                return reference.toBuilder().jobId(jobId).lastStateStoreUpdateTime(updateTime).build();
+                            } else {
+                                return reference;
+                            }
+                        }).collect(Collectors.toUnmodifiableList()))
+                .lastUpdateTime(updateTime)
+                .build();
     }
 
     public String getFilename() {
@@ -50,8 +103,20 @@ public class FileReferences {
         return totalReferenceCount;
     }
 
-    public List<FileReference> getReferences() {
-        return references;
+    public int getExternalReferenceCount() {
+        return totalReferenceCount - internalReferences.size();
+    }
+
+    public List<FileReference> getInternalReferences() {
+        return internalReferences;
+    }
+
+    private Builder toBuilder() {
+        return builder()
+                .filename(filename)
+                .internalReferences(internalReferences)
+                .totalReferenceCount(totalReferenceCount)
+                .lastUpdateTime(lastUpdateTime);
     }
 
     @Override
@@ -63,12 +128,12 @@ public class FileReferences {
             return false;
         }
         FileReferences that = (FileReferences) o;
-        return totalReferenceCount == that.totalReferenceCount && Objects.equals(filename, that.filename) && Objects.equals(lastUpdateTime, that.lastUpdateTime) && Objects.equals(references, that.references);
+        return totalReferenceCount == that.totalReferenceCount && Objects.equals(filename, that.filename) && Objects.equals(lastUpdateTime, that.lastUpdateTime) && Objects.equals(internalReferences, that.internalReferences);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(filename, lastUpdateTime, totalReferenceCount, references);
+        return Objects.hash(filename, lastUpdateTime, totalReferenceCount, internalReferences);
     }
 
     @Override
@@ -77,7 +142,7 @@ public class FileReferences {
                 "filename='" + filename + '\'' +
                 ", lastUpdateTime=" + lastUpdateTime +
                 ", totalReferenceCount=" + totalReferenceCount +
-                ", references=" + references +
+                ", references=" + internalReferences +
                 '}';
     }
 
@@ -85,13 +150,9 @@ public class FileReferences {
         private String filename;
         private Instant lastUpdateTime;
         private int totalReferenceCount;
-        private List<FileReference> references;
+        private List<FileReference> internalReferences;
 
         private Builder() {
-        }
-
-        public static Builder builder() {
-            return new Builder();
         }
 
         public Builder filename(String filename) {
@@ -109,9 +170,15 @@ public class FileReferences {
             return this;
         }
 
-        public Builder references(List<FileReference> references) {
-            this.references = references;
+        public Builder internalReferences(List<FileReference> references) {
+            this.internalReferences = references;
             return this;
+        }
+
+        public Builder internalReferencesUpdatedAt(List<FileReference> internalReferences, Instant updateTime) {
+            return internalReferences(internalReferences.stream()
+                    .map(fileReference -> fileReference.toBuilder().lastStateStoreUpdateTime(updateTime).build())
+                    .collect(Collectors.toUnmodifiableList()));
         }
 
         public FileReferences build() {
