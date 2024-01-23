598d359a4caff4a5e862e750370d218a7ff8af19
patchwork01 <110390516+patchwork01@users.noreply.github.com>
2024-01-23 13:14:39 +0000

    Fix CompactSortedFilesIT

--- java/compaction/compaction-job-execution/src/test/java/sleeper/compaction/jobexecution/CompactSortedFilesIT.java
+++ java/compaction/compaction-job-execution/src/test/java/sleeper/compaction/jobexecution/CompactSortedFilesIT.java
@@ -76,7 +76,7 @@ class CompactSortedFilesIT extends CompactSortedFilesTestBase {
 
         // - Check DynamoDBStateStore has correct ready for GC files
         assertThat(stateStore.getReadyForGCFilenamesBefore(Instant.ofEpochMilli(Long.MAX_VALUE)))
-                .containsExactly(file1.getFilename(), file2.getFilename());
+                .containsExactlyInAnyOrder(file1.getFilename(), file2.getFilename());
 
         // - Check DynamoDBStateStore has correct active files
         assertThat(stateStore.getActiveFiles())
