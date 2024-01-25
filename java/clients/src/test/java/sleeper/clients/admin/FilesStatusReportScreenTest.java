9ddc2949051f8b2bbf4eda658596014398fb5f7d
patchwork01 <110390516+patchwork01@users.noreply.github.com>
2024-01-25 10:42:08 +0000

    Fix FilesStatusReportScreenTest

--- java/clients/src/test/java/sleeper/clients/admin/FilesStatusReportScreenTest.java
+++ java/clients/src/test/java/sleeper/clients/admin/FilesStatusReportScreenTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2022-2023 Crown Copyright
+ * Copyright 2022-2024 Crown Copyright
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -89,9 +89,8 @@ class FilesStatusReportScreenTest extends AdminClientMockStoreBase {
                         "--------------------------\n" +
                         "There are 2 leaf partitions and 1 non-leaf partitions")
                 .contains("" +
-                        "Files with no references:\n" +
-                        "[]\n" +
-                        "Active files:");
+                        "Files with no references: none\n" +
+                        "Files with references:");
         confirmAndVerifyNoMoreInteractions();
     }
 
@@ -115,9 +114,8 @@ class FilesStatusReportScreenTest extends AdminClientMockStoreBase {
                         "--------------------------\n" +
                         "There are 2 leaf partitions and 1 non-leaf partitions")
                 .contains("" +
-                        "Files with no references:\n" +
-                        "[]\n" +
-                        "Active files:");
+                        "Files with no references: none\n" +
+                        "Files with references:");
         confirmAndVerifyNoMoreInteractions();
     }
 
