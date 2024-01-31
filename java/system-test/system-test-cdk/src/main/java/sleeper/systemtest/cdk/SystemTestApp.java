b459b97a501be33137b0326d2443f020ca4f0217
patchwork01 <110390516+patchwork01@users.noreply.github.com>
2024-01-31 15:09:37 +0000

    Grant access to system test bucket in deployAll system test cluster

--- java/system-test/system-test-cdk/src/main/java/sleeper/systemtest/cdk/SystemTestApp.java
+++ java/system-test/system-test-cdk/src/main/java/sleeper/systemtest/cdk/SystemTestApp.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2022-2023 Crown Copyright
+ * Copyright 2022-2024 Crown Copyright
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -47,11 +47,11 @@ public class SystemTestApp extends SleeperCdkApp {
     @Override
     public void create() {
         SystemTestProperties properties = getInstanceProperties();
-        new SystemTestBucketStack(this, "SystemTestIngestBucket", properties);
+        SystemTestBucketStack bucketStack = new SystemTestBucketStack(this, "SystemTestIngestBucket", properties);
         super.create();
         // Stack for writing random data
         if (properties.getBoolean(SYSTEM_TEST_CLUSTER_ENABLED)) {
-            new SystemTestClusterStack(this, "SystemTest", properties,
+            new SystemTestClusterStack(this, "SystemTest", properties, bucketStack,
                     getCoreStacks(), getIngestStack(), getEmrBulkImportStack());
         }
 
