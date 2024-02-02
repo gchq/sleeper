60acd3aff6e141f2b688699aba9fac8ddca04a25
kr565370 <112558283+kr565370@users.noreply.github.com>
2024-02-02 11:44:42 +0000

    Allow compactAll flag to be passed into compactFiles.sh

--- scripts/deploy/localstack/compactFiles.sh
+++ scripts/deploy/localstack/compactFiles.sh
@@ -18,8 +18,8 @@
 set -e
 unset CDPATH
 
-if [ "$#" -lt 1 ]; then
-  echo "Usage: $0 <instance-id>"
+if [ "$#" -lt 1 ] || [ "$#" -gt 2 ]; then
+  echo "Usage: $0 <instance-id> <optional-compact-all-flag>"
   exit 1
 fi
 INSTANCE_ID=$1;
