1363140d1dd6cd84e14bc9577b803827be012d57
kr565370 <112558283+kr565370@users.noreply.github.com>
2024-02-06 14:56:44 +0000

    Load records before opening output file in test files

--- python/test/exact_query.py
+++ python/test/exact_query.py
@@ -1,4 +1,4 @@
-#  Copyright 2022-2023 Crown Copyright
+#  Copyright 2022-2024 Crown Copyright
 # 
 #  Licensed under the Apache License, Version 2.0 (the "License");
 #  you may not use this file except in compliance with the License.
@@ -29,9 +29,10 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     sleeper_client = SleeperClient(args.instance)
+    records = sleeper_client.exact_key_query(args.table, args.query, args.queryid)
 
     with open(args.outdir + "/" + args.queryid + ".txt", "wb") as file:
         writer = ParquetSerialiser(file)
-        for record in sleeper_client.exact_key_query(args.table, args.query, args.queryid):
+        for record in records:
             writer.write_record(record)
         writer.write_tail()
