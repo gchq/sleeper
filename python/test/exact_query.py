7f624a7b0ed41cfcb644e8d2d2c5640330b050e0
kr565370 <112558283+kr565370@users.noreply.github.com>
2024-02-06 14:59:43 +0000

    Add output to test python files

--- python/test/exact_query.py
+++ python/test/exact_query.py
@@ -32,7 +32,10 @@ if __name__ == "__main__":
     records = sleeper_client.exact_key_query(args.table, args.query, args.queryid)
 
     with open(args.outdir + "/" + args.queryid + ".txt", "wb") as file:
+        print("Opening parquet file to write results")
         writer = ParquetSerialiser(file)
         for record in records:
+            print("Writing record " + record)
             writer.write_record(record)
         writer.write_tail()
+        print("Finished writing file")
