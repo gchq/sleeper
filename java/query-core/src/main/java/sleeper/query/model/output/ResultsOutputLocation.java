e52bcb85c3180497955f90a622555f2eb0cf2208
kr565370 <112558283+kr565370@users.noreply.github.com>
2024-02-08 10:48:08 +0000

    Rename query module to query-core

--- java/query-core/src/main/java/sleeper/query/model/output/ResultsOutputLocation.java
+++ java/query-core/src/main/java/sleeper/query/model/output/ResultsOutputLocation.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2022-2023 Crown Copyright
+ * Copyright 2022-2024 Crown Copyright
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,8 +16,8 @@
 package sleeper.query.model.output;
 
 public class ResultsOutputLocation {
-    private String type;
-    private String location;
+    private final String type;
+    private final String location;
 
     public ResultsOutputLocation(String type, String location) {
         this.type = type;
