7682db1a90c3fd7530872e315837204cc5d21f1e
kr565370 <112558283+kr565370@users.noreply.github.com>
2024-02-15 16:50:09 +0000

    Add clients for putting table online/taking table offline

---
+++ java/clients/src/main/java/sleeper/clients/status/update/TakeTableOffline.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2022-2024 Crown Copyright
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package sleeper.clients.status.update;
+
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import sleeper.configuration.properties.instance.InstanceProperties;
+import sleeper.configuration.table.index.DynamoDBTableIndex;
+import sleeper.core.table.TableIdentity;
+import sleeper.core.table.TableIndex;
+import sleeper.core.table.TableNotFoundException;
+
+import static sleeper.configuration.utils.AwsV1ClientHelper.buildAwsV1Client;
+
+public class TakeTableOffline {
+    private static final Logger LOGGER = LoggerFactory.getLogger(RenameTable.class);
+
+    private final TableIndex tableIndex;
+
+    public TakeTableOffline(AmazonDynamoDB dynamoDB, InstanceProperties instanceProperties) {
+        this(new DynamoDBTableIndex(instanceProperties, dynamoDB));
+    }
+
+    public TakeTableOffline(TableIndex tableIndex) {
+        this.tableIndex = tableIndex;
+    }
+
+    public void takeOffline(String tableName) {
+        TableIdentity tableIdentity = tableIndex.getTableByName(tableName)
+                .orElseThrow(() -> TableNotFoundException.withTableName(tableName));
+        tableIndex.takeOffline(tableIdentity);
+        LOGGER.info("Successfully took table offline {}", tableName);
+    }
+
+    public static void main(String[] args) {
+        if (args.length != 2) {
+            System.out.println("Usage: <instance-id> <table-name>");
+        }
+        AmazonS3 s3Client = buildAwsV1Client(AmazonS3ClientBuilder.standard());
+        AmazonDynamoDB dynamoDBClient = buildAwsV1Client(AmazonDynamoDBClientBuilder.standard());
+        try {
+            InstanceProperties instanceProperties = new InstanceProperties();
+            instanceProperties.loadFromS3GivenInstanceId(s3Client, args[0]);
+            new TakeTableOffline(dynamoDBClient, instanceProperties).takeOffline(args[1]);
+        } finally {
+            dynamoDBClient.shutdown();
+            s3Client.shutdown();
+        }
+    }
+}
