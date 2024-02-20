c0f1f7a03c6b445c7dae149de09e41d7f1b0ce8d
patchwork01 <110390516+patchwork01@users.noreply.github.com>
2024-02-20 15:15:23 +0000

    Add SplitIntoBatches utility

---
+++ java/core/src/main/java/sleeper/core/util/SplitIntoBatches.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2022-2024 Crown Copyright
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package sleeper.core.util;
+
+import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class SplitIntoBatches {
+    public static <T> Stream<List<T>> splitListIntoBatchesOf(int batchSize, List<T> list) {
+        if (batchSize < 1) {
+            throw new IllegalArgumentException("Batch size must be at least 1, found " + batchSize);
+        }
+        return IntStream.iterate(0, i -> i < list.size(), i -> i + batchSize)
+                .mapToObj(i -> list.subList(i, Math.min(i + batchSize, list.size())));
+    }
+}
