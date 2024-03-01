5c95f02f3cc76515c38f9f0ee881935ed1fd0dc2
kr565370 <112558283+kr565370@users.noreply.github.com>
2024-03-01 12:06:11 +0000

    Improve grammar in property description

--- java/configuration/src/main/java/sleeper/configuration/properties/instance/PartitionSplittingProperty.java
+++ java/configuration/src/main/java/sleeper/configuration/properties/instance/PartitionSplittingProperty.java
@@ -16,15 +16,14 @@
 
 package sleeper.configuration.properties.instance;
 
-
 import sleeper.configuration.properties.SleeperPropertyIndex;
 
 import java.util.List;
 
 public interface PartitionSplittingProperty {
     UserDefinedInstanceProperty PARTITION_SPLITTING_TRIGGER_PERIOD_IN_MINUTES = Index.propertyBuilder("sleeper.partition.splitting.period.minutes")
-            .description("The frequency in minutes with which the lambda that finds partitions that need splitting " +
-                    "and sends jobs to the splitting lambda runs.")
+            .description("The frequency in minutes with which the lambda runs to finds partitions that need splitting " +
+                    "and send jobs to the splitting lambda.")
             .defaultValue("30")
             .propertyGroup(InstancePropertyGroup.PARTITION_SPLITTING)
             .runCdkDeployWhenChanged(true).build();
