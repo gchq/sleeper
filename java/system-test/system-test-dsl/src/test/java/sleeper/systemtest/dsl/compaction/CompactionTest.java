899406d5fcd9aea2fc82e66f43950883d9a3d48c
patchwork01 <110390516+patchwork01@users.noreply.github.com>
2024-03-05 11:15:14 +0000

    Update DSL CompactionTest

--- java/system-test/system-test-dsl/src/test/java/sleeper/systemtest/dsl/compaction/CompactionTest.java
+++ java/system-test/system-test-dsl/src/test/java/sleeper/systemtest/dsl/compaction/CompactionTest.java
@@ -17,8 +17,11 @@
 package sleeper.systemtest.dsl.compaction;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
+import sleeper.compaction.strategy.impl.BasicCompactionStrategy;
 import sleeper.core.partition.PartitionTree;
 import sleeper.core.partition.PartitionsBuilder;
 import sleeper.core.statestore.FileReferenceFactory;
@@ -32,46 +35,176 @@ import java.util.stream.LongStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static sleeper.configuration.properties.table.TableProperty.COMPACTION_FILES_BATCH_SIZE;
+import static sleeper.configuration.properties.table.TableProperty.COMPACTION_STRATEGY_CLASS;
+import static sleeper.configuration.properties.table.TableProperty.INGEST_FILE_WRITING_STRATEGY;
+import static sleeper.configuration.properties.validation.IngestFileWritingStrategy.ONE_FILE_PER_LEAF;
 import static sleeper.core.statestore.FilesReportTestHelper.activeAndReadyForGCFiles;
 import static sleeper.core.testutils.printers.FileReferencePrinter.printFiles;
+import static sleeper.systemtest.dsl.sourcedata.GenerateNumberedValue.addPrefix;
+import static sleeper.systemtest.dsl.sourcedata.GenerateNumberedValue.numberStringAndZeroPadTo;
+import static sleeper.systemtest.dsl.sourcedata.GenerateNumberedValueOverrides.overrideField;
 import static sleeper.systemtest.dsl.testutil.InMemoryTestInstance.DEFAULT_SCHEMA;
+import static sleeper.systemtest.dsl.testutil.InMemoryTestInstance.ROW_KEY_FIELD_NAME;
 import static sleeper.systemtest.dsl.testutil.InMemoryTestInstance.withDefaultProperties;
 
 @InMemoryDslTest
 public class CompactionTest {
-
-    private final PartitionTree partitions = new PartitionsBuilder(DEFAULT_SCHEMA).singlePartition("root").buildTree();
-    private final FileReferenceFactory fileFactory = FileReferenceFactory.from(partitions);
 
     @BeforeEach
     void setUp(SleeperSystemTest sleeper) throws Exception {
         sleeper.connectToInstance(withDefaultProperties("main"));
     }
 
-    @Test
-    void shouldCompactFilesUsingDefaultCompactionStrategy(SleeperSystemTest sleeper) {
-        // Given
-        sleeper.updateTableProperties(Map.of(
-                COMPACTION_FILES_BATCH_SIZE, "5"));
-        // Files with records 9, 9, 9, 9, 10 (which match SizeRatioStrategy criteria)
-        RecordNumbers numbers = sleeper.scrambleNumberedRecords(LongStream.range(0, 46));
-        sleeper.ingest().direct(null)
-                .numberedRecords(numbers.range(0, 9))
-                .numberedRecords(numbers.range(9, 18))
-                .numberedRecords(numbers.range(18, 27))
-                .numberedRecords(numbers.range(27, 36))
-                .numberedRecords(numbers.range(36, 46));
+    @Nested
+    @DisplayName("Merge whole files together")
+    class MergeFiles {
+        private PartitionTree expectedPartitions;
+        private FileReferenceFactory fileFactory;
 
-        // When
-        sleeper.compaction().createJobs(1).invokeTasks(1).waitForJobs();
+        @BeforeEach
+        void setUp(SleeperSystemTest sleeper) {
+            expectedPartitions = new PartitionsBuilder(DEFAULT_SCHEMA).singlePartition("root").buildTree();
+            fileFactory = FileReferenceFactory.from(expectedPartitions);
+        }
 
-        // Then
-        assertThat(sleeper.directQuery().allRecordsInTable())
-                .containsExactlyInAnyOrderElementsOf(sleeper.generateNumberedRecords(LongStream.range(0, 46)));
-        assertThat(printFiles(sleeper.partitioning().tree(), sleeper.tableFiles().all()))
-                .isEqualTo(printFiles(partitions, activeAndReadyForGCFiles(
-                        List.of(fileFactory.rootFile("output.parquet", 46)),
-                        List.of("file1.parquet", "file2.parquet", "file3.parquet", "file4.parquet", "file5.parquet"))));
+        @Test
+        void shouldCompactFilesUsingDefaultCompactionStrategy(SleeperSystemTest sleeper) {
+            // Given
+            sleeper.updateTableProperties(Map.of(
+                    COMPACTION_FILES_BATCH_SIZE, "5"));
+            // Files with records 9, 9, 9, 9, 10 (which match SizeRatioStrategy criteria)
+            RecordNumbers numbers = sleeper.scrambleNumberedRecords(LongStream.range(0, 46));
+            sleeper.ingest().direct(null)
+                    .numberedRecords(numbers.range(0, 9))
+                    .numberedRecords(numbers.range(9, 18))
+                    .numberedRecords(numbers.range(18, 27))
+                    .numberedRecords(numbers.range(27, 36))
+                    .numberedRecords(numbers.range(36, 46));
+
+            // When
+            sleeper.compaction().createJobs(1).invokeTasks(1).waitForJobs();
+
+            // Then
+            assertThat(sleeper.directQuery().allRecordsInTable())
+                    .containsExactlyInAnyOrderElementsOf(sleeper.generateNumberedRecords(LongStream.range(0, 46)));
+            assertThat(printFiles(sleeper.partitioning().tree(), sleeper.tableFiles().all()))
+                    .isEqualTo(printFiles(expectedPartitions, activeAndReadyForGCFiles(
+                            List.of(fileFactory.rootFile("output.parquet", 46)),
+                            List.of("file1.parquet", "file2.parquet", "file3.parquet", "file4.parquet", "file5.parquet"))));
+        }
+
+        @Test
+        void shouldCompactFilesUsingBasicCompactionStrategy(SleeperSystemTest sleeper) {
+            // Given
+            sleeper.updateTableProperties(Map.of(
+                    COMPACTION_STRATEGY_CLASS, BasicCompactionStrategy.class.getName(),
+                    COMPACTION_FILES_BATCH_SIZE, "2"));
+            RecordNumbers numbers = sleeper.scrambleNumberedRecords(LongStream.range(0, 100));
+            sleeper.ingest().direct(null)
+                    .numberedRecords(numbers.range(0, 25))
+                    .numberedRecords(numbers.range(25, 50))
+                    .numberedRecords(numbers.range(50, 75))
+                    .numberedRecords(numbers.range(75, 100));
+
+            // When
+            sleeper.compaction().createJobs(2).invokeTasks(1).waitForJobs();
+
+            // Then
+            assertThat(sleeper.directQuery().allRecordsInTable())
+                    .containsExactlyInAnyOrderElementsOf(sleeper.generateNumberedRecords(LongStream.range(0, 100)));
+            assertThat(printFiles(sleeper.partitioning().tree(), sleeper.tableFiles().all()))
+                    .isEqualTo(printFiles(expectedPartitions, activeAndReadyForGCFiles(
+                            List.of(fileFactory.rootFile("output1.parquet", 50),
+                                    fileFactory.rootFile("output2.parquet", 50)),
+                            List.of("file1.parquet", "file2.parquet", "file3.parquet", "file4.parquet"))));
+        }
     }
 
+    @Nested
+    @DisplayName("Merge parts of files referenced on multiple partitions")
+    class MergePartialFiles {
+
+        private PartitionTree partitions;
+        private FileReferenceFactory fileFactory;
+
+        @BeforeEach
+        void setUp(SleeperSystemTest sleeper) {
+            sleeper.setGeneratorOverrides(overrideField(
+                    ROW_KEY_FIELD_NAME, numberStringAndZeroPadTo(2).then(addPrefix("row-"))));
+            partitions = new PartitionsBuilder(DEFAULT_SCHEMA)
+                    .rootFirst("root")
+                    .splitToNewChildren("root", "L", "R", "row-50")
+                    .splitToNewChildren("L", "LL", "LR", "row-25")
+                    .splitToNewChildren("R", "RL", "RR", "row-75")
+                    .buildTree();
+            sleeper.partitioning().setPartitions(partitions);
+            fileFactory = FileReferenceFactory.from(partitions);
+        }
+
+        @Test
+        void shouldCompactOneFileIntoExistingFilesOnLeafPartitions(SleeperSystemTest sleeper) throws Exception {
+            // Given a compaction strategy which will always compact two files together
+            sleeper.updateTableProperties(Map.of(
+                    COMPACTION_STRATEGY_CLASS, BasicCompactionStrategy.class.getName(),
+                    COMPACTION_FILES_BATCH_SIZE, "2"));
+            // A file which we add to all 4 leaf partitions
+            sleeper.sourceFiles().inDataBucket().writeSketches()
+                    .createWithNumberedRecords("file.parquet", LongStream.range(0, 50).map(n -> n * 2));
+            sleeper.ingest().toStateStore().addFileWithRecordEstimatesOnPartitions(
+                    "file.parquet", Map.of(
+                            "LL", 12L,
+                            "LR", 12L,
+                            "RL", 12L,
+                            "RR", 12L));
+            // And a file in each leaf partition
+            sleeper.updateTableProperties(Map.of(INGEST_FILE_WRITING_STRATEGY, ONE_FILE_PER_LEAF.toString()));
+            sleeper.ingest().direct(null).numberedRecords(LongStream.range(0, 50).map(n -> n * 2 + 1));
+
+            // When we run compaction
+            sleeper.compaction().createJobs(4).invokeTasks(1).waitForJobs();
+
+            // Then the same records should be present, in one file on each leaf partition
+            assertThat(sleeper.directQuery().allRecordsInTable())
+                    .containsExactlyInAnyOrderElementsOf(sleeper.generateNumberedRecords(LongStream.range(0, 100)));
+            assertThat(printFiles(sleeper.partitioning().tree(), sleeper.tableFiles().all()))
+                    .isEqualTo(printFiles(partitions, activeAndReadyForGCFiles(
+                            List.of(fileFactory.partitionFile("LL", 25),
+                                    fileFactory.partitionFile("LR", 25),
+                                    fileFactory.partitionFile("RL", 25),
+                                    fileFactory.partitionFile("RR", 25)),
+                            List.of("file.parquet", "ll.parquet", "lr.parquet", "rl.parquet", "rr.parquet"))));
+        }
+
+        @Test
+        void shouldCompactOneFileFromRootIntoExistingFilesOnLeafPartitions(SleeperSystemTest sleeper) throws Exception {
+            // Given a compaction strategy which will always compact two files together
+            sleeper.updateTableProperties(Map.of(
+                    COMPACTION_STRATEGY_CLASS, BasicCompactionStrategy.class.getName(),
+                    COMPACTION_FILES_BATCH_SIZE, "2"));
+            // And a file which we add to the root partition
+            sleeper.sourceFiles().inDataBucket().writeSketches()
+                    .createWithNumberedRecords("file.parquet", LongStream.range(0, 50).map(n -> n * 2));
+            sleeper.ingest().toStateStore().addFileOnPartition("file.parquet", "root", 50);
+            // And a file in each leaf partition
+            sleeper.updateTableProperties(Map.of(INGEST_FILE_WRITING_STRATEGY, ONE_FILE_PER_LEAF.toString()));
+            sleeper.ingest().direct(null).numberedRecords(LongStream.range(0, 50).map(n -> n * 2 + 1));
+
+            // When we split the file from the root partition into separate references in the leaf partitions
+            // And we run compaction
+            sleeper.compaction()
+                    .createJobs(0).createJobs(4) // Split down two levels of the tree
+                    .invokeTasks(1).waitForJobs();
+
+            // Then the same records should be present, in one file on each leaf partition
+            assertThat(sleeper.directQuery().allRecordsInTable())
+                    .containsExactlyInAnyOrderElementsOf(sleeper.generateNumberedRecords(LongStream.range(0, 100)));
+            assertThat(printFiles(sleeper.partitioning().tree(), sleeper.tableFiles().all()))
+                    .isEqualTo(printFiles(partitions, activeAndReadyForGCFiles(
+                            List.of(fileFactory.partitionFile("LL", 25),
+                                    fileFactory.partitionFile("LR", 25),
+                                    fileFactory.partitionFile("RL", 25),
+                                    fileFactory.partitionFile("RR", 25)),
+                            List.of("file.parquet", "ll.parquet", "lr.parquet", "rl.parquet", "rr.parquet"))));
+        }
+    }
 }
