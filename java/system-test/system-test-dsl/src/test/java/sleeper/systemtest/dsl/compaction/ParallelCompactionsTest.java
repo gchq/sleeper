15395dbbd9f53b934b2025989113d027ce676380
patchwork01 <110390516+patchwork01@users.noreply.github.com>
2024-03-13 16:18:46 +0000

    Make ParallelCompactionsTest work

--- java/system-test/system-test-dsl/src/test/java/sleeper/systemtest/dsl/compaction/ParallelCompactionsTest.java
+++ java/system-test/system-test-dsl/src/test/java/sleeper/systemtest/dsl/compaction/ParallelCompactionsTest.java
@@ -16,16 +16,20 @@
 package sleeper.systemtest.dsl.compaction;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import sleeper.compaction.strategy.impl.BasicCompactionStrategy;
+import sleeper.configuration.properties.validation.IngestFileWritingStrategy;
 import sleeper.core.partition.PartitionsBuilder;
+import sleeper.core.record.Record;
 import sleeper.core.schema.Schema;
 import sleeper.systemtest.dsl.SleeperSystemTest;
 import sleeper.systemtest.dsl.testutil.InMemoryDslTest;
 
+import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.stream.IntStream;
 import java.util.stream.LongStream;
 
@@ -33,6 +37,7 @@ import static java.util.stream.Collectors.toUnmodifiableList;
 import static org.assertj.core.api.Assertions.assertThat;
 import static sleeper.configuration.properties.table.TableProperty.COMPACTION_FILES_BATCH_SIZE;
 import static sleeper.configuration.properties.table.TableProperty.COMPACTION_STRATEGY_CLASS;
+import static sleeper.configuration.properties.table.TableProperty.INGEST_FILE_WRITING_STRATEGY;
 import static sleeper.systemtest.dsl.sourcedata.GenerateNumberedValue.addPrefix;
 import static sleeper.systemtest.dsl.sourcedata.GenerateNumberedValue.numberStringAndZeroPadTo;
 import static sleeper.systemtest.dsl.sourcedata.GenerateNumberedValueOverrides.overrideField;
@@ -41,7 +46,6 @@ import static sleeper.systemtest.dsl.testutil.InMemoryTestInstance.PARALLEL_COMP
 import static sleeper.systemtest.dsl.testutil.InMemoryTestInstance.ROW_KEY_FIELD_NAME;
 
 @InMemoryDslTest
-@Disabled("TODO")
 public class ParallelCompactionsTest {
     private final Schema schema = DEFAULT_SCHEMA;
     public static final int NUMBER_OF_COMPACTIONS = 5;
@@ -55,38 +59,45 @@ public class ParallelCompactionsTest {
     void shouldApplyOneCompactionPerPartition(SleeperSystemTest sleeper) {
         // Given we have partitions split evenly across the intended range of records
         sleeper.setGeneratorOverrides(overrideField(ROW_KEY_FIELD_NAME,
-                numberStringAndZeroPadTo(3).then(addPrefix("row-"))));
+                numberStringAndZeroPadTo(4).then(addPrefix("row-"))));
+        List<String> leafIds = IntStream.range(0, NUMBER_OF_COMPACTIONS)
+                .mapToObj(i -> "" + i)
+                .collect(toUnmodifiableList());
+        List<Object> splitPoints = IntStream.range(1, NUMBER_OF_COMPACTIONS)
+                .map(i -> 10000 * i / NUMBER_OF_COMPACTIONS)
+                .mapToObj(i -> "row-" + numberStringAndZeroPadTo(4, i))
+                .collect(toUnmodifiableList());
         sleeper.partitioning().setPartitions(new PartitionsBuilder(schema)
-                .leavesWithSplits(
-                        IntStream.range(0, NUMBER_OF_COMPACTIONS)
-                                .mapToObj(i -> "" + i)
-                                .collect(toUnmodifiableList()),
-                        IntStream.range(1, NUMBER_OF_COMPACTIONS)
-                                .map(i -> 10000 * i / NUMBER_OF_COMPACTIONS)
-                                .mapToObj(i -> numberStringAndZeroPadTo(3, i))
-                                .collect(toUnmodifiableList()))
+                .leavesWithSplits(leafIds, splitPoints)
                 .anyTreeJoiningAllLeaves()
                 .buildTree());
+        // And we configure to compact every partition
+        sleeper.updateTableProperties(Map.of(
+                INGEST_FILE_WRITING_STRATEGY, IngestFileWritingStrategy.ONE_FILE_PER_LEAF.toString(),
+                COMPACTION_STRATEGY_CLASS, BasicCompactionStrategy.class.getName(),
+                COMPACTION_FILES_BATCH_SIZE, "2"));
         // And we have records spread across all partitions in two files per partition
         sleeper.ingest().direct(null)
                 .numberedRecords(LongStream.range(0, 10000))
                 .numberedRecords(LongStream.range(0, 10000));
-        // And we configure to compact every partition
-        sleeper.updateTableProperties(Map.of(
-                COMPACTION_STRATEGY_CLASS, BasicCompactionStrategy.class.getName(),
-                COMPACTION_FILES_BATCH_SIZE, "2"));
 
         // When we run compaction
-        sleeper.compaction().createJobs(NUMBER_OF_COMPACTIONS).waitForJobs();
+        sleeper.compaction().createJobs(NUMBER_OF_COMPACTIONS).invokeTasks(NUMBER_OF_COMPACTIONS).waitForJobs();
 
         // Then we have one output file per compaction
         assertThat(sleeper.tableFiles().references())
                 .hasSize(NUMBER_OF_COMPACTIONS);
         // And we have the same records afterwards
-        assertThat(sleeper.directQuery().allRecordsInTable())
-                .containsExactlyInAnyOrderElementsOf(sleeper.generateNumberedRecords(
+        assertThat(inAnyOrder(sleeper.directQuery().allRecordsInTable()))
+                .isEqualTo(inAnyOrder(sleeper.generateNumberedRecords(
                         LongStream.range(0, 10000)
-                                .flatMap(i -> LongStream.of(i, i))));
+                                .flatMap(i -> LongStream.of(i, i)))));
+    }
+
+    private static Set<Record> inAnyOrder(Iterable<Record> records) {
+        Set<Record> set = new HashSet<>();
+        records.forEach(set::add);
+        return set;
     }
 
 }
