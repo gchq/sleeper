e5ee2870ea93bb181670736a178ab769355e898e
patchwork01 <110390516+patchwork01@users.noreply.github.com>
2024-03-26 14:03:07 +0000

    Tweak formatting in FileIngestRequestTestHelperTest

--- java/ingest/ingest-batcher-core/src/test/java/sleeper/ingest/batcher/testutil/FileIngestRequestTestHelperTest.java
+++ java/ingest/ingest-batcher-core/src/test/java/sleeper/ingest/batcher/testutil/FileIngestRequestTestHelperTest.java
@@ -46,7 +46,8 @@ class FileIngestRequestTestHelperTest {
         assertThat(List.of(
                 helper.fileRequest().build(),
                 helper.fileRequest().build(),
-                helper.fileRequest().build())).containsExactly(
+                helper.fileRequest().build()))
+                .containsExactly(
                         assertHelper.fileRequest().file("test-bucket/auto-named-file-1.parquet").build(),
                         assertHelper.fileRequest().file("test-bucket/auto-named-file-2.parquet").build(),
                         assertHelper.fileRequest().file("test-bucket/auto-named-file-3.parquet").build());
@@ -57,7 +58,8 @@ class FileIngestRequestTestHelperTest {
         assertThat(List.of(
                 helper.fileRequest().build(),
                 helper.fileRequest().build(),
-                helper.fileRequest().build())).containsExactly(
+                helper.fileRequest().build()))
+                .containsExactly(
                         assertHelper.fileRequest().receivedTime(Instant.parse("2023-05-19T15:33:42Z")).build(),
                         assertHelper.fileRequest().receivedTime(Instant.parse("2023-05-19T15:33:43Z")).build(),
                         assertHelper.fileRequest().receivedTime(Instant.parse("2023-05-19T15:33:44Z")).build());
