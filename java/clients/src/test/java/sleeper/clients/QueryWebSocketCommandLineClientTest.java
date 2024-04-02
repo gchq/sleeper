483983d9b2febc0bf1d0a9e4bf4c05901714cb76
kr565370 <112558283+kr565370@users.noreply.github.com>
2024-04-02 10:57:07 +0100

    Ignore exception thrown in QueryWebSocketCommandLineClientTest

--- java/clients/src/test/java/sleeper/clients/QueryWebSocketCommandLineClientTest.java
+++ java/clients/src/test/java/sleeper/clients/QueryWebSocketCommandLineClientTest.java
@@ -39,6 +39,7 @@ import sleeper.query.model.QuerySerDe;
 
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.CompletionException;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static sleeper.clients.QueryClientTestConstants.EXACT_QUERY_OPTION;
@@ -476,11 +477,14 @@ public class QueryWebSocketCommandLineClientTest {
     }
 
     protected void runQueryClient(String queryId, Client webSocketClient) throws Exception {
-        new QueryWebSocketCommandLineClient(instanceProperties, tableIndex, new FixedTablePropertiesProvider(tableProperties),
-                in.consoleIn(), out.consoleOut(), new QueryWebSocketClient(instanceProperties,
-                        new FixedTablePropertiesProvider(tableProperties), out.consoleOut(), webSocketClient),
-                () -> queryId)
-                .run();
+        try {
+            new QueryWebSocketCommandLineClient(instanceProperties, tableIndex, new FixedTablePropertiesProvider(tableProperties),
+                    in.consoleIn(), out.consoleOut(), new QueryWebSocketClient(instanceProperties,
+                            new FixedTablePropertiesProvider(tableProperties), out.consoleOut(), webSocketClient),
+                    () -> queryId)
+                    .run();
+        } catch (CompletionException e) {
+        }
     }
 
     private FakeWebSocketClient withResponses(WebSocketResponse... responses) {
