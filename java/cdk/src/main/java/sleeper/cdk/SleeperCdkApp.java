18c0d28d04db7291b988deef926bf16b9eb33ce4
kr565370 <112558283+kr565370@users.noreply.github.com>
2024-04-08 14:03:56 +0100

    Create alarm for table metrics DLQ

--- java/cdk/src/main/java/sleeper/cdk/SleeperCdkApp.java
+++ java/cdk/src/main/java/sleeper/cdk/SleeperCdkApp.java
@@ -144,7 +144,7 @@ public class SleeperCdkApp extends Stack {
                 new TableIndexStack(this, "TableIndex", instanceProperties, policiesStack),
                 policiesStack, stateStoreStacks, dataStack);
         if (optionalStacks.contains(TableMetricsStack.class.getSimpleName())) {
-            new TableMetricsStack(this, "TableMetrics", instanceProperties, jars, coreStacks);
+            new TableMetricsStack(this, "TableMetrics", instanceProperties, jars, topicStack.getTopic(), coreStacks);
         }
 
         // Stack for Athena analytics
