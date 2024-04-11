99aad48c450d2837e70e2a6929d08491cd14f28f
kr565370 <112558283+kr565370@users.noreply.github.com>
2024-04-11 13:50:59 +0100

    Add missing javadoc for TableIndex

--- java/core/src/main/java/sleeper/core/table/TableIndex.java
+++ java/core/src/main/java/sleeper/core/table/TableIndex.java
@@ -19,18 +19,61 @@ package sleeper.core.table;
 import java.util.Optional;
 import java.util.stream.Stream;
 
+/**
+ * An interface for a class that stores table statuses.
+ */
 public interface TableIndex {
+    /**
+     * Stores a table status.
+     *
+     * @param  table                       the table status to add
+     * @throws TableAlreadyExistsException if the table already exists
+     */
     void create(TableStatus table) throws TableAlreadyExistsException;
 
+    /**
+     * Streams all table statuses.
+     *
+     * @return all table statuses in the store
+     */
     Stream<TableStatus> streamAllTables();
 
+    /**
+     * Streams table statuses that are marked as online.
+     *
+     * @return all online table statuses in the store
+     */
     Stream<TableStatus> streamOnlineTables();
 
+    /**
+     * Gets a table by the table name.
+     *
+     * @param  tableName the table name
+     * @return           the table status with the provided table name, or an empty optional if no table exists with
+     *                   that name
+     */
     Optional<TableStatus> getTableByName(String tableName);
 
+    /**
+     * Gets a table by the table ID.
+     *
+     * @param  tableUniqueId the table ID
+     * @return               the table status with the provided table ID, or an empty optional if no table exists with
+     *                       that ID
+     */
     Optional<TableStatus> getTableByUniqueId(String tableUniqueId);
 
+    /**
+     * Removes a table status from the store.
+     *
+     * @param table the table status to remove
+     */
     void delete(TableStatus table);
 
+    /**
+     * Updates a table status in the store.
+     *
+     * @param table the updated table status
+     */
     void update(TableStatus table);
 }
