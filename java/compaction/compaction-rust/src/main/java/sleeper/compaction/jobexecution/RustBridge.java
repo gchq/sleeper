4e848cb1507c2b0cd3d0bd62a0b1bb8315c83e80
m09526 <m09526@users.noreply.github.com>
2024-04-18 15:49:13 +0000

    Updated struct for Java bridge

--- java/compaction/compaction-rust/src/main/java/sleeper/compaction/jobexecution/RustBridge.java
+++ java/compaction/compaction-rust/src/main/java/sleeper/compaction/jobexecution/RustBridge.java
@@ -16,10 +16,10 @@
 package sleeper.compaction.jobexecution;
 
 import jnr.ffi.LibraryLoader;
+import jnr.ffi.NativeType;
 import jnr.ffi.Struct;
 import jnr.ffi.annotations.In;
 import jnr.ffi.annotations.Out;
-import jnr.ffi.types.size_t;
 import org.scijava.nativelib.JniExtractor;
 import org.scijava.nativelib.NativeLoader;
 import org.slf4j.Logger;
@@ -27,6 +27,8 @@ import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Objects;
 
 public class RustBridge {
     /**
@@ -37,9 +39,9 @@ public class RustBridge {
 
     /** Paths in the JAR file where a native library may have been placed. */
     private static final String[] LIB_PATHS = {"natives/release",
-            "natives/x86_64-unknown-linux-gnu/release", "natives/aarch64-unknown-linux-gnu/release",
-            // Rust debug builds will place libraries in different locations
-            "natives/x86_64-unknown-linux-gnu/debug", "natives/aarch64-unknown-linux-gnu/debug" };
+        "natives/x86_64-unknown-linux-gnu/release", "natives/aarch64-unknown-linux-gnu/release",
+        // Rust debug builds will place libraries in different locations
+        "natives/x86_64-unknown-linux-gnu/debug", "natives/aarch64-unknown-linux-gnu/debug"};
 
     private static final Logger LOGGER = LoggerFactory.getLogger(RustBridge.class);
 
@@ -54,7 +56,7 @@ public class RustBridge {
      * the wrong CPU architecture, loading will fail and the next path will be tried. This way, we
      * maintain a single JAR file that can work across multiple CPU architectures.
      *
-     * @return the native compaction object
+     * @return             the native compaction object
      * @throws IOException if an error occurs during loading or linking the native library
      */
     public static synchronized Compaction getRustCompactor() throws IOException {
@@ -87,10 +89,10 @@ public class RustBridge {
      * The library named should be given without platform prefixes, e.g. "foo" will be expanded into
      * "libfoo.so" or "foo.dll" as appropriate for this platform.
      *
-     * @param clazz the Java interface type for the native library
-     * @param libName the library name to extract without platform prefixes.
-     * @return the absolute extracted path, or null if the library couldn't be found
-     * @throws IOException if an error occured during file extraction
+     * @param  clazz                the Java interface type for the native library
+     * @param  libName              the library name to extract without platform prefixes.
+     * @return                      the absolute extracted path, or null if the library couldn't be found
+     * @throws IOException          if an error occured during file extraction
      * @throws UnsatisfiedLinkError if the library could not be found or loaded
      */
     public static <T> T extractAndLink(Class<T> clazz, String libName) throws IOException {
@@ -119,9 +121,201 @@ public class RustBridge {
     }
 
     /**
+     * The compaction input data that will be populated from the Java side.
+     */
+    public static class FFICompactionParams extends Struct {
+        /** Array of input files to compact. */
+        public final Array<String> input_files = new Array<>(this);
+        /** Output file name. */
+        public final Struct.UTF8StringRef output_file = new Struct.UTF8StringRef();
+        /** Names of Sleeper row key columns from schema. */
+        public final Array<String> row_key_cols = new Array<>(this);
+        /** Names of Sleeper sort key columns from schema. */
+        public final Array<String> sort_key_cols = new Array<>(this);
+        /** Maximum size of output Parquet row group in bytes. */
+        public final Struct.size_t max_row_group_size = new Struct.size_t();
+        /** Maximum size of output Parquet page size. */
+        public final Struct.size_t max_page_size = new Struct.size_t();
+        /** Output Parquet compression codec. */
+        public final Struct.UTF8StringRef compression = new Struct.UTF8StringRef();
+        /** Output Parquet writer version. Must be 1.0 or 2.0 */
+        public final Struct.UTF8StringRef writer_version = new Struct.UTF8StringRef();
+        /** Column min/max values truncation length in output Parquet. */
+        public final Struct.size_t column_truncate_length = new Struct.size_t();
+        /** Max sizeof statistics block in output Parquet. */
+        public final Struct.size_t stats_truncate_length = new Struct.size_t();
+        /** Should row key columns use dictionary encoding in output Parquet. */
+        public final Struct.Boolean dict_enc_row_keys = new Struct.Boolean();
+        /** Should sort key columns use dictionary encoding in output Parquet. */
+        public final Struct.Boolean dict_enc_sort_keys = new Struct.Boolean();
+        /** Should value columns use dictionary encoding in output Parquet. */
+        public final Struct.Boolean dict_enc_values = new Struct.Boolean();
+        /** Compaction partition region minimums. MUST BE SAME LENGTH AS row_key_cols. */
+        public final Array<Object> region_mins = new Array<>(this);
+        /** Compaction partition region maximums. MUST BE SAME LENGTH AS row_key_cols. */
+        public final Array<Object> region_maxs = new Array<>(this);
+        /** Compaction partition region minimums are inclusive? MUST BE SAME LENGTH AS row_key_cols. */
+        public final Array<Boolean> region_mins_inclusive = new Array<>(this);
+        /** Compaction partition region maximums are inclusive? MUST BE SAME LENGTH AS row_key_cols. */
+        public final Array<Boolean> region_maxs_inclusive = new Array<>(this);
+
+        public FFICompactionParams(jnr.ffi.Runtime runtime) {
+            super(runtime);
+        }
+
+        /**
+         * Validate state of struct.
+         * 
+         * @throws IllegalStateException when a invariant fails
+         */
+        public void validate() {
+            input_files.validate();
+            row_key_cols.validate();
+            sort_key_cols.validate();
+            region_mins.validate();
+            region_maxs.validate();
+            region_mins_inclusive.validate();
+            region_maxs_inclusive.validate();
+            // Check lengths
+            long rowKeys = row_key_cols.len.get();
+            if (rowKeys != region_maxs.len.get()) {
+                throw new IllegalStateException("region maxs has length " + region_maxs.len.get() + " but there are " + rowKeys + " row key columns");
+            }
+            if (rowKeys != region_mins.len.get()) {
+                throw new IllegalStateException("region mins has length " + region_mins.len.get() + " but there are " + rowKeys + " row key columns");
+            }
+            if (rowKeys != region_mins_inclusive.len.get()) {
+                throw new IllegalStateException("region mins inclusives has length " + region_mins_inclusive.len.get() + " but there are " + rowKeys + " row key columns");
+            }
+            if (rowKeys != region_maxs_inclusive.len.get()) {
+                throw new IllegalStateException("region maxs inclusives has length " + region_maxs_inclusive.len.get() + " but there are " + rowKeys + " row key columns");
+            }
+        }
+    }
+
+    /**
+     * Array class that can be inside a Struct. Creates a dynamic array that can be passed to C.
+     * Strong references are maintained for allocated memory so GC will dispose of memory when
+     * this object is collected.
+     */
+    public static class Array<T> {
+        // Length of array
+        public final Struct.size_t len;
+        // Pointer to base of dynamically allocated array
+        public final Struct.Pointer arrayBase;
+        // Reference to dynamically allocated array to prevent GC until Array instance is collected
+        public jnr.ffi.Pointer basePtr;
+        // Reference to dynamically allocated items to prevent GC until Array instance is collected
+        public jnr.ffi.Pointer[] items;
+
+        public Array(Struct enclosing) {
+            this.len = enclosing.new size_t();
+            this.arrayBase = enclosing.new Pointer();
+        }
+
+        /**
+         * Create a dynamic array of items in this array.
+         * 
+         * A base pointer is allocated pointers set to other
+         * dynamically allocated memory containing items from array.
+         * 
+         * @param arr
+         */
+        public void populate(final T[] arr) {
+            final jnr.ffi.Runtime r = len.struct().getRuntime();
+            // Calculate size needed for array of pointers
+            int ptrSize = r.findType(NativeType.ADDRESS).size();
+            int size = arr.length * ptrSize;
+            // Allocate some memory for string pointers
+            this.basePtr = r.getMemoryManager().allocateDirect(size);
+            this.arrayBase.set(basePtr);
+
+            this.items = new jnr.ffi.Pointer[arr.length];
+
+            for (int i = 0; i < arr.length; i++) {
+                setValue(arr[i], i, r);
+            }
+
+            // Bulk set the pointers in the base array
+            this.basePtr.put(0, this.items, 0, this.items.length);
+
+            // Set length of array in struct
+            this.len.set(arr.length);
+        }
+
+        /**
+         * Check class invariants.
+         * 
+         * @throws IllegalStateException if a violation is found
+         */
+        public void validate() {
+            if (len.get() == 0) {
+                if (basePtr != null || items != null) {
+                    throw new IllegalStateException("array length is 0 but pointers not null");
+                }
+            } else {
+                if (len.get() != items.length) {
+                    throw new IllegalStateException("length of " + len.get() + " doesn't match items length of " + items.length);
+                }
+                Objects.requireNonNull(this.basePtr, "base pointer is null");
+                Objects.requireNonNull(this.items, "items array is null");
+                if (this.arrayBase.get().address() != this.basePtr.address()) {
+                    throw new IllegalStateException("array base pointer and stored base pointer differ!");
+                }
+                for (jnr.ffi.Pointer p : items) {
+                    Objects.requireNonNull(p, "NULL pointer found in pointer array store");
+                }
+            }
+        }
+
+        /**
+         * Sets a given value in the array to a specific value. The data
+         * is byte encoded.
+         * 
+         * Intended for internal use only.
+         * 
+         * @param  <E>                       item type, must be int, long String or byte[], or boolean
+         * @param  item                      the item to encode
+         * @param  idx                       array position to use
+         * @param  r                         struct runtime
+         * @throws ClassCastException        if item is of wrong class
+         * @throws IndexOutOfBoundsException if idx is invalid
+         */
+        protected <E> void setValue(E item, int idx, jnr.ffi.Runtime r) {
+            if (item instanceof Integer) {
+                int e = (int) item;
+                this.items[idx] = r.getMemoryManager().allocateDirect(r.findType(NativeType.SINT).size());
+                this.items[idx].putInt(0, e);
+            } else if (item instanceof Long) {
+                long e = (long) item;
+                this.items[idx] = r.getMemoryManager().allocateDirect(r.findType(NativeType.SLONGLONG).size());
+                this.items[idx].putLong(0, e);
+            } else if (item instanceof String) {
+                String e = (String) item;
+                byte[] bytes = e.getBytes(StandardCharsets.UTF_8);
+                // Add one for NULL terminator
+                int stringSize = bytes.length + 1;
+                // Allocate memory for string and set it
+                this.items[idx] = r.getMemoryManager().allocateDirect(stringSize);
+                this.items[idx].putString(0, e, stringSize, StandardCharsets.UTF_8);
+            } else if (item instanceof byte[]) {
+                byte[] e = (byte[]) item;
+                this.items[idx] = r.getMemoryManager().allocateDirect(e.length);
+                this.items[idx].put(0, e, 0, e.length);
+            } else if (item instanceof Boolean) {
+                boolean e = (boolean) item;
+                this.items[idx] = r.getMemoryManager().allocateDirect(1);
+                this.items[idx].putByte(0, e ? (byte) 1 : (byte) 0);
+            } else {
+                throw new ClassCastException("Can't cast " + item.getClass() + " to a valid Sleeper row key type");
+            }
+        }
+    }
+
+    /**
      * The compaction output data that the native code will populate.
      */
-    @SuppressWarnings(value = { "checkstyle:membername", "checkstyle:parametername" })
+    @SuppressWarnings(value = {"checkstyle:membername", "checkstyle:parametername"})
     public static class FFICompactionResult extends Struct {
         public final Struct.size_t rows_read = new Struct.size_t();
         public final Struct.size_t rows_written = new Struct.size_t();
@@ -134,17 +328,13 @@ public class RustBridge {
     /**
      * The interface for the native library we are calling.
      */
-    public interface Compaction {
+    public static interface Compaction {
         FFICompactionResult allocate_result();
 
         void free_result(@In FFICompactionResult res);
 
         @SuppressWarnings(value = "checkstyle:parametername")
-        int ffi_merge_sorted_files(@In String[] input_file_paths, @size_t long input_file_paths_len,
-                String output_file_path, @size_t long row_group_size, @size_t long max_page_size,
-                @In long[] row_key_columns, @size_t long row_key_columns_len,
-                @In long[] sort_columns, @size_t long sort_columns_len,
-                @Out FFICompactionResult result);
+        int ffi_merge_sorted_files(@In FFICompactionParams input, @Out FFICompactionResult result);
     }
 
     private RustBridge() {
