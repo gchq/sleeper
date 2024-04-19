9951f6447b6aadb66c355573363d949a2f37d28b
kr565370 <112558283+kr565370@users.noreply.github.com>
2024-04-19 16:06:16 +0100

    Remove use of TransactionLogStateStoreTestBase

--- java/statestore/src/test/java/sleeper/statestore/transactionlog/DynamoDBTransactionLogSnapshotStoreIT.java
+++ java/statestore/src/test/java/sleeper/statestore/transactionlog/DynamoDBTransactionLogSnapshotStoreIT.java
@@ -15,22 +15,41 @@
  */
 package sleeper.statestore.transactionlog;
 
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
+import sleeper.configuration.properties.instance.InstanceProperties;
 import sleeper.configuration.properties.table.TableProperties;
 import sleeper.core.schema.Schema;
+import sleeper.dynamodb.tools.DynamoDBContainer;
 
 import java.time.Instant;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static sleeper.configuration.properties.InstancePropertiesTestHelper.createTestInstanceProperties;
 import static sleeper.configuration.properties.table.TablePropertiesTestHelper.createTestTableProperties;
 import static sleeper.core.schema.SchemaTestHelper.schemaWithKey;
+import static sleeper.dynamodb.tools.GenericContainerAwsV1ClientHelper.buildAwsV1Client;
 
-public class DynamoDBTransactionLogSnapshotStoreIT extends TransactionLogStateStoreTestBase {
+@Testcontainers
+public class DynamoDBTransactionLogSnapshotStoreIT {
+    @Container
+    private static DynamoDBContainer dynamoDb = new DynamoDBContainer();
+    private static AmazonDynamoDB dynamoDBClient;
+    private final InstanceProperties instanceProperties = createTestInstanceProperties();
     private final Schema schema = schemaWithKey("key");
     private final TableProperties tableProperties = createTestTableProperties(instanceProperties, schema);
-    private TransactionLogSnapshotStore snapshotStore = snapshotStore();
+    private TransactionLogSnapshotStore store = snapshotStore();
+
+    @BeforeAll
+    public static void initDynamoClient() {
+        dynamoDBClient = buildAwsV1Client(dynamoDb, dynamoDb.getDynamoPort(), AmazonDynamoDBClientBuilder.standard());
+    }
 
     @BeforeEach
     void setup() {
@@ -40,22 +59,22 @@ public class DynamoDBTransactionLogSnapshotStoreIT extends TransactionLogStateSt
     @Test
     void shouldStoreFilesSnapshot() throws Exception {
         // Given / When
-        snapshotStore.saveFiles("snapshot/1-files.parquet", 1);
+        store.saveFiles("snapshot/1-files.parquet", 1);
 
         // Then
-        assertThat(snapshotStore.getFilesSnapshots())
+        assertThat(store.getFilesSnapshots())
                 .containsExactly(TransactionLogSnapshot.forFiles("snapshot/1-files.parquet", 1));
-        assertThat(snapshotStore.getPartitionsSnapshots()).isEmpty();
+        assertThat(store.getPartitionsSnapshots()).isEmpty();
     }
 
     @Test
     void shouldStorePartitionsSnapshot() throws Exception {
         // Given / When
-        snapshotStore.savePartitions("snapshot/1-partitions.parquet", 1);
+        store.savePartitions("snapshot/1-partitions.parquet", 1);
 
         // Then
-        assertThat(snapshotStore.getFilesSnapshots()).isEmpty();
-        assertThat(snapshotStore.getPartitionsSnapshots())
+        assertThat(store.getFilesSnapshots()).isEmpty();
+        assertThat(store.getPartitionsSnapshots())
                 .containsExactly(TransactionLogSnapshot.forPartitions("snapshot/1-partitions.parquet", 1));
     }
 
