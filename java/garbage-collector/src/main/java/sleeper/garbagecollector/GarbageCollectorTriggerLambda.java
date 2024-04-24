6cb4c8835b26b0b79c93fbae4096edff42197095
patchwork01 <110390516+patchwork01@users.noreply.github.com>
2024-04-24 14:01:22 +0100

    Use PropertiesReloader in GarbageCollectorTriggerLambda

--- java/garbage-collector/src/main/java/sleeper/garbagecollector/GarbageCollectorTriggerLambda.java
+++ java/garbage-collector/src/main/java/sleeper/garbagecollector/GarbageCollectorTriggerLambda.java
@@ -29,6 +29,7 @@ import com.amazonaws.services.sqs.model.SendMessageBatchRequestEntry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import sleeper.configuration.properties.PropertiesReloader;
 import sleeper.configuration.properties.instance.InstanceProperties;
 import sleeper.configuration.table.index.DynamoDBTableIndex;
 import sleeper.core.table.TableIndex;
@@ -51,24 +52,27 @@ import static sleeper.configuration.properties.instance.GarbageCollectionPropert
 public class GarbageCollectorTriggerLambda implements RequestHandler<ScheduledEvent, Void> {
     private static final Logger LOGGER = LoggerFactory.getLogger(GarbageCollectorTriggerLambda.class);
 
-    private final InstanceProperties instanceProperties = new InstanceProperties();
-    private final TableIndex tableIndex;
+    private final AmazonDynamoDB dynamoClient = AmazonDynamoDBClientBuilder.defaultClient();
     private final AmazonSQS sqsClient = AmazonSQSClientBuilder.defaultClient();
+    private final InstanceProperties instanceProperties = new InstanceProperties();
+    private final PropertiesReloader propertiesReloader;
 
     public GarbageCollectorTriggerLambda() {
         String configBucketName = System.getenv(CONFIG_BUCKET.toEnvironmentVariable());
         AmazonS3 s3Client = AmazonS3ClientBuilder.defaultClient();
-        AmazonDynamoDB dynamoClient = AmazonDynamoDBClientBuilder.defaultClient();
         instanceProperties.loadFromS3(s3Client, configBucketName);
-        tableIndex = new DynamoDBTableIndex(instanceProperties, dynamoClient);
+        propertiesReloader = PropertiesReloader.ifConfigured(s3Client, instanceProperties);
     }
 
     @Override
     public Void handleRequest(ScheduledEvent event, Context context) {
         Instant startTime = Instant.now();
         LOGGER.info("Lambda triggered at {}, started at {}", event.getTime(), startTime);
+        propertiesReloader.reloadIfNeeded();
+
         String queueUrl = instanceProperties.get(GARBAGE_COLLECTOR_QUEUE_URL);
         boolean offlineEnabled = instanceProperties.getBoolean(GARBAGE_COLLECT_OFFLINE_TABLES);
+        TableIndex tableIndex = new DynamoDBTableIndex(instanceProperties, dynamoClient);
         SplitIntoBatches.reusingListOfSize(10,
                 offlineEnabled ? tableIndex.streamAllTables() : tableIndex.streamOnlineTables(),
                 tables -> sendMessageBatch(tables, queueUrl));
