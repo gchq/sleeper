661b57faa6a4e36ccb77a50d6ee21ad66ba3acdf
patchwork01 <110390516+patchwork01@users.noreply.github.com>
2024-04-26 10:21:39 +0100

    Comment explaining maximum batch size

--- java/common/common-invoke-tables/src/main/java/sleeper/invoke/tables/InvokeForTables.java
+++ java/common/common-invoke-tables/src/main/java/sleeper/invoke/tables/InvokeForTables.java
@@ -36,6 +36,7 @@ public class InvokeForTables {
     }
 
     public static void sendOneMessagePerTable(AmazonSQS sqsClient, String queueUrl, Stream<TableStatus> tables) {
+        // Limit to stay under the maximum size for an SQS sendMessageBatch call.
         SplitIntoBatches.reusingListOfSize(10, tables,
                 batch -> sendMessageBatch(sqsClient, queueUrl, batch));
     }
