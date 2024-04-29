8e9ab0098f08396b7d92887030437f6b7863b2bc
kr565370 <112558283+kr565370@users.noreply.github.com>
2024-04-29 14:39:36 +0100

    Initialise configuration in field declaration

--- java/statestore/src/test/java/sleeper/statestore/transactionlog/TransactionLogStateStoreTestBase.java
+++ java/statestore/src/test/java/sleeper/statestore/transactionlog/TransactionLogStateStoreTestBase.java
@@ -46,7 +46,7 @@ public class TransactionLogStateStoreTestBase {
     protected static AmazonDynamoDB dynamoDBClient;
     protected static AmazonS3 s3Client;
     protected final InstanceProperties instanceProperties = createTestInstanceProperties();
-    protected Configuration configuration;
+    protected final Configuration configuration = HadoopConfigurationLocalStackUtils.getHadoopConfiguration(localStackContainer);
 
     @BeforeAll
     public static void initDynamoClient() {
@@ -62,7 +62,6 @@ public class TransactionLogStateStoreTestBase {
     @BeforeEach
     void setUpBase() {
         new TransactionLogStateStoreCreator(instanceProperties, dynamoDBClient, s3Client).create();
-        configuration = HadoopConfigurationLocalStackUtils.getHadoopConfiguration(localStackContainer);
     }
 
     public StateStore createStateStore(TableProperties tableProperties) {
