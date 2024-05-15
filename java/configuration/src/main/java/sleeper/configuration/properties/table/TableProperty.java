290ff254b9e9c141604d96ee4393c0e88d1737b1
kr565370 <112558283+kr565370@users.noreply.github.com>
2024-05-15 11:29:39 +0100

    Update property description

--- java/configuration/src/main/java/sleeper/configuration/properties/table/TableProperty.java
+++ java/configuration/src/main/java/sleeper/configuration/properties/table/TableProperty.java
@@ -239,7 +239,7 @@ public interface TableProperty extends SleeperProperty {
     TableProperty STATESTORE_CLASSNAME = Index.propertyBuilder("sleeper.table.statestore.classname")
             .defaultValue("sleeper.statestore.transactionlog.DynamoDBTransactionLogStateStore")
             .description("The name of the class used for the metadata store. " +
-                    "The default is S3StateStore. Options are:\n" +
+                    "The default is DynamoDBTransactionLogStateStore. Options are:\n" +
                     "sleeper.statestore.transactionlog.DynamoDBTransactionLogStateStore\n" +
                     "sleeper.statestore.transactionlog.DynamoDBTransactionLogStateStoreNoSnapshots\n" +
                     "sleeper.statestore.s3.S3StateStore\n" +
