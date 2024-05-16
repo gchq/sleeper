318c6914c5431427a0d4d11cfea95b143253448a
patchwork01 <110390516+patchwork01@users.noreply.github.com>
2024-05-16 13:02:47 +0100

    Split grant for config bucket

--- java/cdk/src/main/java/sleeper/cdk/stack/ConfigBucketStack.java
+++ java/cdk/src/main/java/sleeper/cdk/stack/ConfigBucketStack.java
@@ -57,7 +57,8 @@ public class ConfigBucketStack extends NestedStack {
         configBucket.grantRead(policiesStack.getDirectIngestPolicyForGrants());
         configBucket.grantRead(policiesStack.getIngestByQueuePolicyForGrants());
         configBucket.grantReadWrite(policiesStack.getEditTablesPolicyForGrants());
-        configBucket.grantReadWrite(policiesStack.getClearInstancePolicyForGrants());
+        configBucket.grantRead(policiesStack.getClearInstancePolicyForGrants());
+        configBucket.grantDelete(policiesStack.getClearInstancePolicyForGrants());
 
         Utils.addStackTagIfSet(this, instanceProperties);
     }
