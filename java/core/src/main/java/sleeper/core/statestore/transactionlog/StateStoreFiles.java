95d741edf9659126fe584a798d9cc377d2f06c8b
patchwork01 <110390516+patchwork01@users.noreply.github.com>
2024-05-22 12:49:41 +0100

    Note that StateStoreFiles is not thread safe

--- java/core/src/main/java/sleeper/core/statestore/transactionlog/StateStoreFiles.java
+++ java/core/src/main/java/sleeper/core/statestore/transactionlog/StateStoreFiles.java
@@ -30,7 +30,8 @@ import static java.util.stream.Collectors.toUnmodifiableList;
 
 /**
  * Holds the state of file references in a state store backed by a transaction log. This object is mutable, is cached in
- * memory in the state store, and is updated by applying each transaction in the log in sequence.
+ * memory in the state store, and is updated by applying each transaction in the log in sequence. This is not thread
+ * safe.
  * <p>
  * The methods to update this object should only ever be called by the transactions.
  */
