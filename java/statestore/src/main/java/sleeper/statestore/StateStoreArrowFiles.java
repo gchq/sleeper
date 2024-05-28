38488c3e7cd4c4dd595281bbed5ec5ca6810b402
patchwork01 <110390516+patchwork01@users.noreply.github.com>
2024-05-28 14:03:35 +0100

    Set up write to Arrow vector

--- java/statestore/src/main/java/sleeper/statestore/StateStoreArrowFiles.java
+++ java/statestore/src/main/java/sleeper/statestore/StateStoreArrowFiles.java
@@ -17,6 +17,7 @@ package sleeper.statestore;
 
 import org.apache.arrow.memory.BufferAllocator;
 import org.apache.arrow.vector.VectorSchemaRoot;
+import org.apache.arrow.vector.ipc.ArrowStreamWriter;
 import org.apache.arrow.vector.types.Types;
 import org.apache.arrow.vector.types.pojo.ArrowType.Utf8;
 import org.apache.arrow.vector.types.pojo.Field;
@@ -24,6 +25,7 @@ import org.apache.arrow.vector.types.pojo.Schema;
 
 import sleeper.core.statestore.AllReferencesToAFile;
 
+import java.io.IOException;
 import java.nio.channels.ReadableByteChannel;
 import java.nio.channels.WritableByteChannel;
 import java.util.Collection;
@@ -32,13 +34,22 @@ import java.util.stream.Stream;
 
 public class StateStoreArrowFiles {
 
-    private static final Schema FILES_SCHEMA = createFilesSchema();
+    private static final Field FILENAME = Field.notNullable("filename", Utf8.INSTANCE);
+    private static final Field UPDATE_TIME = Field.notNullable("updateTime", Types.MinorType.TIMESTAMPMILLI.getType());
+    private static final Schema FILES_SCHEMA = new Schema(List.of(FILENAME, UPDATE_TIME));
 
     private StateStoreArrowFiles() {
     }
 
-    public static void write(Collection<AllReferencesToAFile> files, BufferAllocator allocator, WritableByteChannel channel) {
-        try (VectorSchemaRoot smallBatchVectorSchemaRoot = VectorSchemaRoot.create(FILES_SCHEMA, allocator)) {
+    public static void write(Collection<AllReferencesToAFile> files, BufferAllocator allocator, WritableByteChannel channel) throws IOException {
+        try (VectorSchemaRoot vectorSchemaRoot = VectorSchemaRoot.create(FILES_SCHEMA, allocator);
+                ArrowStreamWriter writer = new ArrowStreamWriter(vectorSchemaRoot, null, channel)) {
+            vectorSchemaRoot.getFieldVectors().forEach(fieldVector -> fieldVector.setInitialCapacity(files.size()));
+            vectorSchemaRoot.allocateNew();
+            writer.start();
+            for (AllReferencesToAFile file : files) {
+                vectorSchemaRoot.getVector(FILENAME);
+            }
 
         }
     }
@@ -46,11 +57,5 @@ public class StateStoreArrowFiles {
     public static Stream<AllReferencesToAFile> read(ReadableByteChannel channel) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'read'");
-    }
-
-    private static Schema createFilesSchema() {
-        return new Schema(List.of(
-                Field.notNullable("filename", Utf8.INSTANCE),
-                Field.notNullable("updateTime", Types.MinorType.TIMESTAMPMILLI.getType())));
     }
 }
