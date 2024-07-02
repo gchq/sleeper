b2503de1ca8ca15c4c08a57600ac14ef1610b489
kr565370 <112558283+kr565370@users.noreply.github.com>
2024-07-02 15:27:22 +0100

    Use IngestFactory to create ingest coordinator in direct write backed by arrow

--- java/configuration/src/test/java/sleeper/configuration/properties/InstancePropertiesTestHelper.java
+++ java/configuration/src/test/java/sleeper/configuration/properties/InstancePropertiesTestHelper.java
@@ -50,6 +50,7 @@ import static sleeper.configuration.properties.instance.CommonProperty.SUBNETS;
 import static sleeper.configuration.properties.instance.CommonProperty.VPC_ID;
 import static sleeper.configuration.properties.instance.DefaultProperty.DEFAULT_BULK_IMPORT_FILES_COMMIT_ASYNC;
 import static sleeper.configuration.properties.instance.DefaultProperty.DEFAULT_MIN_TRANSACTIONS_AHEAD_TO_LOAD_SNAPSHOT;
+import static sleeper.configuration.properties.instance.IngestProperty.INGEST_PARTITION_REFRESH_PERIOD_IN_SECONDS;
 import static sleeper.configuration.properties.instance.InstanceProperties.getConfigBucketFromInstanceId;
 
 public class InstancePropertiesTestHelper {
@@ -102,6 +103,7 @@ public class InstancePropertiesTestHelper {
         instanceProperties.setNumber(MAXIMUM_CONNECTIONS_TO_S3, 5);
         instanceProperties.setNumber(DEFAULT_MIN_TRANSACTIONS_AHEAD_TO_LOAD_SNAPSHOT, 1);
         instanceProperties.set(DEFAULT_BULK_IMPORT_FILES_COMMIT_ASYNC, "false");
+        instanceProperties.setNumber(INGEST_PARTITION_REFRESH_PERIOD_IN_SECONDS, Integer.MAX_VALUE);
         return instanceProperties;
     }
 
--- java/ingest/ingest-runner/src/test/java/sleeper/ingest/testutils/IngestCoordinatorFactory.java
+++ java/ingest/ingest-runner/src/test/java/sleeper/ingest/testutils/IngestCoordinatorFactory.java
@@ -16,7 +16,11 @@
 
 package sleeper.ingest.testutils;
 
+import sleeper.configuration.jars.ObjectFactory;
+import sleeper.configuration.properties.instance.InstanceProperties;
+import sleeper.configuration.properties.table.TableProperties;
 import sleeper.core.record.Record;
+import sleeper.ingest.IngestFactory;
 import sleeper.ingest.impl.IngestCoordinator;
 import sleeper.ingest.impl.ParquetConfiguration;
 import sleeper.ingest.impl.partitionfilewriter.AsyncS3PartitionFileWriterFactory;
@@ -25,9 +29,14 @@ import sleeper.ingest.impl.recordbatch.arraylist.ArrayListRecordBatchFactory;
 import sleeper.ingest.impl.recordbatch.arrow.ArrowRecordBatchFactory;
 import sleeper.ingest.impl.recordbatch.arrow.ArrowRecordWriter;
 import sleeper.ingest.impl.recordbatch.arrow.ArrowRecordWriterAcceptingRecords;
+import sleeper.statestore.FixedStateStoreProvider;
 
 import java.util.function.Consumer;
 
+import static sleeper.configuration.properties.InstancePropertiesTestHelper.createTestInstanceProperties;
+import static sleeper.configuration.properties.table.TablePropertiesTestHelper.createTestTableProperties;
+import static sleeper.configuration.properties.table.TableProperty.INGEST_FILE_WRITING_STRATEGY;
+import static sleeper.configuration.properties.table.TableProperty.ITERATOR_CLASS_NAME;
 import static sleeper.ingest.testutils.IngestCoordinatorTestHelper.parquetConfiguration;
 import static sleeper.ingest.testutils.IngestCoordinatorTestHelper.standardIngestCoordinatorBuilder;
 
@@ -51,9 +60,21 @@ public class IngestCoordinatorFactory {
                 .recordWriter(recordWriter)
                 .localWorkingDirectory(parameters.getWorkingDir());
         arrowConfig.accept(arrowConfigBuilder);
-        return standardIngestCoordinatorBuilder(parameters,
-                arrowConfigBuilder.build(),
-                DirectPartitionFileWriterFactory.from(
+        InstanceProperties instanceProperties = createTestInstanceProperties();
+        TableProperties table = createTestTableProperties(instanceProperties, parameters.getSchema());
+        table.set(ITERATOR_CLASS_NAME, parameters.getIteratorClassName());
+        table.set(INGEST_FILE_WRITING_STRATEGY, parameters.getIngestFileWritingStrategy().toString());
+        IngestFactory factory = IngestFactory.builder()
+                .instanceProperties(instanceProperties)
+                .hadoopConfiguration(parameters.getHadoopConfiguration())
+                .localDir(parameters.getWorkingDir())
+                .objectFactory(ObjectFactory.noUserJars())
+                .s3AsyncClient(parameters.getS3AsyncClient())
+                .stateStoreProvider(new FixedStateStoreProvider(table, parameters.getStateStore()))
+                .build();
+        return factory.ingestCoordinatorBuilder(table)
+                .recordBatchFactory(arrowConfigBuilder.build())
+                .partitionFileWriterFactory(DirectPartitionFileWriterFactory.from(
                         parquetConfiguration, filePathPrefix,
                         parameters.getFileNameGenerator()))
                 .build();
@@ -65,7 +86,8 @@ public class IngestCoordinatorFactory {
         }, new ArrowRecordWriterAcceptingRecords());
     }
 
-    public static IngestCoordinator<Record> ingestCoordinatorAsyncWriteBackedByArrow(IngestCoordinatorTestParameters parameters) {
+    public static IngestCoordinator<Record> ingestCoordinatorAsyncWriteBackedByArrow(
+            IngestCoordinatorTestParameters parameters) {
         try {
             ParquetConfiguration parquetConfiguration = parquetConfiguration(parameters);
             return standardIngestCoordinatorBuilder(parameters,
@@ -103,7 +125,8 @@ public class IngestCoordinatorFactory {
             Consumer<ArrayListRecordBatchFactory.Builder<Record>> arrowConfig) {
         try {
             ParquetConfiguration parquetConfiguration = parquetConfiguration(parameters);
-            ArrayListRecordBatchFactory.Builder<Record> arrayListRecordBatch = (ArrayListRecordBatchFactory.Builder<Record>) ArrayListRecordBatchFactory.builder()
+            ArrayListRecordBatchFactory.Builder<Record> arrayListRecordBatch = (ArrayListRecordBatchFactory.Builder<Record>) ArrayListRecordBatchFactory
+                    .builder()
                     .parquetConfiguration(parquetConfiguration)
                     .maxNoOfRecordsInLocalStore(1000)
                     .maxNoOfRecordsInMemory(100000)
