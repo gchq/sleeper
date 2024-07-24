71b078c657331f623ce717949c976d37fb0bf5c5
kr565370 <112558283+kr565370@users.noreply.github.com>
2024-07-24 14:30:32 +0100

    Set ingest async property to false in tests

--- java/ingest/ingest-runner/src/test/java/sleeper/ingest/job/IngestJobRunnerIT.java
+++ java/ingest/ingest-runner/src/test/java/sleeper/ingest/job/IngestJobRunnerIT.java
@@ -394,6 +394,7 @@ class IngestJobRunnerIT {
         TableProperties tableProperties = new TableProperties(instanceProperties);
         tableProperties.set(TABLE_NAME, tableName);
         tableProperties.set(TABLE_ID, tableId);
+        tableProperties.set(INGEST_FILES_COMMIT_ASYNC, "false");
         tableProperties.setSchema(schema);
         return tableProperties;
     }
