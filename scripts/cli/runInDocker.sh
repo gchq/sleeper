c97c6f9ba3d06b71dcaf6863e82d7ecfefcc454a
patchwork01 <110390516+patchwork01@users.noreply.github.com>
2024-08-01 13:17:18 +0000

    Set Docker group ID in CLI

--- scripts/cli/runInDocker.sh
+++ scripts/cli/runInDocker.sh
@@ -72,8 +72,13 @@ build_temp_runner_image() {
   # Propagate current user IDs to image, to avoid mixed file ownership
   local SET_UID=$(id -u)
   local SET_GID=$(id -g)
+  local SET_DOCKER_GID=$(getent group docker | cut -d: -f3)
   TEMP_RUNNER_IMAGE="sleeper-runner:$TEMP_TAG"
-  docker build "$RUNNER_PATH" -t "$TEMP_RUNNER_IMAGE" --build-arg RUN_IMAGE="$RUN_IMAGE" --build-arg SET_UID=$SET_UID --build-arg SET_GID=$SET_GID &> /dev/null
+  docker build "$RUNNER_PATH" -t "$TEMP_RUNNER_IMAGE" \
+    --build-arg RUN_IMAGE="$RUN_IMAGE" \
+    --build-arg SET_UID=$SET_UID \
+    --build-arg SET_GID=$SET_GID \
+    --build-arg SET_DOCKER_GID=$SET_DOCKER_GID &> /dev/null
 }
 
 run_in_environment_docker() {
