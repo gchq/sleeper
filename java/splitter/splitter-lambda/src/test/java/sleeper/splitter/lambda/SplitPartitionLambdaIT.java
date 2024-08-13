c79e301f575e04dca24da9bb2ec02df6ca7024fd
patchwork01 <110390516+patchwork01@users.noreply.github.com>
2024-08-13 14:43:44 +0000

    Set up integration test

--- java/splitter/splitter-lambda/src/test/java/sleeper/splitter/lambda/SplitPartitionLambdaIT.java
+++ java/splitter/splitter-lambda/src/test/java/sleeper/splitter/lambda/SplitPartitionLambdaIT.java
@@ -19,44 +19,99 @@ import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-import org.junit.jupiter.api.BeforeEach;
+import com.amazonaws.services.sqs.AmazonSQS;
+import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
+import org.apache.hadoop.conf.Configuration;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
 import org.testcontainers.containers.localstack.LocalStackContainer;
+import org.testcontainers.containers.localstack.LocalStackContainer.Service;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
+import software.amazon.awssdk.services.s3.S3AsyncClient;
 
+import sleeper.configuration.jars.ObjectFactory;
 import sleeper.configuration.properties.instance.InstanceProperties;
+import sleeper.configuration.properties.table.S3TableProperties;
+import sleeper.configuration.properties.table.TableProperties;
+import sleeper.configuration.properties.table.TablePropertiesProvider;
 import sleeper.configuration.table.index.DynamoDBTableIndexCreator;
 import sleeper.core.CommonTestConstants;
+import sleeper.core.partition.PartitionTree;
+import sleeper.core.partition.PartitionsBuilder;
+import sleeper.core.record.Record;
+import sleeper.core.schema.Schema;
+import sleeper.core.schema.type.IntType;
+import sleeper.core.statestore.FileReference;
+import sleeper.core.statestore.StateStore;
+import sleeper.core.statestore.StateStoreException;
+import sleeper.ingest.IngestFactory;
+import sleeper.ingest.IngestResult;
+import sleeper.io.parquet.utils.HadoopConfigurationLocalStackUtils;
+import sleeper.splitter.find.SplitPartitionJobDefinition;
+import sleeper.splitter.find.SplitPartitionJobDefinitionSerDe;
+import sleeper.statestore.StateStoreProvider;
 import sleeper.statestore.transactionlog.TransactionLogStateStoreCreator;
 
+import java.nio.file.Path;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toUnmodifiableList;
+import static org.assertj.core.api.Assertions.assertThat;
 import static sleeper.configuration.properties.InstancePropertiesTestHelper.createTestInstanceProperties;
 import static sleeper.configuration.properties.instance.CdkDefinedInstanceProperty.CONFIG_BUCKET;
 import static sleeper.configuration.properties.instance.CdkDefinedInstanceProperty.DATA_BUCKET;
+import static sleeper.configuration.properties.table.TablePropertiesTestHelper.createTestTableProperties;
+import static sleeper.configuration.properties.table.TableProperty.TABLE_ID;
 import static sleeper.configuration.testutils.LocalStackAwsV1ClientHelper.buildAwsV1Client;
+import static sleeper.core.schema.SchemaTestHelper.schemaWithKey;
+import static sleeper.ingest.testutils.LocalStackAwsV2ClientHelper.buildAwsV2Client;
+import static sleeper.io.parquet.utils.HadoopConfigurationLocalStackUtils.getHadoopConfiguration;
 
 @Testcontainers
 public class SplitPartitionLambdaIT {
 
     @Container
     public static LocalStackContainer localStackContainer = new LocalStackContainer(DockerImageName.parse(CommonTestConstants.LOCALSTACK_DOCKER_IMAGE))
-            .withServices(LocalStackContainer.Service.S3, LocalStackContainer.Service.DYNAMODB);
+            .withServices(Service.S3, Service.DYNAMODB, Service.SQS);
 
-    private final AmazonDynamoDB dynamoDB = buildAwsV1Client(localStackContainer, LocalStackContainer.Service.DYNAMODB, AmazonDynamoDBClientBuilder.standard());
-    private final AmazonS3 s3 = buildAwsV1Client(localStackContainer, LocalStackContainer.Service.S3, AmazonS3ClientBuilder.standard());
+    private final AmazonDynamoDB dynamoDB = buildAwsV1Client(localStackContainer, Service.DYNAMODB, AmazonDynamoDBClientBuilder.standard());
+    private final AmazonS3 s3 = buildAwsV1Client(localStackContainer, Service.S3, AmazonS3ClientBuilder.standard());
+    private final AmazonSQS sqs = buildAwsV1Client(localStackContainer, Service.SQS, AmazonSQSClientBuilder.standard());
     private final InstanceProperties instanceProperties = createInstance();
+    private final Schema schema = schemaWithKey("key", new IntType());
+    private final PartitionTree partitionTree = new PartitionsBuilder(schema).singlePartition("root").buildTree();
+    private final TableProperties tableProperties = createTable(schema, partitionTree);
+    private final Configuration conf = getHadoopConfiguration(localStackContainer);
+    private final SplitPartitionLambda lambda = new SplitPartitionLambda(instanceProperties, conf, s3, dynamoDB, sqs);
+    private final SplitPartitionJobDefinitionSerDe serDe = new SplitPartitionJobDefinitionSerDe(tablePropertiesProvider());
 
-    @BeforeEach
-    void setUp() {
-
-    }
+    @TempDir
+    private Path tempDir;
 
     @Test
-    @Disabled
-    void shouldSendAsyncCommitRequest() {
+    @Disabled("TODO")
+    void shouldCommitToStateStoreDirectly() throws Exception {
+        // Given
+        List<String> filenames = ingestRecordsGetFilenames(IntStream.rangeClosed(1, 100)
+                .mapToObj(i -> new Record(Map.of("key", i))));
 
+        // When
+        lambda.splitPartitionFromJson(serDe.toJson(new SplitPartitionJobDefinition(
+                tableProperties.get(TABLE_ID),
+                partitionTree.getRootPartition(), filenames)));
+
+        // Then
+        assertThat(stateStore().getAllPartitions()).containsExactlyInAnyOrderElementsOf(
+                new PartitionsBuilder(schema)
+                        .rootFirst("root")
+                        .splitToNewChildren("root", "L", "R", 51)
+                        .buildList());
     }
 
     private InstanceProperties createInstance() {
@@ -67,5 +122,44 @@ public class SplitPartitionLambdaIT {
         DynamoDBTableIndexCreator.create(dynamoDB, instanceProperties);
         new TransactionLogStateStoreCreator(instanceProperties, dynamoDB).create();
         return instanceProperties;
+    }
+
+    private TableProperties createTable(Schema schema, PartitionTree partitionTree) {
+        TableProperties tableProperties = createTestTableProperties(instanceProperties, schema);
+        S3TableProperties.getStore(instanceProperties, s3, dynamoDB).createTable(tableProperties);
+        try {
+            stateStoreProvider().getStateStore(tableProperties)
+                    .initialise(partitionTree.getAllPartitions());
+        } catch (StateStoreException e) {
+            throw new RuntimeException(e);
+        }
+        return tableProperties;
+    }
+
+    private TablePropertiesProvider tablePropertiesProvider() {
+        return new TablePropertiesProvider(instanceProperties, s3, dynamoDB);
+    }
+
+    private StateStore stateStore() {
+        return stateStoreProvider().getStateStore(tableProperties);
+    }
+
+    private StateStoreProvider stateStoreProvider() {
+        return new StateStoreProvider(instanceProperties, s3, dynamoDB,
+                HadoopConfigurationLocalStackUtils.getHadoopConfiguration(localStackContainer));
+    }
+
+    private List<String> ingestRecordsGetFilenames(Stream<Record> records) throws Exception {
+        IngestResult result = IngestFactory.builder()
+                .objectFactory(ObjectFactory.noUserJars())
+                .localDir(tempDir.toString())
+                .stateStoreProvider(stateStoreProvider())
+                .instanceProperties(instanceProperties)
+                .hadoopConfiguration(conf)
+                .s3AsyncClient(buildAwsV2Client(localStackContainer, Service.S3, S3AsyncClient.builder()))
+                .build().ingestFromRecordIterator(tableProperties, records.iterator());
+        return result.getFileReferenceList().stream()
+                .map(FileReference::getFilename)
+                .collect(toUnmodifiableList());
     }
 }
