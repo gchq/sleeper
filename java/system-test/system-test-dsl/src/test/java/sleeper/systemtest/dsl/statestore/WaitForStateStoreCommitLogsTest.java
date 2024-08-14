11c5aea292943d5e83499af6d2e2fafd5a23eedf
patchwork01 <110390516+patchwork01@users.noreply.github.com>
2024-08-14 14:04:01 +0000

    Rename WaitForStateStoreCommitLogsTest

--- java/system-test/system-test-dsl/src/test/java/sleeper/systemtest/dsl/statestore/WaitForStateStoreCommitLogsTest.java
+++ java/system-test/system-test-dsl/src/test/java/sleeper/systemtest/dsl/statestore/WaitForStateStoreCommitLogsTest.java
@@ -23,7 +23,7 @@ import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class WaitForStateStoreCommitsTest {
+public class WaitForStateStoreCommitLogsTest {
 
     @Test
     void shouldFindOneCommitWasMadeAgainstCorrectTable() {
