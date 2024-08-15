aef91f9cac9d069ce5934b7adb02886f5c52e905
patchwork01 <110390516+patchwork01@users.noreply.github.com>
2024-08-15 12:03:06 +0000

    Test indexing runs by table ID

--- java/clients/src/test/java/sleeper/clients/status/report/statestore/StateStoreCommitterRunsFromEntriesTest.java
+++ java/clients/src/test/java/sleeper/clients/status/report/statestore/StateStoreCommitterRunsFromEntriesTest.java
@@ -23,7 +23,7 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class StateStoreCommitterRunsTest {
+public class StateStoreCommitterRunsFromEntriesTest {
 
     private List<StateStoreCommitterLogEntry> logs = new ArrayList<>();
 
