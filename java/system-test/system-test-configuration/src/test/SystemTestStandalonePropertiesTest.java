cd0d4d7ff7b1f45925b4a8cf9fb9a425dda9c161
rtjd6554 <174791724+rtjd6554@users.noreply.github.com>
2024-09-16 13:10:09 +0100

    New tests for SystemTestStandaoneProperties

---
+++ java/system-test/system-test-configuration/src/test/SystemTestStandalonePropertiesTest.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2022-2024 Crown Copyright
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import org.junit.jupiter.api.Test;
+
+import sleeper.systemtest.configuration.SystemTestStandaloneProperties;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static sleeper.configuration.properties.InstancePropertiesTestHelper.createTestInstanceProperties;
+import static sleeper.systemtest.configuration.SystemTestProperty.SYSTEM_TEST_BUCKET_NAME;
+import static sleeper.systemtest.configuration.SystemTestProperty.SYSTEM_TEST_CLUSTER_ENABLED;
+
+class SystemTestStandalonePropertiesTest {
+
+    @Test
+    void shouldPassWithValidSystemTestStandaloneProperties() {
+        // Given
+        SystemTestStandaloneProperties properties = validStandaloneProperties();
+
+        // When / Then
+        assertThatCode(properties::validate)
+                .doesNotThrowAnyException();
+    }
+
+    @Test
+    void shouldReturnNullWhenStandalonePropertyWithDefaultNotSet() {
+        // Given
+        SystemTestStandaloneProperties properties = validStandaloneProperties();
+
+        // When / Then
+        assertThat(properties.get(SYSTEM_TEST_BUCKET_NAME)).isNull();
+    }
+
+    @Test
+    void shouldReturnDefaultValueForPropertyWhenValueNotSet() {
+        // Given
+        SystemTestStandaloneProperties properties = validStandaloneProperties();
+
+        // When
+        properties.set(SYSTEM_TEST_CLUSTER_ENABLED, null);
+
+        // Then
+        assertThat(properties.get()).isEqualTo();
+    }
+
+    private SystemTestStandaloneProperties validStandaloneProperties() {
+        SystemTestStandaloneProperties properties = new SystemTestStandaloneProperties(createTestInstanceProperties().getProperties());
+        properties.setNumber(NUMBER_OF_RECORDS_PER_INGEST, 12);
+        return properties;
+    }
+}
