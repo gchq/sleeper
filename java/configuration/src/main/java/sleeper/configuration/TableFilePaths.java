e96b2bfbf4dd34f637c8ef1a66b283fad011f5f5
patchwork01 <110390516+patchwork01@users.noreply.github.com>
2024-09-24 08:56:34 +0000

    Javadoc TableFilePaths

--- java/configuration/src/main/java/sleeper/configuration/TableFilePaths.java
+++ java/configuration/src/main/java/sleeper/configuration/TableFilePaths.java
@@ -24,6 +24,9 @@ import static sleeper.configuration.properties.instance.CdkDefinedInstanceProper
 import static sleeper.configuration.properties.instance.CommonProperty.FILE_SYSTEM;
 import static sleeper.configuration.properties.table.TableProperty.TABLE_ID;
 
+/**
+ * Generator for paths to Sleeper table data files in the data bucket.
+ */
 public class TableFilePaths {
 
     private final String filePathPrefix;
@@ -32,22 +35,61 @@ public class TableFilePaths {
         this.filePathPrefix = filePathPrefix;
     }
 
+    /**
+     * Creates an instance of this class with the given prefix for paths. Paths will be generated assuming that this
+     * prefix puts you at the base of the data bucket.
+     *
+     * @param  prefix the path prefix
+     * @return        an instance of this class
+     */
     public static TableFilePaths fromPrefix(String prefix) {
         return new TableFilePaths(prefix);
     }
 
+    /**
+     * Creates an instance of this class to generate full paths as they will be held in references to files in the state
+     * store.
+     *
+     * @param  instanceProperties the instance properties
+     * @param  tableProperties    the table properties
+     * @return                    an instance of this class
+     */
     public static TableFilePaths buildDataFilePathPrefix(InstanceProperties instanceProperties, TableProperties tableProperties) {
         return new TableFilePaths(instanceProperties.get(FILE_SYSTEM) + instanceProperties.get(DATA_BUCKET) + "/" + tableProperties.get(TABLE_ID));
     }
 
+    /**
+     * Generates a path to a data file in the given partition. Will have `.parquet` appended at the end. The filename
+     * must match the filename used for the corresponding sketches file.
+     *
+     * @param  partition the partition
+     * @param  fileName  the filename without file type
+     * @return           the file path
+     */
     public String constructPartitionParquetFilePath(Partition partition, String fileName) {
         return constructPartitionParquetFilePath(partition.getId(), fileName);
     }
 
+    /**
+     * Generates a path to a data file in the given partition. Will have `.parquet` appended at the end. The filename
+     * must match the filename used for the corresponding sketches file.
+     *
+     * @param  partitionId the partition ID
+     * @param  fileName    the filename without file type
+     * @return             the file path
+     */
     public String constructPartitionParquetFilePath(String partitionId, String fileName) {
         return String.format("%s/data/partition_%s/%s.parquet", filePathPrefix, partitionId, fileName);
     }
 
+    /**
+     * Generates a path to a sketches file in the given partition. Will have `.sketches` appended at the end. The
+     * filename must match the filename used for the corresponding data file.
+     *
+     * @param  partition the partition
+     * @param  fileName  the filename without file type
+     * @return           the file path
+     */
     public String constructQuantileSketchesFilePath(Partition partition, String fileName) {
         return String.format("%s/data/partition_%s/%s.sketches", filePathPrefix, partition.getId(), fileName);
     }
