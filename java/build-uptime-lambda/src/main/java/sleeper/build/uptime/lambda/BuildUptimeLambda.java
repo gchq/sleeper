041bcd626847dad76080dbe089d26e3d7ed3305e
patchwork01 <110390516+patchwork01@users.noreply.github.com>
2024-10-04 09:25:57 +0000

    Set up build-uptime-lambda module

---
+++ java/build-uptime-lambda/src/main/java/sleeper/build/uptime/lambda/BuildUptimeLambda.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2022-2024 Crown Copyright
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package sleeper.build.uptime.lambda;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.ScheduledEvent;
+
+public class BuildUptimeLambda implements RequestHandler<ScheduledEvent, Void> {
+
+    @Override
+    public Void handleRequest(ScheduledEvent event, Context context) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'handleRequest'");
+    }
+
+}
