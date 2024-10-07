37a9bf1f86dd6c32f1e25c87f51a418464165dd7
patchwork01 <110390516+patchwork01@users.noreply.github.com>
2024-10-07 10:03:47 +0000

    Upgrade Ubuntu in build EC2

--- java/cdk-environment/src/main/java/sleeper/environment/cdk/config/AppParameters.java
+++ java/cdk-environment/src/main/java/sleeper/environment/cdk/config/AppParameters.java
@@ -28,7 +28,7 @@ public class AppParameters {
     public static final StringParameter BUILD_FORK = StringParameter.keyAndDefault("fork", "gchq");
     public static final StringParameter BUILD_BRANCH = StringParameter.keyAndDefault("branch", "develop");
 
-    public static final StringParameter BUILD_IMAGE_NAME = StringParameter.keyAndDefault("buildImageName", "ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*");
+    public static final StringParameter BUILD_IMAGE_NAME = StringParameter.keyAndDefault("buildImageName", "ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*");
     public static final StringParameter BUILD_IMAGE_OWNER = StringParameter.keyAndDefault("buildImageOwner", "099720109477");
     public static final StringParameter BUILD_IMAGE_LOGIN_USER = StringParameter.keyAndDefault("buildImageLoginUser", "ubuntu");
     public static final StringParameter BUILD_IMAGE_ROOT_DEVICE_NAME = StringParameter.keyAndDefault("buildImageRootDeviceName", "/dev/sda1");
