83d70760d1b3b9058e2899aa7ad20a630348bf9c
patchwork01 <110390516+patchwork01@users.noreply.github.com>
2024-10-29 14:23:20 +0000

    Refactor should_merge_two_files

--- rust/compaction/tests/compaction_test.rs
+++ rust/compaction/tests/compaction_test.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashMap, fs::File, path::PathBuf, sync::Arc};
+use std::{collections::HashMap, fs::File, sync::Arc};
 
 use arrow::{
     array::{Int32Array, RecordBatch},
@@ -10,25 +10,22 @@ use datafusion::parquet::{
     basic::Compression,
     file::properties::WriterProperties,
 };
-use tempfile::tempdir;
+use tempfile::{tempdir, TempDir};
 use url::Url;
 
 #[tokio::test]
 async fn should_merge_two_files() {
     // Given
     let dir = tempdir().unwrap();
-    let file_1 = dir.path().join("file1.parquet");
-    let file_2 = dir.path().join("file2.parquet");
-    let output = dir.path().join("output.parquet");
+    let file_1 = file(&dir, "file1.parquet");
+    let file_2 = file(&dir, "file2.parquet");
+    let output = file(&dir, "output.parquet");
     write_file_of_ints(&file_1, "key", Int32Array::from(vec![1, 3]));
     write_file_of_ints(&file_2, "key", Int32Array::from(vec![2, 4]));
 
     let input = CompactionInput {
-        input_files: Vec::from([
-            Url::from_file_path(file_1).unwrap(),
-            Url::from_file_path(file_2).unwrap(),
-        ]),
-        output_file: Url::from_file_path(&output).unwrap(),
+        input_files: Vec::from([file_1, file_2]),
+        output_file: output.clone(),
         row_key_cols: Vec::from(["key".to_string()]),
         region: HashMap::from([(
             "key".to_string(),
@@ -47,13 +44,17 @@ async fn should_merge_two_files() {
 
     // Then
     assert_eq!(
-        read_file_of_ints(&output),
+        read_file_of_ints(&output, "key"),
         Int32Array::from(vec![1, 2, 3, 4])
     );
     assert_eq!([result.rows_read, result.rows_written], [4, 4]);
 }
 
-fn write_file_of_ints(file: &PathBuf, field_name: impl Into<String>, data: Int32Array) {
+fn file(dir: &TempDir, name: impl Into<String>) -> Url {
+    Url::from_file_path(dir.path().join(name.into())).unwrap()
+}
+
+fn write_file_of_ints(path: &Url, field_name: &str, data: Int32Array) {
     let schema = Schema::new(vec![Field::new(field_name, DataType::Int32, false)]);
     let batch = RecordBatch::try_new(Arc::new(schema), vec![Arc::new(data)]).unwrap();
     let props = WriterProperties::builder()
@@ -63,15 +64,14 @@ fn write_file_of_ints(file: &PathBuf, field_name: impl Into<String>, data: Int32
         .set_statistics_truncate_length(Some(2147483647))
         .set_dictionary_enabled(true)
         .build();
-    let f = File::create_new(file).unwrap();
-
-    let mut writer = ArrowWriter::try_new(f, batch.schema(), Some(props)).unwrap();
+    let file = File::create_new(path.path()).unwrap();
+    let mut writer = ArrowWriter::try_new(file, batch.schema(), Some(props)).unwrap();
     writer.write(&batch).expect("Writing batch");
     writer.close().unwrap();
 }
 
-fn read_file_of_ints(path: &PathBuf) -> Int32Array {
-    let file = File::open(path).unwrap();
+fn read_file_of_ints(path: &Url, field_name: &str) -> Int32Array {
+    let file = File::open(path.path()).unwrap();
     let batch = ArrowReaderBuilder::try_new(file)
         .unwrap()
         .build()
@@ -80,7 +80,8 @@ fn read_file_of_ints(path: &PathBuf) -> Int32Array {
         .unwrap()
         .unwrap();
     batch
-        .column(0)
+        .column_by_name(field_name)
+        .unwrap()
         .as_any()
         .downcast_ref::<Int32Array>()
         .unwrap()
