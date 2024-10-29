e764b0e80695ca2795f8ad2d495f22e28995246f
patchwork01 <110390516+patchwork01@users.noreply.github.com>
2024-10-29 15:22:48 +0000

    Avoid unwraps in tests

--- rust/compaction/tests/compaction_test.rs
+++ rust/compaction/tests/compaction_test.rs
@@ -4,6 +4,7 @@ use arrow::{
     array::{Int32Array, RecordBatch},
     datatypes::{DataType, Field, Schema},
 };
+use color_eyre::eyre::Error;
 use compaction::{merge_sorted_files, ColRange, CompactionInput, PartitionBound};
 use datafusion::parquet::{
     arrow::{arrow_reader::ArrowReaderBuilder, ArrowWriter},
@@ -14,14 +15,14 @@ use tempfile::{tempdir, TempDir};
 use url::Url;
 
 #[tokio::test]
-async fn should_merge_two_files() {
+async fn should_merge_two_files() -> Result<(), Error> {
     // Given
-    let dir = tempdir().unwrap();
+    let dir = tempdir()?;
     let file_1 = file(&dir, "file1.parquet");
     let file_2 = file(&dir, "file2.parquet");
     let output = file(&dir, "output.parquet");
-    write_file_of_ints(&file_1, "key", Int32Array::from(vec![1, 3]));
-    write_file_of_ints(&file_2, "key", Int32Array::from(vec![2, 4]));
+    write_file_of_ints(&file_1, "key", Int32Array::from(vec![1, 3]))?;
+    write_file_of_ints(&file_2, "key", Int32Array::from(vec![2, 4]))?;
 
     let input = CompactionInput {
         input_files: Vec::from([file_1, file_2]),
@@ -32,23 +33,24 @@ async fn should_merge_two_files() {
     };
 
     // When
-    let result = merge_sorted_files(&input).await.unwrap();
+    let result = merge_sorted_files(&input).await?;
 
     // Then
     assert_eq!(
-        read_file_of_ints(&output, "key"),
+        read_file_of_ints(&output, "key")?,
         Int32Array::from(vec![1, 2, 3, 4])
     );
     assert_eq!([result.rows_read, result.rows_written], [4, 4]);
+    Ok(())
 }
 
 fn file(dir: &TempDir, name: impl Into<String>) -> Url {
     Url::from_file_path(dir.path().join(name.into())).unwrap()
 }
 
-fn write_file_of_ints(path: &Url, field_name: &str, data: Int32Array) {
+fn write_file_of_ints(path: &Url, field_name: &str, data: Int32Array) -> Result<(), Error> {
     let schema = Schema::new(vec![Field::new(field_name, DataType::Int32, false)]);
-    let batch = RecordBatch::try_new(Arc::new(schema), vec![Arc::new(data)]).unwrap();
+    let batch = RecordBatch::try_new(Arc::new(schema), vec![Arc::new(data)])?;
     let props = WriterProperties::builder()
         .set_writer_version(datafusion::parquet::file::properties::WriterVersion::PARQUET_2_0)
         .set_compression(Compression::ZSTD(Default::default()))
@@ -56,28 +58,26 @@ fn write_file_of_ints(path: &Url, field_name: &str, data: Int32Array) {
         .set_statistics_truncate_length(Some(2147483647))
         .set_dictionary_enabled(true)
         .build();
-    let file = File::create_new(path.path()).unwrap();
-    let mut writer = ArrowWriter::try_new(file, batch.schema(), Some(props)).unwrap();
-    writer.write(&batch).expect("Writing batch");
-    writer.close().unwrap();
+    let file = File::create_new(path.path())?;
+    let mut writer = ArrowWriter::try_new(file, batch.schema(), Some(props))?;
+    writer.write(&batch)?;
+    writer.close()?;
+    Ok(())
 }
 
-fn read_file_of_ints(path: &Url, field_name: &str) -> Int32Array {
-    let file = File::open(path.path()).unwrap();
-    let batch = ArrowReaderBuilder::try_new(file)
-        .unwrap()
-        .build()
-        .unwrap()
+fn read_file_of_ints(path: &Url, field_name: &str) -> Result<Int32Array, Error> {
+    let file = File::open(path.path())?;
+    let batch = ArrowReaderBuilder::try_new(file)?
+        .build()?
         .next()
-        .unwrap()
-        .unwrap();
-    batch
+        .ok_or_else(|| Error::msg("no record batch found"))??;
+    Ok(batch
         .column_by_name(field_name)
-        .unwrap()
+        .ok_or_else(|| Error::msg("field not found"))?
         .as_any()
         .downcast_ref::<Int32Array>()
-        .unwrap()
-        .to_owned()
+        .ok_or_else(|| Error::msg("could not read field as an integer"))?
+        .to_owned())
 }
 
 fn single_int_range(field_name: &str, min: i32, max: i32) -> HashMap<String, ColRange<'_>> {
