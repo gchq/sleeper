115ba889bcbb964bdcbc0728cd87bc4550c96f68
patchwork01 <110390516+patchwork01@users.noreply.github.com>
2024-10-30 11:33:05 +0000

    Test multidimensional region

--- rust/compaction/tests/compaction_test.rs
+++ rust/compaction/tests/compaction_test.rs
@@ -27,7 +27,7 @@ async fn should_merge_two_files() -> Result<(), Error> {
     let input = CompactionInput {
         input_files: Vec::from([file_1, file_2]),
         output_file: output.clone(),
-        row_key_cols: Vec::from([String::from("key")]),
+        row_key_cols: row_key_cols(["key"]),
         region: single_int_range("key", 0, 5),
         ..Default::default()
     };
@@ -54,7 +54,7 @@ async fn should_merge_files_with_overlapping_data() -> Result<(), Error> {
     let input = CompactionInput {
         input_files: Vec::from([file_1, file_2]),
         output_file: output.clone(),
-        row_key_cols: Vec::from([String::from("key")]),
+        row_key_cols: row_key_cols(["key"]),
         region: single_int_range("key", 0, 5),
         ..Default::default()
     };
@@ -81,7 +81,7 @@ async fn should_exclude_data_not_in_region() -> Result<(), Error> {
     let input = CompactionInput {
         input_files: Vec::from([file_1, file_2]),
         output_file: output.clone(),
-        row_key_cols: Vec::from([String::from("key")]),
+        row_key_cols: row_key_cols(["key"]),
         region: single_int_range("key", 2, 4),
         ..Default::default()
     };
@@ -95,8 +95,63 @@ async fn should_exclude_data_not_in_region() -> Result<(), Error> {
     Ok(())
 }
 
+#[tokio::test]
+async fn should_exclude_data_not_in_multidimensional_region() -> Result<(), Error> {
+    // Given
+    let dir = tempdir()?;
+    let file_1 = file(&dir, "file1.parquet");
+    let file_2 = file(&dir, "file2.parquet");
+    let output = file(&dir, "output.parquet");
+    let schema = Arc::new(Schema::new(vec![
+        Field::new("key1", DataType::Int32, false),
+        Field::new("key2", DataType::Int32, false),
+    ]));
+    let data_1 = RecordBatch::try_new(
+        schema.clone(),
+        vec![
+            Arc::new(Int32Array::from(vec![1, 2, 3])),
+            Arc::new(Int32Array::from(vec![11, 12, 13])),
+        ],
+    )?;
+    let data_2 = RecordBatch::try_new(
+        schema,
+        vec![
+            Arc::new(Int32Array::from(vec![2, 3, 4])),
+            Arc::new(Int32Array::from(vec![22, 23, 24])),
+        ],
+    )?;
+    write_file(&file_1, data_1)?;
+    write_file(&file_2, data_2)?;
+
+    let input = CompactionInput {
+        input_files: Vec::from([file_1, file_2]),
+        output_file: output.clone(),
+        row_key_cols: row_key_cols(["key1", "key2"]),
+        region: HashMap::from([
+            region_entry("key1", int_range(2, 4)),
+            region_entry("key2", int_range(13, 23)),
+        ]),
+        ..Default::default()
+    };
+
+    // When
+    let result = merge_sorted_files(&input).await?;
+
+    // Then
+    assert_eq!(
+        read_file_of_int_fields(&output, ["key1", "key2"])?,
+        vec![[2, 22], [3, 13]]
+    );
+    assert_eq!([result.rows_read, result.rows_written], [2, 2]);
+    Ok(())
+}
+
 fn file(dir: &TempDir, name: &str) -> Url {
     Url::from_file_path(dir.path().join(name)).unwrap()
+}
+
+fn row_key_cols<const N: usize>(names: [&str; N]) -> Vec<String> {
+    names.into_iter().map(String::from).collect()
 }
 
 fn write_file_of_ints(path: &Url, field_name: &str, data: Int32Array) -> Result<(), Error> {
@@ -129,20 +184,59 @@ fn read_file_of_ints(path: &Url, field_name: &str) -> Result<Vec<i32>, Error> {
     let file = File::open(path.path())?;
     let mut data: Vec<i32> = Vec::new();
     for result in ArrowReaderBuilder::try_new(file)?.build()? {
-        let batch = result?;
-        let batch_data = batch
-            .column_by_name(field_name)
-            .ok_or_else(|| Error::msg("field not found"))?
-            .as_any()
-            .downcast_ref::<Int32Array>()
-            .ok_or_else(|| Error::msg("could not read field as an integer"))?;
-        data.extend(batch_data.values());
+        data.extend(get_int_array(field_name, &result?)?.values());
     }
     Ok(data)
 }
 
+fn read_file_of_int_fields<const N: usize>(
+    path: &Url,
+    field_names: [&str; N],
+) -> Result<Vec<[i32; N]>, Error> {
+    let file = File::open(path.path())?;
+    let mut data: Vec<[i32; N]> = Vec::new();
+    for result in ArrowReaderBuilder::try_new(file)?.build()? {
+        let batch = result?;
+        let arrays: Vec<&Int32Array> = get_int_arrays(&batch, field_names)?;
+        data.extend((0..batch.num_rows()).map(|row_number| read_row(row_number, &arrays)));
+    }
+    Ok(data)
+}
+
+fn get_int_arrays<'b, const N: usize>(
+    batch: &'b RecordBatch,
+    field_names: [&str; N],
+) -> Result<Vec<&'b Int32Array>, Error> {
+    let result: Result<Vec<&Int32Array>, Error> = field_names
+        .iter()
+        .map(|field_name| get_int_array(field_name, &batch))
+        .collect();
+    Ok(result?)
+}
+
+fn read_row<const N: usize>(row_number: usize, arrays: &Vec<&Int32Array>) -> [i32; N] {
+    let mut row = [0; N];
+    for i in 0..N {
+        row[i] = arrays.get(i).unwrap().value(row_number);
+    }
+    row
+}
+
+fn get_int_array<'b>(field_name: &str, batch: &'b RecordBatch) -> Result<&'b Int32Array, Error> {
+    Ok(batch
+        .column_by_name(field_name)
+        .ok_or_else(|| Error::msg("field not found"))?
+        .as_any()
+        .downcast_ref::<Int32Array>()
+        .ok_or_else(|| Error::msg("could not read field as an integer"))?)
+}
+
 fn single_int_range(field_name: &str, min: i32, max: i32) -> HashMap<String, ColRange<'_>> {
-    return HashMap::from([(String::from(field_name), int_range(min, max))]);
+    HashMap::from([region_entry(field_name, int_range(min, max))])
+}
+
+fn region_entry<'r>(field_name: &str, range: ColRange<'r>) -> (String, ColRange<'r>) {
+    (String::from(field_name), range)
 }
 
 fn int_range<'r>(min: i32, max: i32) -> ColRange<'r> {
