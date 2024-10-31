8455afc1c17d5df2cb50ff4ce2982e640845bc1d
patchwork01 <110390516+patchwork01@users.noreply.github.com>
2024-10-31 13:13:41 +0000

    Add CompactionDataFusionPerformanceST

--- java/system-test/system-test-suite/src/test/java/sleeper/systemtest/suite/CompactionDataFusionPerformanceST.java
+++ java/system-test/system-test-suite/src/test/java/sleeper/systemtest/suite/CompactionDataFusionPerformanceST.java
@@ -13,13 +13,13 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package sleeper.systemtest.suite;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import sleeper.core.properties.validation.CompactionMethod;
 import sleeper.core.util.PollWithRetries;
 import sleeper.systemtest.dsl.SleeperSystemTest;
 import sleeper.systemtest.dsl.extension.AfterTestReports;
@@ -28,22 +28,24 @@ import sleeper.systemtest.suite.testutil.Expensive;
 import sleeper.systemtest.suite.testutil.SystemTest;
 
 import java.time.Duration;
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static sleeper.core.properties.table.TableProperty.COMPACTION_METHOD;
 import static sleeper.systemtest.configuration.SystemTestIngestMode.DIRECT;
 import static sleeper.systemtest.configuration.SystemTestProperty.INGEST_MODE;
 import static sleeper.systemtest.configuration.SystemTestProperty.NUMBER_OF_RECORDS_PER_INGEST;
 import static sleeper.systemtest.configuration.SystemTestProperty.NUMBER_OF_WRITERS;
-import static sleeper.systemtest.suite.fixtures.SystemTestInstance.COMPACTION_PERFORMANCE;
+import static sleeper.systemtest.suite.fixtures.SystemTestInstance.COMPACTION_ON_DATAFUSION;
 import static sleeper.systemtest.suite.testutil.FileReferenceSystemTestHelper.numberOfRecordsIn;
 
 @SystemTest
 @Expensive // Expensive because it takes a long time to compact this many records on fairly large ECS instances.
-public class CompactionPerformanceST {
+public class CompactionDataFusionPerformanceST {
 
     @BeforeEach
     void setUp(SleeperSystemTest sleeper, AfterTestReports reporting) {
-        sleeper.connectToInstance(COMPACTION_PERFORMANCE);
+        sleeper.connectToInstance(COMPACTION_ON_DATAFUSION);
         reporting.reportAlways(SystemTestReports.SystemTestBuilder::compactionTasksAndJobs);
     }
 
@@ -53,8 +55,9 @@ public class CompactionPerformanceST {
     }
 
     @Test
-    void shouldMeetCompactionPerformanceStandards(SleeperSystemTest sleeper) {
+    void shouldMeetCompactionPerformanceStandardsWithDataFusion(SleeperSystemTest sleeper) {
         // Given
+        sleeper.updateTableProperties(Map.of(COMPACTION_METHOD, CompactionMethod.DATAFUSION.toString()));
         sleeper.systemTestCluster().updateProperties(properties -> {
             properties.setEnum(INGEST_MODE, DIRECT);
             properties.setNumber(NUMBER_OF_WRITERS, 110);
