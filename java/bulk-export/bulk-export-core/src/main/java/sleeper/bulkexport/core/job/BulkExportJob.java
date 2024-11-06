ae5454de0a123e1aab9db95ba7fcd43fb18ffb20
patchwork01 <110390516+patchwork01@users.noreply.github.com>
2024-11-06 10:19:32 +0000

    Set bulk-export-core base package name

--- java/bulk-export/bulk-export-core/src/main/java/sleeper/bulkexport/core/job/BulkExportJob.java
+++ java/bulk-export/bulk-export-core/src/main/java/sleeper/bulkexport/core/job/BulkExportJob.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package sleeper.bulkexport.job;
+package sleeper.bulkexport.core.job;
 
 /**
  * The Bulk export job.
