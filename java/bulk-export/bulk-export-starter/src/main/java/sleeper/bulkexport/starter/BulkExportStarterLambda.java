56874b7e6618350cf7ccf3623802b809ad28d7ca
patchwork01 <110390516+patchwork01@users.noreply.github.com>
2024-11-06 10:21:11 +0000

    Set base package name in bulk-export-starter

--- java/bulk-export/bulk-export-starter/src/main/java/sleeper/bulkexport/starter/BulkExportStarterLambda.java
+++ java/bulk-export/bulk-export-starter/src/main/java/sleeper/bulkexport/starter/BulkExportStarterLambda.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package sleeper.bulkexport;
+package sleeper.bulkexport.starter;
 
 /**
  * Lambda to start the bulk export job.
