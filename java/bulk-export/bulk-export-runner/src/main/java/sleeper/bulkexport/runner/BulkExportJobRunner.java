cebc333701df4ac528bb4044ba2508dacc716a26
patchwork01 <110390516+patchwork01@users.noreply.github.com>
2024-11-06 10:20:38 +0000

    Set base package name in bulk-export-runner

--- java/bulk-export/bulk-export-runner/src/main/java/sleeper/bulkexport/runner/BulkExportJobRunner.java
+++ java/bulk-export/bulk-export-runner/src/main/java/sleeper/bulkexport/runner/BulkExportJobRunner.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package sleeper.bulkexport;
+package sleeper.bulkexport.runner;
 
 /**
  * Bulk export job runner.
