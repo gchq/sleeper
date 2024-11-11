b602fa2df22de36cf21468c2ca52b990bd2f6e52
m09526 <m09526@users.noreply.github.com>
2024-11-11 14:10:21 +0000

    Print type

--- cpp/src/cmdline/lub.cpp
+++ cpp/src/cmdline/lub.cpp
@@ -1,10 +1,16 @@
 #include "lub.hpp"
 #include <cudf/copying.hpp>
 #include <cudf/scalar/scalar.hpp>
+#include <cudf/utilities/type_dispatcher.hpp>
 #include <memory>
+#include <type_info>
 
 void findLeastUpperBound(std::vector<cudf::table_view> const &views) noexcept {
-    for (auto const &v : views) {
+    auto action = [](auto &&t) { std::cout << "Type test " << typeid(t).name() << " t\n"; };
+
+    for (::size_t c = 0, auto const &v : views) {
         std::unique_ptr<cudf::scalar> lastElement = cudf::get_element(v.column(0), v.column(0).size() - 1);
-        }
+        cudf::type_dispatcher(lastElement->type, action, lastElement);
+        c++;
+    }
 }
