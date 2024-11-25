3eedf7383a3c40152a73811e3e641d9f3f8d3d83
patchwork01 <110390516+patchwork01@users.noreply.github.com>
2024-11-25 09:19:03 +0000

    Set up OptionalFeaturesDisabledST for non-paused instance

--- java/system-test/system-test-suite/src/main/java/sleeper/systemtest/suite/fixtures/SystemTestInstance.java
+++ java/system-test/system-test-suite/src/main/java/sleeper/systemtest/suite/fixtures/SystemTestInstance.java
@@ -70,7 +70,6 @@ import static sleeper.core.properties.validation.OptionalStack.EmrServerlessBulk
 import static sleeper.core.properties.validation.OptionalStack.GarbageCollectorStack;
 import static sleeper.core.properties.validation.OptionalStack.IngestBatcherStack;
 import static sleeper.core.properties.validation.OptionalStack.IngestStack;
-import static sleeper.systemtest.dsl.instance.SystemTestInstanceConfiguration.noSourceBucket;
 import static sleeper.systemtest.dsl.instance.SystemTestInstanceConfiguration.usingSystemTestDefaults;
 
 public class SystemTestInstance {
@@ -88,7 +87,11 @@ public class SystemTestInstance {
     public static final SystemTestInstanceConfiguration COMPACTION_ON_EC2 = usingSystemTestDefaults("cptec2", SystemTestInstance::createCompactionOnEC2Configuration);
     public static final SystemTestInstanceConfiguration COMMITTER_THROUGHPUT = usingSystemTestDefaults("cmmitr", SystemTestInstance::createStateStoreCommitterThroughputConfiguration);
     public static final SystemTestInstanceConfiguration REENABLE_OPTIONAL_STACKS = usingSystemTestDefaults("opstck", SystemTestInstance::createReenableOptionalStacksConfiguration);
-    public static final SystemTestInstanceConfiguration OPTIONAL_FEATURES_DISABLED = noSourceBucket("xf-off", SystemTestInstance::createOptionalFeaturesDisabledConfiguration);
+    public static final SystemTestInstanceConfiguration OPTIONAL_FEATURES_DISABLED = SystemTestInstanceConfiguration.builder()
+            .shortName("xf-off")
+            .deployConfig(SystemTestInstance::createOptionalFeaturesDisabledConfiguration)
+            .useSystemTestIngestSourceBucket(false)
+            .build();
 
     private static final String MAIN_EMR_MASTER_TYPES = "m7i.xlarge,m6i.xlarge,m6a.xlarge,m5.xlarge,m5a.xlarge";
     private static final String MAIN_EMR_EXECUTOR_TYPES = "m7i.4xlarge,m6i.4xlarge,m6a.4xlarge,m5.4xlarge,m5a.4xlarge";
