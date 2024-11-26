8bd69a60fb9f48632424c52fd984d5c569d5d051
rtjd6554 <174791724+rtjd6554@users.noreply.github.com>
2024-11-26 14:27:02 +0000

    Generate Enum for LoggingGroupName

--- java/cdk/src/main/java/sleeper/cdk/stack/core/LoggingStack.java
+++ java/cdk/src/main/java/sleeper/cdk/stack/core/LoggingStack.java
@@ -139,4 +139,67 @@ public class LoggingStack extends NestedStack {
     private String addNamePrefixes(String shortName) {
         return String.join("-", "sleeper", Utils.cleanInstanceId(instanceProperties), shortName);
     }
+
+    public enum LoggingGroupName {
+
+        VPC_CHECK("vpc-check"),
+        VPC_CHECK_PROVIDER("vpc-check-provider"),
+        CONFIG_AUTODELETE("config-autodelete"),
+        CONFIG_AUTODELETE_PROVIDER("config-autodelete-provider"),
+        TABLE_DATA_AUTODELETE("table-data-autodelete"),
+        TABLE_DATA_AUTODELETE_PROVIDER("table-data-autodelete-provider"),
+        STATESTORE_COMMITTER("statestore-committer"),
+
+        // Accessed via CoreStacks getters
+        PROPERTIES_WRITER("properties-writer"),
+        PROPERTIES_WRITER_PROVIDER("properties-writer-provider"),
+        STATE_SNAPSHOT_CREATION_TRIGGER("state-snapshot-creation-trigger"),
+        STATE_SNAPSHOT_CREATION("state-snapshot-creation"),
+        STATE_SNAPSHOT_DELETION_TRIGGER("state-snapshot-deletion-trigger"),
+        STATE_SNAPSHOT_DELETION("state-snapshot-deletion"),
+        STATE_TRANSACTION_DELETION_TRIGGER("state-transaction-deletion-trigger"),
+        STATE_TRANSACTION_DELETION("state-transaction-deletion"),
+        METRICS_TRIGGER("metrics-trigger"),
+        METRICS_PUBLISHER("metrics-publisher"),
+        BULK_IMPORT_EMRSERVERLESS_START("bulk-import-EMRServerless-start"),
+        BULK_IMPORT_NONPERSISTENTEMR_START("bulk-import-NonPersistentEMR-start"),
+        BULK_IMPORT_PERSISTENTEMR_START("bulk-import-PersistentEMR-start"),
+        BULK_IMPORT_EKS_STARTER("bulk-import-eks-starter"),
+        BULK_IMPORT_EKS("bulk-import-eks"),
+        EKSBULKIMPORTSTATEMACHINE("EksBulkImportStateMachine", "/aws/vendedlogs/states/"),
+        BULK_IMPORT_AUTODELETE("bulk-import-autodelete"),
+        BULK_IMPORT_AUTODELETE_PROVIDER("bulk-import-autodelete-provider"),
+        INGEST_TASKS("IngestTasks"),
+        INGEST_CREATE_TASKS("ingest-create-tasks"),
+        INGEST_BATCHER_SUBMIT_FILES("ingest-batcher-submit-files"),
+        INGEST_BATCHER_CREATE_JOBS("ingest-batcher-create-jobs"),
+        PARTITION_SPLITTING_TRIGGER("partition-splitting-trigger"),
+        PARTITION_SPLITTING_FIND_TO_SPLIT("partition-splitting-find-to-split"),
+        PARTITION_SPLITTING_HANDLER("partition-splitting-handler"),
+        FARGATECOMPACTIONTASKS("FargateCompactionTasks"),
+        EC2COMPACTIONTASKS("EC2CompactionTasks"),
+        COMPACTION_JOB_CREATION_TRIGGER("compaction-job-creation-trigger"),
+        COMPACTION_JOB_CREATION_HANDLER("compaction-job-creation-handler"),
+        COMPACTION_TASKS_CREATOR("compaction-tasks-creator"),
+        COMPACTION_CUSTOM_TERMINATION("compaction-custom-termination"),
+        GARBAGE_COLLECTOR_TRIGGER("garbage-collector-trigger"),
+        GARBAGE_COLLECTOR("garbage-collector"),
+        QUERY_EXECUTOR("query-executor"),
+        QUERY_LEAF_PARTITION("query-leaf-partition"),
+        QUERY_WEBSOCKET_HANDLER("query-websocket-handler"),
+        QUERY_RESULTS_AUTODELETE("query-results-autodelete"),
+        QUERY_RESULTS_AUTODELETE_PROVIDER("query-results-autodelete-provider"),
+        QUERY_KEEP_WARM("query-keep-warm"),
+        SIMPLE_ATHENA_HANDLER("Simple-athena-handler"),
+        ITERATORAPPLYING_ATHENA_HANDLER("IteratorApplying-athena-handler"),
+        SPILL_BUCKET_AUTODELETE("spill-bucket-autodelete"),
+        SPILL_BUCKET_AUTODELETE_PROVIDER("spill-bucket-autodelete-provider");
+
+        LoggingGroupName(String shortName) {
+        }
+
+        LoggingGroupName(String shortName, String prefix) {
+        }
+
+    }
 }
