8418c69d9ab78cfe179d5b48933775bf17e62fe7
rtjd6554 <rtjd6554@proton.me>
2025-01-10 09:08:13 +0000

    Replace &str with String

--- rust/compaction/src/datafusion/udf.rs
+++ rust/compaction/src/datafusion/udf.rs
@@ -123,7 +123,7 @@ impl ScalarUDFImpl for SketchUDF {
     fn as_any(&self) -> &dyn Any {
         self
     }
-    fn name(&self) -> &str {
+    fn name(&self) -> &[String] {
         "sketch"
     }
     fn signature(&self) -> &Signature {
