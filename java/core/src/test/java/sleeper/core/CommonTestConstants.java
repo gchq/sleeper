98d3caa348b1e53a73cffa98e5ed1189275222a2
patchwork01 <110390516+patchwork01@users.noreply.github.com>
2025-01-17 09:03:03 +0000

    Upgrade LocalStack to 4.0.3 for all LocalStack tests

--- java/core/src/test/java/sleeper/core/CommonTestConstants.java
+++ java/core/src/test/java/sleeper/core/CommonTestConstants.java
@@ -19,10 +19,7 @@ package sleeper.core;
  * Fixes versions of images for TestContainers.
  */
 public final class CommonTestConstants {
-    // Temporarily using a different version of LocalStack for tests with SQS SDK v2, see issue:
-    // https://github.com/gchq/sleeper/issues/3449
-    public static final String LOCALSTACK_DOCKER_IMAGE = "localstack/localstack:1.4.0";
-    public static final String LOCALSTACK_DOCKER_IMAGE_V2 = "localstack/localstack:4.0.3";
+    public static final String LOCALSTACK_DOCKER_IMAGE = "localstack/localstack:4.0.3";
 
     private CommonTestConstants() {
         // Empty
