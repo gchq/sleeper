7f826e19668874a5b95e82ce57333d45b186b52a
patchwork01 <110390516+patchwork01@users.noreply.github.com>
2025-01-20 15:04:50 +0000

    Start test setup in CompactionCommitBatcherTest

---
+++ java/compaction/compaction-core/src/test/java/sleeper/compaction/core/job/commit/CompactionCommitBatcherTest.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2022-2024 Crown Copyright
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package sleeper.compaction.core.job.commit;
+
+import org.junit.jupiter.api.Test;
+
+import sleeper.compaction.core.job.CompactionJob;
+import sleeper.compaction.core.job.CompactionJobFactory;
+import sleeper.core.properties.instance.InstanceProperties;
+import sleeper.core.properties.table.TableProperties;
+import sleeper.core.properties.table.TablePropertiesStore;
+import sleeper.core.properties.testutils.InMemoryTableProperties;
+
+import java.util.List;
+
+import static sleeper.core.properties.table.TableProperty.TABLE_ID;
+import static sleeper.core.properties.testutils.InstancePropertiesTestHelper.createTestInstanceProperties;
+import static sleeper.core.properties.testutils.TablePropertiesTestHelper.createTestTableProperties;
+import static sleeper.core.schema.SchemaTestHelper.schemaWithKey;
+
+public class CompactionCommitBatcherTest {
+
+    private final InstanceProperties instanceProperties = createTestInstanceProperties();
+    private final TablePropertiesStore tables = InMemoryTableProperties.getStore();
+
+    @Test
+    void shouldSendOneCompactionCommit() {
+        // Given
+        TableProperties table = createTable();
+        CompactionJob job = jobFactory(table).createCompactionJobWithFilenames(
+                "test-job", List.of("test.parquet"), "root");
+        List<CompactionCommitRequest> requests = List.of(commitRequest(table, job));
+    }
+
+    private TableProperties createTable() {
+        return createTestTableProperties(instanceProperties, schemaWithKey("key"));
+    }
+
+    private CompactionJobFactory jobFactory(TableProperties tableProperties) {
+        return new CompactionJobFactory(instanceProperties, tableProperties);
+    }
+
+    private CompactionCommitRequest commitRequest(TableProperties tableProperties, CompactionJob job) {
+        return new CompactionCommitRequest(tableProperties.get(TABLE_ID),
+                job.replaceFileReferencesRequestBuilder(100).build());
+    }
+
+}
