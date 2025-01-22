9fccfd8ea27ebf37da40403269aaf5d9b3c16d79
patchwork01 <110390516+patchwork01@users.noreply.github.com>
2025-01-22 11:50:52 +0000

    Test saving & loading partition transaction by table properties

--- java/statestore/src/test/java/sleeper/statestore/transactionlog/S3TransactionBodyStoreIT.java
+++ java/statestore/src/test/java/sleeper/statestore/transactionlog/S3TransactionBodyStoreIT.java
@@ -15,56 +15,82 @@
  */
 package sleeper.statestore.transactionlog;
 
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-import org.apache.hadoop.conf.Configuration;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import org.testcontainers.containers.localstack.LocalStackContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
+import sleeper.core.partition.PartitionsBuilder;
 import sleeper.core.properties.instance.InstanceProperties;
 import sleeper.core.properties.table.TableProperties;
-import sleeper.localstack.test.SleeperLocalStackContainer;
-import sleeper.parquet.utils.HadoopConfigurationLocalStackUtils;
+import sleeper.core.statestore.transactionlog.PartitionTransaction;
+import sleeper.core.statestore.transactionlog.StateStoreTransaction;
+import sleeper.core.statestore.transactionlog.TransactionBodyStore;
+import sleeper.core.statestore.transactionlog.TransactionBodyStoreProvider;
+import sleeper.core.statestore.transactionlog.transactions.InitialisePartitionsTransaction;
+import sleeper.core.statestore.transactionlog.transactions.TransactionType;
+import sleeper.statestore.testutil.LocalStackTestBase;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static sleeper.core.properties.instance.CdkDefinedInstanceProperty.DATA_BUCKET;
+import static sleeper.core.properties.table.TableProperty.TABLE_ID;
 import static sleeper.core.properties.testutils.InstancePropertiesTestHelper.createTestInstanceProperties;
-import static sleeper.localstack.test.LocalStackAwsV1ClientHelper.buildAwsV1Client;
+import static sleeper.core.properties.testutils.TablePropertiesTestHelper.createTestTableProperties;
+import static sleeper.core.schema.SchemaTestHelper.schemaWithKey;
 
-@Testcontainers
-public class S3TransactionBodyStoreIT {
+public class S3TransactionBodyStoreIT extends LocalStackTestBase {
 
-    @Container
-    public static LocalStackContainer localStackContainer = SleeperLocalStackContainer.create(LocalStackContainer.Service.S3);
-    protected static AmazonS3 s3Client;
-    protected final InstanceProperties instanceProperties = createTestInstanceProperties();
-    protected final TableProperties tableProperties = null;
-    protected final Configuration configuration = HadoopConfigurationLocalStackUtils.getHadoopConfiguration(localStackContainer);
-
-    @BeforeAll
-    public static void initClient() {
-        s3Client = buildAwsV1Client(localStackContainer, LocalStackContainer.Service.S3, AmazonS3ClientBuilder.standard());
-    }
+    InstanceProperties instanceProperties = createTestInstanceProperties();
+    TableProperties tableProperties = createTestTableProperties(instanceProperties, schemaWithKey("key"));
+    String tableId = tableProperties.get(TABLE_ID);
 
     @BeforeEach
     void setUp() {
-        //new S3TransactionBodyStore(instanceProperties, tableProperties, s3Client);
         s3Client.createBucket(instanceProperties.get(DATA_BUCKET));
     }
 
-    @Test
-    @Disabled
-    void shouldProvideTransactionBodyStoreForFilesWithGivenTableId() {
+    @Nested
+    @DisplayName("Support all transactions")
+    class SupportAllTransactions {
 
+        private final TransactionBodyStoreProvider provider = S3TransactionBodyStore.createProvider(instanceProperties, s3Client);
+        private final PartitionTransaction transaction = new InitialisePartitionsTransaction(
+                new PartitionsBuilder(tableProperties.getSchema()).singlePartition("root").buildList());
+
+        @Test
+        void shouldSaveAndLoadPartitionTransactionByTableProperties() {
+            // Given
+            TransactionBodyStore store = provider.getTransactionBodyStore(tableProperties);
+            String key = TransactionBodyStore.createObjectKey(tableProperties);
+
+            // When
+            store.store(key, transaction);
+            StateStoreTransaction<?> found = store.getBody(key, TransactionType.INITIALISE_PARTITIONS);
+
+            // Then
+            assertThat(found).isEqualTo(transaction);
+        }
+
+        @Test
+        @Disabled
+        void shouldSaveAndLoadPartitionTransactionByTableId() {
+            // Given
+            TransactionBodyStore store = provider.getTransactionBodyStore(tableId);
+            String key = TransactionBodyStore.createObjectKey(tableId);
+
+            // When
+            store.store(key, transaction);
+            StateStoreTransaction<?> found = store.getBody(key, TransactionType.INITIALISE_PARTITIONS);
+
+            // Then
+            assertThat(found).isEqualTo(transaction);
+        }
     }
 
-    @Test
-    @Disabled
-    void shouldProvideTransactionBodyStoreWhenGivenTableProperties() {
+    @Nested
+    @DisplayName("Support only file transactions")
+    class SupportOnlyFileTransactions {
 
     }
 
