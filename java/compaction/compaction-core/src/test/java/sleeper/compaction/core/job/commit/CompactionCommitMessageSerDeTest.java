3b62c9e2a9324e7aa09c03224f6c47db578344b8
patchwork01 <110390516+patchwork01@users.noreply.github.com>
2025-01-28 13:26:04 +0000

    Adjust CompactionCommitMessageSerDeTest for representative size

--- java/compaction/compaction-core/src/test/java/sleeper/compaction/core/job/commit/CompactionCommitMessageSerDeTest.java
+++ java/compaction/compaction-core/src/test/java/sleeper/compaction/core/job/commit/CompactionCommitMessageSerDeTest.java
@@ -21,30 +21,52 @@ import org.junit.jupiter.api.Test;
 
 import sleeper.core.partition.PartitionTree;
 import sleeper.core.partition.PartitionsBuilder;
+import sleeper.core.properties.instance.InstanceProperties;
+import sleeper.core.properties.table.TableProperties;
+import sleeper.core.schema.Schema;
+import sleeper.core.statestore.FileReference;
 import sleeper.core.statestore.FileReferenceFactory;
 import sleeper.core.statestore.ReplaceFileReferencesRequest;
+import sleeper.core.table.TableFilePaths;
+import sleeper.core.table.TableIdGenerator;
 
 import java.util.List;
+import java.util.stream.IntStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static sleeper.core.properties.instance.CommonProperty.ID_MAX_LENGTH;
+import static sleeper.core.properties.table.TableProperty.TABLE_ID;
+import static sleeper.core.properties.table.TableProperty.TABLE_NAME;
+import static sleeper.core.properties.testutils.InstancePropertiesTestHelper.createTestInstancePropertiesWithId;
 import static sleeper.core.schema.SchemaTestHelper.schemaWithKey;
+import static sleeper.core.testutils.SupplierTestHelper.exampleUUID;
 
 public class CompactionCommitMessageSerDeTest {
 
+    InstanceProperties instanceProperties = createTestInstancePropertiesWithId("I".repeat(ID_MAX_LENGTH));
+    Schema schema = schemaWithKey("key");
+    String tableId = TableIdGenerator.fromRandomSeed(0).generateString();
+    TableProperties tableProperties = createTestTableProperties(instanceProperties, schema, tableId);
+    TableFilePaths filePaths = TableFilePaths.buildDataFilePathPrefix(instanceProperties, tableProperties);
     CompactionCommitMessageSerDe serDe = new CompactionCommitMessageSerDe();
 
     @Test
     void shouldSerialiseCompactionCommitRequest() {
         // Given
-        PartitionTree partitions = new PartitionsBuilder(schemaWithKey("key")).singlePartition("root").buildTree();
+        PartitionTree partitions = new PartitionsBuilder(schema).singlePartition(exampleUUID("partn", 0)).buildTree();
+        List<String> inputFiles = IntStream.range(0, 9)
+                .mapToObj(i -> filePaths.constructPartitionParquetFilePath(
+                        partitions.getRootPartition(), exampleUUID("file", i)))
+                .toList();
+        FileReference outputFile = FileReferenceFactory.from(partitions).rootFile(exampleUUID("file", 9), 200);
         ReplaceFileReferencesRequest filesRequest = ReplaceFileReferencesRequest.builder()
-                .jobId("test-job")
-                .taskId("test-task")
-                .jobRunId("test-run")
-                .inputFiles(List.of("test.parquet"))
-                .newReference(FileReferenceFactory.from(partitions).rootFile("output.parquet", 200))
+                .jobId(exampleUUID("job", 1))
+                .taskId(exampleUUID("task", 1))
+                .jobRunId(exampleUUID("run", 1))
+                .inputFiles(inputFiles)
+                .newReference(outputFile)
                 .build();
-        CompactionCommitMessage message = new CompactionCommitMessage("test-table", filesRequest);
+        CompactionCommitMessage message = new CompactionCommitMessage(tableId, filesRequest);
         Runnable callbackOnFail = () -> {
         };
 
@@ -54,8 +76,15 @@ public class CompactionCommitMessageSerDeTest {
         CompactionCommitMessageHandle found = serDe.fromJsonWithCallbackOnFail(json, callbackOnFail);
 
         // Then
-        assertThat(found).isEqualTo(new CompactionCommitMessageHandle("test-table", filesRequest, callbackOnFail));
+        assertThat(found).isEqualTo(new CompactionCommitMessageHandle(tableId, filesRequest, callbackOnFail));
         Approvals.verify(jsonPretty, new Options().forFile().withExtension(".json"));
+    }
+
+    private static TableProperties createTestTableProperties(InstanceProperties instanceProperties, Schema schema, String tableId) {
+        TableProperties tableProperties = new TableProperties(instanceProperties);
+        tableProperties.set(TABLE_ID, tableId);
+        tableProperties.set(TABLE_NAME, "test");
+        return tableProperties;
     }
 
 }
