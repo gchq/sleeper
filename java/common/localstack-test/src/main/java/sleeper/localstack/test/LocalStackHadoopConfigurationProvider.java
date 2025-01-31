f2696e47d5a5dcd8ff2f137b5e284ddcbed000b4
patchwork01 <110390516+patchwork01@users.noreply.github.com>
2025-01-31 12:42:38 +0000

    Add missing private constructor

--- java/common/localstack-test/src/main/java/sleeper/localstack/test/LocalStackHadoopConfigurationProvider.java
+++ java/common/localstack-test/src/main/java/sleeper/localstack/test/LocalStackHadoopConfigurationProvider.java
@@ -20,6 +20,9 @@ import org.testcontainers.containers.localstack.LocalStackContainer;
 
 public class LocalStackHadoopConfigurationProvider {
 
+    private LocalStackHadoopConfigurationProvider() {
+    }
+
     /**
      * Only use one Hadoop configuration from here at once if your code uses the Hadoop file system, for example if you
      * write Parquet files to S3A. The Hadoop FileSystem caches the S3AFileSystem objects which actually communicate
