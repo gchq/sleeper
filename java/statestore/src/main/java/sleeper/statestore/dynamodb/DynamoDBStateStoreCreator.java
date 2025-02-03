f1f21c1770d3b233b795de39e22a1f4a95764527
rtjd6554 <rtjd6554@proton.me>
2025-02-03 09:12:11 +0000

    Spotbugs fix

--- java/statestore/src/main/java/sleeper/statestore/dynamodb/DynamoDBStateStoreCreator.java
+++ java/statestore/src/main/java/sleeper/statestore/dynamodb/DynamoDBStateStoreCreator.java
@@ -16,26 +16,16 @@
 package sleeper.statestore.dynamodb;
 
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
-import com.amazonaws.services.dynamodbv2.model.AttributeDefinition;
-import com.amazonaws.services.dynamodbv2.model.KeySchemaElement;
 
 import sleeper.core.properties.instance.InstanceProperties;
-import sleeper.dynamodb.tools.DynamoDBUtils;
-
-import java.util.List;
-import java.util.Objects;
 
 /**
  * Creates the tables necessary for a DynamoDB state store. Mainly used for testing purposes as the creation of the
  * tables in real deployments is normally done using CDK.
  */
 public class DynamoDBStateStoreCreator {
-    private final InstanceProperties instanceProperties;
-    private final AmazonDynamoDB dynamoDB;
 
     public DynamoDBStateStoreCreator(InstanceProperties instanceProperties, AmazonDynamoDB dynamoDB) {
-        this.instanceProperties = Objects.requireNonNull(instanceProperties, "instanceProperties must not be null");
-        this.dynamoDB = Objects.requireNonNull(dynamoDB, "dynamoDB must not be null");
     }
 
     /**
@@ -54,12 +44,4 @@ public class DynamoDBStateStoreCreator {
 
     }
 
-    private void initialiseTable(
-            String tableName,
-            List<AttributeDefinition> attributeDefinitions,
-            List<KeySchemaElement> keySchemaElements) {
-        DynamoDBUtils.initialiseTable(dynamoDB,
-                tableName, attributeDefinitions, keySchemaElements,
-                instanceProperties.getTags());
-    }
 }
