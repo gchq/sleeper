44de805d865c4ad45e9d0adb023b3651c498150c
rtjd6554 <rtjd6554@proton.me>
2025-02-05 15:08:30 +0000

    Initial framework

---
+++ java/core/src/test/java/sleeper/core/statestore/transactionlog/TransactionLogStateStoreTrackerUpdateTest.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2022-2024 Crown Copyright
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package sleeper.core.statestore.transactionlog;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import sleeper.core.partition.PartitionsBuilder;
+import sleeper.core.schema.type.LongType;
+import sleeper.core.statestore.FileReference;
+
+import java.util.List;
+
+import static sleeper.core.schema.SchemaTestHelper.schemaWithKey;
+import static sleeper.core.statestore.AssignJobIdRequest.assignJobOnPartitionToFiles;
+
+public class TransactionLogStateStoreTrackerUpdateTest extends InMemoryTransactionLogStateStoreTestBase {
+
+    private TransactionLogStateStore committerStore;
+    private TransactionLogStateStore followerStore;
+
+    @BeforeEach
+    void setUp() {
+        initialiseWithPartitions(new PartitionsBuilder(schemaWithKey("key", new LongType())).singlePartition("root"));
+        committerStore = (TransactionLogStateStore) super.store;
+
+        followerStore = stateStoreBuilder(schemaWithKey("key", new LongType())).build();
+    }
+
+    @Test
+    @Disabled
+    void shouldUpdateTransactionLogBasedOnStateStoreProvided() {
+        FileReference oldFile = factory.rootFile("oldFile", 100L);
+        FileReference newFile = factory.rootFile("newFile", 100L);
+
+        committerStore.addFiles(List.of(oldFile));
+        committerStore.assignJobIds(List.of(
+                assignJobOnPartitionToFiles("job1", "root", List.of("oldFile"))));
+
+    }
+}
