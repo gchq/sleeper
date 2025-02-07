e4375ba89c80b6fc714ae3e9c86610d0e9dab326
patchwork01 <110390516+patchwork01@users.noreply.github.com>
2025-02-07 15:28:51 +0000

    Reduce log level when reading files

--- java/statestore/src/main/java/sleeper/statestore/StateStoreFilesArrowFormat.java
+++ java/statestore/src/main/java/sleeper/statestore/StateStoreFilesArrowFormat.java
@@ -139,7 +139,7 @@ public class StateStoreFilesArrowFormat {
                 totalFileReferences += references.size();
             }
         }
-        LOGGER.info("Read {} files with {} references", totalFiles, totalFileReferences);
+        LOGGER.debug("Read {} files with {} references", totalFiles, totalFileReferences);
         return files;
     }
 
