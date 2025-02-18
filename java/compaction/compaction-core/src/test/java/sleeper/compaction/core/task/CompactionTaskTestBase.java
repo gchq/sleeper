73774115603502297f8e9df7ef8c79bb53132592
rtjd6554 <rtjd6554@proton.me>
2025-02-18 10:47:04 +0000

    Update to CompactioTaskTestBase

--- java/compaction/compaction-core/src/test/java/sleeper/compaction/core/task/CompactionTaskTestBase.java
+++ java/compaction/compaction-core/src/test/java/sleeper/compaction/core/task/CompactionTaskTestBase.java
@@ -35,7 +35,8 @@ import sleeper.core.statestore.ReplaceFileReferencesRequest;
 import sleeper.core.statestore.StateStore;
 import sleeper.core.statestore.StateStoreProvider;
 import sleeper.core.statestore.commit.StateStoreCommitRequest;
-import sleeper.core.statestore.testutils.FixedStateStoreProvider;
+import sleeper.core.statestore.testutils.InMemoryTransactionLogsPerTable;
+import sleeper.core.statestore.transactionlog.InMemoryTransactionLogStateStore;
 import sleeper.core.statestore.transactionlog.transaction.impl.ReplaceFileReferencesTransaction;
 import sleeper.core.tracker.compaction.job.InMemoryCompactionJobTracker;
 import sleeper.core.tracker.compaction.task.CompactionTaskTracker;
@@ -48,11 +49,9 @@ import sleeper.core.util.ThreadSleepTestHelper;
 import java.time.Duration;
 import java.time.Instant;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 import java.util.Queue;
 import java.util.UUID;
@@ -69,7 +68,6 @@ import static sleeper.core.properties.table.TableProperty.TABLE_NAME;
 import static sleeper.core.properties.testutils.InstancePropertiesTestHelper.createTestInstanceProperties;
 import static sleeper.core.properties.testutils.TablePropertiesTestHelper.createTestTableProperties;
 import static sleeper.core.schema.SchemaTestHelper.schemaWithKey;
-import static sleeper.core.statestore.testutils.StateStoreTestHelper.inMemoryStateStoreWithSinglePartition;
 
 public class CompactionTaskTestBase {
     protected static final String DEFAULT_TABLE_ID = "test-table-id";
@@ -80,7 +78,7 @@ public class CompactionTaskTestBase {
     protected final InstanceProperties instanceProperties = createTestInstanceProperties();
     protected final Schema schema = schemaWithKey("key");
     private final List<TableProperties> tables = new ArrayList<>();
-    private final Map<String, StateStore> stateStoreByTableId = new HashMap<>();
+    private final StateStoreProvider stateStoreProvider = InMemoryTransactionLogStateStore.createProvider(instanceProperties, new InMemoryTransactionLogsPerTable());
     protected final TableProperties tableProperties = createTable(DEFAULT_TABLE_ID, DEFAULT_TABLE_NAME);
     protected final StateStore stateStore = stateStore(tableProperties);
     protected final FileReferenceFactory factory = FileReferenceFactory.from(stateStore);
@@ -106,12 +104,12 @@ public class CompactionTaskTestBase {
         tableProperties.set(TABLE_ID, tableId);
         tableProperties.set(TABLE_NAME, tableName);
         tables.add(tableProperties);
-        stateStoreByTableId.put(tableId, inMemoryStateStoreWithSinglePartition(schema));
+        stateStore(tableProperties).initialise();
         return tableProperties;
     }
 
     protected StateStore stateStore(TableProperties table) {
-        return stateStoreByTableId.get(table.get(TABLE_ID));
+        return stateStoreProvider.getStateStore(table);
     }
 
     protected void runTask(CompactionRunner compactor) throws Exception {
@@ -178,7 +176,7 @@ public class CompactionTaskTestBase {
     }
 
     private StateStoreProvider stateStoreProvider() {
-        return FixedStateStoreProvider.byTableId(stateStoreByTableId);
+        return stateStoreProvider;
     }
 
     private Supplier<String> jobRunIdsInSequence() {
