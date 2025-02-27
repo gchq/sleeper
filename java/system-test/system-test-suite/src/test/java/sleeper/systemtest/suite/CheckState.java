bed8ed2130611a8fc0fba0de5589b2e5be624370
patchwork01 <110390516+patchwork01@users.noreply.github.com>
2025-02-27 12:55:09 +0000

    Unit test code for loading table state

--- java/system-test/system-test-suite/src/test/java/sleeper/systemtest/suite/CheckState.java
+++ java/system-test/system-test-suite/src/test/java/sleeper/systemtest/suite/CheckState.java
@@ -19,23 +19,21 @@ import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-import org.apache.hadoop.conf.Configuration;
 
 import sleeper.configuration.properties.S3InstanceProperties;
 import sleeper.configuration.properties.S3TableProperties;
 import sleeper.core.properties.instance.InstanceProperties;
 import sleeper.core.properties.table.TableProperties;
 import sleeper.core.statestore.FileReference;
+import sleeper.core.statestore.transactionlog.log.TransactionBodyStore;
 import sleeper.core.statestore.transactionlog.log.TransactionLogEntry;
 import sleeper.core.statestore.transactionlog.log.TransactionLogRange;
+import sleeper.core.statestore.transactionlog.log.TransactionLogStore;
 import sleeper.core.statestore.transactionlog.state.StateStoreFiles;
 import sleeper.core.statestore.transactionlog.transaction.StateStoreTransaction;
-import sleeper.core.statestore.transactionlog.transaction.TransactionSerDe;
 import sleeper.core.statestore.transactionlog.transaction.TransactionSerDeProvider;
-import sleeper.parquet.utils.HadoopConfigurationProvider;
 import sleeper.statestore.transactionlog.DynamoDBTransactionLogStore;
 import sleeper.statestore.transactionlog.S3TransactionBodyStore;
-import sleeper.systemtest.dsl.util.SystemTestSchema;
 
 import java.util.LinkedList;
 import java.util.List;
@@ -45,32 +43,32 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static sleeper.core.properties.table.TableProperty.TABLE_ID;
 
 public class CheckState {
-    private CheckState() {
+
+    private final List<Entry> filesLog;
+
+    private CheckState(List<Entry> filesLog) {
+        this.filesLog = filesLog;
     }
 
-    static TransactionSerDe serDe = new TransactionSerDe(SystemTestSchema.DEFAULT_SCHEMA);
-    static AmazonS3 s3Client = AmazonS3ClientBuilder.defaultClient();
-    static AmazonDynamoDB dynamoClient = AmazonDynamoDBClientBuilder.defaultClient();
-    static Configuration hadoopConf = HadoopConfigurationProvider.getConfigurationForClient();
-    static InstanceProperties properties = S3InstanceProperties.loadFromBucket(s3Client, System.getenv("CONFIG_BUCKET"));
-    static TableProperties tableProperties = S3TableProperties.createProvider(properties, s3Client, dynamoClient).getById(System.getenv("TABLE_ID"));
-    static DynamoDBTransactionLogStore filesLogStore = DynamoDBTransactionLogStore.forFiles(properties, tableProperties, dynamoClient, s3Client);
-    static S3TransactionBodyStore bodyStore = new S3TransactionBodyStore(properties, s3Client, TransactionSerDeProvider.forOneTable(tableProperties));
-    static List<Entry> filesLog = filesLogStore
-            .readTransactions(TransactionLogRange.fromMinimum(1))
-            .map(entry -> {
-                StateStoreTransaction<?> transaction = entry.getTransactionOrLoadFromPointer(tableProperties.get(TABLE_ID), bodyStore);
-                return new Entry(entry, transaction);
-            })
-            .collect(toCollection(LinkedList::new));
-
     public static void main(String[] args) {
-        StateStoreFiles state = filesStateAtTransaction(10);
+        CheckState check = load();
+        StateStoreFiles state = check.filesStateAtTransaction(10);
         long totalRecords = state.references().mapToLong(FileReference::getNumberOfRecords).sum();
         assertThat(totalRecords).isEqualTo(10_000_000_000L);
     }
 
-    private static StateStoreFiles filesStateAtTransaction(long transactionNumber) {
+    public static CheckState load(TableProperties tableProperties, TransactionLogStore logStore, TransactionBodyStore bodyStore) {
+        List<Entry> log = logStore
+                .readTransactions(TransactionLogRange.fromMinimum(1))
+                .map(entry -> {
+                    StateStoreTransaction<?> transaction = entry.getTransactionOrLoadFromPointer(tableProperties.get(TABLE_ID), bodyStore);
+                    return new Entry(entry, transaction);
+                })
+                .collect(toCollection(LinkedList::new));
+        return new CheckState(log);
+    }
+
+    public StateStoreFiles filesStateAtTransaction(long transactionNumber) {
         StateStoreFiles state = new StateStoreFiles();
         for (Entry entry : filesLog) {
             entry.apply(state);
@@ -95,5 +93,15 @@ public class CheckState {
         public long transactionNumber() {
             return original.getTransactionNumber();
         }
+    }
+
+    private static CheckState load() {
+        AmazonS3 s3Client = AmazonS3ClientBuilder.defaultClient();
+        AmazonDynamoDB dynamoClient = AmazonDynamoDBClientBuilder.defaultClient();
+        InstanceProperties properties = S3InstanceProperties.loadFromBucket(s3Client, System.getenv("CONFIG_BUCKET"));
+        TableProperties tableProperties = S3TableProperties.createProvider(properties, s3Client, dynamoClient).getById(System.getenv("TABLE_ID"));
+        DynamoDBTransactionLogStore filesLogStore = DynamoDBTransactionLogStore.forFiles(properties, tableProperties, dynamoClient, s3Client);
+        S3TransactionBodyStore bodyStore = new S3TransactionBodyStore(properties, s3Client, TransactionSerDeProvider.forOneTable(tableProperties));
+        return load(tableProperties, filesLogStore, bodyStore);
     }
 }
---
+++ java/system-test/system-test-suite/src/test/java/sleeper/systemtest/suite/CheckStateTest.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright 2022-2024 Crown Copyright
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package sleeper.systemtest.suite;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import sleeper.core.properties.instance.InstanceProperties;
+import sleeper.core.properties.table.TableProperties;
+import sleeper.core.schema.Schema;
+import sleeper.core.statestore.FileReference;
+import sleeper.core.statestore.FileReferenceFactory;
+import sleeper.core.statestore.StateStore;
+import sleeper.core.statestore.testutils.InMemoryTransactionLogStateStore;
+import sleeper.core.statestore.testutils.InMemoryTransactionLogs;
+import sleeper.core.statestore.transactionlog.state.StateStoreFiles;
+
+import java.time.Instant;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static sleeper.core.properties.testutils.InstancePropertiesTestHelper.createTestInstanceProperties;
+import static sleeper.core.properties.testutils.TablePropertiesTestHelper.createTestTableProperties;
+import static sleeper.core.schema.SchemaTestHelper.schemaWithKey;
+import static sleeper.core.statestore.testutils.StateStoreUpdatesWrapper.update;
+
+public class CheckStateTest {
+
+    static final Instant UPDATE_TIME = Instant.parse("2025-02-27T12:43:00Z");
+    InstanceProperties instanceProperties = createTestInstanceProperties();
+    Schema schema = schemaWithKey("key");
+    TableProperties tableProperties = createTestTableProperties(instanceProperties, schemaWithKey("key"));
+    InMemoryTransactionLogs transactionLogs = new InMemoryTransactionLogs();
+    StateStore stateStore = InMemoryTransactionLogStateStore.createAndInitialise(tableProperties, transactionLogs);
+
+    @BeforeEach
+    void setUp() {
+        stateStore.fixFileUpdateTime(UPDATE_TIME);
+    }
+
+    @Test
+    void shouldLoadFilesState() {
+        // Given
+        FileReference file1 = fileFactory().rootFile("test1.parquet", 100);
+        FileReference file2 = fileFactory().rootFile("test2.parquet", 100);
+        update(stateStore).addFile(file1);
+        update(stateStore).addFile(file2);
+
+        // When
+        StateStoreFiles state = checkState().filesStateAtTransaction(2);
+
+        // Then
+        assertThat(state.references()).containsExactly(file1, file2);
+    }
+
+    private FileReferenceFactory fileFactory() {
+        return FileReferenceFactory.fromUpdatedAt(stateStore, UPDATE_TIME);
+    }
+
+    private CheckState checkState() {
+        return CheckState.load(tableProperties, transactionLogs.getFilesLogStore(), transactionLogs.getTransactionBodyStore());
+    }
+
+}
