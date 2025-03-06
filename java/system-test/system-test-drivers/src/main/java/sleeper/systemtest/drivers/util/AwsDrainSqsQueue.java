d312e8a7ca2186439e2a19af8a481d1feaf07fa8
patchwork01 <110390516+patchwork01@users.noreply.github.com>
2025-03-06 15:47:36 +0000

    Use LinkedBlockingQueue for thread pool in AwsDrainSqsQueue

--- java/system-test/system-test-drivers/src/main/java/sleeper/systemtest/drivers/util/AwsDrainSqsQueue.java
+++ java/system-test/system-test-drivers/src/main/java/sleeper/systemtest/drivers/util/AwsDrainSqsQueue.java
@@ -28,7 +28,7 @@ import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Future;
-import java.util.concurrent.SynchronousQueue;
+import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collector;
@@ -45,7 +45,7 @@ public class AwsDrainSqsQueue {
     private static final ExecutorService EXECUTOR = new ThreadPoolExecutor(
             0, 40,
             60L, TimeUnit.SECONDS,
-            new SynchronousQueue<Runnable>());
+            new LinkedBlockingQueue<Runnable>());
 
     private final SqsClient sqsClient;
     private final int numThreads;
