12a0f0a2590b169f3164c01fd411d72488b111ec
patchwork01 <110390516+patchwork01@users.noreply.github.com>
2025-03-17 08:53:29 +0000

    Get table properties & state store from client

--- java/clients/src/main/java/sleeper/clients/AwsSleeperClient.java
+++ java/clients/src/main/java/sleeper/clients/AwsSleeperClient.java
@@ -17,14 +17,19 @@ package sleeper.clients;
 
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.s3.AmazonS3;
+import org.apache.hadoop.conf.Configuration;
 
 import sleeper.configuration.properties.S3InstanceProperties;
 import sleeper.configuration.properties.S3TableProperties;
 import sleeper.configuration.table.index.DynamoDBTableIndex;
 import sleeper.core.properties.instance.InstanceProperties;
+import sleeper.core.properties.table.TableProperties;
 import sleeper.core.properties.table.TablePropertiesProvider;
+import sleeper.core.statestore.StateStore;
+import sleeper.core.statestore.StateStoreProvider;
 import sleeper.core.table.TableIndex;
 import sleeper.core.table.TableStatus;
+import sleeper.statestore.StateStoreFactory;
 
 import java.util.stream.Stream;
 
@@ -33,22 +38,43 @@ public class AwsSleeperClient {
     private final InstanceProperties instanceProperties;
     private final TableIndex tableIndex;
     private final TablePropertiesProvider tablePropertiesProvider;
+    private final StateStoreProvider stateStoreProvider;
 
-    private AwsSleeperClient(InstanceProperties instanceProperties, TableIndex tableIndex, TablePropertiesProvider tablePropertiesProvider) {
+    private AwsSleeperClient(
+            InstanceProperties instanceProperties, TableIndex tableIndex, TablePropertiesProvider tablePropertiesProvider,
+            StateStoreProvider stateStoreProvider) {
         this.instanceProperties = instanceProperties;
         this.tableIndex = tableIndex;
         this.tablePropertiesProvider = tablePropertiesProvider;
+        this.stateStoreProvider = stateStoreProvider;
     }
 
-    public static AwsSleeperClient byInstanceId(AmazonS3 s3Client, AmazonDynamoDB dynamoClient, String instanceId) {
+    public static AwsSleeperClient byInstanceId(AmazonS3 s3Client, AmazonDynamoDB dynamoClient, Configuration hadoopConf, String instanceId) {
         InstanceProperties instanceProperties = S3InstanceProperties.loadGivenInstanceId(s3Client, instanceId);
         TableIndex tableIndex = new DynamoDBTableIndex(instanceProperties, dynamoClient);
         TablePropertiesProvider tablePropertiesProvider = S3TableProperties.createProvider(instanceProperties, tableIndex, s3Client);
-        return new AwsSleeperClient(instanceProperties, tableIndex, tablePropertiesProvider);
+        StateStoreProvider stateStoreProvider = StateStoreFactory.createProvider(instanceProperties, s3Client, dynamoClient, hadoopConf);
+        return new AwsSleeperClient(instanceProperties, tableIndex, tablePropertiesProvider, stateStoreProvider);
+    }
+
+    public InstanceProperties getInstanceProperties() {
+        return instanceProperties;
     }
 
     public Stream<TableStatus> streamAllTables() {
         return tableIndex.streamAllTables();
+    }
+
+    public TableProperties getTableProperties(TableStatus tableStatus) {
+        return tablePropertiesProvider.get(tableStatus);
+    }
+
+    public TableProperties getTableProperties(String tableName) {
+        return tablePropertiesProvider.getByName(tableName);
+    }
+
+    public StateStore getStateStore(String tableName) {
+        return stateStoreProvider.getStateStore(getTableProperties(tableName));
     }
 
 }
