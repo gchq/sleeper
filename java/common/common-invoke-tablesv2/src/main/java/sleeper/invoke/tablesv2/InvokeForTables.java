d7cd50b2376691b557311f416b79d4173e05f546
rtjd6554 <rtjd6554@proton.me>
2025-05-09 08:16:33 +0000

    3269: New module v2 for common-invoke-table and class migration

---
+++ java/common/common-invoke-tablesv2/src/main/java/sleeper/invoke/tablesv2/InvokeForTables.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright 2022-2025 Crown Copyright
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package sleeper.invoke.tablesv2;
+
+import com.amazonaws.services.sqs.AmazonSQS;
+import com.amazonaws.services.sqs.model.SendMessageBatchRequest;
+import com.amazonaws.services.sqs.model.SendMessageBatchRequestEntry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import sleeper.core.table.TableIndex;
+import sleeper.core.table.TableNotFoundException;
+import sleeper.core.table.TableStatus;
+import sleeper.core.util.SplitIntoBatches;
+
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toUnmodifiableList;
+
+/**
+ * Class used for controlling sqs messages sent for table to keep within aws design constraints.
+ */
+public class InvokeForTables {
+    public static final Logger LOGGER = LoggerFactory.getLogger(InvokeForTables.class);
+
+    private InvokeForTables() {
+    }
+
+    /**
+     * Splits steam of tables into batchs to allow messages to be within maximum size for an SQS message.
+     *
+     * @param sqsClient the sqs client
+     * @param queueUrl  the queue url
+     * @param tables    stream of table to send messages for
+     */
+    public static void sendOneMessagePerTable(AmazonSQS sqsClient, String queueUrl, Stream<TableStatus> tables) {
+        // Limit to stay under the maximum size for an SQS sendMessageBatch call.
+        SplitIntoBatches.reusingListOfSize(10, tables,
+                batch -> sendMessageBatch(sqsClient, queueUrl, batch));
+    }
+
+    /**
+     * Generates a stream of tables from index to allow for batching.
+     *
+     * @param sqsClient  the sqs client
+     * @param queueUrl   the queue url
+     * @param tableIndex index containing table details
+     * @param tableNames list of table names
+     */
+    public static void sendOneMessagePerTableByName(
+            AmazonSQS sqsClient, String queueUrl, TableIndex tableIndex, List<String> tableNames) {
+        List<TableStatus> tables = tableNames.stream().map(name -> tableIndex.getTableByName(name)
+                .orElseThrow(() -> TableNotFoundException.withTableName(name)))
+                .collect(toUnmodifiableList());
+        sendOneMessagePerTable(sqsClient, queueUrl, tables.stream());
+    }
+
+    private static void sendMessageBatch(AmazonSQS sqsClient, String queueUrl, List<TableStatus> tablesBatch) {
+        LOGGER.info("Sending table batch of size {} to SQS queue {}: {}", tablesBatch.size(), queueUrl, tablesBatch);
+        sqsClient.sendMessageBatch(new SendMessageBatchRequest()
+                .withQueueUrl(queueUrl)
+                .withEntries(tablesBatch.stream()
+                        .map(table -> new SendMessageBatchRequestEntry()
+                                .withMessageDeduplicationId(UUID.randomUUID().toString())
+                                .withId(table.getTableUniqueId())
+                                .withMessageGroupId(table.getTableUniqueId())
+                                .withMessageBody(table.getTableUniqueId()))
+                        .collect(toUnmodifiableList())));
+    }
+}
---
+++ java/common/common-invoke-tablesv2/src/test/java/sleeper/invoke/tablesv2/InvokeForTablesIT.java
@@ -0,0 +1,129 @@
+/*
+ * Copyright 2022-2025 Crown Copyright
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package sleeper.invoke.tablesv2;
+
+import com.amazonaws.services.sqs.model.Message;
+import com.amazonaws.services.sqs.model.ReceiveMessageRequest;
+import com.amazonaws.services.sqs.model.ReceiveMessageResult;
+import org.junit.jupiter.api.Test;
+
+import sleeper.core.table.InMemoryTableIndex;
+import sleeper.core.table.TableIndex;
+import sleeper.core.table.TableNotFoundException;
+import sleeper.localstack.test.LocalStackTestBase;
+
+import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toUnmodifiableList;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static sleeper.core.table.TableStatusTestHelper.uniqueIdAndName;
+
+public class InvokeForTablesIT extends LocalStackTestBase {
+
+    @Test
+    void shouldSendOneMessage() {
+        // Given
+        String queueUrl = createFifoQueueGetUrl();
+
+        // When
+        InvokeForTables.sendOneMessagePerTable(sqsClient, queueUrl, Stream.of(
+                uniqueIdAndName("table-id", "table-name")));
+
+        // Then
+        assertThat(receiveTableIdMessages(queueUrl, 2))
+                .containsExactly("table-id");
+    }
+
+    @Test
+    void shouldSendMoreMessagesThanFitInAnSqsSendMessageBatch() {
+        // Given a FIFO queue
+        String queueUrl = createFifoQueueGetUrl();
+
+        // When we send more than the SQS hard limit of 10 messages to send in a single batch
+        InvokeForTables.sendOneMessagePerTable(sqsClient, queueUrl,
+                IntStream.rangeClosed(1, 11)
+                        .mapToObj(i -> uniqueIdAndName("table-id-" + i, "table-name-" + i)));
+
+        // Then we can receive those messages
+        assertThat(receiveTableIdMessages(queueUrl, 10)).containsExactly(
+                "table-id-1", "table-id-2", "table-id-3", "table-id-4", "table-id-5",
+                "table-id-6", "table-id-7", "table-id-8", "table-id-9", "table-id-10");
+        assertThat(receiveTableIdMessages(queueUrl, 10)).containsExactly(
+                "table-id-11");
+    }
+
+    @Test
+    void shouldLookUpTableByName() {
+        // Given
+        String queueUrl = createFifoQueueGetUrl();
+        TableIndex tableIndex = new InMemoryTableIndex();
+        tableIndex.create(uniqueIdAndName("table-id", "table-name"));
+
+        // When
+        InvokeForTables.sendOneMessagePerTableByName(sqsClient, queueUrl, tableIndex, List.of("table-name"));
+
+        // Then
+        assertThat(receiveTableIdMessages(queueUrl, 2))
+                .containsExactly("table-id");
+    }
+
+    @Test
+    void shouldFailLookUpTableByName() {
+        // Given
+        String queueUrl = createFifoQueueGetUrl();
+        TableIndex tableIndex = new InMemoryTableIndex();
+
+        // When / Then
+        assertThatThrownBy(() -> InvokeForTables.sendOneMessagePerTableByName(
+                sqsClient, queueUrl, tableIndex, List.of("missing-table")))
+                .isInstanceOf(TableNotFoundException.class);
+        assertThat(receiveTableIdMessages(queueUrl, 1))
+                .isEmpty();
+    }
+
+    @Test
+    void shouldFailLookUpTableByNameOnSecondPage() {
+        // Given
+        String queueUrl = createFifoQueueGetUrl();
+        TableIndex tableIndex = new InMemoryTableIndex();
+        IntStream.rangeClosed(1, 11)
+                .mapToObj(i -> uniqueIdAndName("table-id-" + i, "table-name-" + i))
+                .forEach(tableIndex::create);
+
+        // When / Then
+        assertThatThrownBy(() -> InvokeForTables.sendOneMessagePerTableByName(sqsClient, queueUrl, tableIndex,
+                IntStream.rangeClosed(1, 12)
+                        .mapToObj(i -> "table-name-" + i)
+                        .collect(toUnmodifiableList())))
+                .isInstanceOf(TableNotFoundException.class);
+        assertThat(receiveTableIdMessages(queueUrl, 10))
+                .isEmpty();
+    }
+
+    private List<String> receiveTableIdMessages(String queueUrl, int maxMessages) {
+        ReceiveMessageResult result = sqsClient.receiveMessage(
+                new ReceiveMessageRequest(queueUrl)
+                        .withMaxNumberOfMessages(maxMessages)
+                        .withWaitTimeSeconds(0));
+        return result.getMessages().stream()
+                .map(Message::getBody)
+                .collect(toUnmodifiableList());
+    }
+
+}
