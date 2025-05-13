6fcb8012b4cf21c864f0571e88e9dd71c5d71fc2
patchwork01 <110390516+patchwork01@users.noreply.github.com>
2025-05-13 09:51:03 +0000

    4835 Move task requirement properties models to core module

--- java/common/common-task/src/main/java/sleeper/task/common/CompactionTaskHostScaler.java
+++ java/common/common-task/src/main/java/sleeper/task/common/CompactionTaskHostScaler.java
@@ -18,8 +18,8 @@ package sleeper.task.common;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import sleeper.configuration.CompactionTaskRequirements;
 import sleeper.core.properties.instance.InstanceProperties;
+import sleeper.core.properties.validation.CompactionTaskRequirements;
 
 import java.util.HashMap;
 import java.util.Locale;
