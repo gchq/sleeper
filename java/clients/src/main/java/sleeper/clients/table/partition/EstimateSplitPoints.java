d52e059441e2037d361e5d9e132a2b2ee39f33fe
patchwork01 <110390516+patchwork01@users.noreply.github.com>
2025-05-22 12:19:39 +0000

    4883 Fix exception message

--- java/clients/src/main/java/sleeper/clients/table/partition/EstimateSplitPoints.java
+++ java/clients/src/main/java/sleeper/clients/table/partition/EstimateSplitPoints.java
@@ -72,7 +72,7 @@ public class EstimateSplitPoints {
         // the correct number of split points, and need to remove the first and last entries.
         Object[] splitPoints = sketch.getQuantiles(getRanks());
         if (splitPoints.length != numPartitions - 1) {
-            throw new RuntimeException("There should have been " + (numPartitions - 1) + "partitions; got " + splitPoints.length);
+            throw new RuntimeException("There should have been " + (numPartitions - 1) + " split points; got " + splitPoints.length);
         }
 
         // Remove any duplicate values (which means the number of split points returned may be less than that requested.
