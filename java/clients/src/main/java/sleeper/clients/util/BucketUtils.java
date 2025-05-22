7c8ee3b3c0ab7319ed5412e8969717851b241b43
patchwork01 <110390516+patchwork01@users.noreply.github.com>
2025-05-22 09:34:06 +0000

    4820 Remove unused code

--- java/clients/src/main/java/sleeper/clients/util/BucketUtils.java
+++ java/clients/src/main/java/sleeper/clients/util/BucketUtils.java
@@ -16,12 +16,6 @@
 
 package sleeper.clients.util;
 
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.model.DeleteObjectsRequest;
-import com.amazonaws.services.s3.model.DeleteObjectsResult;
-import com.amazonaws.services.s3.model.ListObjectsV2Request;
-import com.amazonaws.services.s3.model.ListObjectsV2Result;
-import com.amazonaws.services.s3.model.S3ObjectSummary;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import software.amazon.awssdk.services.s3.S3Client;
@@ -30,7 +24,6 @@ import software.amazon.awssdk.services.s3.model.ListObjectsV2Response;
 import software.amazon.awssdk.services.s3.model.NoSuchBucketException;
 import software.amazon.awssdk.services.s3.model.ObjectIdentifier;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Predicate;
 
@@ -82,48 +75,5 @@ public class BucketUtils {
             }
         }
         LOGGER.info("A total of {} objects were deleted", totalObjectsDeleted);
-    }
-
-    public static void deleteAllObjectsInBucketWithPrefix(AmazonS3 s3Client, String bucketName, String prefix) {
-        deleteObjectsInBucketWithPrefix(s3Client, bucketName, prefix, key -> true);
-    }
-
-    public static void deleteObjectsInBucketWithPrefix(AmazonS3 s3Client, String bucketName, String prefix, Predicate<String> keyPredicate) {
-        List<String> objectKeysForDeletion = new ArrayList<>();
-        ListObjectsV2Request req = new ListObjectsV2Request()
-                .withBucketName(bucketName)
-                .withPrefix(prefix + "/")
-                //Keys set to 1000 as this is the maximum value permitted by S3 delete action
-                .withMaxKeys(1000);
-        ListObjectsV2Result result;
-
-        LOGGER.info("Deleting all objects in the bucket {} with prefix {}", bucketName, prefix);
-        int totalObjectsDeleted = 0;
-        do {
-            objectKeysForDeletion.clear();
-            result = s3Client.listObjectsV2(req);
-            for (S3ObjectSummary objectSummary : result.getObjectSummaries()) {
-                if (keyPredicate.test(objectSummary.getKey())) {
-                    objectKeysForDeletion.add(objectSummary.getKey());
-                }
-            }
-            String token = result.getNextContinuationToken();
-            req.setContinuationToken(token);
-            totalObjectsDeleted += deleteObjects(s3Client, bucketName, objectKeysForDeletion);
-        } while (result.isTruncated());
-        LOGGER.info("A total of {} objects were deleted", totalObjectsDeleted);
-    }
-
-    private static int deleteObjects(AmazonS3 s3Client, String bucketName, List<String> keys) {
-        int successfulDeletes = 0;
-        if (!keys.isEmpty()) {
-            DeleteObjectsRequest multiObjectDeleteRequest = new DeleteObjectsRequest(bucketName)
-                    .withKeys(keys.toArray(new String[0]))
-                    .withQuiet(false);
-            DeleteObjectsResult delObjRes = s3Client.deleteObjects(multiObjectDeleteRequest);
-            successfulDeletes = delObjRes.getDeletedObjects().size();
-            LOGGER.info("{} objects successfully deleted from S3 bucket: {}", successfulDeletes, bucketName);
-        }
-        return successfulDeletes;
     }
 }
