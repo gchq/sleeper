62e95f0f8e5487eb53f0b9dfb9b78f5f517ca5cc
rtjd6554 <rtjd6554@proton.me>
2025-05-29 11:42:46 +0000

    3283: Seperate module for bulk-import-starterv2

---
+++ java/bulk-import/bulk-import-starterv2/src/main/java/sleeper/bulkimport/starterv2/BulkImportStarterLambda.java
@@ -0,0 +1,109 @@
+/*
+ * Copyright 2022-2025 Crown Copyright
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package sleeper.bulkimport.starterv2;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.SQSEvent;
+import org.apache.hadoop.conf.Configuration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
+import software.amazon.awssdk.services.s3.S3AsyncClient;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.transfer.s3.S3TransferManager;
+
+import sleeper.bulkimport.core.job.BulkImportJob;
+import sleeper.bulkimport.core.job.BulkImportJobSerDe;
+import sleeper.bulkimport.starterv2.executor.BulkImportExecutor;
+import sleeper.bulkimport.starterv2.executor.BulkImportJobWriterToS3;
+import sleeper.bulkimport.starterv2.executor.PlatformExecutor;
+import sleeper.configurationv2.properties.S3InstanceProperties;
+import sleeper.configurationv2.properties.S3PropertiesReloader;
+import sleeper.configurationv2.properties.S3TableProperties;
+import sleeper.configurationv2.table.index.DynamoDBTableIndex;
+import sleeper.core.properties.PropertiesReloader;
+import sleeper.core.properties.instance.InstanceProperties;
+import sleeper.core.properties.table.TablePropertiesProvider;
+import sleeper.core.tracker.ingest.job.IngestJobTracker;
+import sleeper.ingest.core.job.IngestJobMessageHandler;
+import sleeper.ingest.trackerv2.job.IngestJobTrackerFactory;
+import sleeper.parquet.utils.HadoopConfigurationProvider;
+import sleeper.parquet.utils.HadoopPathUtils;
+import sleeper.statestorev2.StateStoreFactory;
+
+import java.time.Instant;
+
+import static sleeper.core.properties.instance.CdkDefinedInstanceProperty.CONFIG_BUCKET;
+
+/**
+ * Consumes bulk import jobs from SQS and starts them in the execution platform. An environment variable configures
+ * which platform bulk import jobs will be executed on, using an instance of {@link PlatformExecutor}.
+ */
+public class BulkImportStarterLambda implements RequestHandler<SQSEvent, Void> {
+    private static final Logger LOGGER = LoggerFactory.getLogger(BulkImportStarterLambda.class);
+
+    private final PropertiesReloader propertiesReloader;
+    private final BulkImportExecutor executor;
+    private final IngestJobMessageHandler<BulkImportJob> ingestJobMessageHandler;
+
+    public BulkImportStarterLambda() {
+        S3Client s3 = S3Client.create();
+        DynamoDbClient dynamo = DynamoDbClient.create();
+        S3AsyncClient s3AsyncClient = S3AsyncClient.crtCreate();
+        S3TransferManager s3TransferManager = S3TransferManager.builder().s3Client(s3AsyncClient).build();
+        InstanceProperties instanceProperties = S3InstanceProperties.loadFromBucket(s3, System.getenv(CONFIG_BUCKET.toEnvironmentVariable()));
+        TablePropertiesProvider tablePropertiesProvider = S3TableProperties.createProvider(instanceProperties, s3, dynamo);
+        PlatformExecutor platformExecutor = PlatformExecutor.fromEnvironment(
+                instanceProperties, tablePropertiesProvider);
+        Configuration hadoopConfig = HadoopConfigurationProvider.getConfigurationForLambdas(instanceProperties);
+        IngestJobTracker ingestJobTracker = IngestJobTrackerFactory.getTracker(dynamo, instanceProperties);
+        executor = new BulkImportExecutor(instanceProperties, tablePropertiesProvider,
+                StateStoreFactory.createProvider(instanceProperties, s3, dynamo, s3TransferManager),
+                ingestJobTracker, new BulkImportJobWriterToS3(instanceProperties, s3),
+                platformExecutor, Instant::now);
+        propertiesReloader = S3PropertiesReloader.ifConfigured(s3, instanceProperties, tablePropertiesProvider);
+        ingestJobMessageHandler = messageHandlerBuilder()
+                .tableIndex(new DynamoDBTableIndex(instanceProperties, dynamo))
+                .ingestJobTracker(ingestJobTracker)
+                .expandDirectories(files -> HadoopPathUtils.expandDirectories(files, hadoopConfig, instanceProperties))
+                .build();
+    }
+
+    public BulkImportStarterLambda(BulkImportExecutor executor, IngestJobMessageHandler<BulkImportJob> messageHandler) {
+        this.executor = executor;
+        this.propertiesReloader = PropertiesReloader.neverReload();
+        this.ingestJobMessageHandler = messageHandler;
+    }
+
+    public static IngestJobMessageHandler.Builder<BulkImportJob> messageHandlerBuilder() {
+        return IngestJobMessageHandler.builder()
+                .deserialiser(new BulkImportJobSerDe()::fromJson)
+                .toIngestJob(BulkImportJob::toIngestJob)
+                .applyIngestJobChanges(BulkImportJob::applyIngestJobChanges);
+    }
+
+    @Override
+    public Void handleRequest(SQSEvent event, Context context) {
+        LOGGER.info("Received request: {}", event);
+        propertiesReloader.reloadIfNeeded();
+        event.getRecords().stream()
+                .map(SQSEvent.SQSMessage::getBody)
+                .flatMap(message -> ingestJobMessageHandler.deserialiseAndValidate(message).stream())
+                .forEach(executor::runJob);
+        return null;
+    }
+}
---
+++ java/bulk-import/bulk-import-starterv2/src/test/java/sleeper/bulkimport/starterv2/executor/ReturnBulkImportJobToQueueIT.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright 2022-2025 Crown Copyright
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package sleeper.bulkimport.starterv2.executor;
+
+import com.amazonaws.services.sqs.model.Message;
+import com.amazonaws.services.sqs.model.ReceiveMessageRequest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import sleeper.bulkimport.core.job.BulkImportJob;
+import sleeper.bulkimport.core.job.BulkImportJobSerDe;
+import sleeper.bulkimport.starterv2.executor.persistent.ReturnBulkImportJobToQueue;
+import sleeper.core.properties.instance.InstanceProperties;
+import sleeper.localstack.test.LocalStackTestBase;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static sleeper.core.properties.instance.CdkDefinedInstanceProperty.BULK_IMPORT_PERSISTENT_EMR_JOB_QUEUE_URL;
+import static sleeper.core.properties.instance.PersistentEMRProperty.BULK_IMPORT_PERSISTENT_EMR_REQUEUE_DELAY_SECONDS;
+import static sleeper.core.properties.testutils.InstancePropertiesTestHelper.createTestInstanceProperties;
+
+public class ReturnBulkImportJobToQueueIT extends LocalStackTestBase {
+
+    InstanceProperties instanceProperties = createTestInstanceProperties();
+
+    @BeforeEach
+    void setUp() {
+        instanceProperties.set(BULK_IMPORT_PERSISTENT_EMR_JOB_QUEUE_URL, createSqsQueueGetUrl());
+        instanceProperties.set(BULK_IMPORT_PERSISTENT_EMR_REQUEUE_DELAY_SECONDS, "1");
+    }
+
+    @Test
+    void shouldSendJobWithDelay() {
+        // Given
+        BulkImportJob job = BulkImportJob.builder()
+                .tableName("test-table")
+                .id("test-job")
+                .files(List.of("file.parquet"))
+                .build();
+
+        // When
+        ReturnBulkImportJobToQueue.forFullPersistentEmrCluster(instanceProperties, sqsClientV2)
+                .send(job);
+
+        // Then
+        assertThat(receiveBulkImportJobsWithWaitTimeSeconds(0))
+                .isEmpty();
+        assertThat(receiveBulkImportJobsWithWaitTimeSeconds(2))
+                .containsExactly(job);
+    }
+
+    private List<BulkImportJob> receiveBulkImportJobsWithWaitTimeSeconds(int waitTimeSeconds) {
+        return sqsClient.receiveMessage(
+                new ReceiveMessageRequest(instanceProperties.get(BULK_IMPORT_PERSISTENT_EMR_JOB_QUEUE_URL))
+                        .withWaitTimeSeconds(waitTimeSeconds))
+                .getMessages().stream()
+                .map(Message::getBody)
+                .map(new BulkImportJobSerDe()::fromJson)
+                .toList();
+    }
+
+}
