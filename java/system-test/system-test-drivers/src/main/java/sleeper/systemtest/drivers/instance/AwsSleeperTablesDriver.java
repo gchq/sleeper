f0a5524f7adb9db72e88cf4edfb3a17267130c9e
patchwork01 <110390516+patchwork01@users.noreply.github.com>
2025-06-04 09:07:00 +0000

    4855 Upgrade AwsSleeperTablesDriver

--- java/system-test/system-test-drivers/src/main/java/sleeper/systemtest/drivers/instance/AwsSleeperTablesDriver.java
+++ java/system-test/system-test-drivers/src/main/java/sleeper/systemtest/drivers/instance/AwsSleeperTablesDriver.java
@@ -16,20 +16,19 @@
 
 package sleeper.systemtest.drivers.instance;
 
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
-import com.amazonaws.services.s3.AmazonS3;
-import org.apache.hadoop.conf.Configuration;
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
+import software.amazon.awssdk.services.s3.S3Client;
 
 import sleeper.clients.table.AddTable;
-import sleeper.configuration.properties.S3TableProperties;
-import sleeper.configuration.table.index.DynamoDBTableIndex;
+import sleeper.configurationv2.properties.S3TableProperties;
+import sleeper.configurationv2.table.index.DynamoDBTableIndex;
 import sleeper.core.properties.instance.InstanceProperties;
 import sleeper.core.properties.table.TableProperties;
 import sleeper.core.properties.table.TablePropertiesProvider;
 import sleeper.core.properties.table.TablePropertiesStore;
 import sleeper.core.statestore.StateStoreProvider;
 import sleeper.core.table.TableIndex;
-import sleeper.statestore.StateStoreFactory;
+import sleeper.statestorev2.StateStoreFactory;
 import sleeper.systemtest.drivers.util.SystemTestClients;
 import sleeper.systemtest.dsl.instance.SleeperTablesDriver;
 
@@ -38,14 +37,12 @@ import java.io.UncheckedIOException;
 
 public class AwsSleeperTablesDriver implements SleeperTablesDriver {
 
-    private final AmazonS3 s3;
-    private final AmazonDynamoDB dynamoDB;
-    private final Configuration hadoopConfiguration;
+    private final S3Client s3;
+    private final DynamoDbClient dynamoDB;
 
     public AwsSleeperTablesDriver(SystemTestClients clients) {
-        this.s3 = clients.getS3();
-        this.dynamoDB = clients.getDynamoDB();
-        this.hadoopConfiguration = clients.createHadoopConf();
+        this.s3 = clients.getS3V2();
+        this.dynamoDB = clients.getDynamoV2();
     }
 
     public void saveTableProperties(InstanceProperties instanceProperties, TableProperties tableProperties) {
@@ -56,7 +53,7 @@ public class AwsSleeperTablesDriver implements SleeperTablesDriver {
         try {
             new AddTable(instanceProperties, properties,
                     S3TableProperties.createStore(instanceProperties, s3, dynamoDB),
-                    StateStoreFactory.createProvider(instanceProperties, s3, dynamoDB, hadoopConfiguration))
+                    StateStoreFactory.createProvider(instanceProperties, s3, dynamoDB, null))
                     .run();
         } catch (IOException e) {
             throw new UncheckedIOException(e);
@@ -68,7 +65,7 @@ public class AwsSleeperTablesDriver implements SleeperTablesDriver {
     }
 
     public StateStoreProvider createStateStoreProvider(InstanceProperties instanceProperties) {
-        return StateStoreFactory.createProvider(instanceProperties, s3, dynamoDB, hadoopConfiguration);
+        return StateStoreFactory.createProvider(instanceProperties, s3, dynamoDB, null);
     }
 
     public TableIndex tableIndex(InstanceProperties instanceProperties) {
