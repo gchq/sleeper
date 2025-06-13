ce4a6809c1def6f40d962a4e54131671c0808492
patchwork01 <110390516+patchwork01@users.noreply.github.com>
2025-06-13 10:15:15 +0000

    4806 Mock server side error from S3

--- java/ingest/ingest-batcher-submitter/src/test/java/sleeper/ingest/batcher/submitter/IngestBatcherSubmitterLambdaWireMockIT.java
+++ java/ingest/ingest-batcher-submitter/src/test/java/sleeper/ingest/batcher/submitter/IngestBatcherSubmitterLambdaWireMockIT.java
@@ -18,16 +18,10 @@ package sleeper.ingest.batcher.submitter;
 import com.github.tomakehurst.wiremock.junit5.WireMockRuntimeInfo;
 import com.github.tomakehurst.wiremock.junit5.WireMockTest;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.services.s3.S3Client;
-import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.services.s3.model.S3Exception;
 import software.amazon.awssdk.services.sqs.SqsClient;
-import software.amazon.awssdk.services.sqs.model.Message;
-import software.amazon.awssdk.services.sqs.model.ReceiveMessageRequest;
-import software.amazon.awssdk.services.sqs.model.ReceiveMessageResponse;
 
 import sleeper.core.properties.instance.InstanceProperties;
 import sleeper.core.properties.table.TableProperties;
@@ -35,18 +29,16 @@ import sleeper.core.table.InMemoryTableIndex;
 import sleeper.core.table.TableIndex;
 import sleeper.core.table.TableStatusTestHelper;
 import sleeper.ingest.batcher.core.IngestBatcherStore;
-import sleeper.ingest.batcher.core.IngestBatcherTrackedFile;
 import sleeper.ingest.batcher.core.testutil.InMemoryIngestBatcherStore;
 
 import java.time.Instant;
-import java.util.List;
-import java.util.UUID;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
-import static com.github.tomakehurst.wiremock.client.WireMock.post;
+import static com.github.tomakehurst.wiremock.client.WireMock.findUnmatchedRequests;
+import static com.github.tomakehurst.wiremock.client.WireMock.get;
 import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
 import static org.assertj.core.api.Assertions.assertThat;
-import static sleeper.core.properties.instance.CdkDefinedInstanceProperty.INGEST_BATCHER_SUBMIT_DLQ_URL;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static sleeper.core.properties.table.TableProperty.TABLE_ID;
 import static sleeper.core.properties.table.TableProperty.TABLE_NAME;
 import static sleeper.core.properties.testutils.InstancePropertiesTestHelper.createTestInstanceProperties;
@@ -58,92 +50,46 @@ import static sleeper.localstack.test.WiremockAwsV2ClientHelper.wiremockAwsV2Cli
 @WireMockTest
 public class IngestBatcherSubmitterLambdaWireMockIT {
 
-    private S3Client s3Client;
-    private SqsClient sqsClient;
-    //private DynamoDbClient dynamoClient;
-    private static final Instant RECEIVED_TIME = Instant.parse("2023-06-16T10:57:00Z");
-    private final String testBucket = UUID.randomUUID().toString();
     private final InstanceProperties instanceProperties = createTestInstanceProperties();
     private final TableIndex tableIndex = new InMemoryTableIndex();
-    private static final String TEST_TABLE_ID = "test-table-id";
     private final TableProperties tableProperties = createTestTableProperties(instanceProperties, createSchemaWithKey("key"));
     private final IngestBatcherStore ingestBatcherStore = new InMemoryIngestBatcherStore();
 
-    //In Progress
-    //Been trying to understand how to make the s3Client call, I think I'm on the right track here but could be wrong
-    //I've taken code from BuildUptimeLambdaIt and WiremockTestHelper to create a mocked object to go in the builder
-    //Those didnt mock the s3Client though so it may not work for it
-    //I havent found how to add a retry mechanic into the sqs for this test, over riding the default behaviour.
+    @BeforeEach
+    void setUp() {
+        tableProperties.set(TABLE_ID, "test-table-id");
+        tableProperties.set(TABLE_NAME, "test-table");
+        tableIndex.create(TableStatusTestHelper.uniqueIdAndName("test-table-id", "test-table"));
+    }
 
-    @Nested
-    @DisplayName("Network Error test")
-    class NetworkErrorTest {
+    @Test
+    void shouldHandleNetworkErrorCorrectly(WireMockRuntimeInfo runtimeInfo) {
+        // Given
+        // Mock Network error
+        stubFor(get("/test-bucket?list-type=2&prefix=test-file-1.parquet")
+                .willReturn(aResponse().withStatus(500)));
 
-        @BeforeEach
-        void setUp(WireMockRuntimeInfo runtimeInfo) {
-            s3Client = wiremockAwsV2Client(runtimeInfo, S3Client.builder());
-            sqsClient = wiremockAwsV2ClientWithRetryAttempts(10, runtimeInfo, SqsClient.builder());
+        // Define job
+        String json = "{" +
+                "\"files\":[\"test-bucket/test-file-1.parquet\"]," +
+                "\"tableName\":\"test-table\"" +
+                "}";
+        IngestBatcherSubmitterLambda lambda = lambda(runtimeInfo);
 
-            tableIndex.create(TableStatusTestHelper.uniqueIdAndName(TEST_TABLE_ID, "test-table"));
-            instanceProperties.set(INGEST_BATCHER_SUBMIT_DLQ_URL, "ingest-batcher-dlq");
-            tableProperties.set(TABLE_ID, TEST_TABLE_ID);
-            tableProperties.set(TABLE_NAME, "test-table");
-        }
+        // When / Then
+        assertThatThrownBy(() -> lambda.handleMessage(json, Instant.parse("2023-06-16T10:57:00Z")))
+                .isInstanceOfSatisfying(S3Exception.class,
+                        e -> assertThat(e.statusCode()).isEqualTo(500));
+        assertThat(findUnmatchedRequests()).isEmpty();
+        assertThat(ingestBatcherStore.getAllFilesNewestFirst()).isEmpty();
+    }
 
-        @Test
-        void shouldHandleNetworkErrorCorrectly(WireMockRuntimeInfo runtimeInfo) {
-            uploadFileToS3("test-file-1.parquet");
-
-            //Mock Network error
-            final IngestBatcherSubmitterLambda lambda = new IngestBatcherSubmitterLambda(
-                    ingestBatcherStore, instanceProperties, tableIndex,
-                    new IngestBatcherSubmitDeadLetterQueue(instanceProperties, sqsClient),
-                    s3Client);
-            stubFor(post("/").willReturn(aResponse().withStatus(200)));
-
-            //Send job
-            String json = "{" +
-                    "\"files\":[\"" + testBucket + "/test-file-1.parquet\"]," +
-                    "\"tableName\":\"test-table\"" +
-                    "}";
-
-            // When
-            lambda.handleMessage(json, RECEIVED_TIME);
-
-            //Assert retry attempted
-            assertThat(ingestBatcherStore.getAllFilesNewestFirst())
-                    .containsExactly(
-                            fileRequest(testBucket + "/test-file-1.parquet"));
-            assertThat(receiveDeadLetters()).isEmpty();
-
-        }
-
-        private void uploadFileToS3(String filePath) {
-            s3Client.putObject(PutObjectRequest.builder()
-                    .bucket(testBucket)
-                    .key(filePath)
-                    .build(),
-                    RequestBody.fromString("test"));
-        }
-
-        private static IngestBatcherTrackedFile fileRequest(String filePath) {
-            return IngestBatcherTrackedFile.builder()
-                    .file(filePath)
-                    .fileSizeBytes(4)
-                    .tableId(TEST_TABLE_ID)
-                    .receivedTime(RECEIVED_TIME).build();
-        }
-
-        private List<String> receiveDeadLetters() {
-            ReceiveMessageResponse result = sqsClient.receiveMessage(ReceiveMessageRequest.builder()
-                    .queueUrl(instanceProperties.get(INGEST_BATCHER_SUBMIT_DLQ_URL))
-                    .maxNumberOfMessages(10)
-                    .waitTimeSeconds(1)
-                    .build());
-            return result.messages().stream()
-                    .map(Message::body)
-                    .toList();
-        }
-
+    private IngestBatcherSubmitterLambda lambda(WireMockRuntimeInfo runtimeInfo) {
+        S3Client s3Client = wiremockAwsV2Client(runtimeInfo, S3Client.builder().forcePathStyle(true));
+        SqsClient sqsClient = wiremockAwsV2ClientWithRetryAttempts(10, runtimeInfo, SqsClient.builder());
+        return new IngestBatcherSubmitterLambda(
+                ingestBatcherStore, instanceProperties, tableIndex,
+                new IngestBatcherSubmitDeadLetterQueue(instanceProperties, sqsClient),
+                s3Client);
     }
 }
