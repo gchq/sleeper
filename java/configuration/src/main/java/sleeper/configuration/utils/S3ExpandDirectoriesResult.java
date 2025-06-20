3137bac89ea4aa0b82ab67e8e6ee1cfc8e1e291b
patchwork01 <110390516+patchwork01@users.noreply.github.com>
2025-06-20 11:42:45 +0000

    5114 Add missing Javadoc

--- java/configuration/src/main/java/sleeper/configuration/utils/S3ExpandDirectoriesResult.java
+++ java/configuration/src/main/java/sleeper/configuration/utils/S3ExpandDirectoriesResult.java
@@ -10,18 +10,36 @@ import java.util.stream.Stream;
  */
 public record S3ExpandDirectoriesResult(List<S3PathContents> paths) {
 
-    public List<String> listHadoopPathsThrowIfAnyPathIsEmpty() {
+    /**
+     * Returns the files that were found, as Hadoop paths. Throws an exception if any requested path is empty. This is
+     * in the format s3a://bucket-name/object-key.
+     *
+     * @return                         the Hadoop paths
+     * @throws S3FileNotFoundException if any requested path is empty
+     */
+    public List<String> listHadoopPathsThrowIfAnyPathIsEmpty() throws S3FileNotFoundException {
         return streamFilesThrowIfAnyPathIsEmpty()
                 .map(S3FileDetails::pathForHadoop)
                 .toList();
     }
 
+    /**
+     * Returns the files that were found, as paths for ingest jobs. Also suitable for bulk import jobs or the ingest
+     * batcher. This is in the format bucket-name/object-key.
+     *
+     * @return the paths
+     */
     public List<String> listJobPaths() {
         return streamFiles()
                 .map(S3FileDetails::pathForJob)
                 .toList();
     }
 
+    /**
+     * Returns the requested paths that were not found. These are returned exactly as found in the request.
+     *
+     * @return the paths
+     */
     public List<String> listMissingPaths() {
         return paths.stream()
                 .filter(S3PathContents::isEmpty)
@@ -29,12 +47,23 @@ public record S3ExpandDirectoriesResult(List<S3PathContents> paths) {
                 .toList();
     }
 
-    public Stream<S3FileDetails> streamFilesThrowIfAnyPathIsEmpty() {
+    /**
+     * Returns the files that were found. Throws an exception if any requested path is empty.
+     *
+     * @return                         the files
+     * @throws S3FileNotFoundException if any requested path is empty
+     */
+    public Stream<S3FileDetails> streamFilesThrowIfAnyPathIsEmpty() throws S3FileNotFoundException {
         return paths.stream()
                 .peek(S3PathContents::throwIfEmpty)
                 .flatMap(path -> path.files().stream());
     }
 
+    /**
+     * Returns the files that were found.
+     *
+     * @return the files
+     */
     public Stream<S3FileDetails> streamFiles() {
         return paths.stream()
                 .flatMap(path -> path.files().stream());
