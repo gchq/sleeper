3b3c8d586401cf9f677def7e45aaf6c417708081
patchwork01 <110390516+patchwork01@users.noreply.github.com>
2025-07-08 12:55:24 +0000

    5189 Adjust assertion

--- java/system-test/system-test-suite/src/test/java/sleeper/systemtest/suite/CompactionDataFusionPerformanceST.java
+++ java/system-test/system-test-suite/src/test/java/sleeper/systemtest/suite/CompactionDataFusionPerformanceST.java
@@ -73,8 +73,7 @@ public class CompactionDataFusionPerformanceST {
         AllReferencesToAllFiles files = sleeper.tableFiles().all();
         assertThat(files.estimateRecordsInTable()).isEqualTo(4_400_000_000L);
         assertThat(files.listFileReferences()).hasSize(10);
-        assertThat(files.getFilesWithReferences())
-                .hasSize(10)
+        assertThat(files.getFilesWithReferences()).hasSize(10)
                 .first()
                 .satisfies(file -> assertThat(SortedRecordsCheck.check(DEFAULT_SCHEMA, sleeper.getRecords(file)))
                         .isEqualTo(SortedRecordsCheck.sorted(file.estimateRecords())));
