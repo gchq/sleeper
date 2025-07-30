e39016b201337ed1afbbfeb9a1644cb604e4a284
205762370+Rob9786 <205762370+Rob9786@users.noreply.github.com>
2025-07-30 13:56:19 +0000

    4526: Initial stab at ECS delete lambda

--- java/cdk-custom-resources/src/main/java/sleeper/cdk/custom/AutoDeleteEcsClusterLambda.java
+++ java/cdk-custom-resources/src/main/java/sleeper/cdk/custom/AutoDeleteEcsClusterLambda.java
@@ -15,6 +15,46 @@
  */
 package sleeper.cdk.custom;
 
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.events.CloudFormationCustomResourceEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import software.amazon.awssdk.services.ecs.EcsClient;
+
+import java.util.Map;
+
 public class AutoDeleteEcsClusterLambda {
+    public static final Logger LOGGER = LoggerFactory.getLogger(AutoDeleteEcsClusterLambda.class);
+
+    private final EcsClient ecsClient;
+
+    public AutoDeleteEcsClusterLambda() {
+        this(EcsClient.create());
+    }
+
+    public AutoDeleteEcsClusterLambda(EcsClient ecsClient) {
+        this.ecsClient = ecsClient;
+    }
+
+    public void handleEvent(CloudFormationCustomResourceEvent event, Context context) {
+        Map<String, Object> resourceProperties = event.getResourceProperties();
+        String clusterName = (String) resourceProperties.get("cluster");
+
+        switch (event.getRequestType()) {
+            case "Create":
+            case "Update":
+                break;
+            case "Delete":
+                deleteCluster(clusterName);
+                break;
+            default:
+                throw new IllegalArgumentException("Invalid request type: " + event.getRequestType());
+        }
+    }
+
+    private void deleteCluster(String clusterName) {
+        // Need to de-register containers from cluster
+        ecsClient.deleteCluster(request -> request.cluster(clusterName));
+    }
 
 }
