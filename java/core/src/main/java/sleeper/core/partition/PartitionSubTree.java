5e8240d2fa450b450b73d88f57e81981885d298b
rtjd6554 <rtjd6554@proton.me>
2025-07-31 10:31:38 +0000

    5275: Initial framework for PartitionSubTree

---
+++ java/core/src/main/java/sleeper/core/partition/PartitionSubTree.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2022-2025 Crown Copyright
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package sleeper.core.partition;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Generates a subtree of PartitionTree for given number of leaf nodes.
+ */
+public class PartitionSubTree extends PartitionTree {
+    private final ArrayList<String> resetLeafIds = new ArrayList<>();
+
+    public PartitionSubTree(PartitionTree originalTree, int leafPartitionCount) {
+        super(List.of(originalTree.getRootPartition()));
+
+        // Check loop has count incremented by 1 to account for root partition that always must exist
+        while (this.getAllPartitions().size() < leafPartitionCount + 1) {
+            resetLeafIds.clear();
+            getAllPartitions().forEach(partition -> {
+                partition.getChildPartitionIds().forEach(
+                        partitionId -> {
+                            if (!idToPartition.containsKey(partitionId)) {
+                                idToPartition.put(partitionId, originalTree.getPartition(partitionId));
+                                resetLeafIds.add(partitionId);
+                            }
+                        });
+            });
+        }
+
+        // Last gathered selection of ids are set to be leaves of new tree.
+        resetLeafIds.forEach(leafId -> {
+            idToPartition.put(leafId, idToPartition.get(leafId)
+                    .toBuilder()
+                    .leafPartition(true)
+                    .build());
+        });
+    }
+}
