b8a136e6a5e8c454e2cce5aaa3eba32362ad41d2
rtjd6554 <rtjd6554@proton.me>
2025-08-01 13:54:04 +0000

    5275: Class rename

--- java/core/src/test/java/sleeper/core/partition/PartitionSubTreeFactoryTest.java
+++ java/core/src/test/java/sleeper/core/partition/PartitionSubTreeFactoryTest.java
@@ -19,7 +19,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class PartitionSubTreeTest extends PartitionTreeTestBase {
+public class PartitionSubTreeFactoryTest extends PartitionTreeTestBase {
 
     @Test
     void shouldCreateSubTreeWithExactLeafPartitionCount() {
