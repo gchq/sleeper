c64c1d4d34b8653c46d956b1c59d5320cc5135bb
m09526 <m09526@users.noreply.github.com>
2025-08-14 16:37:13 +0000

    WIP process query config on Rust side

--- rust/sleeper_df/src/objects.rs
+++ rust/sleeper_df/src/objects.rs
@@ -19,7 +19,8 @@ use crate::unpack::{
 };
 use color_eyre::eyre::bail;
 use sleeper_core::{
-    ColRange, CommonConfig, CompletionOptions, SleeperParquetOptions, SleeperPartitionRegion,
+    ColRange, CommonConfig, CompletionOptions, LeafPartitionQueryConfig, SleeperParquetOptions,
+    SleeperPartitionRegion,
 };
 use std::{
     borrow::Borrow,
@@ -28,120 +29,13 @@ use std::{
 };
 use url::Url;
 
-/// Contains all the common input data for setting up a Sleeper DataFusion operation.
-///
-/// See `java/compaction/compaction-datafusion/src/main/java/sleeper/compaction/datafusion/DataFusionFunctions.java`
-/// for details. Field ordering and types MUST match between the two definitions!
+/// Contains all output data from a compaction operation.
 #[repr(C)]
-pub struct FFICommonConfig {
-    pub override_aws_config: bool,
-    pub aws_region: *const c_char,
-    pub aws_endpoint: *const c_char,
-    pub aws_access_key: *const c_char,
-    pub aws_secret_key: *const c_char,
-    pub aws_allow_http: bool,
-    pub input_files_len: usize,
-    pub input_files: *const *const c_char,
-    pub input_files_sorted: bool,
-    pub output_file: *const c_char,
-    pub row_key_cols_len: usize,
-    pub row_key_cols: *const *const c_char,
-    pub row_key_schema_len: usize,
-    pub row_key_schema: *const *const c_int,
-    pub sort_key_cols_len: usize,
-    pub sort_key_cols: *const *const c_char,
-    pub max_row_group_size: usize,
-    pub max_page_size: usize,
-    pub compression: *const c_char,
-    pub writer_version: *const c_char,
-    pub column_truncate_length: usize,
-    pub stats_truncate_length: usize,
-    pub dict_enc_row_keys: bool,
-    pub dict_enc_sort_keys: bool,
-    pub dict_enc_values: bool,
-    pub region: *const FFISleeperRegion,
-    pub iterator_config: *const c_char,
-}
-
-impl<'a> TryFrom<&'a FFICommonConfig> for CommonConfig<'a> {
-    type Error = color_eyre::eyre::Report;
-
-    fn try_from(params: &'a FFICommonConfig) -> Result<CommonConfig<'a>, Self::Error> {
-        if params.iterator_config.is_null() {
-            bail!("FFICompactionsParams iterator_config is NULL");
-        }
-        if params.output_file.is_null() {
-            bail!("FFICompactionParams output_file is NULL");
-        }
-        if params.compression.is_null() {
-            bail!("FFICompactionParams compression is NULL");
-        }
-        if params.writer_version.is_null() {
-            bail!("FFICompactionParams writer_version is NULL");
-        }
-        if params.region.is_null() {
-            bail!("FFICompactionParams region is NULL");
-        }
-        // We do this separately since we need the values for computing the region
-        let row_key_cols = unpack_string_array(params.row_key_cols, params.row_key_cols_len)?
-            .into_iter()
-            .map(String::from)
-            .collect::<Vec<_>>();
-
-        // Contains numeric types to indicate schema types
-        let schema_types =
-            unpack_primitive_array(params.row_key_schema, params.row_key_schema_len)?;
-
-        let ffi_region = unsafe { params.region.as_ref() }.unwrap();
-        let region = FFISleeperRegion::to_sleeper_region(ffi_region, &row_key_cols, &schema_types)?;
-
-        // Extract iterator config
-        let iterator_config = Some(
-            unsafe { CStr::from_ptr(params.iterator_config) }
-                .to_str()?
-                .to_owned(),
-        )
-        // Set option to None if config is empty
-        .and_then(|v| if v.trim().is_empty() { None } else { Some(v) });
-
-        let opts = SleeperParquetOptions {
-            max_row_group_size: params.max_row_group_size,
-            max_page_size: params.max_page_size,
-            compression: unsafe { CStr::from_ptr(params.compression) }
-                .to_str()?
-                .to_owned(),
-            writer_version: unsafe { CStr::from_ptr(params.writer_version) }
-                .to_str()?
-                .to_owned(),
-            column_truncate_length: params.column_truncate_length,
-            stats_truncate_length: params.stats_truncate_length,
-            dict_enc_row_keys: params.dict_enc_row_keys,
-            dict_enc_sort_keys: params.dict_enc_sort_keys,
-            dict_enc_values: params.dict_enc_values,
-        };
-
-        Self::try_new(
-            unpack_aws_config(params)?,
-            unpack_string_array(params.input_files, params.input_files_len)?
-                .into_iter()
-                .map(Url::parse)
-                .collect::<Result<Vec<_>, _>>()?,
-            params.input_files_sorted,
-            row_key_cols,
-            unpack_string_array(params.sort_key_cols, params.sort_key_cols_len)?
-                .into_iter()
-                .map(String::from)
-                .collect(),
-            region,
-            CompletionOptions::File {
-                output_file: unsafe { CStr::from_ptr(params.output_file) }
-                    .to_str()
-                    .map(Url::parse)??,
-                opts,
-            },
-            iterator_config,
-        )
-    }
+pub struct FFICompactionResult {
+    /// The total number of rows read by a compaction.
+    pub rows_read: usize,
+    /// The total number of rows written by a compaction.
+    pub rows_written: usize,
 }
 
 /// Represents a Sleeper region in a C ABI struct.
@@ -211,13 +105,120 @@ impl<'a> FFISleeperRegion {
     }
 }
 
-/// Contains all output data from a compaction operation.
+/// Contains all the common input data for setting up a Sleeper DataFusion operation.
+///
+/// See `java/compaction/compaction-datafusion/src/main/java/sleeper/compaction/datafusion/DataFusionFunctions.java`
+/// for details. Field ordering and types MUST match between the two definitions!
 #[repr(C)]
-pub struct FFICompactionResult {
-    /// The total number of rows read by a compaction.
-    pub rows_read: usize,
-    /// The total number of rows written by a compaction.
-    pub rows_written: usize,
+pub struct FFICommonConfig {
+    pub override_aws_config: bool,
+    pub aws_region: *const c_char,
+    pub aws_endpoint: *const c_char,
+    pub aws_access_key: *const c_char,
+    pub aws_secret_key: *const c_char,
+    pub aws_allow_http: bool,
+    pub input_files_len: usize,
+    pub input_files: *const *const c_char,
+    pub input_files_sorted: bool,
+    pub output_file: *const c_char,
+    pub row_key_cols_len: usize,
+    pub row_key_cols: *const *const c_char,
+    pub row_key_schema_len: usize,
+    pub row_key_schema: *const *const c_int,
+    pub sort_key_cols_len: usize,
+    pub sort_key_cols: *const *const c_char,
+    pub max_row_group_size: usize,
+    pub max_page_size: usize,
+    pub compression: *const c_char,
+    pub writer_version: *const c_char,
+    pub column_truncate_length: usize,
+    pub stats_truncate_length: usize,
+    pub dict_enc_row_keys: bool,
+    pub dict_enc_sort_keys: bool,
+    pub dict_enc_values: bool,
+    pub region: *const FFISleeperRegion,
+    pub iterator_config: *const c_char,
+}
+
+impl<'a> TryFrom<&'a FFICommonConfig> for CommonConfig<'a> {
+    type Error = color_eyre::eyre::Report;
+
+    fn try_from(params: &'a FFICommonConfig) -> Result<CommonConfig<'a>, Self::Error> {
+        if params.iterator_config.is_null() {
+            bail!("FFICommonConfig iterator_config is NULL");
+        }
+        if params.output_file.is_null() {
+            bail!("FFICommonConfig output_file is NULL");
+        }
+        if params.compression.is_null() {
+            bail!("FFICommonConfig compression is NULL");
+        }
+        if params.writer_version.is_null() {
+            bail!("FFICommonConfig writer_version is NULL");
+        }
+        if params.region.is_null() {
+            bail!("FFICommonConfig region is NULL");
+        }
+        // We do this separately since we need the values for computing the region
+        let row_key_cols = unpack_string_array(params.row_key_cols, params.row_key_cols_len)?
+            .into_iter()
+            .map(String::from)
+            .collect::<Vec<_>>();
+
+        // Contains numeric types to indicate schema types
+        let schema_types =
+            unpack_primitive_array(params.row_key_schema, params.row_key_schema_len)?;
+
+        let ffi_region = unsafe { params.region.as_ref() }.unwrap();
+        let region = FFISleeperRegion::to_sleeper_region(ffi_region, &row_key_cols, &schema_types)?;
+
+        // Extract iterator config
+        let iterator_config = Some(
+            unsafe { CStr::from_ptr(params.iterator_config) }
+                .to_str()?
+                .to_owned(),
+        )
+        // Set option to None if config is empty
+        .and_then(|v| if v.trim().is_empty() { None } else { Some(v) });
+
+        let opts = SleeperParquetOptions {
+            max_row_group_size: params.max_row_group_size,
+            max_page_size: params.max_page_size,
+            compression: unsafe { CStr::from_ptr(params.compression) }
+                .to_str()?
+                .to_owned(),
+            writer_version: unsafe { CStr::from_ptr(params.writer_version) }
+                .to_str()?
+                .to_owned(),
+            column_truncate_length: params.column_truncate_length,
+            stats_truncate_length: params.stats_truncate_length,
+            dict_enc_row_keys: params.dict_enc_row_keys,
+            dict_enc_sort_keys: params.dict_enc_sort_keys,
+            dict_enc_values: params.dict_enc_values,
+        };
+
+        Self::try_new(
+            unpack_aws_config(params)?,
+            unpack_string_array(params.input_files, params.input_files_len)?
+                .into_iter()
+                .map(Url::parse)
+                .collect::<Result<Vec<_>, _>>()?,
+            params.input_files_sorted,
+            row_key_cols,
+            unpack_string_array(params.sort_key_cols, params.sort_key_cols_len)?
+                .into_iter()
+                .map(String::from)
+                .collect(),
+            region,
+            CompletionOptions::File {
+                output_file: unsafe { CStr::from_ptr(params.output_file) }
+                    .to_str()
+                    .map(Url::parse)??,
+                opts,
+            },
+            iterator_config,
+        )
+    }
 }
 
 /// Contains all information needed for a Sleeper leaf partition query from a foreign function.
@@ -233,4 +234,17 @@ pub struct FFILeafPartitionQueryConfig {
     pub write_quantile_sketch: bool,
     /// Should logical and physical query plans be written to log?
     pub explain_plans: bool,
+}
+
+impl<'a> TryFrom<&'a FFILeafPartitionQueryConfig> for LeafPartitionQueryConfig<'a> {
+    type Error = color_eyre::eyre::Report;
+
+    fn try_from(config: &'a FFILeafPartitionQueryConfig) -> Result<Self, Self::Error> {
+        let Some(ffi_common) = (unsafe { config.common.as_ref() }) else {
+            bail!("FFILeafPartitionQueryConfig common is NULL");
+        };
+        let common = TryInto::<CommonConfig>::try_into(ffi_common)?;
+
+        //TODO: unpack query regions
+    }
 }
