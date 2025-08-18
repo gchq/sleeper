10aebf12ef939c8e6f0ff65fca04007616abd4a7
ab295382 <134393532+ab295382@users.noreply.github.com>
2025-08-18 10:21:45 +0000

    5317 - Updating the help docs for ArgumentParser

--- python/test/range_query_via_web_socket.py
+++ python/test/range_query_via_web_socket.py
@@ -27,7 +27,11 @@ async def run():
     sleeper_client = SleeperClient(args.instance)
 
     logger.debug("Sending query via web socket")
-    rows = await sleeper_client.web_socket_exact_key_query(table_name=args.table, keys=args.keys, query_id=args.queryid, strings_base64_encoded=args.base64_encoded)
+    """[{'column1': {'min': 1, 'max': 10}}, {'column2': {'min': 5, 'max': 15}}]"""
+
+    rows = await sleeper_client.web_socket_range_key_query(
+        table_name=args.table, keys=args.keys, query_id=args.queryid, min_inclusive=args.min_inclusive, max_inclusive=args.max_inclusive, strings_base64_encoded=args.base64_encoded
+    )
 
     file_path = args.outdir + "/" + args.queryid + ".parquet"
     logger.debug(f"Saving resulst to disk at {file_path}")
@@ -45,8 +49,10 @@ if __name__ == "__main__":
     parser.add_argument("--instance", required=True, help="Sleeper Instance Id")
     parser.add_argument("--table", required=True, help="The table to query")
     parser.add_argument("--queryid", required=True, help="An id to idnetify the query")
+    parser.add_argument("--min_inclusive", required=False, default=True, help="Include the min value in the results")
+    parser.add_argument("--max_inclusive", required=False, default=True, help="Include the max value in the results")
     parser.add_argument("--base64_encoded", required=False, default=False, help="Set if the data in Sleeper is base64 encoded")
-    parser.add_argument("--keys", type=json.loads, required=True, help='Example: "{"key":["a", "z"]}"')
+    parser.add_argument("--keys", type=json.loads, required=True, help='Example: "[{"key1": {"min": "a", "max": "b"}}, {"key2": {"min": "x", "max": "z"}}]"')
     parser.add_argument("--outdir", required=True, help="Output directory to save the results Parquet file to")
     parser.add_argument("--debug", action="store_true", help="Enable debug logging")
 
