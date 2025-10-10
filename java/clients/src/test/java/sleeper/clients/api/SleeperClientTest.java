4e7543be347aa2fef4a31a5eb1f167075fd3d1d0
ab295382 <134393532+ab295382@users.noreply.github.com>
2025-10-10 14:12:05 +0000

    5318 - Adding Sleeper Client tesrts for Web Socket queries

--- java/clients/src/test/java/sleeper/clients/api/SleeperClientTest.java
+++ java/clients/src/test/java/sleeper/clients/api/SleeperClientTest.java
@@ -20,6 +20,9 @@ import org.junit.jupiter.api.Test;
 import sleeper.bulkexport.core.model.BulkExportQuery;
 import sleeper.bulkimport.core.configuration.BulkImportPlatform;
 import sleeper.bulkimport.core.job.BulkImportJob;
+import sleeper.clients.query.FakeWebSocketConnection;
+import sleeper.clients.query.FakeWebSocketConnection.WebSocketResponse;
+import sleeper.clients.query.QueryWebSocketClient;
 import sleeper.core.iterator.IteratorCreationException;
 import sleeper.core.iterator.closeable.CloseableIterator;
 import sleeper.core.partition.PartitionsBuilder;
@@ -29,19 +32,25 @@ import sleeper.core.properties.table.TableProperties;
 import sleeper.core.range.Range.RangeFactory;
 import sleeper.core.range.Region;
 import sleeper.core.row.Row;
+import sleeper.core.schema.Field;
 import sleeper.core.schema.Schema;
 import sleeper.core.schema.type.StringType;
 import sleeper.ingest.batcher.core.IngestBatcherSubmitRequest;
 import sleeper.ingest.core.job.IngestJob;
 import sleeper.ingest.runner.impl.IngestCoordinator;
 import sleeper.query.core.model.Query;
+import sleeper.query.core.model.QuerySerDe;
+import sleeper.query.core.output.ResultsOutputLocation;
 import sleeper.query.core.rowretrieval.QueryExecutor;
+import sleeper.query.runner.websocket.QueryWebSocketMessage;
+import sleeper.query.runner.websocket.QueryWebSocketMessageSerDe;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -57,6 +66,9 @@ class SleeperClientTest {
     Schema schema = createSchemaWithKey("key", new StringType());
     InMemorySleeperInstance instance = new InMemorySleeperInstance(instanceProperties);
     SleeperClient sleeperClient = instance.sleeperClientBuilder().build();
+    QuerySerDe querySerDe = new QuerySerDe(schema);
+    QueryWebSocketMessageSerDe serDe = QueryWebSocketMessageSerDe.withNoBatchSize(schema);
+    Field rowKey = schema.getField("key").orElseThrow();
 
     @Test
     void validateThatClientCannotBeCreatedWithNulls() {
@@ -156,6 +168,66 @@ class SleeperClientTest {
     }
 
     @Test
+    void shouldTestWebSocketExactQuery() throws Exception {
+        FakeWebSocketConnection connection = new FakeWebSocketConnection();
+        String tableName = "table-name";
+        addTable(tableName);
+        ingest(tableName, List.of(
+                new Row(Map.of("key", "123")),
+                new Row(Map.of("key", "456")),
+                new Row(Map.of("key", "789"))));
+
+        Query query = exactQuery("test-query-id", "123", tableName);
+        Row expectedRow = new Row(Map.of("key", "123"));
+        connection.setFakeResponses(
+                message(queryResult("test-query-id", expectedRow)),
+                message(completedQuery("test-query-id", 1L)));
+        QueryWebSocketClient realClient = new QueryWebSocketClient(
+                instanceProperties, instance.tablePropertiesProvider(), connection.createAdapter(), 0);
+
+        // When / Then
+        assertThat(runQueryFuture(query, realClient))
+                .isCompletedWithValue(List.of(expectedRow));
+        assertThat(connection.isConnected()).isFalse();
+        assertThat(connection.isClosed()).isTrue();
+        assertThat(connection.getSentMessages())
+                .containsExactly(querySerDe.toJson(query));
+    }
+
+    @Test
+    void shouldTestWebSocketRangeQuery() throws Exception {
+        FakeWebSocketConnection connection = new FakeWebSocketConnection();
+        String tableName = "table-name";
+        addTable(tableName);
+        ingest(tableName, List.of(
+                new Row(Map.of("key", "aaa")),
+                new Row(Map.of("key", "bbb")),
+                new Row(Map.of("key", "ccc"))));
+
+        Query query = rangeQuery("test-query-id", "aaa", "bbb", tableName);
+        Row expectedRow1 = new Row(Map.of("key", "aaa"));
+        Row expectedRow2 = new Row(Map.of("key", "bbb"));
+
+        connection.setFakeResponses(
+                message(createdSubQueries("test-query-id", "subquery-1", "subquery-2")),
+                message(queryResult("subquery-1", expectedRow1)),
+                message(completedQuery("subquery-1", 1L)),
+                message(queryResult("subquery-2", expectedRow2)),
+                message(completedQuery("subquery-2", 1L)));
+
+        QueryWebSocketClient realClient = new QueryWebSocketClient(
+                instanceProperties, instance.tablePropertiesProvider(), connection.createAdapter(), 0);
+
+        // When / Then
+        assertThat(runQueryFuture(query, realClient))
+                .isCompletedWithValue(List.of(expectedRow1, expectedRow2));
+        assertThat(connection.isConnected()).isFalse();
+        assertThat(connection.isClosed()).isTrue();
+        assertThat(connection.getSentMessages())
+                .containsExactly(querySerDe.toJson(query));
+    }
+
+    @Test
     void shouldIngestParquetFilesFromS3() {
         String tableName = "ingest-table";
         List<String> fileList = List.of("filename1.parquet", "filename2.parquet");
@@ -230,7 +302,7 @@ class SleeperClientTest {
     }
 
     private void addTable(String tableName) {
-        TableProperties tableProperties = createTableProperties("test-table");
+        TableProperties tableProperties = createTableProperties(tableName);
         sleeperClient.addTable(tableProperties, List.of());
     }
 
@@ -242,6 +314,43 @@ class SleeperClientTest {
         } catch (IteratorCreationException | IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private String createdSubQueries(String queryId, String... subQueryIds) {
+        return serDe.toJson(QueryWebSocketMessage.queryWasSplitToSubqueries(queryId, List.of(subQueryIds)));
+    }
+
+    private WebSocketResponse message(String message) {
+        return client -> client.onMessage(message);
+    }
+
+    private CompletableFuture<List<Row>> runQueryFuture(Query query, QueryWebSocketClient client) throws Exception {
+        return client.submitQuery(query);
+    }
+
+    private Query exactQuery(String queryId, String value, String tableName) {
+        return Query.builder()
+                .tableName(tableName)
+                .queryId(queryId)
+                .regions(List.of(new Region(rangeFactory().createExactRange(rowKey, value))))
+                .build();
+    }
+
+    private Query rangeQuery(String queryId, String min, String max, String tableName) {
+        return Query.builder()
+                .tableName(tableName)
+                .queryId(queryId)
+                .regions(List.of(new Region(rangeFactory().createRange(rowKey, min, max))))
+                .build();
+    }
+
+    private String queryResult(String queryId, Row... rows) {
+        return serDe.toJson(QueryWebSocketMessage.rowsBatch(queryId, List.of(rows)));
+    }
+
+    private String completedQuery(String queryId, long rowCount) {
+        return serDe.toJson(QueryWebSocketMessage.queryCompleted(queryId, rowCount,
+                List.of(new ResultsOutputLocation("websocket-endpoint", "test-endpoint"))));
     }
 
     private RangeFactory rangeFactory() {
