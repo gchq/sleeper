1964f6ad0fd47a334c792b11f83951cb6a288899
patchwork01 <110390516+patchwork01@users.noreply.github.com>
2025-10-10 12:39:39 +0000

    5777 Log DataFusion root error

--- rust/sleeper_core/src/lib.rs
+++ rust/sleeper_core/src/lib.rs
@@ -368,19 +368,9 @@ pub async fn run_compaction(config: &CommonConfig<'_>) -> Result<CompactionResul
         .map_err(|e| {
             error!("DataFusion error: {e}");
             error!("DataFusion error debug output: {e:?}");
-            if let DataFusionError::ArrowError(
-                ArrowError::ExternalError(external_error),
-                backtrace,
-            ) = &e
-            {
-                error!("Found Arrow external error");
-                if let Some(backtrace_str) = backtrace {
-                    error!("Backtrace: {backtrace_str}");
-                }
-                error!("Backtrace debug output: {backtrace:?}");
-                error!("External error: {external_error}");
-                error!("External error debug output: {external_error:?}");
-            }
+            let root_error = e.find_root();
+            error!("DataFusion root error: {root_error}");
+            error!("DataFusion root error debug output: {root_error:?}");
             e.into()
         })
 }
