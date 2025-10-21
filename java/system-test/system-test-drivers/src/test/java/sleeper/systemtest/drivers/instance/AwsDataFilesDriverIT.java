d5cf2a6d83095ce7b6def879195d5094ba9d52f7
patchwork01 <110390516+patchwork01@users.noreply.github.com>
2025-10-21 15:10:59 +0000

    5873 Merge DataFileDuplicationIT into AwsDataFilesDriverIT

--- java/system-test/system-test-drivers/src/test/java/sleeper/systemtest/drivers/instance/AwsDataFilesDriverIT.java
+++ java/system-test/system-test-drivers/src/test/java/sleeper/systemtest/drivers/instance/AwsDataFilesDriverIT.java
@@ -15,30 +15,53 @@
  */
 package sleeper.systemtest.drivers.instance;
 
+import org.apache.hadoop.fs.Path;
+import org.apache.parquet.hadoop.ParquetWriter;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import sleeper.core.iterator.closeable.CloseableIterator;
+import sleeper.core.properties.instance.InstanceProperties;
+import sleeper.core.properties.table.TableProperties;
 import sleeper.core.row.Row;
+import sleeper.core.schema.Schema;
+import sleeper.core.schema.type.StringType;
+import sleeper.core.statestore.FileReference;
+import sleeper.core.statestore.FileReferenceFactory;
+import sleeper.core.statestore.StateStore;
+import sleeper.localstack.test.SleeperLocalStackClients;
+import sleeper.parquet.row.ParquetReaderIterator;
+import sleeper.parquet.row.ParquetRowReaderFactory;
+import sleeper.parquet.row.ParquetRowWriterFactory;
 import sleeper.systemtest.drivers.testutil.LocalStackDslTest;
 import sleeper.systemtest.dsl.SleeperSystemTest;
+import sleeper.systemtest.dsl.SystemTestContext;
+import sleeper.systemtest.dsl.util.DataFileDuplications;
 
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import java.util.stream.LongStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static sleeper.core.schema.SchemaTestHelper.createSchemaWithKey;
 import static sleeper.systemtest.drivers.testutil.LocalStackTestInstance.LOCALSTACK_MAIN;
+import static sleeper.systemtest.dsl.util.SystemTestSchema.DEFAULT_SCHEMA;
 
 @LocalStackDslTest
 public class AwsDataFilesDriverIT {
 
     @BeforeEach
     void setUp(SleeperSystemTest sleeper) {
-        sleeper.connectToInstanceAddOnlineTable(LOCALSTACK_MAIN);
+        sleeper.connectToInstanceNoTables(LOCALSTACK_MAIN);
     }
 
     @Test
     void shouldReadFile(SleeperSystemTest sleeper) throws Exception {
         // Given
+        sleeper.tables().create("test", DEFAULT_SCHEMA);
         sleeper.sourceFiles().inDataBucket()
                 .createWithNumberedRows("test.parquet", LongStream.of(1, 3, 2));
         sleeper.ingest().toStateStore().addFileOnEveryPartition("test.parquet", 3);
@@ -54,4 +77,60 @@ public class AwsDataFilesDriverIT {
                 });
     }
 
+    @Test
+    void shouldDuplicateFiles(SleeperSystemTest sleeper, SystemTestContext context) {
+        // Given
+        sleeper.tables().create("test", createSchemaWithKey("key", new StringType()));
+        FileReference file1 = referenceFactory(context).rootFile("file-1", 1);
+        FileReference file2 = referenceFactory(context).rootFile("file-2", 1);
+        writeRows(context, file1, List.of(new Row(Map.of("key", "value-1"))));
+        writeRows(context, file2, List.of(new Row(Map.of("key", "value-2")), new Row(Map.of("key", "value-3"))));
+
+        // When
+        DataFileDuplications duplications = sleeper.ingest().toStateStore().duplicateFilesOnSamePartitions(1, List.of(file1, file2));
+
+        // Then
+        List<FileReference> results = duplications.streamNewReferences().toList();
+        assertThat(results).hasSize(2);
+        assertThat(readRows(context, results.get(0)))
+                .containsExactly(new Row(Map.of("key", "value-1")));
+        assertThat(readRows(context, results.get(1)))
+                .containsExactly(new Row(Map.of("key", "value-2")), new Row(Map.of("key", "value-3")));
+    }
+
+    private FileReferenceFactory referenceFactory(SystemTestContext context) {
+        InstanceProperties instanceProperties = context.instance().getInstanceProperties();
+        TableProperties tableProperties = context.instance().getTableProperties();
+        StateStore stateStore = context.instance().getStateStore();
+        return FileReferenceFactory.from(instanceProperties, tableProperties, stateStore);
+    }
+
+    private void writeRows(SystemTestContext context, FileReference file, List<Row> rows) {
+        try (ParquetWriter<Row> writer = ParquetRowWriterFactory.createParquetRowWriter(
+                new Path(file.getFilename()), schema(context), SleeperLocalStackClients.HADOOP_CONF)) {
+            for (Row row : rows) {
+                writer.write(row);
+            }
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
+    }
+
+    private List<Row> readRows(SystemTestContext context, FileReference file) {
+        try (ParquetReaderIterator iterator = new ParquetReaderIterator(
+                ParquetRowReaderFactory.parquetRowReaderBuilder(
+                        new Path(file.getFilename()), schema(context))
+                        .withConf(SleeperLocalStackClients.HADOOP_CONF)
+                        .build())) {
+            List<Row> rows = new ArrayList<>();
+            iterator.forEachRemaining(rows::add);
+            return rows;
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
+    }
+
+    private Schema schema(SystemTestContext context) {
+        return context.instance().getTableProperties().getSchema();
+    }
 }
