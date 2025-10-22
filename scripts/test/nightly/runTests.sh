0c7ac78f02ae79345780d72cca3da903648a0258
ca61688 <206189192+ca61688@users.noreply.github.com>
2025-10-22 12:49:13 +0000

    5988: Test fix for python issue

--- scripts/test/nightly/runTests.sh
+++ scripts/test/nightly/runTests.sh
@@ -75,7 +75,7 @@ sudo rm -rf quick
 mkdir quick
 sudo cp -r java quick/java
 sudo cp -r scripts quick/scripts
-sudo cp -r python quick/python
+cp -r python quick/python
 sudo cp -r code-style quick/code-style
 sudo cp -r rust quick/rust
 cp README.md quick
