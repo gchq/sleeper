1893d77278f46c8e33108b7eeacb9d0f0aa5a9e1
ca61688 <206189192+ca61688@users.noreply.github.com>
2025-10-27 11:10:57 +0000

    5688: Test copying files with rsync

---
+++ scripts/functions/checkInstalled.sh
@@ -0,0 +1,10 @@
+checkInstalled() {
+    if [ $(dpkg-query -W -f='${Status}' $1 2>/dev/null | grep -c "ok installed") -eq 0 ];
+    then
+    echo "$1 not installed. Installing now..."
+    sudo apt-get update
+    sudo apt-get install --yes $1
+    else
+    echo "$1 already installed."
+    fi
+}
