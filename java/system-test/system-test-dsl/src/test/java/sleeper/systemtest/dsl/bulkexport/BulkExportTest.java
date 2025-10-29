799350f7a92a0e9fb17e4cc3bc32abb4f5afab7f
rtjd6554 <rtjd6554@proton.me>
2025-10-29 10:23:18 +0000

    5898: Initial framework

---
+++ java/system-test/system-test-dsl/src/test/java/sleeper/systemtest/dsl/bulkexport/BulkExportTest.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2022-2025 Crown Copyright
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package sleeper.systemtest.dsl.bulkexport;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import sleeper.systemtest.dsl.SleeperSystemTest;
+import sleeper.systemtest.dsl.testutil.InMemoryDslTest;
+
+import static sleeper.systemtest.dsl.testutil.InMemoryTestInstance.IN_MEMORY_MAIN;
+
+@InMemoryDslTest
+public class BulkExportTest {
+
+    @BeforeEach
+    void setUp(SleeperSystemTest sleeper) throws Exception {
+        sleeper.connectToInstanceNoTables(IN_MEMORY_MAIN);
+        /*
+         * sleeper.tables().createWithProperties("compaction", DEFAULT_SCHEMA, Map.of(
+         * COMPACTION_STRATEGY_CLASS, BasicCompactionStrategy.class.getName(),
+         * COMPACTION_FILES_BATCH_SIZE, "2"));
+         */
+    }
+
+    @Test
+    void shouldPerformBasicBulkExport(SleeperSystemTest sleeper) throws Exception {
+
+    }
+
+}
