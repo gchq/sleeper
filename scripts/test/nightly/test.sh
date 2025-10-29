b1d88767d9ffebec608cef8580fb6b599f6c89b6
ca61688 <206189192+ca61688@users.noreply.github.com>
2025-10-29 10:41:46 +0000

    5688: Fix rsync pausing issue

--- scripts/test/nightly/test.sh
+++ scripts/test/nightly/test.sh
@@ -1,7 +1,7 @@
 if [ $(dpkg-query -W -f='${Status}' $1 2>/dev/null | grep -c "ok installed") -eq 0 ]; then
         echo "$1 not installed. Installing now..."
         sudo apt-get update
-        sudo yes | apt-get install $1
+        sudo apt-get install --yes $1
         echo "test"
     else
         echo "$1 already installed."
