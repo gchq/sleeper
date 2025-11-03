599add9ae53ba915fbd708d9b0542652f185dbf0
patchwork01 <110390516+patchwork01@users.noreply.github.com>
2025-11-03 08:48:29 +0000

    5389 Add SleeperArtefactsCdkApp

---
+++ java/cdk/src/main/java/sleeper/cdk/SleeperArtefactsCdkApp.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2022-2025 Crown Copyright
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package sleeper.cdk;
+
+import software.amazon.awscdk.App;
+import software.amazon.awscdk.AppProps;
+import software.amazon.awscdk.Environment;
+import software.amazon.awscdk.StackProps;
+
+import sleeper.cdk.stack.SleeperArtefactsStack;
+
+public class SleeperArtefactsCdkApp {
+
+    public static void main(String[] args) {
+        App app = new App(AppProps.builder()
+                .analyticsReporting(false)
+                .build());
+        Environment environment = Environment.builder()
+                .account(System.getenv("CDK_DEFAULT_ACCOUNT"))
+                .region(System.getenv("CDK_DEFAULT_REGION"))
+                .build();
+        new SleeperArtefactsStack(app, "SleeperArtefacts", StackProps.builder()
+                .stackName("SleeperArtefacts")
+                .env(environment)
+                .build());
+        app.synth();
+    }
+
+}
