a1614b8565ba9078833d20724d8682b285a32b7d
m09526 <m09526@users.noreply.github.com>
2025-11-04 15:46:12 +0000

    Fix merge issues

--- rust/apps/tests/compact_cli.rs
+++ rust/apps/tests/compact_cli.rs
@@ -19,8 +19,8 @@ use predicates::prelude::*; // Used for writing assertions
 use std::process::Command; // Run programs
 
 #[test]
-fn invalid_region_maxs() -> Result<()> {
-    let mut cmd = cargo::cargo_bin!("compact")?;
+fn invalid_region_maxs() {
+    let mut cmd = Command::new(cargo_bin!("compact"));
     cmd.args([
         "/tmp/output.parquet",
         "/tmp/input.parquet",
