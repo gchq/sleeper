b43bc2486cdf00be6b6ee6176fbd7b0db58534e1
patchwork01 <110390516+patchwork01@users.noreply.github.com>
2025-12-09 15:39:43 +0000

    6233 Add Trivy to builder image

--- scripts/dev/trivy.sh
+++ scripts/dev/trivy.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker run -v $HOME/.trivy/cache:/root/.cache -v /var/run/docker.sock/:/var/run/docker.sock aquasec/trivy "$@"
+docker run -v $HOME/.cache/trivy:/root/.cache -v /var/run/docker.sock/:/var/run/docker.sock aquasec/trivy "$@"
