226f990d789a49102813236e868b8153f482a953
patchwork01 <110390516+patchwork01@users.noreply.github.com>
2025-12-19 13:13:30 +0000

    6273 Javadoc TearDownClients

--- java/clients/src/main/java/sleeper/clients/teardown/TearDownClients.java
+++ java/clients/src/main/java/sleeper/clients/teardown/TearDownClients.java
@@ -20,31 +20,34 @@ import software.amazon.awssdk.services.cloudformation.CloudFormationClient;
 import software.amazon.awssdk.services.cloudwatchevents.CloudWatchEventsClient;
 
 import java.io.IOException;
-import java.util.Objects;
 
+/**
+ * AWS clients used when tearing down Sleeper instances and system test deployments.
+ */
 public class TearDownClients {
 
     private final CloudWatchEventsClient cloudWatch;
     private final CloudFormationClient cloudFormation;
 
-    private TearDownClients(Builder builder) {
-        cloudWatch = Objects.requireNonNull(builder.cloudWatch, "cloudWatch must not be null");
-        cloudFormation = Objects.requireNonNull(builder.cloudFormation, "cloudFormation must not be null");
+    private TearDownClients(CloudWatchEventsClient cloudWatch, CloudFormationClient cloudFormation) {
+        this.cloudWatch = cloudWatch;
+        this.cloudFormation = cloudFormation;
     }
 
+    /**
+     * Performs some operation that needs an instance of this class. Creates AWS clients and then closes them at the
+     * end. This is a convenience for use in a main method.
+     *
+     * @param  operation            the operation to perform with the clients
+     * @throws IOException          if the operation threw an IOException
+     * @throws InterruptedException if the operation threw an InterruptedException
+     */
     public static void withDefaults(TearDownOperation operation) throws IOException, InterruptedException {
         try (CloudWatchEventsClient cloudWatchClient = CloudWatchEventsClient.create();
                 CloudFormationClient cloudFormationClient = CloudFormationClient.create()) {
-            TearDownClients clients = builder()
-                    .cloudWatch(cloudWatchClient)
-                    .cloudFormation(cloudFormationClient)
-                    .build();
+            TearDownClients clients = new TearDownClients(cloudWatchClient, cloudFormationClient);
             operation.tearDown(clients);
         }
-    }
-
-    public static Builder builder() {
-        return new Builder();
     }
 
     public CloudWatchEventsClient getCloudWatch() {
@@ -55,29 +58,20 @@ public class TearDownClients {
         return cloudFormation;
     }
 
-    public static final class Builder {
-        private CloudWatchEventsClient cloudWatch;
-        private CloudFormationClient cloudFormation;
-
-        private Builder() {
-        }
-
-        public Builder cloudWatch(CloudWatchEventsClient cloudWatch) {
-            this.cloudWatch = cloudWatch;
-            return this;
-        }
-
-        public Builder cloudFormation(CloudFormationClient cloudFormation) {
-            this.cloudFormation = cloudFormation;
-            return this;
-        }
-
-        public TearDownClients build() {
-            return new TearDownClients(this);
-        }
-    }
-
+    /**
+     * An operation that deletes one or more Sleeper instances and/or system test deployments. The tear down clients
+     * will be passed as a parameter.
+     */
     public interface TearDownOperation {
+
+        /**
+         * Performs the operation.
+         *
+         * @param  clients              the tear down clients
+         * @throws IOException          if an IOException occurs, usually when removing a local copy of configuration
+         *                              files
+         * @throws InterruptedException if an InterruptedException occurs, usually while waiting for tear down to finish
+         */
         void tearDown(TearDownClients clients) throws IOException, InterruptedException;
     }
 }
