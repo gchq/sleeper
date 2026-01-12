486ede67b715bac9ee6b331ed18306734d92b4d2
patchwork01 <110390516+patchwork01@users.noreply.github.com>
2026-01-12 14:30:03 +0000

    6173 Add nested test class

--- java/partitions/splitter/src/test/java/sleeper/splitter/core/extend/ExtendPartitionTreeBasedOnSketchesTest.java
+++ java/partitions/splitter/src/test/java/sleeper/splitter/core/extend/ExtendPartitionTreeBasedOnSketchesTest.java
@@ -15,6 +15,8 @@
  */
 package sleeper.splitter.core.extend;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import sleeper.core.partition.PartitionTree;
@@ -49,55 +51,60 @@ public class ExtendPartitionTreeBasedOnSketchesTest {
     TableProperties tableProperties = createTestTableProperties(instanceProperties, createSchemaWithKey("key", new IntType()));
     Map<String, Sketches> partitionIdToSketches = new HashMap<>();
 
-    @Test
-    void shouldSplitSingleExistingPartitionOnce() {
-        // Given
-        tableProperties.setNumber(BULK_IMPORT_MIN_LEAF_PARTITION_COUNT, 2);
-        PartitionTree tree = new PartitionsBuilder(tableProperties).singlePartition("root").buildTree();
-        setPartitionSketchData("root", List.of(
-                new Row(Map.of("key", 25)),
-                new Row(Map.of("key", 50)),
-                new Row(Map.of("key", 75))));
+    @Nested
+    @DisplayName("Split from a single root partition")
+    class SplitSingleRoot {
 
-        // When
-        ExtendPartitionTreeTransaction transaction = createTransaction(tree);
+        @Test
+        void shouldSplitSingleExistingPartitionOnce() {
+            // Given
+            tableProperties.setNumber(BULK_IMPORT_MIN_LEAF_PARTITION_COUNT, 2);
+            PartitionTree tree = new PartitionsBuilder(tableProperties).singlePartition("root").buildTree();
+            setPartitionSketchData("root", List.of(
+                    new Row(Map.of("key", 25)),
+                    new Row(Map.of("key", 50)),
+                    new Row(Map.of("key", 75))));
 
-        // Then
-        assertThat(transaction).isEqualTo(transactionWithUpdatedAndNewPartitions(
-                new PartitionsBuilder(tableProperties).singlePartition("root")
-                        .splitToNewChildren("root", "P1", "P2", 50)
-                        .buildTree(),
-                List.of("root"),
-                List.of("P1", "P2")));
-    }
+            // When
+            ExtendPartitionTreeTransaction transaction = createTransaction(tree);
 
-    @Test
-    void shouldSplitFromSingleExistingPartitionTwice() {
-        // Given
-        tableProperties.setNumber(BULK_IMPORT_MIN_LEAF_PARTITION_COUNT, 3);
-        PartitionTree tree = new PartitionsBuilder(tableProperties).singlePartition("root").buildTree();
-        setPartitionSketchData("root", List.of(
-                new Row(Map.of("key", 10)),
-                new Row(Map.of("key", 20)),
-                new Row(Map.of("key", 30)),
-                new Row(Map.of("key", 40)),
-                new Row(Map.of("key", 50)),
-                new Row(Map.of("key", 60)),
-                new Row(Map.of("key", 70)),
-                new Row(Map.of("key", 80))));
+            // Then
+            assertThat(transaction).isEqualTo(transactionWithUpdatedAndNewPartitions(
+                    new PartitionsBuilder(tableProperties).singlePartition("root")
+                            .splitToNewChildren("root", "P1", "P2", 50)
+                            .buildTree(),
+                    List.of("root"),
+                    List.of("P1", "P2")));
+        }
 
-        // When
-        ExtendPartitionTreeTransaction transaction = createTransaction(tree);
+        @Test
+        void shouldSplitFromSingleExistingPartitionTwice() {
+            // Given
+            tableProperties.setNumber(BULK_IMPORT_MIN_LEAF_PARTITION_COUNT, 3);
+            PartitionTree tree = new PartitionsBuilder(tableProperties).singlePartition("root").buildTree();
+            setPartitionSketchData("root", List.of(
+                    new Row(Map.of("key", 10)),
+                    new Row(Map.of("key", 20)),
+                    new Row(Map.of("key", 30)),
+                    new Row(Map.of("key", 40)),
+                    new Row(Map.of("key", 50)),
+                    new Row(Map.of("key", 60)),
+                    new Row(Map.of("key", 70)),
+                    new Row(Map.of("key", 80))));
 
-        // Then
-        assertThat(transaction).isEqualTo(transactionWithUpdatedAndNewPartitions(
-                new PartitionsBuilder(tableProperties).singlePartition("root")
-                        .splitToNewChildren("root", "P1", "P2", 50)
-                        .splitToNewChildren("P1", "P3", "P4", 30)
-                        .splitToNewChildren("P2", "P5", "P6", 70)
-                        .buildTree(),
-                List.of("root"),
-                List.of("P1", "P2", "P3", "P4", "P5", "P6")));
+            // When
+            ExtendPartitionTreeTransaction transaction = createTransaction(tree);
+
+            // Then
+            assertThat(transaction).isEqualTo(transactionWithUpdatedAndNewPartitions(
+                    new PartitionsBuilder(tableProperties).singlePartition("root")
+                            .splitToNewChildren("root", "P1", "P2", 50)
+                            .splitToNewChildren("P1", "P3", "P4", 30)
+                            .splitToNewChildren("P2", "P5", "P6", 70)
+                            .buildTree(),
+                    List.of("root"),
+                    List.of("P1", "P2", "P3", "P4", "P5", "P6")));
+        }
     }
 
     private ExtendPartitionTreeTransaction createTransaction(PartitionTree tree) {
