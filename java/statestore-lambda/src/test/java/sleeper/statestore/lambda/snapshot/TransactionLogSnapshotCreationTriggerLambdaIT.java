c6a6d4018c18c6a02c266b27c02a43028e0f39a7
patchwork01 <110390516+patchwork01@users.noreply.github.com>
2026-03-10 13:30:58 +0000

    6765 Set up tests

--- java/statestore-lambda/src/test/java/sleeper/statestore/lambda/snapshot/TransactionLogSnapshotCreationTriggerLambdaIT.java
+++ java/statestore-lambda/src/test/java/sleeper/statestore/lambda/snapshot/TransactionLogSnapshotCreationTriggerLambdaIT.java
@@ -20,17 +20,25 @@ import org.joda.time.DateTime;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import software.amazon.awssdk.services.sqs.model.Message;
+import software.amazon.awssdk.services.sqs.model.MessageSystemAttributeName;
 
+import sleeper.configuration.properties.S3InstanceProperties;
 import sleeper.configuration.properties.S3TableProperties;
 import sleeper.configuration.table.index.DynamoDBTableIndexCreator;
 import sleeper.core.properties.instance.InstanceProperties;
 import sleeper.core.properties.table.TableProperties;
 import sleeper.core.properties.table.TablePropertiesStore;
 import sleeper.localstack.test.LocalStackTestBase;
+import sleeper.statestore.transactionlog.DynamoDBTransactionLogStateStore;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.tuple;
+import static sleeper.core.properties.PropertiesUtils.loadProperties;
 import static sleeper.core.properties.instance.CdkDefinedInstanceProperty.CONFIG_BUCKET;
 import static sleeper.core.properties.instance.CdkDefinedInstanceProperty.TRANSACTION_LOG_SNAPSHOT_CREATION_QUEUE_URL;
+import static sleeper.core.properties.table.TableProperty.STATESTORE_CLASSNAME;
+import static sleeper.core.properties.table.TableProperty.TABLE_ID;
 import static sleeper.core.properties.table.TableProperty.TABLE_NAME;
 import static sleeper.core.properties.testutils.InstancePropertiesTestHelper.createTestInstanceProperties;
 import static sleeper.core.properties.testutils.TablePropertiesTestHelper.createTestTableProperties;
@@ -46,14 +54,51 @@ public class TransactionLogSnapshotCreationTriggerLambdaIT extends LocalStackTes
         createBucket(instanceProperties.get(CONFIG_BUCKET));
         DynamoDBTableIndexCreator.create(dynamoClient, instanceProperties);
         instanceProperties.set(TRANSACTION_LOG_SNAPSHOT_CREATION_QUEUE_URL, createFifoQueueGetUrl());
+        S3InstanceProperties.saveToS3(s3Client, instanceProperties);
     }
 
     @Test
-    @Disabled("TODO")
     void shouldTriggerSnapshotCreationForTableWithTransactionLogStateStore() {
         // Given
         TableProperties tableProperties = createTestTableProperties(instanceProperties, createSchemaWithKey("key"));
         tableProperties.set(TABLE_NAME, "test-table");
+        tableProperties.set(STATESTORE_CLASSNAME, DynamoDBTransactionLogStateStore.class.getSimpleName());
+        tablePropertiesStore.createTable(tableProperties);
+
+        // When
+        invokeLambda();
+
+        // Then
+        assertThat(receiveMessagesAndMessageGroupId(instanceProperties.get(TRANSACTION_LOG_SNAPSHOT_CREATION_QUEUE_URL)))
+                .extracting(Message::body, this::getMessageGroupId)
+                .containsExactly(tuple(tableProperties.get(TABLE_ID), tableProperties.get(TABLE_ID)));
+    }
+
+    @Test
+    void shouldNotTriggerSnapshotCreationForTableWithNonTransactionLogStateStore() {
+        // Given
+        TableProperties tableProperties = createTestTableProperties(instanceProperties, createSchemaWithKey("key"));
+        tableProperties.set(TABLE_NAME, "test-table");
+        tableProperties.set(STATESTORE_CLASSNAME, "OtherStateStore");
+        tablePropertiesStore.createTable(tableProperties);
+
+        // When
+        invokeLambda();
+
+        // Then
+        assertThat(receiveMessagesAndMessageGroupId(instanceProperties.get(TRANSACTION_LOG_SNAPSHOT_CREATION_QUEUE_URL)))
+                .isEmpty();
+    }
+
+    @Test
+    @Disabled("TODO")
+    void shouldNotTriggerSnapshotCreationForTableWithInvalidSchema() {
+        // Given
+        TableProperties tableProperties = new TableProperties(instanceProperties,
+                loadProperties("" +
+                        "sleeper.table.name=myTable\n" +
+                        "sleeper.table.schema={}\n"));
+        tableProperties.set(STATESTORE_CLASSNAME, DynamoDBTransactionLogStateStore.class.getSimpleName());
         tablePropertiesStore.createTable(tableProperties);
 
         // When
@@ -70,5 +115,9 @@ public class TransactionLogSnapshotCreationTriggerLambdaIT extends LocalStackTes
 
     private TransactionLogSnapshotCreationTriggerLambda lambda() {
         return new TransactionLogSnapshotCreationTriggerLambda(s3Client, dynamoClient, sqsClient, instanceProperties.get(CONFIG_BUCKET));
+    }
+
+    private String getMessageGroupId(Message message) {
+        return message.attributes().get(MessageSystemAttributeName.MESSAGE_GROUP_ID);
     }
 }
