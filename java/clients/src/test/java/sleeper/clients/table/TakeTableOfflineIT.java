fe1d032b3570cb5eec596e9db02e6b5c6da798bd
ca61688 <206189192+ca61688@users.noreply.github.com>
2026-03-19 15:39:24 +0000

    6767: Add TakeTableOfflineIT

---
+++ java/clients/src/test/java/sleeper/clients/table/TakeTableOfflineIT.java
@@ -0,0 +1,115 @@
+/*
+ * Copyright 2022-2025 Crown Copyright
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package sleeper.clients.table;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+
+import sleeper.configuration.properties.S3TableProperties;
+import sleeper.configuration.table.index.DynamoDBTableIndexCreator;
+import sleeper.core.properties.instance.InstanceProperties;
+import sleeper.core.properties.table.TableProperties;
+import sleeper.core.properties.table.TablePropertiesStore;
+import sleeper.core.schema.Schema;
+import sleeper.core.table.TableNotFoundException;
+import sleeper.core.table.TableStatus;
+import sleeper.localstack.test.LocalStackTestBase;
+import sleeper.statestore.transactionlog.TransactionLogStateStoreCreator;
+
+import java.io.IOException;
+import java.nio.file.Path;
+
+import static java.nio.file.Files.createTempDirectory;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static sleeper.core.properties.instance.CdkDefinedInstanceProperty.CONFIG_BUCKET;
+import static sleeper.core.properties.instance.CdkDefinedInstanceProperty.DATA_BUCKET;
+import static sleeper.core.properties.instance.TableDefaultProperty.DEFAULT_INGEST_PARTITION_FILE_WRITER_TYPE;
+import static sleeper.core.properties.table.TableProperty.SCHEMA;
+import static sleeper.core.properties.table.TableProperty.TABLE_ID;
+import static sleeper.core.properties.table.TableProperty.TABLE_NAME;
+import static sleeper.core.properties.table.TableProperty.TABLE_ONLINE;
+import static sleeper.core.properties.testutils.InstancePropertiesTestHelper.createTestInstanceProperties;
+import static sleeper.core.properties.testutils.TablePropertiesTestHelper.createTestTableProperties;
+import static sleeper.core.schema.SchemaTestHelper.createSchemaWithKey;
+import static sleeper.core.table.TableStatusTestHelper.uniqueIdAndName;
+
+public class TakeTableOfflineIT extends LocalStackTestBase {
+    @TempDir
+    private Path tempDir;
+
+    private final InstanceProperties instanceProperties = createTestInstanceProperties();
+    private final Schema schema = createSchemaWithKey("key1");
+    private final TablePropertiesStore propertiesStore = S3TableProperties.createStore(instanceProperties, s3Client, dynamoClient);
+    private String inputFolderName;
+
+    @BeforeEach
+    void setUp() throws IOException {
+        instanceProperties.set(DEFAULT_INGEST_PARTITION_FILE_WRITER_TYPE, "direct");
+        createBucket(instanceProperties.get(CONFIG_BUCKET));
+        createBucket(instanceProperties.get(DATA_BUCKET));
+        DynamoDBTableIndexCreator.create(dynamoClient, instanceProperties);
+        new TransactionLogStateStoreCreator(instanceProperties, dynamoClient).create();
+        inputFolderName = createTempDirectory(tempDir, null).toString();
+    }
+
+    @Test
+    void shouldTakeTableOffline() throws Exception {
+        // Given
+        createTable(uniqueIdAndName("test-table-1", "table-1"));
+
+        // When
+        takeTableOffline("table-1");
+
+        // Then
+        assertThat(propertiesStore.loadByName("table-1").get(TABLE_ONLINE)).isEqualTo("false");
+    }
+
+    @Test
+    void shouldTakeTableOfflineDespiteInvalidSchema() throws Exception {
+        // Given
+        TableProperties table = createTable(uniqueIdAndName("test-table-1", "table-1"));
+        table.set(SCHEMA, "{}");
+        propertiesStore.update(table);
+
+        // When
+        takeTableOffline("table-1");
+
+        // Then
+        assertThat(propertiesStore.loadByName("table-1").get(TABLE_ONLINE)).isEqualTo("false");
+    }
+
+    @Test
+    void shouldFailToDeleteTableThatDoesNotExist() {
+        // When / Then
+        assertThatThrownBy(() -> takeTableOffline("table-1"))
+                .isInstanceOf(TableNotFoundException.class);
+    }
+
+    private void takeTableOffline(String tableName) throws Exception {
+        new TakeTableOffline(propertiesStore).takeOffline(tableName);
+    }
+
+    private TableProperties createTable(TableStatus tableStatus) {
+        TableProperties table = createTestTableProperties(instanceProperties, schema);
+        table.set(TABLE_ID, tableStatus.getTableUniqueId());
+        table.set(TABLE_NAME, tableStatus.getTableName());
+        propertiesStore.save(table);
+        return table;
+    }
+}
