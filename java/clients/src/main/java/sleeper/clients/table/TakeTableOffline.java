54acabc09a12819b526f1305ade77160af21afc9
ca61688 <206189192+ca61688@users.noreply.github.com>
2026-03-24 15:27:44 +0000

    6767: Add validation back in to instance properties

--- java/clients/src/main/java/sleeper/clients/table/TakeTableOffline.java
+++ java/clients/src/main/java/sleeper/clients/table/TakeTableOffline.java
@@ -53,7 +53,7 @@ public class TakeTableOffline {
 
         try (S3Client s3Client = buildAwsV2Client(S3Client.builder());
                 DynamoDbClient dynamoClient = buildAwsV2Client(DynamoDbClient.builder())) {
-            InstanceProperties instanceProperties = S3InstanceProperties.loadGivenInstanceIdNoValidation(s3Client, args[0]);
+            InstanceProperties instanceProperties = S3InstanceProperties.loadGivenInstanceId(s3Client, args[0]);
             TablePropertiesStore tablePropertiesStore = S3TableProperties.createStore(instanceProperties, s3Client, dynamoClient);
             new TakeTableOffline(tablePropertiesStore).takeOffline(args[1]);
         }
