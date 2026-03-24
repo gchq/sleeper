1267df66082f35d43a0d73dd9e2131f6215d682f
rtjd6554 <rtjd6554@proton.me>
2026-03-24 07:55:48 +0000

    6749: Update text description

--- java/deployment/cdk/src/main/java/sleeper/cdk/stack/RestApiStack.java
+++ java/deployment/cdk/src/main/java/sleeper/cdk/stack/RestApiStack.java
@@ -66,7 +66,7 @@ public class RestApiStack extends NestedStack {
         String functionName = String.join("-", "sleeper", instanceId, "rest-api-handler");
         IFunction lambda = lambdaCode.buildFunction(LambdaHandler.REST_API_HANDLER, constructId, builder -> builder
                 .functionName(functionName)
-                .description("Function for creating REST API for interacting with SLEEPER")
+                .description("Function for implementing a REST API for interacting with SLEEPER")
                 .environment(env)
                 .memorySize(1024)
                 .logGroup(logGroup)
