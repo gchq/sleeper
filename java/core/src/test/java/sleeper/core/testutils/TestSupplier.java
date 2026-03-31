7afa89a7796f12348d5fe270d6f586fca73e6eb0
ca61688 <206189192+ca61688@users.noreply.github.com>
2026-03-31 13:49:00 +0000

    6818: Create wrapper around test supplier for better assertions

---
+++ java/core/src/test/java/sleeper/core/testutils/TestSupplier.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2022-2025 Crown Copyright
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package sleeper.core.testutils;
+
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Supplier;
+
+/**
+ * Wrapper class around the Supplier Instant interface to be able to assert cleanly there are no remaining expected
+ * time calls.
+ */
+public class TestSupplier implements Supplier<Instant> {
+
+    List<Instant> remainingTimes;
+
+    public TestSupplier(List<Instant> initialTimes) {
+        this.remainingTimes = new ArrayList<>(initialTimes);
+    }
+
+    @Override
+    public Instant get() {
+        return remainingTimes.remove(0);
+    }
+
+    public List<Instant> getRemainingTimes() {
+        return remainingTimes;
+    }
+
+}
