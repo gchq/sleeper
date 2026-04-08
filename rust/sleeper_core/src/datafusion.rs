f65045f44f4ede77318b4f1c1368076038127951
rtjd6554 <rtjd6554@proton.me>
2026-04-08 12:35:13 +0000

    6926: Update copyright python

--- rust/sleeper_core/src/datafusion.rs
+++ rust/sleeper_core/src/datafusion.rs
@@ -3,7 +3,7 @@
 //!
 //! This allows for multi-threaded data processing and optimised Parquet reading.
 /*
-* Copyright 2022-2025 Crown Copyright
+* Copyright 2022-2026 Crown Copyright
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
