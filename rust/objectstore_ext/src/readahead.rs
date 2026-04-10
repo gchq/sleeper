963d5967cd7e00f7b62a1fb3801797ac9eebb6df
rtjd6554 <rtjd6554@proton.me>
2026-04-10 09:57:11 +0000

    6899: Update delete_stream

--- rust/objectstore_ext/src/readahead.rs
+++ rust/objectstore_ext/src/readahead.rs
@@ -645,7 +645,7 @@ impl<T: ObjectStore> ObjectStore for ReadaheadStore<T> {
     ) -> BoxStream<'static, Result<Path>> {
         locations
             .map(move |location| async move {
-                let path = &location.unwrap();
+                let path = &location.unwrap().clone();
                 self.remove_cache_for(path);
                 self.inner.delete(path).await
             })
