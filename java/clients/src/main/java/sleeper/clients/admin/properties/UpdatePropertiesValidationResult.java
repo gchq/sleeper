179e43a903794c7edd7a5553a3ad91a41a4ddddf
205762370+Rob9786 <205762370+Rob9786@users.noreply.github.com>
2026-04-15 12:16:25 +0000

    6769: Added UpdatePropertiesValidationRequest

---
+++ java/clients/src/main/java/sleeper/clients/admin/properties/UpdatePropertiesValidationResult.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2022-2025 Crown Copyright
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package sleeper.clients.admin.properties;
+
+import sleeper.core.properties.SleeperProperty;
+
+import java.util.Collections;
+import java.util.Set;
+
+public class UpdatePropertiesValidationResult {
+
+    private Set<SleeperProperty> invalidProperties;
+    private Set<SleeperProperty> invalidBeforeProperties;
+
+    public UpdatePropertiesValidationResult(Set<SleeperProperty> invalidProperties, Set<SleeperProperty> invalidBeforeProperties) {
+        this.invalidProperties = invalidProperties;
+        this.invalidBeforeProperties = invalidBeforeProperties;
+    }
+
+    public UpdatePropertiesValidationResult() {
+        this.invalidProperties = Collections.emptySet();
+        this.invalidBeforeProperties = Collections.emptySet();
+    }
+
+    public Set<SleeperProperty> getInvalidProperties() {
+        return invalidProperties;
+    }
+
+    public Boolean checkInvalidBeforeProperty(SleeperProperty property) {
+        if (!property.isEditable() && invalidBeforeProperties.contains(property)) {
+            return true;
+        }
+        return false;
+    }
+
+    public void setInvalidBeforeProperties(Set<SleeperProperty> invalidBeforeProperties) {
+        this.invalidBeforeProperties = invalidBeforeProperties;
+    }
+
+    public void setInvalidProperties(Set<SleeperProperty> invalidProperties) {
+        this.invalidProperties = invalidProperties;
+    }
+
+}
