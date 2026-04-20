1a2a283d58e0753eebe205bde7d37b3591d3d4f0
patchwork01 <110390516+patchwork01@users.noreply.github.com>
2026-04-20 13:59:00 +0000

    6997 Add SpotBugs suppression

--- java/system-test/system-test-cdk/src/main/java/sleeper/systemtest/cdk/CopyPublicJarCdkApp.java
+++ java/system-test/system-test-cdk/src/main/java/sleeper/systemtest/cdk/CopyPublicJarCdkApp.java
@@ -15,6 +15,7 @@
  */
 package sleeper.systemtest.cdk;
 
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import software.amazon.awscdk.App;
 import software.amazon.awscdk.AppProps;
 import software.amazon.awscdk.CfnOutput;
@@ -39,6 +40,7 @@ import java.nio.file.Path;
 /**
  * An app to test copying a jar file from a public repository to an S3 bucket with a CDK custom resource.
  */
+@SuppressFBWarnings("MC_OVERRIDABLE_METHOD_CALL_IN_CONSTRUCTOR")
 public class CopyPublicJarCdkApp extends Stack {
 
     public CopyPublicJarCdkApp(Construct scope, String id, StackProps stackProps, Props props) {
