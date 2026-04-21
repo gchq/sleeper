8df15d1ca5f89b12d6ffb851b3023d8953448447
m09526 <m09526@users.noreply.github.com>
2026-04-21 09:27:22 +0000

    Updated to remove code

--- rust/sleeper_df/src/unpack.rs
+++ rust/sleeper_df/src/unpack.rs
@@ -14,13 +14,8 @@
 * See the License for the specific language governing permissions and
 * limitations under the License.
 */
-use crate::objects::{FFIBytes, FFIElementType};
-use color_eyre::eyre::{Result, bail, eyre};
-use sleeper_core::PartitionBound;
-use std::{
-    ffi::{CStr, c_char, c_void},
-    slice,
-};
+use color_eyre::eyre::Result;
+use std::ffi::{CStr, c_char, c_void};
 
 pub fn unpack_str<'a>(pointer: *const c_char) -> Result<&'a str> {
     Ok(unsafe { CStr::from_ptr(pointer) }.to_str()?)
@@ -28,89 +23,4 @@ pub fn unpack_str<'a>(pointer: *const c_char) -> Result<&'a str> {
 
 pub fn unpack_string(pointer: *const c_char) -> Result<String> {
     unpack_str(pointer).map(ToOwned::to_owned)
-}
-
-/// Create a vector of a generic type.
-///
-/// # Errors
-/// If the array length is invalid, then behaviour is undefined.
-pub fn unpack_typed_array<T: Copy>(array_base: *const *const T, len: usize) -> Result<Vec<T>> {
-    if len == 0 {
-        return Ok(Vec::new());
-    }
-    if array_base.is_null() {
-        bail!("NULL pointer for array_base in generic typed array");
-    }
-
-    unsafe { slice::from_raw_parts(array_base, len) }
-        .iter()
-        .map(|p| {
-            if p.is_null() {
-                Err(eyre!("Found NULL pointer in generic typed array"))
-            } else {
-                Ok(unsafe { **p })
-            }
-        })
-        .collect()
-}
-
-/// Create a vector of a variant array type. Each element may be a
-/// i32, i64, String or byte array. The schema types define what decoding is attempted.
-///
-/// # Errors
-/// If the array length is invalid, then behaviour is undefined.
-/// If the schema types are incorrect, then behaviour is undefined.
-///
-/// # Panics
-/// If the length of the `schema_types` array doesn't match the length specified.
-/// If `nulls_present` is false and a NULL pointer is found.
-///
-/// Also panics if a negative array length is found in decoding byte arrays or strings.
-pub fn unpack_variant_array<'a>(
-    array_base: *const *const c_void,
-    len: usize,
-    schema_types: &[FFIElementType],
-    nulls_present: bool,
-) -> Result<Vec<PartitionBound<'a>>> {
-    assert!(
-        len <= schema_types.len(),
-        "More array elements than schema_types!"
-    );
-    if array_base.is_null() {
-        bail!("NULL pointer for array_base in variant array");
-    }
-    unsafe { slice::from_raw_parts(array_base, len) }
-        .iter()
-        .zip(schema_types.iter())
-        .map(|(&bptr, type_id)| {
-            if !nulls_present && bptr.is_null() {
-                Err(eyre!("Found NULL pointer in variant array"))
-            } else {
-                match type_id {
-                    FFIElementType::Int32 => Ok(match unsafe { bptr.cast::<i32>().as_ref() } {
-                        Some(v) => PartitionBound::Int32(*v),
-                        None => PartitionBound::Unbounded,
-                    }),
-                    FFIElementType::Int64 => Ok(match unsafe { bptr.cast::<i64>().as_ref() } {
-                        Some(v) => PartitionBound::Int64(*v),
-                        None => PartitionBound::Unbounded,
-                    }),
-                    FFIElementType::String => {
-                        match unsafe { bptr.cast::<FFIBytes>().as_ref() } {
-                            //unpack length (signed because it's from Java)
-                            Some(bytes) => Ok(PartitionBound::String(bytes.try_into()?)),
-                            None => Ok(PartitionBound::Unbounded),
-                        }
-                    }
-                    FFIElementType::ByteArray => {
-                        match unsafe { bptr.cast::<FFIBytes>().as_ref() } {
-                            //unpack length (signed because it's from Java)
-                            Some(bytes) => Ok(PartitionBound::ByteArray(bytes.into())),
-                            None => Ok(PartitionBound::Unbounded),
-                        }
-                    }
-                }
-            }
-        })
-        .collect()
 }
