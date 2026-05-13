35e7591d0e88e1db77c9dbd2b9a34a1be4e9397c
205762370+Rob9786 <205762370+Rob9786@users.noreply.github.com>
2026-05-13 12:07:34 +0000

    7165: Added JavaDoc

--- java/system-test/system-test-dsl/src/main/java/sleeper/systemtest/dsl/SentJobsContext.java
+++ java/system-test/system-test-dsl/src/main/java/sleeper/systemtest/dsl/SentJobsContext.java
@@ -22,18 +22,36 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+/**
+ * Store the ingest job Id's.
+ */
 public class SentJobsContext {
     private final List<String> jobIds = new ArrayList<>();
     private GeneratedIngestSourceFiles lastGeneratedFiles;
 
+    /**
+     * Add a new job Id.
+     *
+     * @param jobId the job Id to add
+     */
     public void addJobId(String jobId) {
         jobIds.add(jobId);
     }
 
+    /**
+     * Add new job Id's.
+     *
+     * @param ids the job Id's to add
+     */
     public void addAllJobIds(List<String> ids) {
         jobIds.addAll(ids);
     }
 
+    /**
+     * Clear all stored job Id's and add new ones.
+     *
+     * @param ids the job Id's to store in the context
+     */
     public void setJobIds(List<String> ids) {
         jobIds.clear();
         jobIds.addAll(ids);
@@ -43,6 +61,11 @@ public class SentJobsContext {
         this.lastGeneratedFiles = files;
     }
 
+    /**
+     * Get the job Id's stored in the context.
+     *
+     * @return the job Id's
+     */
     public List<String> getJobIds() {
         if (jobIds.isEmpty() && lastGeneratedFiles != null) {
             jobIds.addAll(lastGeneratedFiles.getJobIdsFromIndividualFiles());
