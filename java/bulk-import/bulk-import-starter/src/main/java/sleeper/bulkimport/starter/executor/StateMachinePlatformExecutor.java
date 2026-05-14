3e1313941cd4cd53b7f1eba2f81c970831bf94c3
205762370+Rob9786 <205762370+Rob9786@users.noreply.github.com>
2026-05-14 10:21:08 +0000

    7151: Changed to use jobid in job execution name

--- java/bulk-import/bulk-import-starter/src/main/java/sleeper/bulkimport/starter/executor/StateMachinePlatformExecutor.java
+++ java/bulk-import/bulk-import-starter/src/main/java/sleeper/bulkimport/starter/executor/StateMachinePlatformExecutor.java
@@ -144,14 +144,14 @@ public class StateMachinePlatformExecutor implements PlatformExecutor {
     }
 
     private static String jobExecutionName(BulkImportJob job) {
-        String tableName = job.getTableName();
+        String tableId = job.getTableId();
         String jobId = job.getId();
         // See maximum length restriction in AWS documentation:
         // https://docs.aws.amazon.com/step-functions/latest/apireference/API_StartExecution.html#API_StartExecution_RequestParameters
-        int spaceForTableName = 80 - jobId.length() - 1;
-        if (tableName.length() > spaceForTableName) {
-            tableName = tableName.substring(0, spaceForTableName);
+        int spaceForTableId = 80 - jobId.length() - 1;
+        if (tableId.length() > spaceForTableId) {
+            tableId = tableId.substring(0, spaceForTableId);
         }
-        return String.join("-", tableName, jobId);
+        return String.join("-", tableId, jobId);
     }
 }
