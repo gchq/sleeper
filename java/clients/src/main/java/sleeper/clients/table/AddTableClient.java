7c9b25995aa22789d788a0e355b9de6bd6e174eb
ca61688 <206189192+ca61688@users.noreply.github.com>
2026-06-30 15:44:47 +0100

    7274: Surpress find bugs null check on readFile method

--- java/clients/src/main/java/sleeper/clients/table/AddTableClient.java
+++ java/clients/src/main/java/sleeper/clients/table/AddTableClient.java
@@ -16,6 +16,7 @@
 
 package sleeper.clients.table;
 
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 import software.amazon.awssdk.services.s3.S3AsyncClient;
 import software.amazon.awssdk.services.s3.S3Client;
@@ -180,20 +181,12 @@ public class AddTableClient {
         }
     }
 
+    @SuppressFBWarnings(value = "NP_NULL_ON_SOME_PATH_FROM_RETURN_VALUE", justification = "FindBugs not accepting null checks as solution")
     private static String readFile(FileReader reader, Path path) {
         try {
             return reader.readString(path);
         } catch (IOException e) {
-            String filename = null;
-            if (path != null) {
-                Path filenamePath = path.getFileName();
-                if (filenamePath != null) {
-                    filename = filenamePath.toString();
-                }
-            }
-
-            filename = filename == null ? "" : filename;
-
+            String filename = path.getFileName().toString();
             String argString = filename.contains("schema") ? "--schema" : "--table-properties";
             throw new CommandArgumentsException(String.format("No %s file was found at the supplied location. " +
                     "Provide a path to one using %s or add one to the config directory specified by --config-dir.", filename, argString));
