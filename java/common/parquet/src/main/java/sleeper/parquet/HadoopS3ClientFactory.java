0dc7756df2fc0a4b26796e9dc6b4e5997bf2b899
rtjd6554 <rtjd6554@proton.me>
2026-07-01 10:00:43 +0000

    7459: Tidy up code

--- java/common/parquet/src/main/java/sleeper/parquet/HadoopS3ClientFactory.java
+++ java/common/parquet/src/main/java/sleeper/parquet/HadoopS3ClientFactory.java
@@ -41,7 +41,6 @@ import java.net.URISyntaxException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import static org.apache.hadoop.fs.s3a.Constants.AWS_S3_DEFAULT_REGION;
 import static org.apache.hadoop.fs.s3a.Constants.AWS_SERVICE_IDENTIFIER_S3;
 import static org.apache.hadoop.fs.s3a.Constants.CENTRAL_ENDPOINT;
 import static org.apache.hadoop.fs.s3a.Constants.DEFAULT_SECURE_CONNECTIONS;
@@ -53,10 +52,6 @@ import static org.apache.hadoop.fs.s3a.auth.SignerFactory.createHttpSigner;
 import static org.apache.hadoop.fs.s3a.impl.InternalConstants.AUTH_SCHEME_AWS_SIGV_4;
 
 public class HadoopS3ClientFactory extends Configured implements S3ClientFactory {
-
-    private static final String S3_SERVICE_NAME = "s3";
-
-    private static final Pattern VPC_ENDPOINT_PATTERN = Pattern.compile("^(?:.+\\.)?([a-z0-9-]+)\\.vpce\\.amazonaws\\.(?:com|com\\.cn)$");
 
     public HadoopS3ClientFactory() {
     }
@@ -90,7 +85,6 @@ public class HadoopS3ClientFactory extends Configured implements S3ClientFactory
                 .serviceConfiguration(serviceConfiguration);
 
         if (conf.getBoolean(HTTP_SIGNER_ENABLED, HTTP_SIGNER_ENABLED_DEFAULT)) {
-            // use an http signer through an AuthScheme
             final AuthScheme<AwsCredentialsIdentity> signer = createHttpSigner(conf, AUTH_SCHEME_AWS_SIGV_4, HTTP_SIGNER_CLASS_NAME);
             builder.putAuthScheme(signer);
         }
@@ -105,26 +99,17 @@ public class HadoopS3ClientFactory extends Configured implements S3ClientFactory
         final String configuredRegion = parameters.getRegion();
         Region region = null;
 
-        // If the region was configured, set it.
         if (configuredRegion != null && !configuredRegion.isEmpty()) {
             region = Region.of(configuredRegion);
         }
 
         if (endpoint != null) {
             boolean endpointEndsWithCentral = endpointStr.endsWith(CENTRAL_ENDPOINT);
-
-            // No region was configured,
-            // determine the region from the endpoint.
             if (region == null) {
                 region = getS3RegionFromEndpoint(endpointStr,
                         endpointEndsWithCentral);
             }
 
-            // No need to override endpoint with "s3.amazonaws.com".
-            // Let the client take care of endpoint resolution. Overriding
-            // the endpoint with "s3.amazonaws.com" causes 400 Bad Request
-            // errors for non-existent buckets and objects.
-            // ref: https://github.com/aws/aws-sdk-java-v2/issues/4846
             if (!endpointEndsWithCentral) {
                 builder.endpointOverride(endpoint);
             } else {
@@ -135,8 +120,6 @@ public class HadoopS3ClientFactory extends Configured implements S3ClientFactory
         if (region != null) {
             builder.region(region);
         } else if (configuredRegion == null) {
-            // no region is configured, and none could be determined from the endpoint.
-            // Use EU_WEST_2 as default.
             region = Region.EU_WEST_2;
             builder.crossRegionAccessEnabled(true);
             builder.region(region);
@@ -144,13 +127,10 @@ public class HadoopS3ClientFactory extends Configured implements S3ClientFactory
     }
 
     private static URI getS3Endpoint(String endpoint, final Configuration conf) {
-
         boolean secureConnections = conf.getBoolean(SECURE_CONNECTIONS, DEFAULT_SECURE_CONNECTIONS);
-
         String protocol = secureConnections ? "https" : "http";
 
         if (endpoint == null || endpoint.isEmpty()) {
-            // don't set an endpoint if none is configured, instead let the SDK figure it out.
             return null;
         }
 
@@ -167,36 +147,22 @@ public class HadoopS3ClientFactory extends Configured implements S3ClientFactory
 
     static Region getS3RegionFromEndpoint(final String endpoint,
             final boolean endpointEndsWithCentral) {
+        Pattern endpointPattern = Pattern.compile("^(?:.+\\.)?([a-z0-9-]+)\\.vpce\\.amazonaws\\.(?:com|com\\.cn)$");
 
         if (!endpointEndsWithCentral) {
-            // S3 VPC endpoint parsing
-            Matcher matcher = VPC_ENDPOINT_PATTERN.matcher(endpoint);
+            Matcher matcher = endpointPattern.matcher(endpoint);
             if (matcher.find()) {
                 return Region.of(matcher.group(1));
             }
-
-            return AwsHostNameUtils.parseSigningRegion(endpoint, S3_SERVICE_NAME).orElse(null);
+            return AwsHostNameUtils.parseSigningRegion(endpoint, "s3").orElse(null);
         }
-
-        // Select default region here to enable cross-region access.
-        // If both "fs.s3a.endpoint" and "fs.s3a.endpoint.region" are empty,
-        // Spark sets "fs.s3a.endpoint" to "s3.amazonaws.com".
-        // This applies to Spark versions with the changes of SPARK-35878.
-        // ref:
-        // https://github.com/apache/spark/blob/v3.5.0/core/
-        // src/main/scala/org/apache/spark/deploy/SparkHadoopUtil.scala#L528
-        // If we do not allow cross region access, Spark would not be able to
-        // access any bucket that is not present in the given region.
-        // Hence, we should use default region us-east-2 to allow cross-region
-        // access.
-        return Region.of(AWS_S3_DEFAULT_REGION);
+        return Region.EU_WEST_2;
     }
 
     protected ClientOverrideConfiguration.Builder createClientOverrideConfiguration(
             S3ClientCreationParameters parameters, Configuration conf) throws IOException {
         final ClientOverrideConfiguration.Builder clientOverrideConfigBuilder = AWSClientConfig.createClientConfigBuilder(conf, AWS_SERVICE_IDENTIFIER_S3);
 
-        // add any headers
         parameters.getHeaders().forEach((h, v) -> clientOverrideConfigBuilder.putHeader(h, v));
 
         if (!StringUtils.isEmpty(parameters.getUserAgentSuffix())) {
@@ -219,19 +185,13 @@ public class HadoopS3ClientFactory extends Configured implements S3ClientFactory
     }
 
     @Override
-    public S3AsyncClient createS3AsyncClient(
-            final URI uri,
-            final S3ClientCreationParameters parameters) throws IOException {
-
-        Configuration conf = getConf();
-        String bucket = uri.getHost();
-
+    public S3AsyncClient createS3AsyncClient(final URI uri, final S3ClientCreationParameters parameters) throws IOException {
         MultipartConfiguration multipartConfiguration = MultipartConfiguration.builder()
                 .minimumPartSizeInBytes(parameters.getMinimumPartSize())
                 .thresholdInBytes(parameters.getMultiPartThreshold())
                 .build();
 
-        return configureClientBuilder(S3AsyncClient.builder(), parameters, conf, bucket)
+        return configureClientBuilder(S3AsyncClient.builder(), parameters, getConf(), uri.getHost())
                 .multipartConfiguration(multipartConfiguration)
                 .multipartEnabled(parameters.isMultipartCopy())
                 .build();
