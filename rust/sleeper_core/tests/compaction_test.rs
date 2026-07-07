660a11847f1c109832f5d910c4da3701a720f411
patchwork01 <110390516+patchwork01@users.noreply.github.com>
2026-07-07 10:04:13 +0000

    7612 Revert assertion change

--- rust/sleeper_core/tests/compaction_test.rs
+++ rust/sleeper_core/tests/compaction_test.rs
@@ -385,9 +385,7 @@ async fn should_merge_empty_files() -> Result<(), Error> {
     let result = run_compaction(&input, &Arc::new(SleeperContext::default()), None).await?;
 
     // Then
-    let output_rows = read_file_of_ints(&output, "key")?;
-    let expected_rows: Vec<i32> = vec![];
-    assert_eq!(output_rows, expected_rows);
+    assert_eq!(read_file_of_ints(&output, "key")?, Vec::<i32>::new());
     assert_eq!([result.rows_read, result.rows_written], [0, 0]);
     assert_eq!(read_sketch_approx_row_count(&sketches).await?, 0);
     Ok(())
