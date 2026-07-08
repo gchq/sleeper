00e9ebc058e2ed8c50e4b1cbba5e98c78fc3d72b
rtjd6554 <rtjd6554@proton.me>
2026-07-08 13:40:34 +0000

    7574: New method for basic hadoop config

--- java/bulk-import/bulk-import-core/src/main/java/sleeper/bulkimport/core/configuration/SparkConfigurationUtils.java
+++ java/bulk-import/bulk-import-core/src/main/java/sleeper/bulkimport/core/configuration/SparkConfigurationUtils.java
@@ -28,7 +28,9 @@ import static sleeper.core.properties.instance.BulkImportProperty.BULK_IMPORT_SP
 import static sleeper.core.properties.instance.BulkImportProperty.BULK_IMPORT_SPARK_SPECULATION_QUANTILE;
 import static sleeper.core.properties.instance.CdkDefinedInstanceProperty.BULK_IMPORT_EKS_CLUSTER_ENDPOINT;
 import static sleeper.core.properties.instance.CdkDefinedInstanceProperty.BULK_IMPORT_EKS_NAMESPACE;
+import static sleeper.core.properties.instance.CdkDefinedInstanceProperty.REGION;
 import static sleeper.core.properties.instance.CommonProperty.MAXIMUM_CONNECTIONS_TO_S3;
+import static sleeper.core.properties.instance.CommonProperty.S3_UPLOAD_BLOCK_SIZE;
 import static sleeper.core.properties.instance.EKSProperty.BULK_IMPORT_EKS_SPARK_DEFAULT_PARALLELISM;
 import static sleeper.core.properties.instance.EKSProperty.BULK_IMPORT_EKS_SPARK_DRIVER_CORES;
 import static sleeper.core.properties.instance.EKSProperty.BULK_IMPORT_EKS_SPARK_DRIVER_EXTRA_JAVA_OPTIONS;
@@ -311,7 +313,7 @@ public class SparkConfigurationUtils {
     }
 
     private static Map<String, String> getBaseSparkConfiguration(InstanceProperties instanceProperties) {
-        Map<String, String> sparkConf = new TreeMap<>();
+        Map<String, String> sparkConf = setBasicHadoopConfig(new TreeMap<>(), instanceProperties);
         // The following value is not mentioned in the blog linked at the top of this class, but setting this explicitly
         // was found necessary to stop "Decompression error: Version not supported" errors -
         // only a value of "lz4" has been tested.
@@ -329,5 +331,24 @@ public class SparkConfigurationUtils {
         sparkConf.put("spark.hadoop.fs.s3a.create.performance", "true");
 
         return sparkConf;
+    }
+
+    /**
+     * Mirroring of the Hadoop configuration that is set within {@link HadoopConfigurationProvider} to ensure
+     * consistency across the application.
+     *
+     * @param  conf               the map configuration used for spark
+     * @param  instanceProperties the properties to use
+     * @return                    the original tree map with details set
+     */
+    private static Map<String, String> setBasicHadoopConfig(TreeMap<String, String> conf, InstanceProperties instanceProperties) {
+        conf.put("spark.hadoop.fs.s3a.endpoint.region", instanceProperties.get(REGION));
+        conf.put("spark.hadoop.fs.s3a.readahead.range", instanceProperties.get(REGION)); //tableProperties.get(S3A_READAHEAD_RANGE)
+        conf.put("spark.hadoop.fs.s3a.block.size", instanceProperties.get(S3_UPLOAD_BLOCK_SIZE));
+        conf.put("spark.hadoop.fs.s3a.bucket.probe", "0");
+        conf.put("spark.hadoop.fs.s3a.fast.upload", "true");
+        conf.put("spark.hadoop.fs.s3a.endpoint", instanceProperties.get(REGION));
+        conf.put("spark.hadoop.fs.s3a.path.style.access", "true");
+        return conf;
     }
 }
