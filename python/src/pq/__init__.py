6909f6de3704cb8abbc1b356a2881aab47334764
ab295382 <134393532+ab295382@users.noreply.github.com>
2026-07-23 12:37:36 +0000

    7734 Updated the docstrings for __init__ files

--- python/src/pq/__init__.py
+++ python/src/pq/__init__.py
@@ -1,4 +1,4 @@
-"""Parquet Serialiser and Deserialiser."""
+"""Public API for Parquet serialisation and deserialisation."""
 
 from pq.parquet_deserial import ParquetDeserialiser
 from pq.parquet_serial import ParquetSerialiser
