7fe5e086ec0487d39d1d1cd575f9a5868d65150f
ca61688 <206189192+ca61688@users.noreply.github.com>
2026-07-24 10:46:45 +0100

    7446: Remove redundent null check

--- java/rest-api/src/main/java/sleeper/restapi/addTable/AddTableRoute.java
+++ java/rest-api/src/main/java/sleeper/restapi/addTable/AddTableRoute.java
@@ -71,10 +71,6 @@ public class AddTableRoute implements Route {
             return Route.errorResponse(400, "invalid_request", e.getMessage());
         }
 
-        if (request == null) {
-            return Route.errorResponse(400, "invalid_request", "Request body is empty");
-        }
-
         TableProperties tableProperties = request.getProperties();
         List<Object> splitPoints = request.getSplitPoints();
 
