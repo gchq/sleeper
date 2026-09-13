69980d7d172c820284920ee9315a88bd3c0ad5a9
Sylvester Kaczmarek <16242628+sylvesterkaczmarek@users.noreply.github.com>
2026-09-13 12:24:37 +0100

    Issue 7188 - Configure nullable value probability

--- java/system-test/system-test-configuration/src/main/java/sleeper/systemtest/configuration/SystemTestProperty.java
+++ java/system-test/system-test-configuration/src/main/java/sleeper/systemtest/configuration/SystemTestProperty.java
@@ -138,6 +138,16 @@ public interface SystemTestProperty extends InstanceProperty {
     SystemTestProperty RANDOM_BYTE_ARRAY_LENGTH = Index.propertyBuilder("sleeper.systemtest.random.bytearray.length")
             .description("The length of byte arrays generated randomly during random row generation")
             .defaultValue("10").validationPredicate(SleeperPropertyValueUtils::isNonNegativeInteger).build();
+    SystemTestProperty RANDOM_NULL_PROBABILITY = Index.propertyBuilder("sleeper.systemtest.random.null.probability")
+            .description("The probability of generating null for nullable value fields during random row generation")
+            .defaultValue("0.2").validationPredicate(value -> {
+                try {
+                    double probability = Double.parseDouble(value);
+                    return probability >= 0 && probability <= 1;
+                } catch (NumberFormatException e) {
+                    return false;
+                }
+            }).build();
     SystemTestProperty MAX_ENTRIES_RANDOM_MAP = Index.propertyBuilder("sleeper.systemtest.random.map.length")
             .description("The maximum number of entries in maps generated randomly during random row generation\n" +
                     "(the number of entries in the map will range randomly from 0 to this number)")
