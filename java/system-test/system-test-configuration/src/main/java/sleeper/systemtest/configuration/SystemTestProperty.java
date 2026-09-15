f499c3ae90cf27a4ae67ef334e662a7e089c5992
Sylvester Kaczmarek <16242628+sylvesterkaczmarek@users.noreply.github.com>
2026-09-15 21:54:47 +0100

    Issue 7188 - Reuse double range validation
    Signed-off-by: Sylvester Kaczmarek <16242628+sylvesterkaczmarek@users.noreply.github.com>

--- java/system-test/system-test-configuration/src/main/java/sleeper/systemtest/configuration/SystemTestProperty.java
+++ java/system-test/system-test-configuration/src/main/java/sleeper/systemtest/configuration/SystemTestProperty.java
@@ -140,14 +140,7 @@ public interface SystemTestProperty extends InstanceProperty {
             .defaultValue("10").validationPredicate(SleeperPropertyValueUtils::isNonNegativeInteger).build();
     SystemTestProperty RANDOM_NULL_PROBABILITY = Index.propertyBuilder("sleeper.systemtest.random.null.probability")
             .description("The probability of generating null for nullable value fields during random row generation")
-            .defaultValue("0.2").validationPredicate(value -> {
-                try {
-                    double probability = Double.parseDouble(value);
-                    return probability >= 0 && probability <= 1;
-                } catch (NumberFormatException e) {
-                    return false;
-                }
-            }).build();
+            .defaultValue("0.2").validationPredicate(value -> SleeperPropertyValueUtils.isDoubleInRangeInclusive(value, 0.0, 1.0)).build();
     SystemTestProperty MAX_ENTRIES_RANDOM_MAP = Index.propertyBuilder("sleeper.systemtest.random.map.length")
             .description("The maximum number of entries in maps generated randomly during random row generation\n" +
                     "(the number of entries in the map will range randomly from 0 to this number)")
