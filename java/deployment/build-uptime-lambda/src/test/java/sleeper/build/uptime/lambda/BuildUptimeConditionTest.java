0a18138049d7f0b54f05f2aeddcffdddb5a48ec8
patchwork01 <110390516+patchwork01@users.noreply.github.com>
2026-09-21 09:19:44 +0000

    8204 Fix build uptime condition

---
+++ java/deployment/build-uptime-lambda/src/test/java/sleeper/build/uptime/lambda/BuildUptimeConditionTest.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright 2022-2026 Crown Copyright
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package sleeper.build.uptime.lambda;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.Instant;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static sleeper.build.uptime.lambda.BuildUptimeCondition.TEST_FINISHED_FROM_TODAY;
+
+public class BuildUptimeConditionTest {
+
+    private final Map<String, String> s3PathToObject = new HashMap<>();
+    private Instant timeNow = Instant.now();
+
+    @Test
+    void shouldNotFindTestFinishedFromTodayWhenNoSummaryIsInBucket() {
+        // When / Then
+        assertThat(checkTestFinishedFromTodayInBucket("test-bucket"))
+                .isFalse();
+    }
+
+    @Test
+    void shouldNotFindTestFinishedFromTodayWhenLastRunIsMoreThan12HoursOld() {
+        // Given
+        putSummaryJsonInBucket("test-bucket", "{" +
+                "\"executions\": [{" +
+                "\"startTime\": \"2026-09-20T20:00:00Z\"" +
+                "}]}");
+        timeNow = Instant.parse("2026-09-21T08:00:01Z");
+
+        // When / Then
+        assertThat(checkTestFinishedFromTodayInBucket("test-bucket"))
+                .isFalse();
+    }
+
+    @Test
+    void shouldFindTestFinishedFromTodayWhenLastRunIsLessThan12HoursOld() {
+        // Given
+        putSummaryJsonInBucket("test-bucket", "{" +
+                "\"executions\": [{" +
+                "\"startTime\": \"2026-09-20T20:00:00Z\"" +
+                "}]}");
+        timeNow = Instant.parse("2026-09-21T07:59:59Z");
+
+        // When / Then
+        assertThat(checkTestFinishedFromTodayInBucket("test-bucket"))
+                .isTrue();
+    }
+
+    private boolean checkTestFinishedFromTodayInBucket(String bucket) {
+        return BuildUptimeCondition.conditionAndBucket(TEST_FINISHED_FROM_TODAY, bucket)
+                .check(getS3ObjectAsString(), timeNow);
+    }
+
+    private void putSummaryJsonInBucket(String bucket, String summaryJson) {
+        s3PathToObject.put(bucket + "/summary.json", summaryJson);
+    }
+
+    private GetS3ObjectAsString getS3ObjectAsString() {
+        return (bucket, key) -> Optional.ofNullable(s3PathToObject.get(bucket + "/" + key));
+    }
+
+}
